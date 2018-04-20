b83abdb32c5647bb127a3a2e3af8714a012873ca
DIYgod <diy.d.god@gmail.com>
2018-04-20 18:32:45 +0800

    add pixiv config

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
     cacheType: 'memory',      // support memory and redis, set empty to disable cache
     cacheExpire: 5 * 60,
     ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-    // pixiv: {
-    //     client_id: 'XXXXXXXXXXXXXXXXXXX',
-    //     client_secret: 'XXXXXXXXXXXXXXXXX',
-    //     username: 'xxxx@xxx.xx',
-    //     password: 'xxxxx'
-    // }
+    pixiv: {
+        client_id: 'MOBrBDS8blbauoSck0ZfDbtuzpyT',
+        client_secret: 'lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj',
+        username: 'rsshub@tmpmail.org',
+        password: 'rsshubpixiv'
+    }
 };
