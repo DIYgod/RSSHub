2ccaca0d9d8f57482682552f7233f18df2d2c282
eyhn <cneyhn@gmail.com>
2018-05-06 22:33:18 +0800

    feat: pixiv bookmarks rss

--- router.js
+++ router.js
@@ -61,7 +61,8 @@ router.get('/mzitu/post/:id', require('./routes/mzitu/post'));
 router.get('/mzitu/tag/:tag', require('./routes/mzitu/tag'));
 
 // // Pixiv
-router.get('/pixiv/user/:id', require('./routes/pixiv/user'));
+router.get('/pixiv/user/:id/bookmarks', require('./routes/pixiv/bookmarks'));
+router.get('/pixiv/user/:id/', require('./routes/pixiv/user'));
 router.get('/pixiv/ranking/:mode/:date?', require('./routes/pixiv/ranking'));
 
 // 豆瓣
---
+++ routes/pixiv/api/getBookmarks.js
@@ -0,0 +1,25 @@
+
+const axios = require('axios');
+const maskHeader = require('../constants').maskHeader;
+
+/**
+ * 获取用户的收藏
+ * 
+ * @param {string} user_id 目标用户id
+ * @param {string} token pixiv oauth token
+ * @returns {Promise<axios.AxiosResponse<{illusts: illust[]}>>}
+ */
+module.exports = async function getBookmarks(user_id, token) {
+  return await axios({
+    method: 'get',
+    url: 'https://app-api.pixiv.net/v1/user/bookmarks/illust',
+    headers: {
+      ...maskHeader,
+      'Authorization': 'Bearer ' + token
+    },
+    params: {
+      user_id: user_id,
+      restrict: 'public'
+    },
+  });
+}
