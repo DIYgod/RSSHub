c0b9b11882c592563a8742e60d8edb6d6a171729
jjeejj <wenjunjiang1993@163.com>
2018-05-06 10:50:49 +0800

    增加 笔趣阁 https://www.biquge5200.com/ 小说 RSS

---
+++ routes/biquge/chapter.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const art = require('art-template');
+const path = require('path');
+const cheerio = require('cheerio');
+const config = require('../../config');
+const iconv = require('iconv-lite');
+
+const baseUrl = 'https://www.biquge5200.com/';
+//获取小说的最新章节列表
+module.exports = async (ctx) => {
+    const id = ctx.params.id; //小说id
+
+    const response = await axios({
+        method: 'get',
+        url: `${baseUrl}${id}/`,
+        headers: {
+            'User-Agent': config.ua,
+            'Host':'www.biquge5200.com',
+            'Referer': `${baseUrl}${id}/`
+        },
+        // responseEncoding: 'gbk', //该配置项在 0.18版本中没有打包进去
+        responseType: 'arraybuffer'
+    });
+    const responseHtml = iconv.decode(response.data, 'GBK');;
+    // console.log('responseHtml',responseHtml);
+    let $ = cheerio.load(responseHtml);
+    const title = $('#list>dl>dt>b').eq(0).text();
+    const description = $('#intro>p').eq(0).text();
+    const cover_url = $('#fmimg>img').eq(0).attr('src');
+    const list = $('dd', '#list>dl').slice(0, 9);
+    let chapter_item = [];
+    for (let i = 0; i < list.length; i++) {
+        const el = $(list[i]).find('a').eq(0);
+        const item = {
+            title: el.text(),
+            link: el.attr('href')
+        };
+        chapter_item.push(item)
+    };
+    // console.log('chapter_item',chapter_item)
+    ctx.body = art(path.resolve(__dirname, '../../views/rss.art'), {
+        title: `笔趣阁 ${title}`,
+        link: `${baseUrl}${id}/`,
+        image: cover_url,
+        description: description,
+        lastBuildDate: new Date().toUTCString(),
+        item: chapter_item,
+    });
+};
