d25c2a96ead81362299d9e517558e131ef808815
DerekCao <marcello@163.com>
2018-05-13 22:36:47 +0800

    fix #109

--- routes/jike/user.js
+++ routes/jike/user.js
@@ -9,7 +9,7 @@ module.exports = async (ctx) => {
         url: 'https://app.jike.ruguoapp.com/1.0/personalUpdate/single',
         headers: {
             'User-Agent': config.ua,
-            'Referer': `https://web.okjike.com/user/${id}`,
+            'Referer': `https://web.okjike.com/user/${id}/post`,
             'App-Version': '4.1.0',
             'platform': 'web'
         },
@@ -46,12 +46,17 @@ module.exports = async (ctx) => {
             const shortenTitle = content.length > 75 ? `${content.substr(0, 75)}...` : content;
 
             if (item.type === 'REPOST') {
+                const targetLinkMap = {
+                    'OFFICIAL_MESSAGE': `https://web.okjike.com/message-detail/${item.target.id}/officialMessage`,
+                    'ORIGINAL_POST': `https://web.okjike.com/post-detail/${item.target.id}/originalPost`,
+                    'REPOST': `https://web.okjike.com/post-detail/${item.target.id}/repost`
+                };
                 let repostImgTemplate = '';
                 item.target.pictures && item.target.pictures.forEach((item) => {
                     repostImgTemplate += `<p><img style="box-shadow: 0 0 1px rgba(0,0,0,.2);" referrerpolicy="no-referrer" src="${item.thumbnailUrl}"></p>`;
                 });
 
-                let repostContent = `<a style="padding:15px;margin-top:10px;display:block;border-radius:4px;background-color:#fff;border:1px solid rgba(0,0,0,.08);text-decoration:none;" href="https://web.okjike.com/post-detail/${item.target.id}/originalPost" target="_blank"><p>@${item.target.user.screenName}</p>${item.target.content}${repostImgTemplate}</a>`;
+                let repostContent = `<a style="padding:15px;margin-top:10px;display:block;border-radius:4px;background-color:#fff;border:1px solid rgba(0,0,0,.08);text-decoration:none;" href="${targetLinkMap[item.target.type]}" target="_blank"><p>@${item.target.user.screenName}</p>${item.target.content}${repostImgTemplate}</a>`;
                 content = `${content}${repostContent}`;
             }
 
@@ -59,7 +64,7 @@ module.exports = async (ctx) => {
                 title: `${typeMap[item.type]}了: ${shortenTitle}`,
                 description: `${content}${linkTemplate}${imgTemplate}`,
                 pubDate: new Date(item.createdAt).toUTCString(),
-                link: `https://web.okjike.com/message-detail/${item.id}/officialMessage`
+                link: `https://web.okjike.com/post-detail/${item.id}/originalPost`
             }
         }),
     };
