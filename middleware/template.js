88b4554eacf284c35d7f0d42e9e250e5a1afd83b
阿卡琳 <hufan.akarin@Gmail.com>
2018-05-19 02:13:16 +0800

    feat(template): add atom.art

--- middleware/template.js
+++ middleware/template.js
@@ -3,10 +3,42 @@ const path = require('path');
 const config = require('../config');
 
 module.exports = async (ctx, next) => {
+    const pathname = ctx.request.path;
+
+    const paterns = [
+        () => {
+            const patern = '.atom';
+            if (pathname.endsWith(patern)) {
+                ctx.request.path = pathname.slice(0, -patern.length);
+                ctx.state.template = path.resolve(__dirname, '../views/atom.art');
+
+                return true;
+            }
+        },
+        () => {
+            const patern = '.rss';
+            if (pathname.endsWith(patern)) {
+                ctx.request.path = pathname.slice(0, -patern.length);
+                ctx.state.template = path.resolve(__dirname, '../views/rss.art');
+
+                return true;
+            }
+        },
+        () => {
+            // default
+            ctx.state.template = path.resolve(__dirname, '../views/rss.art');
+
+            return true;
+        },
+    ];
+
+    paterns.find((p) => p());
+
     await next();
     if (!ctx.body) {
-        ctx.body = art(path.resolve(__dirname, '../views/rss.art'), {
+        ctx.body = art(ctx.state.template, {
             lastBuildDate: new Date().toUTCString(),
+            updated: new Date().toISOString(),
             ttl: config.cacheExpire,
             ...ctx.state.data,
         });
