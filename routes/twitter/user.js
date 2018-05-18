a565235c5b2e390780d4f55388731b5ff5b126e7
DIYgod <diy.d.god@gmail.com>
2018-05-18 16:52:21 +0800

    rss: twitter user: add images

--- routes/twitter/user.js
+++ routes/twitter/user.js
@@ -16,11 +16,18 @@ module.exports = async (ctx) => {
         title: `${data[0].user.name} 的 Twitter`,
         link: `https://twitter.com/${id}/`,
         description: data[0].user.description,
-        item: data.map((item) => ({
-            title: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.text.length > 30 ? item.text.slice(0, 30) + '...' : item.text}`,
-            description: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.text}`,
-            pubDate: new Date(item.created_at).toUTCString(),
-            link: `https://twitter.com/${id}/status/${item.id_str}`,
-        })),
+        item: data.map((item) => {
+            let img = '';
+            item.extended_entities &&
+                item.extended_entities.media.forEach((item) => {
+                    img += `<br>${item.type === 'video' ? 'Video: ' : ''}<img referrerpolicy="no-referrer" src="${item.media_url_https}">`;
+                });
+            return {
+                title: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.text.length > 30 ? item.text.slice(0, 30) + '...' : item.text}`,
+                description: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.text}${img}`,
+                pubDate: new Date(item.created_at).toUTCString(),
+                link: `https://twitter.com/${id}/status/${item.id_str}`,
+            };
+        }),
     };
 };
