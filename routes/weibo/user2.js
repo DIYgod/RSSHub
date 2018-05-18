b21dc61da88e0781627ac954d3082187caeaa417
DIYgod <diy.d.god@gmail.com>
2018-05-18 11:48:03 +0800

    rss: map -> each

--- routes/weibo/user2.js
+++ routes/weibo/user2.js
@@ -23,7 +23,7 @@ module.exports = async (ctx) => {
     const wbs = [];
     const items = $('.wgtCell');
     let wb, item, titleEle;
-    items.map((index, ele) => {
+    items.each((index, ele) => {
         wb = {};
         item = $(ele);
         titleEle = item.find('.wgtCell_txt');
