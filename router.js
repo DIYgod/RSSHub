e1d817e7219ac886c63a17da97f164aa69319c87
RANPOX <i@ranpox.com>
2018-05-21 11:47:11 +0800

    rss:add infzm news

--- router.js
+++ router.js
@@ -202,4 +202,7 @@ router.get('/geektime/column/:cid', require('./routes/geektime/column'));
 // 爱奇艺
 router.get('/iqiyi/dongman/:id', require('./routes/iqiyi/dongman'));
 
+// infzm
+router.get('/infzm/:id', require('./routes/infzm/news'));
+
 module.exports = router;
