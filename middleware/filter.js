27b96cb3fe0f60c89ee44e27e7cf296c2afb5b49
freemagico <tgly307@163.com>
2018-05-23 13:37:11 +0800

    添加exclude过滤

--- middleware/filter.js
+++ middleware/filter.js
@@ -12,4 +12,15 @@ module.exports = async (ctx, next) => {
             );
         });
     }
+    if (ctx.state.data && ctx.query && (ctx.query.filterout || ctx.query.filterout_title || ctx.query.filterout_description)) {
+        ctx.state.data.item = ctx.state.data.item.filter((item) => {
+            const title = item.title;
+            const description = item.description;
+            return (
+                (ctx.query.filterout && !title.match(ctx.query.filterout) && !description.match(ctx.query.filterout)) ||
+                (ctx.query.filterout_title && !title.match(ctx.query.filterout_title)) ||
+                (ctx.query.filterout_description && !description.match(ctx.query.filterout_description))
+            );
+        });
+    }
 };
