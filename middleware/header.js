36e565d3882507a3a695b1b1f0ca4dcd772b353f
DIYgod <diy.d.god@gmail.com>
2018-06-02 17:22:56 +0800

    app: add Cache-Control header

--- middleware/header.js
+++ middleware/header.js
@@ -1,4 +1,5 @@
 const logger = require('../utils/logger');
+const config = require('../config');
 
 module.exports = async (ctx, next) => {
     logger.info(`${ctx.url}, user IP: ${ctx.ips[0] || ctx.ip}`);
@@ -8,6 +9,7 @@ module.exports = async (ctx, next) => {
         'Access-Control-Allow-Headers': 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
         'Access-Control-Allow-Methods': 'PUT, POST, GET, DELETE, OPTIONS',
         'Content-Type': 'application/xml; charset=utf-8',
+        'Cache-Control': `max-age=${config.cacheExpire}`,
     };
     ctx.set(headers);
     await next();
