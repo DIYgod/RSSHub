b6af63688aed6e6a163062a9f6291cb9c67a2cd0
yangkghjh <yangkghjh@gmail.com>
2018-07-04 04:07:27 +0800

    fixed link missing

--- routes/zimuzu/feed.js
+++ routes/zimuzu/feed.js
@@ -1,26 +1,40 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
+const axios = require('../../utils/axios');
+const cheerio = require('cheerio');
 
 module.exports = async (ctx) => {
     const id = ctx.params.id;
     const session = ctx.params.session;
-    const feed = await parser.parseURL('http://diaodiaode.me/rss/feed/' + id);
-    const items = [];
-    feed.items.forEach((item) => {
-        if (item.title.indexOf(session) != -1) {
-            items.push({
-                title: item.title,
-                pubDate: item.pubDate,
-                guid: item.guid,
-                link: item.magnet,
-            });
-        }
+
+    const response = await axios({
+        method: 'get',
+        url: 'http://diaodiaode.me/rss/feed/' + id,
     });
 
+    const data = response.data;
+    const $ = cheerio.load(data, {
+        xmlMode: true,
+    });
+    const list = $('item');
+
     ctx.state.data = {
-        title: feed.title,
-        link: encodeURI('http://www.zimuzu.tv'),
-        description: feed.description,
-        item: items,
+        title: $('channel>title').text(),
+        link: encodeURI('http://www.zimuzu.tv/resource/' + id),
+        description: $('channel>description').text(),
+        item:
+            list &&
+            list
+                .map((index, item) => {
+                    item = $(item);
+                    title = item.find('title').text();
+                    if (title.indexOf(session) != -1) {
+                        return {
+                            title: title,
+                            pubDate: item.find('pubDate').text(),
+                            guid: item.find('guid').text(),
+                            link: item.find('magnet').text(),
+                        };
+                    }
+                })
+                .get(),
     };
 };
