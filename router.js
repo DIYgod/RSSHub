498ec5d00e3e18b8ce2664a86bd96e51890c1e82
zhboner <zhboner@hotmail.com>
2018-07-05 18:50:23 +1000

    Add wsj

--- router.js
+++ router.js
@@ -331,4 +331,7 @@ router.get('/cctv/:category', require('./routes/cctv/category'));
 // 草榴社区
 router.get('/t66y/:id', require('./routes/t66y/index'));
 
+router.get('/wsj/:cat', require('./routes/wsj/index'));
+router.get('/wsj', require('./routes/wsj/index'));
+
 module.exports = router;
