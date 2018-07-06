5fbb838ede33cd88dd9e75c24e3d2d406c5dc2bc
vhgyux <vhgyux@gmail.com>
2018-07-06 20:06:16 +0800

    add new rss

--- router.js
+++ router.js
@@ -331,4 +331,7 @@ router.get('/cctv/:category', require('./routes/cctv/category'));
 // 草榴社区
 router.get('/t66y/:id', require('./routes/t66y/index'));
 
+// 科技星球
+router.get('/kejixingqiu/home', require('./routes/kejixingqiu/home'));
+
 module.exports = router;
