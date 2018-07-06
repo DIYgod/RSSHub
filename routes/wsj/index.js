ad66cca2673e594eb852f233441471bbe110165c
zhboner <zhboner@outlook.com>
2018-07-06 18:55:58 +1000

    Remove wsj

--- routes/wsj/index.js
+++
@@ -1,6 +0,0 @@
-const lib = require('./lib');
-const base = 'https://cn.wsj.com/zh-hans';
-
-module.exports = async (ctx) => {
-    ctx.state.data = await lib(ctx, base, true);
-};
--- routes/wsj/sectors.js
+++
@@ -1,7 +0,0 @@
-const lib = require('./lib');
-const base = 'https://cn.wsj.com/zh-hans';
-
-module.exports = async (ctx) => {
-    const site = `${base}/news/${ctx.params.cat}`;
-    ctx.state.data = await lib(ctx, site, false);
-};
