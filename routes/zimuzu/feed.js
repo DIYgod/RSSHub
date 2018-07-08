1234791512cd533d01c905fc60c69952eb797627
yangkghjh <yangkghjh@gmail.com>
2018-07-09 02:44:54 +0800

    magnet uri check

--- routes/zimuzu/feed.js
+++ routes/zimuzu/feed.js
@@ -1,6 +1,17 @@
 const axios = require('../../utils/axios');
 const cheerio = require('cheerio');
 const config = require('../../config');
+const mguri = require('magnet-uri');
+
+const parseInfoHash = function(uri) {
+    if (uri) {
+        const uriObj = mguri.decode(uri);
+        const hash = uriObj.infoHash || uri;
+        if (/^[A-Za-z0-9]{40}$/.test(hash)) {
+            return hash.toUpperCase();
+        }
+    }
+};
 
 module.exports = async (ctx) => {
     const id = ctx.params.id;
@@ -30,7 +41,7 @@ module.exports = async (ctx) => {
                     item = $(item);
                     const title = item.find('title').text();
                     const magnet = item.find('magnet').text();
-                    if (magnet) {
+                    if (parseInfoHash(magnet)) {
                         return {
                             title: title,
                             pubDate: item.find('pubDate').text(),
