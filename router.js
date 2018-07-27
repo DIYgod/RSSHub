465ab1f650c13cc65daef05b278454d1cb6a9a10
fengkx <liangkx8237@gmail.com>
2018-07-27 16:39:09 +0800

    fix format warning

--- router.js
+++ router.js
@@ -342,7 +342,7 @@ router.get('/pku/eecs/:type?', require('./routes/pku/eecs'));
 // 机核
 router.get('/gcores/category/:category', require('./routes/gcores/category'));
 
-//国家地理 National Geographic
+// 国家地理 National Geographic
 router.get('/natgeo/:cat/:type/:option?', require('./routes/natgeo/natgeo'));
 
 module.exports = router;
