f5c7ed2af25ddb91cec57ff7db7183665088ff26
Henry <hi@henry.wang>
2018-08-07 19:49:19 +0100

    Add 中国美术馆 -- 焦点专题 as part of #365

--- router.js
+++ router.js
@@ -377,6 +377,7 @@ router.get('/namoc/announcement', require('./routes/namoc/announcement'));
 router.get('/namoc/news', require('./routes/namoc/news'));
 router.get('/namoc/media', require('./routes/namoc/media'));
 router.get('/namoc/exhibition', require('./routes/namoc/exhibition'));
+router.get('/namoc/specials', require('./routes/namoc/specials'));
 
 // 懂球帝
 router.get('/dongqiudi/daily', require('./routes/dongqiudi/index'));
