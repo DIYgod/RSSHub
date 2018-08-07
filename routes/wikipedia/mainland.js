f4f5d4d770c4aef53025681779b05ef2efe9f4d2
Henry <hi@henry.wang>
2018-08-07 13:24:31 +0100

    lint

--- routes/wikipedia/mainland.js
+++ routes/wikipedia/mainland.js
@@ -3,7 +3,6 @@ const config = require('../../config');
 const cheerio = require('cheerio');
 
 module.exports = async (ctx) => {
-
     const url = 'https://zh.wikipedia.org/wiki/Portal:%E4%B8%AD%E5%9C%8B%E5%A4%A7%E9%99%B8%E6%96%B0%E8%81%9E%E5%8B%95%E6%85%8B';
 
     const response = await axios({
@@ -15,8 +14,6 @@ module.exports = async (ctx) => {
     });
 
     const $ = cheerio.load(response.data);
-
-
     const full = $('#mw-content-text tr:nth-child(2) td:nth-child(1) td:nth-child(1)');
     const list = full.find('.mw-headline');
     const items = [];
@@ -24,7 +21,10 @@ module.exports = async (ctx) => {
     for (let i = 0; i < list.length - 1; i++) {
         const item = {
             title: $(list[i]).text(),
-            desc: full.find(`ul:nth-of-type(${i + 1})`).html().replace(/href="\/wiki/g, 'href="https://zh.wikipedia.org/wiki'),
+            desc: full
+                .find(`ul:nth-of-type(${i + 1})`)
+                .html()
+                .replace(/href="\/wiki/g, 'href="https://zh.wikipedia.org/wiki'),
             url,
         };
         items.push(item);
