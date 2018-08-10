c1a52ecf850319cab8e97261ecf5e8701de80528
freemagico <tgly307@163.com>
2018-08-10 16:32:32 +0800

    添加开源中国-资讯中站外链接的处理

--- routes/oschina/news.js
+++ routes/oschina/news.js
@@ -21,29 +21,32 @@ module.exports = async (ctx) => {
     const resultItem = await Promise.all(
         count.map(async (i) => {
             const each = $(list[i]);
-            const url = `https://www.oschina.net${each.find('a', 'h3').attr('href')}`;
+            const originalUrl = each.find('a', 'h3').attr('href');
             const item = {
                 title: each.find('a', 'h3').attr('title'),
-                description: '',
-                link: url,
+                description: each.find('p', '.description').text(),
+                link: originalUrl,
             };
-            const key = 'oschina' + url;
-            const value = await ctx.cache.get(key);
+            if (/^\/news\/.*$/.test(originalUrl)) {
+                item.link = 'https://www.oschina.net' + originalUrl;
+                const key = 'oschina' + item.link;
+                const value = await ctx.cache.get(key);
 
-            if (value) {
-                item.description = value;
-            } else {
-                const detail = await axios({
-                    method: 'get',
-                    url: url,
-                    headers: {
-                        'User-Agent': config.ua,
-                        Referer: url,
-                    },
-                });
-                const content = cheerio.load(detail.data);
-                item.description = content('#articleContent').html();
-                ctx.cache.set(key, item.description, 24 * 60 * 60);
+                if (value) {
+                    item.description = value;
+                } else {
+                    const detail = await axios({
+                        method: 'get',
+                        url: item.link,
+                        headers: {
+                            'User-Agent': config.ua,
+                            Referer: item.link,
+                        },
+                    });
+                    const content = cheerio.load(detail.data);
+                    item.description = content('#articleContent').html();
+                    ctx.cache.set(key, item.description, 24 * 60 * 60);
+                }
             }
             return Promise.resolve(item);
         })
