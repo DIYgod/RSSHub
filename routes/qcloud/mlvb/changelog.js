298ad4d870d3cbfe6a5ee6877543c390ff015cc0
cielpy <beijiu572@gmail.com>
2018-08-12 23:46:02 +0800

    去掉多余缓存

--- routes/qcloud/mlvb/changelog.js
+++ routes/qcloud/mlvb/changelog.js
@@ -5,33 +5,28 @@ const config = require('../../../config');
 module.exports = async (ctx) => {
     const url = 'https://cloud.tencent.com/document/product/454/7878';
 
-    let resultItem = [];
-    const cache = ctx.cache.get(url);
-    if (cache) {
-        resultItem = JSON.parse(cache);
-    } else {
-        const res = await axios({
-            method: 'get',
-            url: url,
-            headers: {
-                'User-Agent': config.ua,
-            },
+    const res = await axios({
+        method: 'get',
+        url: url,
+        headers: {
+            'User-Agent': config.ua,
+        },
+    });
+    const data = res.data;
+    const $ = cheerio.load(data);
+
+    const resultItem = [];
+    $('#docArticleContent')
+        .find('h3')
+        .each(function() {
+            const item = {};
+            item.title = $(this).text();
+            item.description = $(this)
+                .nextUntil('h3')
+                .text();
+            item.link = url;
+            resultItem.push(item);
         });
-        const data = res.data;
-        const $ = cheerio.load(data);
-        $('#docArticleContent')
-            .find('h3')
-            .each(function() {
-                const item = {};
-                item.title = $(this).text();
-                item.description = $(this)
-                    .nextUntil('h3')
-                    .text();
-                item.link = url;
-                resultItem.push(item);
-            });
-        ctx.cache.set(url, JSON.stringify(resultItem), 24 * 60 * 60);
-    }
 
     ctx.state.data = {
         title: '腾讯移动直播 SDK 更新日志',
