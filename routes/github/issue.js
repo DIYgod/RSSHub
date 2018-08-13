b042b0b5a91a4abdc41ddf03270a4ec5dc2242ef
Henry <hi@henry.wang>
2018-08-13 19:57:28 +0100

    Add markdown-it to close #462
    Render markdown to html for github issue desc

--- routes/github/issue.js
+++ routes/github/issue.js
@@ -1,11 +1,12 @@
 const axios = require('../../utils/axios');
 const config = require('../../config');
+const md = require('markdown-it')();
 
 module.exports = async (ctx) => {
     const user = ctx.params.user;
     const repo = ctx.params.repo;
 
-    const host = `https://github.com/repos/${user}/${repo}/issues`;
+    const host = `https://github.com/${user}/${repo}/issues`;
     const url = `https://api.github.com/repos/${user}/${repo}/issues`;
 
     const response = await axios({
@@ -23,13 +24,11 @@ module.exports = async (ctx) => {
     ctx.state.data = {
         title: `${user}/${repo} Issues`,
         link: host,
-        item:
-            data &&
-            data.map((item) => ({
-                title: item.title,
-                description: item.body || 'No description',
-                pubDate: new Date(item.created_at),
-                link: `${host}/${item.number}`,
-            })),
+        item: data.map((item) => ({
+            title: item.title,
+            description: md.render(item.body) || 'No description',
+            pubDate: new Date(item.created_at),
+            link: `${host}/${item.number}`,
+        })),
     };
 };
