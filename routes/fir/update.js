310b136913d1a414be30d09cc1c937a776a5589e
cielpy <beijiu572@gmail.com>
2018-08-13 14:48:39 +0800

    添加 release link

--- routes/fir/update.js
+++ routes/fir/update.js
@@ -23,6 +23,7 @@ module.exports = async (ctx) => {
     const item = {};
     item.title = data.app.name + ' ' + data.app.releases.master.version + '(' + data.app.releases.master.build + ')';
     item.description = data.app.releases.master.changelog;
+    item.link = `https://fir.im/x9zu?release_id=${data.app.releases.master.id}`;
     item.pubDate = new Date(data.app.releases.master.created_at * 1000);
     ctx.state.data = {
         title: title,
