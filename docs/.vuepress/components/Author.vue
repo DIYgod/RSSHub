362a59c38b7daff6cd7cd324e4080434ab51995d
DIYgod <diy.d.god@gmail.com>
2018-08-14 00:46:44 +0800

    docs: add route author

---
+++ docs/.vuepress/components/Author.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="badge">
+    By <a :href="`https://github.com/${uid}`">@{{ uid }}</a>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    uid: {
+      type: String,
+      default: 'DIYgod'
+    },
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.badge
+  display inline-block
+  font-size 14px
+  height 18px
+  line-height 18px
+  border-radius 3px
+  padding 0 6px
+  color white
+  margin-right 5px
+  vertical-align top
+  background-color #F5712C
+a
+  color #fff
+</style>
