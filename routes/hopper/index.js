88705662c2b703b4efba54469172db8122431cdd
DIYgod <diy.d.god@gmail.com>
2018-08-19 16:05:58 +0800

    format

--- routes/hopper/index.js
+++ routes/hopper/index.js
@@ -31,7 +31,7 @@ module.exports = async (ctx) => {
         let lowest = 99999;
         let lowIndex = 0;
 
-        list.map((i, e) => {
+        list.forEach((i, e) => {
             const current = parseInt(
                 $(e)
                     .find('.price')
@@ -47,7 +47,7 @@ module.exports = async (ctx) => {
 
         items.push(formatDesc($(list[lowIndex])));
     } else {
-        list.map((i, e) => {
+        list.forEach((i, e) => {
             items.push(formatDesc($(e)));
         });
     }
