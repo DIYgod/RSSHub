7c874109d910615d67c498df24a6de9fe9cca5bb
DIYgod <diy.d.god@gmail.com>
2018-08-21 12:18:26 +0800

    rss: twitter full text

--- routes/twitter/user.js
+++ routes/twitter/user.js
@@ -8,6 +8,7 @@ module.exports = async (ctx) => {
 
     const result = await T.get('statuses/user_timeline', {
         screen_name: id,
+        tweet_mode: 'extended',
     });
 
     const data = result.data;
@@ -17,14 +18,15 @@ module.exports = async (ctx) => {
         link: `https://twitter.com/${id}/`,
         description: data[0].user.description,
         item: data.map((item) => {
+            item = item.retweeted_status || item;
             let img = '';
             item.extended_entities &&
                 item.extended_entities.media.forEach((item) => {
                     img += `<br>${item.type === 'video' ? 'Video: ' : ''}<img referrerpolicy="no-referrer" src="${item.media_url_https}">`;
                 });
             return {
-                title: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.text.length > 30 ? item.text.slice(0, 30) + '...' : item.text}`,
-                description: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.text}${img}`,
+                title: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.full_text.length > 30 ? item.full_text.slice(0, 30) + '...' : item.full_text}`,
+                description: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.full_text}${img}`,
                 pubDate: new Date(item.created_at).toUTCString(),
                 link: `https://twitter.com/${id}/status/${item.id_str}`,
             };
