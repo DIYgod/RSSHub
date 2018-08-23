49aa1b3c10759d698661e42ee9f017273af26d56
Artin <lengthmin@gmail.com>
2018-08-23 16:44:02 +0000

    网络小说：使用并发请求和缓存加速源

--- routes/novel/wenxuemi.js
+++ routes/novel/wenxuemi.js
@@ -25,31 +25,40 @@ module.exports = async (ctx) => {
     const description = $('.intro_info').text();
     const cover_url = $('img', '.block_img2').attr('src');
     const list = $('li', '.chapter').slice(0, 5);
-    const items = [];
-    for (let i = 0; i < list.length; i++) {
-        // title: e.children[0].data,
-        // link: e.attribs.href,
-        const link =
-            baseUrl +
-            $(list[i])
-                .find('a')
-                .attr('href');
-        const articleHtml = await axios_ins.get(link);
-        const article = iconv.decode(articleHtml.data, 'GBK');
-        const $1 = cheerio.load(article);
-        const res = $1('div#content').text();
-        const item = {
-            title: $(list[i]).text(),
-            description: res,
-            link: link,
-        };
-        items.push(item);
-    }
+
+    const resultItems = await Promise.all(
+        list.toArray().map(async (item) => {
+            const $item = $(item);
+
+            const link = baseUrl + $item.find('a').attr('href');
+            let resultItem = {};
+            const value = await ctx.cache.get(link);
+            if (value) {
+                resultItem = JSON.parse(value);
+            } else {
+                const articleHtml = await axios_ins.get(link);
+                const article = iconv.decode(articleHtml.data, 'GBK');
+                const $1 = cheerio.load(article);
+                const res = $1('div#content').text();
+
+                resultItem = {
+                    title: $item.text(),
+                    description: res,
+                    link: link,
+                };
+
+                ctx.cache.set(link, JSON.stringify(resultItem), 24 * 60 * 60);
+            }
+
+            return Promise.resolve(resultItem);
+        })
+    );
+
     ctx.state.data = {
         title: `${title}`,
         link: `${baseUrl}/files/article/html${id}/`,
         image: cover_url,
         description: description,
-        item: items,
+        item: resultItems,
     };
 };
