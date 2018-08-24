cd5604e43e8b984f4f73fcc137d07ef908cc45ce
Artin <lengthmin@gmail.com>
2018-08-24 03:50:03 +0000

    ximalaya: 修复无法获取音频时间

--- routes/ximalaya/album.js
+++ routes/ximalaya/album.js
@@ -14,15 +14,16 @@ module.exports = async (ctx) => {
     const authorinfo = AlbumInfoResponse.data.data.anchorInfo; // 作者数据
     const trackinfo = AlbumInfoResponse.data.data.tracksInfo; // tracks数据
 
-    const classify = albuminfo.crumbs.categoryPinyin; // 专辑分类
+    const classify = albuminfo.crumbs.categoryPinyin; // 专辑分类的拼音
+    const album_category = albuminfo.crumbs.categoryTitle; // 专辑分类
     const albumUrl = '/' + classify + '/' + id + '/'; // 某分类的链接
-    const ispaid = albuminfo.isPaid;
-    const album_title = albuminfo.albumTitle;
-    const album_url = baseUrl + albumUrl;
-    const album_desc = albuminfo.detailRichIntro + ' ' + authorinfo.anchorName + ' ' + authorinfo.personalIntroduction;
+    const ispaid = albuminfo.isPaid; // 是否需要付费
+    const album_title = albuminfo.albumTitle; // 专辑标题
+    const album_url = baseUrl + albumUrl; // 专辑链接
+    const album_desc = authorinfo.anchorName + ' ' + authorinfo.personalIntroduction + ' </br>' + albuminfo.detailRichIntro;
     const album_cover_url = albuminfo.cover.split('&')[0];
     const album_author = authorinfo.anchorName;
-    const tracks_count = trackinfo.trackTotalCount + 1;
+    const tracks_count = trackinfo.trackTotalCount;
 
     const PlayInfoApi = `http://mobile.ximalaya.com/mobile/v1/album/track?albumId=${id}&pageSize=${tracks_count}`; // 声音播放数据
     const PlayInfoResponse = await axios_ins.get(PlayInfoApi);
@@ -31,12 +32,15 @@ module.exports = async (ctx) => {
 
     const resultItems = await Promise.all(
         playList.map(async (item) => {
-            let title = item.title;
-            let desc = '查看收听提示: ' + baseUrl + albumUrl + item.trackId;
+            const title = item.title;
+            let desc = title + '</br>查看收听提示: </br>' + baseUrl + albumUrl + item.trackId;
             if (ispaid) {
-                title = '[付费内容无法收听] ' + item.title;
-                desc = '[付费内容无法收听] 请到APP购买收听: ' + baseUrl + albumUrl + item.trackId;
+                desc = title + '</br> [付费内容无法收听] 原文链接： </br>' + baseUrl + albumUrl + item.trackId;
             }
+            const enclosure_length = item.duration; // 时间长度：单位（秒）
+            // itunes_duration格式：<itunes:duration>32:46</itunes:duration>
+            const itunes_duration = Math.floor(enclosure_length / 3600) + ':' + Math.floor((enclosure_length % 3600) / 60) + ':' + (((enclosure_length % 3600) % 60) / 100).toFixed(2).slice(-2);
+
             const resultItem = {
                 title: title,
                 link: baseUrl + albumUrl + item.trackId,
@@ -44,8 +48,9 @@ module.exports = async (ctx) => {
                 pubDate: new Date(item.createdAt).toUTCString(),
                 itunes_item_image: item.coverLarge,
                 enclosure_url: item.playPathAacv224,
-                enclosure_length: item.duration,
-                enclosure_type: 'audio/mpeg',
+                enclosure_length: enclosure_length,
+                enclosure_type: 'audio/m4a',
+                itunes_duration: itunes_duration,
             };
             return Promise.resolve(resultItem);
         })
@@ -57,6 +62,7 @@ module.exports = async (ctx) => {
         description: album_desc,
         image: album_cover_url,
         itunes_author: album_author,
+        itunes_category: album_category,
         item: resultItems,
     };
 };
