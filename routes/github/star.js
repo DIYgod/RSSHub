e7d4d06faa8ce221c542c72949c58c65be6a6ec3
Henry <hi@henry.wang>
2018-08-28 02:18:33 +0100

    Use Graphql to get an ordered stargazer list

--- routes/github/star.js
+++ routes/github/star.js
@@ -6,24 +6,41 @@ module.exports = async (ctx) => {
     const repo = ctx.params.repo;
 
     const host = `https://github.com/${user}/${repo}/stargazers`;
-    const url = `https://api.github.com/repos/${user}/${repo}/stargazers`;
+    const url = 'https://api.github.com/graphql';
 
     const response = await axios({
-        method: 'get',
+        method: 'post',
         url,
-        params: {
-            access_token: config.github.access_token,
+        headers: {
+            Authorization: `bearer ${config.github.access_token}`,
+        },
+        data: {
+            query: `
+            {
+                repository(owner: "diygod", name: "rsshub") {
+                  stargazers(last: 10) {
+                    edges {
+                      node {
+                        login
+                        avatarUrl
+                      }
+                    }
+                  }
+                }
+              }
+            `,
         },
     });
-    const data = response.data;
+
+    const data = response.data.data.repository.stargazers.edges.reverse();
 
     ctx.state.data = {
         title: `${user}/${repo}’s stargazers`,
         link: host,
         item: data.map((follower) => ({
-            title: `${follower.login} starred ${user}/${repo}`,
-            description: `${follower.html_url} <br> <img src='${follower.avatar_url}'>`,
-            link: `https://github.com/${follower.login}`,
+            title: `${follower.node.login} starred ${user}/${repo}`,
+            description: `<a href="https://github.com/${follower.node.login}">${follower.node.login}</a> <br> <img sytle="width:50px;" src='${follower.node.avatarUrl}'>`,
+            link: `https://github.com/${follower.node.login}`,
         })),
     };
 };
