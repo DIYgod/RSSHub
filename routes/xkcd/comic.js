9312fa4da22f7b10023c6df674a14e37ae4bf93d
小桐桐 <32215330+arbent@users.noreply.github.com>
2018-08-30 00:26:06 +0800

    Create comic.js
    xkcd的科学漫画

---
+++ routes/xkcd/comic.js
@@ -0,0 +1,32 @@
+const axios = require('../../utils/axios');
+const cheerio = require('cheerio');
+
+module.exports = async (ctx) => {
+    const response = await axios({
+        method: 'get',
+        url: 'https://www.xkcd.com',
+        headers: {
+            Referer: 'https://www.xkcd.com',
+        },
+    });
+
+    const data = response.data;
+    const $ = cheerio.load(data);
+
+    var preUrl = $('[rel="prev"]').attr('href');
+    var realUrl = String(Number(preUrl) + 1)
+
+    ctx.state.data = {
+        title: 'xkcd',
+        link: 'https://www.xkcd.com',
+        description: $('img[title]').attr('title'),
+        item: [
+            {
+                title: item.find('.ctitle').text(),
+                description: $('img[title]').attr('title'),
+                link: "https://www.xkcd.com" + realUrl,
+                itunes_item_image: $('img', 'comic'),
+            },
+        ],
+    };
+};
