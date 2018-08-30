1ef435db489420571b8d8bbbf03f5f3df68accea
Henry Wang <hi@henry.wang>
2018-08-30 14:55:17 +0100

    3dm: fix linting violation: no-await-in-loop (#597)
    #588

--- routes/3dm/news_center.js
+++ routes/3dm/news_center.js
@@ -10,44 +10,44 @@ module.exports = async (ctx) => {
     });
     const data = res.data;
     const $ = cheerio.load(data);
-    const list = $('.news_list .list li');
-    const out = [];
+    const list = $('.news_list .list li')
+        .slice(0, 10)
+        .get();
+    const out = await Promise.all(
+        list.map(async (i) => {
+            const item = $(i);
+            const itemUrl = $(item)
+                .find('.selectarcpost')
+                .attr('href');
+            const cache = await ctx.cache.get(itemUrl);
+            if (cache) {
+                return Promise.resolve(JSON.parse(cache));
+            }
+            const title = $(item)
+                .find('.bt')
+                .text();
+            const content = $(item)
+                .find('p')
+                .text();
+            const pageInfo = $(item)
+                .find('.time')
+                .text();
+            const regex = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}/;
+            const regRes = regex.exec(pageInfo);
+            const time = regRes === null ? new Date() : new Date(regRes[0]);
+            time.setTime(time.getTime() + (sourceTimezoneOffset - time.getTimezoneOffset() / 60) * 60 * 60 * 1000);
+            const single = {
+                title: title,
+                description: content,
+                pubDate: time.toUTCString(),
+                link: itemUrl,
+                guid: itemUrl,
+            };
 
-    for (let i = 0; i < Math.min(list.length, 10); i++) {
-        const $ = cheerio.load(data);
-        const item = list[i];
-        const itemUrl = $(item)
-            .find('.selectarcpost')
-            .attr('href');
-        const cache = await ctx.cache.get(itemUrl);
-        if (cache) {
-            out.push(JSON.parse(cache));
-            continue;
-        }
-        const title = $(item)
-            .find('.bt')
-            .text();
-        const content = $(item)
-            .find('p')
-            .text();
-        const pageInfo = $(item)
-            .find('.time')
-            .text();
-        const regex = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}/;
-        const regRes = regex.exec(pageInfo);
-        const time = regRes === null ? new Date() : new Date(regRes[0]);
-        time.setTime(time.getTime() + (sourceTimezoneOffset - time.getTimezoneOffset() / 60) * 60 * 60 * 1000);
-        const single = {
-            title: title,
-            description: content,
-            pubDate: time.toUTCString(),
-            link: itemUrl,
-            guid: itemUrl,
-        };
-        out.push(single);
-
-        ctx.cache.set(itemUrl, JSON.stringify(single), 24 * 60 * 60);
-    }
+            ctx.cache.set(itemUrl, JSON.stringify(single), 24 * 60 * 60);
+            return Promise.resolve(single);
+        })
+    );
     ctx.state.data = {
         title: $('title')
             .text()
