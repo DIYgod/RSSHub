144296f1f46cc390fc90be181d12e3d74315dca3
DIYgod <diy.d.god@gmail.com>
2018-08-31 15:38:08 +0800

    fix: twitter user url

--- routes/twitter/user.js
+++ routes/twitter/user.js
@@ -19,14 +19,19 @@ module.exports = async (ctx) => {
         description: data[0].user.description,
         item: data.map((item) => {
             item = item.retweeted_status || item;
+            item.full_text = item.full_text.replace(/https:\/\/t\.co(.*)$/, '');
             let img = '';
             item.extended_entities &&
                 item.extended_entities.media.forEach((item) => {
                     img += `<br>${item.type === 'video' ? 'Video: ' : ''}<img referrerpolicy="no-referrer" src="${item.media_url_https}">`;
                 });
+            let url = '';
+            item.entities.urls.forEach((u) => {
+                url += `<a href="${u.expanded_url}" target="_blank"></a>`;
+            });
             return {
                 title: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.full_text.length > 30 ? item.full_text.slice(0, 30) + '...' : item.full_text}`,
-                description: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.full_text}${img}`,
+                description: `${item.in_reply_to_screen_name ? 'Re ' : ''}${item.full_text}${url}${img}`,
                 pubDate: new Date(item.created_at).toUTCString(),
                 link: `https://twitter.com/${id}/status/${item.id_str}`,
             };
