67b3e9d31650a67f81ec29e3c2a1e5f4363e7c20
arbent <ethdthnt@gmail.com>
2018-08-31 19:29:38 +0800

    add npm run format

--- routes/xkcd/comic.js
+++ routes/xkcd/comic.js
@@ -1,32 +1,32 @@
-const axios = require("../../utils/axios");
-const cheerio = require("cheerio");
+const axios = require('../../utils/axios');
+const cheerio = require('cheerio');
 
-module.exports = async ctx => {
-  const response = await axios({
-    method: "get",
-    url: "https://www.xkcd.com",
-    headers: {
-      Referer: "https://www.xkcd.com"
-    }
-  });
+module.exports = async (ctx) => {
+    const response = await axios({
+        method: 'get',
+        url: 'https://www.xkcd.com',
+        headers: {
+            Referer: 'https://www.xkcd.com',
+        },
+    });
 
-  const data = response.data;
-  const $ = cheerio.load(data);
+    const data = response.data;
+    const $ = cheerio.load(data);
 
-  let preUrl = $('[rel="prev"]').attr("href");
-  let realUrl = String(Number(preUrl) + 1);
+    const preUrl = $('[rel="prev"]').attr('href');
+    const realUrl = String(Number(preUrl) + 1);
 
-  ctx.state.data = {
-    title: "xkcd",
-    link: "https://www.xkcd.com",
-    description: $("img[title]").attr("title"),
-    item: [
-      {
-        title: $(".ctitle").text(),
-        description: $("img[title]").attr("title"),
-        link: "https://www.xkcd.com" + realUrl,
-        itunes_item_image: $("img", "comic")
-      }
-    ]
-  };
+    ctx.state.data = {
+        title: 'xkcd',
+        link: 'https://www.xkcd.com',
+        description: $('img[title]').attr('title'),
+        item: [
+            {
+                title: $('.ctitle').text(),
+                description: $('img[title]').attr('title'),
+                link: 'https://www.xkcd.com' + realUrl,
+                itunes_item_image: $('img', 'comic'),
+            },
+        ],
+    };
 };
