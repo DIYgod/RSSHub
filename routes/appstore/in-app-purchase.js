405a55f642ee44bdf1439d61bd5723a96c0642b1
Henry <hi@henry.wang>
2018-09-01 16:09:44 +0100

    Use JSON data

--- routes/appstore/in-app-purchase.js
+++ routes/appstore/in-app-purchase.js
@@ -10,33 +10,28 @@ module.exports = async (ctx) => {
 
     const res = await axios.get(target);
     const $ = cheerio.load(res.data);
-    const titleTemp = $('h1')
-        .contents()
-        .first()
-        .text()
-        .trim();
-    const list = $('ol.list-with-numbers li.list-with-numbers__item');
-    const item = list
-        .map((i, e) => {
-            const title = `${titleTemp} ${$(e)
-                .find('span.list-with-numbers__item__title')
-                .text()
-                .trim()} ${$(e)
-                .find('span.list-with-numbers__item__price')
-                .text()
-                .trim()}`;
+
+    const data = JSON.parse($('#shoebox-ember-data-store')[0].firstChild.data);
+    const attributes = data.data.attributes;
+    const titleTemp = attributes.name;
+    const platform = attributes.kind === 'iosSoftware' ? 'iOS' : 'macOS';
+    let title;
+    let item = [];
+    if (attributes.hasInAppPurchases) {
+        title = `${country === 'cn' ? '内购限免提醒' : 'IAP price watcher'}: ${titleTemp} for ${platform}`;
+        const list = data.included.filter((e) => e.type === 'product/app/in-app');
+        item = list.map((e) => {
+            const title = `${e.attributes.name} is now ${e.attributes.price}`;
             return {
                 link,
                 guid: title,
                 title,
                 pubDate: new Date(Date.now()).toUTCString(),
             };
-        })
-        .get();
-
-    const platform = $('.we-localnav__title__product').text();
-
-    const title = `${country === 'cn' ? '内购限免提醒' : 'IAP price watcher'}: ${titleTemp} for ${platform.startsWith('App') ? 'iOS' : 'macOS'}`;
+        });
+    } else {
+        title = `${titleTemp} for ${platform} ${country === 'cn' ? ' 不包含内购项目' : 'has no IAP items'}`;
+    }
 
     ctx.state.data = {
         title,
