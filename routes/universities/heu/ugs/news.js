0b4c66327c97373849a27f978a29500f3789fe5f
XYenon <XYenon@hrbeu.edu.cn>
2018-09-03 20:46:37 +0800

    哈工程添加分类和文章内容

--- routes/universities/heu/ugs/news.js
+++ routes/universities/heu/ugs/news.js
@@ -1,34 +1,101 @@
 const axios = require('../../../../utils/axios');
 const cheerio = require('cheerio');
+const url = require('url');
+
+const authorMap = {
+    gztz: {
+        all: '/2821',
+    },
+    jwc: {
+        all: '/jwc',
+        jxap: '/2847',
+        ksgl: '/2895',
+        xjgl: '/2902',
+        wytk: '/2897',
+        cjgl: '/2901',
+    },
+    sjjxyjlc: {
+        all: '/3206',
+        syjx: '/2847',
+        sysjs: '/sysjs',
+        xwsx: '/2909',
+        xwlw: '/2910',
+        kcsj: '/2911',
+        cxcy: '/2913',
+        xjjl: '/xjjl',
+    },
+    jypgc: {
+        all: '/3207',
+        jxyjyjxcg: '/2916',
+        zljk: '/2917',
+    },
+    zyjsc: {
+        all: '/3208',
+        zyyjcjs: '/2914',
+        cgsyb: '/2925',
+        jxmsyyxzjjs: '/2918',
+        ktjs: '/2919',
+        syjx: '/2920',
+    },
+    gjdxswhszjd: {
+        all: '/3209',
+    },
+    jsjxfzzx: {
+        all: '/3210',
+        jspx: '/2915',
+    },
+    zhbgs: {
+        all: '/3211',
+        lxkc: '/lxkc',
+    },
+};
+
+const baseUrl = 'http://ugs.hrbeu.edu.cn';
+const type = (filename) => filename.split('.').pop();
 
 module.exports = async (ctx) => {
-    const response = await axios({
-        method: 'get',
-        url: 'http://ugs.hrbeu.edu.cn/2821/list.htm',
-        headers: {
-            Referer: 'http://ugs.hrbeu.edu.cn',
-        },
-    });
+    const author = ctx.params.author || 'gztz';
+    const category = ctx.params.category || 'all';
+    const response = await axios.get(`${baseUrl}${authorMap[author][category]}/list.htm`);
 
-    const data = response.data;
-
+    const { data } = response;
     const $ = cheerio.load(data);
-    const list = $('.wp_article_list_table .border9');
+    const links = $('.wp_article_list_table .border9')
+        .map((i, el) => ({
+            pubDate: new Date($('.date', el).text()).toUTCString(),
+            link: url.resolve(baseUrl, $('a', el).attr('href')),
+            title: $('a', el).text(),
+        }))
+        .get();
+
+    const item = await Promise.all(
+        [...links].slice(0, 10).map(async ({ pubDate, link, title }) => {
+            if (type(link) === 'htm') {
+                const { data } = await axios.get(link);
+                const $ = cheerio.load(data);
+
+                const description =
+                    $('div.wp_articlecontent').html() &&
+                    $('div.wp_articlecontent')
+                        .html()
+                        .replace(/src="\//g, `src="${url.resolve(baseUrl, '.')}`)
+                        .replace(/href="\//g, `href="${url.resolve(baseUrl, '.')}`)
+                        .trim();
+                // check for some bug links.
+                if (!description) {
+                    return;
+                }
+                return Promise.resolve({ pubDate, link, title, description });
+            } else {
+                // file to download
+                return Promise.resolve({ pubDate, link, title, description: '此链接为文件，点击以下载' });
+            }
+        })
+    );
 
     ctx.state.data = {
         title: '哈尔滨工程大学本科生院工作通知',
-        link: 'http://ugs.hrbeu.edu.cn/2821/list.htm',
-        item:
-            list &&
-            list
-                .map((index, item) => {
-                    item = $(item);
-                    return {
-                        title: item.find('a').text(),
-                        pubDate: new Date(item.find('.date').text()).toUTCString(),
-                        link: `http://ugs.hrbeu.edu.cn${item.find('a').attr('href')}`,
-                    };
-                })
-                .get(),
+        link: `${baseUrl}`,
+        item: item.filter((x) => x),
     };
 };
