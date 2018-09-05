3c15741b23d4d7487a620feaf6d9fa34bb991b79
Henry <hi@henry.wang>
2018-09-05 18:40:52 +0100

    Add 中国驻贝尔法斯特总领馆 -- 重要通知

--- router.js
+++ router.js
@@ -540,5 +540,6 @@ router.get('/xclient/app/:name', require('./routes/xclient/app'));
 // 中国驻外使领事馆
 router.get('/embassy/us/chicago', require('./routes/embassy/us/chicago'));
 router.get('/embassy/uk/edinburgh', require('./routes/embassy/uk/edinburgh'));
+router.get('/embassy/uk/belfast', require('./routes/embassy/uk/belfast'));
 
 module.exports = router;
