51f789c19c15495b92d7d426d248d38ca5174e5d
luojunyuan <ljy7723656@gmail.com>
2018-09-08 04:48:28 +0800

    add new

---
+++ routes/universities/cuit/cxxww.js
@@ -0,0 +1,45 @@
+const axios = require('../../../utils/axios');
+const cheerio = require('cheerio');
+const resolve_url = require('url').resolve;
+
+const host = 'http://www.cuit.edu.cn/';
+
+const map = {
+    zhxw: '1',
+    xxgg: '2',
+    jjxw: '3',
+    xxdt: '4',
+    ggjl: '5',
+};
+
+module.exports = async (ctx) => {
+    const type = ctx.params.type || 'zzxw';
+    const link = host + 'NewsList?id=' + map[type];
+
+    const response = await axios({
+        method: 'get',
+        url: link,
+        headers: {
+            Referer: host,
+        },
+    });
+
+    const $ = cheerio.load(response.data);
+
+    ctx.state.data = {
+        link: link,
+        title: $('#NewsTypeName').text(),
+        item: $('.news1-links-ul>li')
+            .map((_, elem) => ({
+                link: resolve_url(link, $('a', elem).attr('href')),
+                title: $('a', elem).text(),
+                pubDate: new Date(
+                    $('.datetime', elem)
+                        .text()
+                        .replace('[', '')
+                        .replace(']', '')
+                ).toUTCString(),
+            }))
+            .get(),
+    };
+};
