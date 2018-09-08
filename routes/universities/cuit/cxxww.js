20b37831e7611c4cae5011b867bc14c207fe936a
luojunyuan <ljy7723656@gmail.com>
2018-09-08 22:11:38 +0800

    re-format

--- routes/universities/cuit/cxxww.js
+++ routes/universities/cuit/cxxww.js
@@ -39,7 +39,7 @@ module.exports = async (ctx) => {
                         .replace('[', '')
                         .replace(']', '')
                 ).toUTCString(),
-                description: ' '
+                description: ' ',
             }))
             .get(),
     };
