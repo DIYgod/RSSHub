12b4324b4eadc31f9d335cc07439fe926683a0f5
binarization <scm@binarization.com>
2018-09-09 12:47:00 +0800

    update: router

---
+++ routes/universities/cqust/jw.js
@@ -0,0 +1,41 @@
+const axios = require('../../../utils/axios');
+const cheerio = require('cheerio');
+const url = require('url').resolve;
+
+const host = 'http://jw.cqust.edu.cn';
+const map = {
+    notify: '/tzgg/tz.htm',
+    news: '/xwdt/xxyw.htm',
+};
+
+module.exports = async (ctx) => {
+    const type = ctx.params.type || 'notify';
+    const link = host + map[type];
+    const response = await axios.get(link);
+    const $ = cheerio.load(response.data);
+    const list = $('li[id^="lineu"]');
+
+    ctx.state.data = {
+        title: '重科教务处',
+        link: link,
+        description: $('meta[name="description"]').attr('content') || '重科教务处',
+        item:
+            list &&
+            list
+                .map((index, item) => {
+                    item = $(item);
+                    return {
+                        title: item.find('a').text(),
+                        description: item.find('p').text() + ' [...]' || '重科教务处',
+                        pubDate: new Date(
+                            item
+                                .find('.shijian')
+                                .text()
+                                .replace('》', '')
+                        ).toUTCString(),
+                        link: url(host, item.find('a').attr('href')),
+                    };
+                })
+                .get(),
+    };
+};
