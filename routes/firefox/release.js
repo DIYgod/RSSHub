cbf7fb1bfccb17eebca4408565b29f6735a3f3a1
Gary Williams <acadsear@yahoo.com>
2018-09-11 12:49:28 +0800

    add Firefox ESR

--- routes/firefox/release.js
+++ routes/firefox/release.js
@@ -8,22 +8,63 @@ module.exports = async (ctx) => {
     if (ctx.params.platform === 'android-beta') {
         ctx.params.platform = 'android/beta';
     }
-    const response = await axios.get(`https://www.mozilla.org/en-US/firefox/${ctx.params.platform}/notes/`);
-    const data = response.data;
-    const $ = cheerio.load(data);
+    if (ctx.params.platform === 'esr') {
+        ctx.params.platform = 'esr';
+    }
 
-    ctx.state.data = {
-        title: `Firefox ${ctx.params.platform} release note`,
-        link: `https://www.mozilla.org/en-US/firefox/${ctx.params.platform}/notes/`,
-        item: [
-            {
-                title: `Firefox ${ctx.params.platform} ${$('.version')
-                    .find('h2')
-                    .text()} release note`,
-                link: `https://www.mozilla.org/en-US/firefox/${ctx.params.platform}/notes/`,
-                description: $('.notes-section').html(),
-            },
-        ],
-        description: $('.description').text(),
-    };
+    if (ctx.params.platform != 'esr') {
+        const response = await axios.get(`https://www.mozilla.org/en-US/firefox/${ctx.params.platform}/notes/`);
+        const data = response.data;
+        const $ = cheerio.load(data);
+
+        ctx.state.data = {
+            title: `Firefox ${ctx.params.platform} release note`,
+            link: `https://www.mozilla.org/en-US/firefox/${ctx.params.platform}/notes/`,
+            item: [
+                {
+                    title: `Firefox ${ctx.params.platform} ${$('.version')
+                        .find('h2')
+                        .text()} release note`,
+                    link: `https://www.mozilla.org/en-US/firefox/${ctx.params.platform}/notes/`,
+                    description: $('.notes-section').html(),
+                },
+            ],
+            description: $('.description').text(),
+        };
+    }
+    else {  // firefox esr
+        const response = await axios.get(`https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox-esr/`);
+        const data = response.data;
+        const $ = cheerio.load(data);
+
+        const obj = $('.level-heading').first();
+        const title = obj.text().replace(/\s\s+/g, ' ').trim();
+        const version = obj.find('a').attr('href');
+
+        var descr = obj.next();
+        descr.find('a').each(function(i,elem) {
+            var h = $(this).attr('href');
+            if (h.startsWith('http')) { }
+            else {
+                if (h.startsWith('/')) {
+                    $(this).attr('href', 'https://www.mozilla.org' + h);
+                }
+            }
+        });
+        descr = descr.html();
+
+        ctx.state.data = {
+            title: `Firefox ESR release note`,
+            link: `https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox-esr/`,
+            item: [
+                {
+                    title: title,
+                    link: `https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox-esr/${version}`,
+                    guid: version,
+                    description: descr,
+                },
+            ],
+        };
+    }
+
 };
