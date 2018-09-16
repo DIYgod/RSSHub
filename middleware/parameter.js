8b64de4b576ba83248ba7009841282004ec3080b
Henry <hi@henry.wang>
2018-09-16 13:17:19 +0100

    处理过滤参数中中文转码的问题
    Fix #403

--- middleware/parameter.js
+++ middleware/parameter.js
@@ -1,26 +1,30 @@
 module.exports = async (ctx, next) => {
     await next();
 
+    // 对所有中文进行转码
+    const encode = (str) => str.replace(/([\u4e00-\u9fa5])/g, (s) => encodeURIComponent(s));
+
     // filter
     if (ctx.state.data && ctx.query && (ctx.query.filter || ctx.query.filter_title || ctx.query.filter_description)) {
         ctx.state.data.item = ctx.state.data.item.filter((item) => {
-            const title = item.title;
-            const description = item.description || title;
+            const title = encode(item.title);
+            const description = item.description ? encode(item.description) : title;
             return !(
-                (ctx.query.filter && !title.match(ctx.query.filter) && !description.match(ctx.query.filter)) ||
-                (ctx.query.filter_title && !title.match(ctx.query.filter_title)) ||
-                (ctx.query.filter_description && !description.match(ctx.query.filter_description))
+                (ctx.query.filter && !title.match(encode(ctx.query.filter)) && !description.match(encode(ctx.query.filter))) ||
+                (ctx.query.filter_title && !title.match(encode(ctx.query.filter_title))) ||
+                (ctx.query.filter_description && !description.match(encode(ctx.query.filter_description)))
             );
         });
     }
+
     if (ctx.state.data && ctx.query && (ctx.query.filterout || ctx.query.filterout_title || ctx.query.filterout_description)) {
         ctx.state.data.item = ctx.state.data.item.filter((item) => {
-            const title = item.title;
-            const description = item.description;
+            const title = encode(item.title);
+            const description = item.description ? encode(item.description) : title;
             return (
-                (ctx.query.filterout && !title.match(ctx.query.filterout) && !description.match(ctx.query.filterout)) ||
-                (ctx.query.filterout_title && !title.match(ctx.query.filterout_title)) ||
-                (ctx.query.filterout_description && !description.match(ctx.query.filterout_description))
+                (ctx.query.filterout && !title.match(encode(ctx.query.filterout)) && !description.match(encode(ctx.query.filterout))) ||
+                (ctx.query.filterout_title && !title.match(encode(ctx.query.filterout_title))) ||
+                (ctx.query.filterout_description && !description.match(encode(ctx.query.filterout_description)))
             );
         });
     }
