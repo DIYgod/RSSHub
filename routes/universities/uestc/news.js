4d9ae9cd0579b9ba33359170c64168ef6d1e5a50
achjqz <32415185+achjqz@users.noreply.github.com>
2018-09-21 18:36:23 +0800

    增加电子科技大学教务处和新闻中心的通知公告 (#752)

---
+++ routes/universities/uestc/news.js
@@ -0,0 +1,37 @@
+const axios = require('../../../utils/axios');
+const cheerio = require('cheerio');
+
+const listUrl = 'http://www.new1.uestc.edu.cn//?n=UestcNews.Front.Category.Page&CatId=';
+const baseUrl = 'http://www.new1.uestc.edu.cn';
+const map = {
+    academy: '66',
+    culture: '67',
+    announcement: '68',
+    notification: '72',
+};
+module.exports = async (ctx) => {
+    const type = ctx.params.type || 'announcement';
+    const response = await axios({
+        method: 'get',
+        url: listUrl + map[type],
+    });
+
+    const data = response.data;
+    const $ = cheerio.load(data);
+    ctx.state.data = {
+        title: '电子科技大学新闻中心',
+        link: baseUrl,
+        item: $('div[id="Degas_news_list"] ul li h3 a')
+            .map((_, elem) => ({
+                link: baseUrl + elem.attribs.href,
+                title: $(elem).text(),
+                pubDate: new Date(
+                    $(elem)
+                        .parent('h3')
+                        .next('span')
+                        .text()
+                ).toUTCString(),
+            }))
+            .get(),
+    };
+};
