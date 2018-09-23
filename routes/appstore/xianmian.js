0427a92d49848329072202447c1609e26f430ed0
Henry <hi@henry.wang>
2018-09-23 01:26:29 +0100

    鲜面应用细微改动

--- routes/appstore/xianmian.js
+++ routes/appstore/xianmian.js
@@ -3,18 +3,18 @@ const axios = require('../../utils/axios');
 module.exports = async (ctx) => {
     const {
         data: { objects: data },
-    } = await axios.get('http://app.so/api/v5/appso/discount/?platform=web&limit=20');
+    } = await axios.get('http://app.so/api/v5/appso/discount/?platform=web&limit=10');
 
     ctx.state.data = {
         title: '每日精品限免 / 促销应用',
         link: 'http://app.so/xianmian/',
         description: '鲜面连线 by AppSo：每日精品限免 / 促销应用',
         item: data.map((item) => ({
-            title: item.app.name,
+            title: `「${item.discount_info[0].discounted_price === 0 ? '免费' : '降价'}」${item.app.name}`,
             description: `
           <img referrerpolicy="no-referrer" src="${item.app.icon.image}"/>
           <br/>
-          原价：${item.discount_info[0].original_price} -> 现售：${item.discount_info[0].discounted_price}
+          原价：¥${item.discount_info[0].original_price} -> 现价：¥${item.discount_info[0].discounted_price}
           <br/>
           平台：${item.app.download_link[0].device}
           <br/>
