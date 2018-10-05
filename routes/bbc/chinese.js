cdabbb69d9921e793f63d73485bb473422ef6abf
henry.wang <hi@henry.wang>
2018-10-05 17:27:53 +0100

    Handle BBC images and videos

--- routes/bbc/chinese.js
+++ routes/bbc/chinese.js
@@ -1,18 +1,32 @@
+const axios = require('../../utils/axios');
+const cheerio = require('cheerio');
+const utils = require('./utils');
 const Parser = require('rss-parser');
 const parser = new Parser();
 
 module.exports = async (ctx) => {
     const feed = await parser.parseURL('http://feeds.bbci.co.uk/zhongwen/simp/rss.xml');
-    const items = [];
-    feed.items.forEach((item) => {
-        items.push({
-            title: item.title,
-            description: item.content,
-            pubDate: item.pubDate,
-            guid: item.guid,
-            link: item.link,
-        });
-    });
+
+    const items = await Promise.all(
+        feed.items.splice(0, 10).map(async (item) => {
+            const response = await axios({
+                method: 'get',
+                url: item.link,
+            });
+
+            const $ = cheerio.load(response.data);
+
+            const description = utils.ProcessFeed($, item.link).html();
+
+            const single = {
+                title: item.title,
+                description,
+                pubDate: item.pubDate,
+                link: item.link,
+            };
+            return Promise.resolve(single);
+        })
+    );
 
     ctx.state.data = {
         title: 'BBC News 中文',
