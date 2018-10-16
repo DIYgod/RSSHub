1eaf520a97a40cbdb9747907b58fbca495a692a1
YueZhu1991 <dxmcyr@gmail.com>
2018-10-16 20:45:50 +0800

    Update jwc.js

--- routes/universities/njust/jwc.js
+++ routes/universities/njust/jwc.js
@@ -2,6 +2,7 @@ const axios = require('../../../utils/axios');
 const cheerio = require('cheerio');
 
 module.exports = async (ctx) => {
+    const baseURL = 'http://jwc.njust.edu.cn';
     const res = await axios({
         method: 'get',
         url: 'http://jwc.njust.edu.cn/1216/list.htm',
@@ -14,20 +15,20 @@ module.exports = async (ctx) => {
     const list = $('tr', 'table#newslist');
 
     ctx.state.data = {
-        title: $('title')
-            .first()
-            .text(),
+        title: '南京理工大学教务处',
         link: 'http://jwc.njust.edu.cn/1216/list.htm',
-        description: '南京理工大学教务处 - 教师通知',
+        description: '教师通知',
+
         item:
             list &&
             list
                 .map((index, item) => {
                     item = $(item);
+
                     return {
                         title: item.find('a').text(),
-                        pubDate: new Date(item.find('.time').text()).toUTCString(),
-                        link: item.find('a').attr('href'),
+                        link: `${baseURL + $(item.find('a')).attr('href')}`,
+                        pubDate: $(item.find('td[width="14%"]'),
                     };
                 })
                 .get(),
