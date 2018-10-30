b2a59d2b9981e8d6b52f847e69644e258a8e9d75
boringzhang <34540342+boringzhang@users.noreply.github.com>
2018-10-30 10:47:23 +0800

    change password

--- config.js
+++ config.js
@@ -47,8 +47,8 @@ module.exports = {
         clientId: process.env.IMGUR_CLIRNT_ID,
     },
     authentication: {
-        name: process.env.HTTP_BASIC_AUTH_NAME || 'usernam3',
-        pass: process.env.HTTP_BASIC_AUTH_PASS || 'passw0rd',
+        name: process.env.HTTP_BASIC_AUTH_NAME || 'need',
+        pass: process.env.HTTP_BASIC_AUTH_PASS || 'passward',
     },
     puppeteerWSEndpoint: process.env.PUPPETEER_WS_ENDPOINT,
     newrelicLicenseKey: process.env.NEWRELIC_LICENSE_KEY,
