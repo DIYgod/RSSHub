b7ff83386c856deebf1b05d4ff5a560091152876
xyqfer <xyqfer@gmail.com>
2018-11-02 10:01:30 +0800

    移除 UA，优化代码

--- routes/google/doodles.js
+++ routes/google/doodles.js
@@ -13,7 +13,6 @@ module.exports = async (ctx) => {
         method: 'get',
         url: `https://www.google.com/doodles/json/${year}/${month}?hl=${language}`,
         headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
             Referer: link,
         },
     });
@@ -21,16 +20,18 @@ module.exports = async (ctx) => {
     ctx.state.data = {
         title: 'Google Doodles',
         link,
-        item: data.map((item) => {
-            const date = `${item.run_date_array[0]}-${item.run_date_array[1]}-${item.run_date_array[2]}`;
+        item:
+            data &&
+            data.map((item) => {
+                const date = `${item.run_date_array[0]}-${item.run_date_array[1]}-${item.run_date_array[2]}`;
 
-            return {
-                title: item.title,
-                description: `<img src="https:${item.url}" referrerpolicy="no-referrer" /><br>${item.share_text}`,
-                pubDate: new Date(date).toUTCString(),
-                guid: item.url,
-                link: `https://www.google.com/search?q=${encodeURIComponent(item.query)}`,
-            };
-        }),
+                return {
+                    title: item.title,
+                    description: `<img src="https:${item.url}" referrerpolicy="no-referrer" /><br>${item.share_text}`,
+                    pubDate: new Date(date).toUTCString(),
+                    guid: item.url,
+                    link: `https://www.google.com/search?q=${encodeURIComponent(item.query)}`,
+                };
+            }),
     };
 };
