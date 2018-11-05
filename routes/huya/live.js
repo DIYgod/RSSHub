5e690b44a8c3c6e2a02c2dddf2569dceba6a10d9
SettingDust <settingdust@gmail.com>
2018-11-05 11:31:25 +0800

    Fix the huya

--- routes/huya/live.js
+++ routes/huya/live.js
@@ -1,31 +1,28 @@
 const axios = require('../../utils/axios');
+const cheerio = require('cheerio');
 
 module.exports = async (ctx) => {
     const id = ctx.params.id;
-    const url = `https://search.cdn.huya.com/?m=Search&do=getSearchContent&q=${id}&typ=-5&rows=1`;
+    const url = `https://huya.com/${id}`;
     const response = await axios({
         method: 'get',
         url: url,
     });
 
-    const data = response.data.response['1'].docs[0];
+    const $ = cheerio.load(response.data);
 
-    let items = [];
-    if (data.gameLiveOn) {
-        items = [
-            {
-                title: `${data.live_intro}`,
-                pubDate: new Date(data.rec_live_time * 1000).toUTCString(),
-                guid: data.uid,
-                link: data.game_liveLink,
-                image: data.game_avatarUrl180,
-            },
-        ];
-    }
+    const timestamp = parseInt(response.data.match(/"startTime":(\d+)/)[1]);
 
     ctx.state.data = {
-        title: `${data.game_nick}的虎牙直播`,
-        link: `https://huya.com/${id}`,
-        item: items,
+        title: `${$('.host-name').text()}的虎牙直播`,
+        link: url,
+        item: [
+            {
+                title: $('#J_roomTitle').text(),
+                guid: timestamp,
+                pubDate: new Date(timestamp).toUTCString(),
+                link: url,
+            },
+        ],
     };
 };
