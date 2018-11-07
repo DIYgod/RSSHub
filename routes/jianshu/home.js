97c196e755d69bb6b4fe23971535b90320fdd67e
Henry Wang <hi@henry.wang>
2018-11-07 13:07:59 +0000

    提供简书全文获取，修复简书日期，更新教程文档 (#1081)
    1. 提供全文获取
    2. 修复无效日期
    3. 更新教程文档

--- routes/jianshu/home.js
+++ routes/jianshu/home.js
@@ -1,5 +1,6 @@
 const axios = require('../../utils/axios');
 const cheerio = require('cheerio');
+const util = require('./utils');
 
 module.exports = async (ctx) => {
     const response = await axios({
@@ -13,24 +14,14 @@ module.exports = async (ctx) => {
     const data = response.data;
 
     const $ = cheerio.load(data);
-    const list = $('.note-list li');
+    const list = $('.note-list li').get();
+
+    const result = await util.ProcessFeed(list, ctx.cache);
 
     ctx.state.data = {
         title: '简书首页',
         link: 'https://www.jianshu.com',
         description: $('meta[name="description"]').attr('content'),
-        item:
-            list &&
-            list
-                .map((index, item) => {
-                    item = $(item);
-                    return {
-                        title: item.find('.title').text(),
-                        description: `作者：${item.find('.nickname').text()}<br>描述：${item.find('.abstract').text()}<br><img referrerpolicy="no-referrer" src="https:${item.find('.img-blur').data('echo')}">`,
-                        pubDate: new Date(item.find('.time').data('shared-at')).toUTCString(),
-                        link: `https://www.jianshu.com${item.find('.title').attr('href')}`,
-                    };
-                })
-                .get(),
+        item: result,
     };
 };
