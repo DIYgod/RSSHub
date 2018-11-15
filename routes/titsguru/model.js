896067f55394191677892082af1f00e8cdc2d2ce
MegrezZhu <mystery490815101@gmail.com>
2018-11-16 01:14:36 +0800

    t-guru: fixed title

--- routes/titsguru/model.js
+++ routes/titsguru/model.js
@@ -3,5 +3,14 @@ const { getPage, normalizeKeyword } = require('./util');
 module.exports = async (ctx) => {
     const { name } = ctx.params;
 
-    ctx.state.data = await getPage(`https://tits-guru.com/boobs-model/${normalizeKeyword(name)}/date`);
+    const formalName = normalizeKeyword(name)
+        .split('-')
+        .map((word) => `${word[0].toUpperCase()}${word.substr(1)}`)
+        .join(' ');
+
+    ctx.state.data = {
+        ...(await getPage(`https://tits-guru.com/boobs-model/${normalizeKeyword(name)}/date`)),
+        title: `TitsGuru - ${formalName}`,
+        description: `TitsGuru - ${formalName}`,
+    };
 };
