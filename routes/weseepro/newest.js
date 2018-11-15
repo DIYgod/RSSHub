026490cf57ae61b99f6fa0cb44cafb4f528611df
xyqfer <xyqfer@gmail.com>
2018-11-15 14:48:35 +0800

    hotfix

--- routes/weseepro/newest.js
+++ routes/weseepro/newest.js
@@ -7,19 +7,53 @@ module.exports = async (ctx) => {
     });
 
     const data = response.data.data.data;
+    const generateMessage = (message) => `
+            <img referrerpolicy="no-referrer" src="${message.link.img_url}"/><br><br>
+            <a href="${message.link.url}">${message.link.summary}</a><br><br>
+            <strong>${message.topMessage.account.name}</strong>: ${message.topMessage.content.replace(/\n/g, '<br>')}<br><br>
+        `;
 
     ctx.state.data = {
         title: '刷屏-最新',
         link: 'https://www.weseepro.com',
-        item: data.map(({ data: { spam } }) => ({
-            title: spam.content,
-            link: `https://www.weseepro.com/mine/article?uuid=${spam.uuid}`,
-            description: `
-                <img referrerpolicy="no-referrer" src="${spam.link.img_url}"/><br><br>
-                <a href="${spam.link.url}">${spam.link.summary}</a><br><br>
-                <strong>${spam.topMessage.account.name}</strong>: ${spam.topMessage.content}
-              `,
-            pubDate: new Date(spam.spam_add_time).toUTCString(),
-        })),
+        item: data.map((item) => {
+            let title;
+            let link;
+            let description;
+            let pubDate;
+
+            if (item.data.spam) {
+                const spam = item.data.spam;
+
+                title = spam.content;
+                link = `https://www.weseepro.com/mine/article?uuid=${spam.uuid}`;
+                description = generateMessage(spam);
+                pubDate = new Date(spam.spam_add_time).toUTCString();
+            } else if (item.data.special) {
+                const special = item.data.special;
+                const specialMessages = item.data.special_messages;
+                const messages = specialMessages.reduce((messages, message) => {
+                    messages += generateMessage(message);
+                    return messages;
+                }, '');
+
+                title = special.title;
+                link = `https://www.weseepro.com/mine/album?uuid=${special.uuid}`;
+                description = `
+                    <img referrerpolicy="no-referrer" src="${special.image}"/><br><br>
+                    ${special.description}<br><br>
+                    ${messages}
+                `;
+            } else {
+                title = description = '未知类型，请点击<a href="https://github.com/DIYgod/RSSHub/issues">链接</a>提交issue';
+            }
+
+            return {
+                title,
+                link,
+                description,
+                pubDate,
+            };
+        }),
     };
 };
