73a01a1ff047576365d7c81caeea9b2bb20f253b
凉凉 <xyqfer@gmail.com>
2018-12-11 11:32:50 +0800

    增加：国家电网停电通知 (#1260)
    Closes #1117

---
+++ routes/tingdiantz/95598.js
@@ -0,0 +1,48 @@
+const axios = require('../../utils/axios');
+const qs = require('querystring');
+
+module.exports = async (ctx) => {
+    const { orgNo, provinceNo, outageStartTime, outageEndTime, scope = '' } = ctx.params;
+    const anHui = provinceNo === '34101' ? '01' : '02';
+    const response = await axios({
+        method: 'post',
+        url: 'http://m.95598.cn/95598/woutageNotice/wqueryOutageNoticeList?partNo=BM08001',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data: qs.stringify({
+            orgNo,
+            provinceNo,
+            outageStartTime,
+            outageEndTime,
+            scope,
+            anHui,
+        }),
+    });
+
+    const items = (response.data.seleList || []).map((item) => {
+        const title = `${item.subsName} | ${item.typeCode || ''}`;
+        const description = `
+        <strong>停电时间：</strong>${item.startTime} - ${item.stopTime} <br>
+        <strong>停电范围：</strong>${item.scope} <br>
+        <strong>停电线路：</strong>${item.lineName} <br>
+        <strong>公变名称：</strong>${item.pubTranName} <br>
+        <strong>停电原因：</strong>${item.poweroffReason} <br>
+      `;
+        const guid = item.powerOffId;
+        const pubDate = new Date(item.startTime).toUTCString();
+
+        return {
+            title,
+            description,
+            guid,
+            pubDate,
+        };
+    });
+
+    ctx.state.data = {
+        title: '国家电网停电通知',
+        link: 'http://m.95598.cn/95598/woutageNotice/winitOutageNotice',
+        item: items,
+    };
+};
