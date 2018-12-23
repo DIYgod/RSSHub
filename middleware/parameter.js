ce6596e7cf98c5e10389eebc140ebf0e24e32e1d
Henry <hi@henry.wang>
2018-12-23 15:58:48 +0800

    过滤作者

--- middleware/parameter.js
+++ middleware/parameter.js
@@ -15,25 +15,29 @@ module.exports = async (ctx, next) => {
     }
 
     // filter
-    if (ctx.state.data && ctx.query && (ctx.query.filter || ctx.query.filter_title || ctx.query.filter_description)) {
+    if (ctx.state.data && ctx.query && (ctx.query.filter || ctx.query.filter_title || ctx.query.filter_description || ctx.query.filter_author)) {
         ctx.state.data.item = ctx.state.data.item.filter((item) => {
             const title = item.title;
             const description = item.description || title;
+            const author = item.author || '';
             return !(
                 (ctx.query.filter && !title.match(ctx.query.filter) && !description.match(ctx.query.filter)) ||
                 (ctx.query.filter_title && !title.match(ctx.query.filter_title)) ||
-                (ctx.query.filter_description && !description.match(ctx.query.filter_description))
+                (ctx.query.filter_description && !description.match(ctx.query.filter_description)) ||
+                (ctx.query.filter_author && !author.match(ctx.query.filter_author))
             );
         });
     }
-    if (ctx.state.data && ctx.query && (ctx.query.filterout || ctx.query.filterout_title || ctx.query.filterout_description)) {
+    if (ctx.state.data && ctx.query && (ctx.query.filterout || ctx.query.filterout_title || ctx.query.filterout_description || ctx.query.filterout_author)) {
         ctx.state.data.item = ctx.state.data.item.filter((item) => {
             const title = item.title;
             const description = item.description || title;
+            const author = item.author || '';
             return (
                 (ctx.query.filterout && !title.match(ctx.query.filterout) && !description.match(ctx.query.filterout)) ||
                 (ctx.query.filterout_title && !title.match(ctx.query.filterout_title)) ||
-                (ctx.query.filterout_description && !description.match(ctx.query.filterout_description))
+                (ctx.query.filterout_description && !description.match(ctx.query.filterout_description)) ||
+                (ctx.query.filterout_author && !author.match(ctx.query.filterout_author))
             );
         });
     }
