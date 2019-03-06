7137a14c2b937c4e51ad1978a70645c317c38298
SettingDust <settingdust@gmail.com>
2019-03-06 20:41:39 +0800

    Add common config test

---
+++ test/utils/common-config.js
@@ -0,0 +1,19 @@
+const buildData = require('../../lib/utils/common-config');
+
+describe('common-config', () => {
+    it('config-content', async () => {
+        const link = 'http://127.0.0.1:1200';
+        const data = await buildData({
+            link,
+            url: link,
+            title: `$('.content>h1>span').text()`,
+            item: {
+                item: 'details>div',
+                title: `$('.debug-key').text()`,
+                description: `$('.debug-value').text()`,
+            },
+        });
+        expect(data.title).toBe('RSSHub');
+        expect(data.item.length).toBe(9);
+    });
+});
