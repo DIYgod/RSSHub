bb7003197233c010724e7443cbda979bd3deb8ba
chenws <chenws199@gmail.com>
2019-08-01 17:19:43 +0800

    fix: 补全测试

--- test/utils/common-utils.js
+++ test/utils/common-utils.js
@@ -16,4 +16,23 @@ describe('common-utils', () => {
 
         expect(result).toBe('<img src="https://user-images.githubusercontent.com/5896343/60762836-02746d80-a060-11e9-8947-76b3b3d3b731.png" referrerpolicy="no-referrer">');
     });
+
+    it('resolveLazyLoadImage', async () => {
+        const testSet = [
+            '<div><img lazyload="http://user-images.test.com/6b3b3d3b731.png" remove="http://githubusercontent.com/b3b3d3b731.png"></div>',
+            '<div><img data-src="https://user-images.test.com/3d3b731.webp" remove="remove" remove2="remove2"></div>',
+            '<div><img src="//user-images.githubusercontent.com/3d3b731.gif" remove="remove"></div>',
+        ];
+        const expectList = [
+            '<img lazyload="http://user-images.test.com/6b3b3d3b731.png" remove="http://githubusercontent.com/b3b3d3b731.png" src="http://user-images.test.com/6b3b3d3b731.png" referrerpolicy="no-referrer">',
+            '<img data-src="https://user-images.test.com/3d3b731.webp" remove="remove" remove2="remove2" src="https://user-images.test.com/3d3b731.webp" referrerpolicy="no-referrer">',
+            '<img src="//user-images.githubusercontent.com/3d3b731.gif" remove="remove" referrerpolicy="no-referrer">',
+        ];
+        testSet.forEach((htmlStr, index) => {
+            const $ = cheerio.load(htmlStr);
+            utils.resolveLazyLoadImage($);
+            const result = $('div').html();
+            expect(result).toBe(expectList[index]);
+        });
+    });
 });
