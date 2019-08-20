58feef9d3b4af0abf27d0bb0bbc1d0078dbf93c2
Cloud <kt286@qq.com>
2019-08-20 15:29:27 +0800

    test: common-config

--- test/utils/common-config.js
+++ test/utils/common-config.js
@@ -1,4 +1,5 @@
 const configUtils = require('../../lib/utils/common-config');
+const nock = require('nock');
 
 describe('index', () => {
     it('transElemText', async () => {
@@ -35,5 +36,45 @@ describe('index', () => {
             title: '%title%',
         };
         expect(configUtils.getProp(data, ['title'], $)).toBe('RSSHub');
+    });
+
+    it('buildData', async () => {
+        nock('http://rsshub.test')
+            .get('/buildData')
+            .reply(function() {
+                return [
+                    200,
+                    `<div class="content">
+                <ul>
+                    <li>
+                        <a href="/1">1</a>
+                        <div class="description">RSSHub1</div>
+                    </li>
+                    <li>
+                        <a href="/2">2</a>
+                        <div class="description">RSSHub2</div>
+                    </li>
+                </ul>
+            </div>`,
+                ];
+            });
+        const data = await configUtils({
+            link: 'http://rsshub.test/buildData',
+            url: 'http://rsshub.test/buildData',
+            title: `%title%`,
+            params: {
+                title: 'buildData',
+            },
+            item: {
+                item: '.content li',
+                title: `$('a').text() + ' - %title%'`,
+                link: `$('a').attr('href')`,
+                description: `$('.description').html()`,
+            },
+        });
+
+        expect(data.item.length).toBe(2);
+        expect(data.item[1].title).toBe('2 - buildData');
+        expect(data.item[1].description).toBe('RSSHub2');
     });
 });
