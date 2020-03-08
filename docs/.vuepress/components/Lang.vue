dc6683615d176266d0db4eacff3e7d617a1ed2d8
sgqy <sgqyzh@gmail.com>
2020-03-08 23:33:09 +0800

    add locale module to docs

---
+++ docs/.vuepress/components/Lang.vue
@@ -0,0 +1,19 @@
+<template>
+    <img class="flag" :src="'locale/' + loc + '.svg'" :alt="loc" />
+</template>
+<script>
+export default {
+    props: {
+        loc: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+<style>
+img.flag {
+    height: 1em;
+    vertical-align: baseline;
+}
+</style>
