d4315986cce1828e79d15eef5832b6bc05306e6f
fengkx <liangkx8237@gmail.com>
2020-03-19 00:15:09 +0800

    feat: smaller docker iamge

---
+++ clean-nm.sh
@@ -0,0 +1,3 @@
+find ./node_modules -type f -name |egrep "(.idea|.vscode|benchmark.js|.eslintrc.js|changelog|AUTHORS|license|LICENSE|LICENCE|.travis.yml|.eslintrc.json|.eslintrc.yml|Makefile|.npmignore|.DS_Store|.jshintrc|.eslintrc.BSD|.editorconfig|tsconfig.json|.coveralls.yml|appveyor.yml|.gitattributes|.eslintignore|.eslintrc|.eslintignore.BSD|.babelrc)"
+find node_modules -type f | egrep "\.(md|mdon|markdown|log|ts|swp|jst|coffee|txt|BSD|m?js.map)$" | xargs rm -f
+find node_modules -type d | egrep "(test|docs|doc|examples|example|.githubs|@types)" | xargs rm -rf
