a74cbc6a14bcc9e19dc4dba76d01ced392d9fd51
NeverBehave <gayhub@never.pet>
2020-04-19 14:02:24 -0400

    fix: newline in testcase

--- test/middleware/anti-hotlink.js
+++ test/middleware/anti-hotlink.js
@@ -19,26 +19,56 @@ describe('anti-hotlink', () => {
         const response = await request.get('/test/complicated');
         const parsed = await parser.parseString(response.text);
         expect(parsed.items[0].content).toBe(
-            `<a href="https://mock.com/DIYgod/RSSHub"/> <img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"> <a href="http://mock.com/DIYgod/RSSHub"/> <img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" data-src="/DIYgod/RSSHub0.jpg" referrerpolicy="no-referrer"> <img data-src="/DIYgod/RSSHub.jpg" src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"> <img data-mock="/DIYgod/RSSHub.png" src="https://i3.wp.com/mock.com/DIYgod/RSSHub.png" referrerpolicy="no-referrer"> <img mock="/DIYgod/RSSHub.gif" src="https://i3.wp.com/mock.com/DIYgod/RSSHub.gif" referrerpolicy="no-referrer"> <img src="https://i3.wp.com/mock.com/DIYgod/DIYgod/RSSHub" referrerpolicy="no-referrer"> <img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/></img></img></img></img></img></img>`
+            `<a href="https://mock.com/DIYgod/RSSHub"/>
+<img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">
+
+<a href="http://mock.com/DIYgod/RSSHub"/>
+<img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" data-src="/DIYgod/RSSHub0.jpg" referrerpolicy="no-referrer">
+<img data-src="/DIYgod/RSSHub.jpg" src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">
+<img data-mock="/DIYgod/RSSHub.png" src="https://i3.wp.com/mock.com/DIYgod/RSSHub.png" referrerpolicy="no-referrer">
+<img mock="/DIYgod/RSSHub.gif" src="https://i3.wp.com/mock.com/DIYgod/RSSHub.gif" referrerpolicy="no-referrer">
+<img src="https://i3.wp.com/mock.com/DIYgod/DIYgod/RSSHub" referrerpolicy="no-referrer">
+<img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/></img></img></img></img></img></img>`
         );
-        expect(parsed.items[1].content).toBe(`<a href="https://mock.com/DIYgod/RSSHub"/> <img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/>`);
+        expect(parsed.items[1].content).toBe(`<a href="https://mock.com/DIYgod/RSSHub"/>
+<img src="https://i3.wp.com/mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/>`);
     });
     it('url', async () => {
         process.env.HOTLINK_TEMPLATE = '${protocol}//${host}${pathname}';
         const response = await request.get('/test/complicated');
         const parsed = await parser.parseString(response.text);
         expect(parsed.items[0].content).toBe(
-            `<a href="https://mock.com/DIYgod/RSSHub"/> <img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"> <a href="http://mock.com/DIYgod/RSSHub"/> <img src="https://mock.com/DIYgod/RSSHub.jpg" data-src="/DIYgod/RSSHub0.jpg" referrerpolicy="no-referrer"> <img data-src="/DIYgod/RSSHub.jpg" src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"> <img data-mock="/DIYgod/RSSHub.png" src="https://mock.com/DIYgod/RSSHub.png" referrerpolicy="no-referrer"> <img mock="/DIYgod/RSSHub.gif" src="https://mock.com/DIYgod/RSSHub.gif" referrerpolicy="no-referrer"> <img src="http://mock.com/DIYgod/DIYgod/RSSHub" referrerpolicy="no-referrer"> <img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/></img></img></img></img></img></img>`
+            `<a href="https://mock.com/DIYgod/RSSHub"/>
+<img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">
+
+<a href="http://mock.com/DIYgod/RSSHub"/>
+<img src="https://mock.com/DIYgod/RSSHub.jpg" data-src="/DIYgod/RSSHub0.jpg" referrerpolicy="no-referrer">
+<img data-src="/DIYgod/RSSHub.jpg" src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">
+<img data-mock="/DIYgod/RSSHub.png" src="https://mock.com/DIYgod/RSSHub.png" referrerpolicy="no-referrer">
+<img mock="/DIYgod/RSSHub.gif" src="https://mock.com/DIYgod/RSSHub.gif" referrerpolicy="no-referrer">
+<img src="http://mock.com/DIYgod/DIYgod/RSSHub" referrerpolicy="no-referrer">
+<img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/></img></img></img></img></img></img>`
         );
-        expect(parsed.items[1].content).toBe(`<a href="https://mock.com/DIYgod/RSSHub"/> <img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/>`);
+        expect(parsed.items[1].content).toBe(`<a href="https://mock.com/DIYgod/RSSHub"/>
+<img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"/>`);
     });
     it('no-template', async () => {
         process.env.HOTLINK_TEMPLATE = '';
         const response = await request.get('/test/complicated');
         const parsed = await parser.parseString(response.text);
         expect(parsed.items[0].content).toBe(
-            `<a href="https://mock.com/DIYgod/RSSHub"></a> <img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"> <a href="http://mock.com/DIYgod/RSSHub"></a> <img src="https://mock.com/DIYgod/RSSHub.jpg" data-src="/DIYgod/RSSHub0.jpg" referrerpolicy="no-referrer"> <img data-src="/DIYgod/RSSHub.jpg" src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer"> <img data-mock="/DIYgod/RSSHub.png" src="https://mock.com/DIYgod/RSSHub.png" referrerpolicy="no-referrer"> <img mock="/DIYgod/RSSHub.gif" src="https://mock.com/DIYgod/RSSHub.gif" referrerpolicy="no-referrer"> <img src="http://mock.com/DIYgod/DIYgod/RSSHub" referrerpolicy="no-referrer"> <img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">`
+            `<a href="https://mock.com/DIYgod/RSSHub"></a>
+<img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">
+
+<a href="http://mock.com/DIYgod/RSSHub"></a>
+<img src="https://mock.com/DIYgod/RSSHub.jpg" data-src="/DIYgod/RSSHub0.jpg" referrerpolicy="no-referrer">
+<img data-src="/DIYgod/RSSHub.jpg" src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">
+<img data-mock="/DIYgod/RSSHub.png" src="https://mock.com/DIYgod/RSSHub.png" referrerpolicy="no-referrer">
+<img mock="/DIYgod/RSSHub.gif" src="https://mock.com/DIYgod/RSSHub.gif" referrerpolicy="no-referrer">
+<img src="http://mock.com/DIYgod/DIYgod/RSSHub" referrerpolicy="no-referrer">
+<img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">`
         );
-        expect(parsed.items[1].content).toBe(`<a href="https://mock.com/DIYgod/RSSHub"></a> <img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">`);
+        expect(parsed.items[1].content).toBe(`<a href="https://mock.com/DIYgod/RSSHub"></a>
+<img src="https://mock.com/DIYgod/RSSHub.jpg" referrerpolicy="no-referrer">`);
     });
 });
