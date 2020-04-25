d1c9812175b22ac651617dc2553da64cb4a71fa1
fengkx <liangkx8237@gmail.com>
2020-04-25 14:23:27 +0800

    use callback() directly instead of pkg.js

--- api/now.js
+++ api/now.js
@@ -1,23 +1,8 @@
 const path = require('path');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
-const RSSHub = require('@/pkg');
 const logger = require('@/utils/logger');
-const config = require('@/config').value;
-
 logger.clear();
-const art = require('art-template');
-const template = path.resolve(__dirname, '../lib/views/rss.art');
-const s = async (req, res) => {
-    RSSHub.init();
-    let path = req.url;
-    if (path.startsWith('/api')) {
-        path = path.substring(4);
-    }
-    const data = await RSSHub.request(path);
-    const rss = art(template, data);
-    res.setHeader('Cache-Control', `max-age=0, s-maxage=${config.cache.routeExpire}`);
-    res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-    res.send(rss);
-};
+const s = require('@/app.js').callback();
+
 module.exports = s;
