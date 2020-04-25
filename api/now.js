367335d2cf75d15167c012b9ab51eed594ff9b25
fengkx <liangkx8237@gmail.com>
2020-04-25 17:59:16 +0800

    explicitly set server max-age

--- api/now.js
+++ api/now.js
@@ -2,7 +2,12 @@ const path = require('path');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
 const logger = require('@/utils/logger');
+const config = require('@/config').value;
 logger.clear();
 const s = require('@/app.js').callback();
 
-module.exports = s;
+module.exports = (req, res) => {
+    s(req, res);
+    // set cdn cache timeout of now
+    res.setHeader('Cache-Control', `max-age=0, s-maxage=${config.cache.routeExpire}`);
+};
