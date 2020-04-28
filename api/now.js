fedfc741d54c548626c464b8a21fe48c3bd55bc1
DIYgod <diy.d.god@gmail.com>
2020-04-28 18:21:36 +0800

    feat: add config noLogfiles, set noLogFiles for now

--- api/now.js
+++ api/now.js
@@ -1,4 +1,9 @@
+const config = require('../lib/config');
 const app = require('../lib/app');
+
+config.set({
+    noLogFiles: true,
+});
 
 module.exports = (req, res) => {
     app.callback()(req, res);
