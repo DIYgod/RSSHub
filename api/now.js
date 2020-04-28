6eec02ea197314675896ba2f4879cecf51cab6cd
DIYgod <diy.d.god@gmail.com>
2020-04-28 18:11:00 +0800

    feat: simplify now.js

--- api/now.js
+++ api/now.js
@@ -1,10 +1,5 @@
-const path = require('path');
-const moduleAlias = require('module-alias');
-moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
-const logger = require('@/utils/logger');
-logger.clear();
-const s = require('@/app.js').callback();
+const app = require('../lib/app');
 
 module.exports = (req, res) => {
-    s(req, res);
+    app.callback()(req, res);
 };
