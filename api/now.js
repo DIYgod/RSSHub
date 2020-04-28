e2869d34be7d7eb197cb7db0cf638a2523a97c0b
fengkx <liangkx8237@gmail.com>
2020-04-28 17:12:33 +0800

    fix: remove setHeader

--- api/now.js
+++ api/now.js
@@ -2,12 +2,9 @@ const path = require('path');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
 const logger = require('@/utils/logger');
-const config = require('@/config').value;
 logger.clear();
 const s = require('@/app.js').callback();
 
 module.exports = (req, res) => {
     s(req, res);
-    // set cdn cache timeout of now
-    res.setHeader('Cache-Control', `max-age=${config.cache.routeExpire}, s-maxage=${config.cache.routeExpire}`);
 };
