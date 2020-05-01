3105e08aff95d4fc51817403b4874f78fbb5a270
Henry <hi@henry.wang>
2020-05-01 19:00:20 +0100

    test: add a test for access key

--- test/middleware/access-control.js
+++ test/middleware/access-control.js
@@ -8,6 +8,7 @@ async function checkBlock(response) {
 }
 
 afterEach(() => {
+    delete process.env.ACCESS_KEY;
     delete process.env.BLACKLIST;
     delete process.env.WHITELIST;
     jest.resetModules();
@@ -17,6 +18,7 @@ afterEach(() => {
 describe('access-control', () => {
     it(`blacklist`, async () => {
         process.env.BLACKLIST = '/test/1,233.233.233.233';
+        process.env.ACCESS_KEY = '1L0veRSSHub';
         server = require('../../lib/index');
         const request = supertest(server);
 
@@ -31,10 +33,23 @@ describe('access-control', () => {
 
         const response22 = await request.get('/test/2').set('X-Forwarded-For', '233.233.233.233');
         checkBlock(response22);
+
+        // wrong key, not on blacklist
+        const response31 = await request.get('/test/2?key=MeL0veRSSHub');
+        expect(response31.status).toBe(200);
+
+        // wrong key, on blacklist
+        const response32 = await request.get('/test/2?key=MeL0veRSSHub').set('X-Forwarded-For', '233.233.233.233');
+        checkBlock(response32);
+
+        // right key, on blacklist
+        const response33 = await request.get('/test/2?key=1L0veRSSHub').set('X-Forwarded-For', '233.233.233.233');
+        expect(response33.status).toBe(200);
     });
 
     it(`whitelist`, async () => {
         process.env.WHITELIST = '/test/1,233.233.233.233';
+        process.env.ACCESS_KEY = '1L0veRSSHub';
         server = require('../../lib/index');
         const request = supertest(server);
 
@@ -49,5 +64,17 @@ describe('access-control', () => {
 
         const response22 = await request.get('/test/2').set('X-Forwarded-For', '233.233.233.233');
         expect(response22.status).toBe(200);
+
+        // wrong key, not on whitelist
+        const response31 = await request.get('/test/2?key=MeL0veRSSHub');
+        checkBlock(response31);
+
+        // wrong key, on whitelist
+        const response32 = await request.get('/test/2?key=MeL0veRSSHub').set('X-Forwarded-For', '233.233.233.233');
+        expect(response32.status).toBe(200);
+
+        // right key
+        const response33 = await request.get('/test/2?key=1L0veRSSHub');
+        expect(response33.status).toBe(200);
     });
 });
