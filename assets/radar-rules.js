63d141d579f92a8afa1b16c8468e16d8598e9af0
MisteryMonster <misterymonster@hotmail.com>
2020-05-07 21:11:33 +0800

    不知道为什么无法提交说明，提示一部分stage什么的

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -1748,4 +1748,15 @@
             },
         ],
     },
+    'qq.com': {
+        _name: '微信',
+        'mp.weixin': [
+            {
+                title: '公众号栏目',
+                docs: 'https://docs.rsshub.app/multimedia.html#xi-ma-la-ya',
+                source: '/mp/homepage',
+                target: (params, url) => `/wechat/mp/homepage/${new URL(url).searchParams.get('__biz')}/${new URL(url).searchParams.get('hid')}/${new URL(url).searchParams.get('cid') ? new URL(url).searchParams.get('cid') : ''}`,
+            },
+        ],
+    },
 });
