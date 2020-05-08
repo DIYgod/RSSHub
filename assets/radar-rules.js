c987c197c75f4eb06edeaf7f070a3adc4d4bb2b5
MisteryMonster <misterymonster@hotmail.com>
2020-05-08 14:54:04 +0800

    粗心大意！

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -1753,7 +1753,7 @@
         'mp.weixin': [
             {
                 title: '公众号栏目',
-                docs: 'https://docs.rsshub.app/multimedia.html#xi-ma-la-ya',
+                docs: 'https://docs.rsshub.app/new-media.html#gong-zhong-hao-lan-mu-fei-tui-song-li-shi-xiao-xi',
                 source: '/mp/homepage',
                 target: (params, url) => `/wechat/mp/homepage/${new URL(url).searchParams.get('__biz')}/${new URL(url).searchParams.get('hid')}/${new URL(url).searchParams.get('cid') ? new URL(url).searchParams.get('cid') : ''}`,
             },
