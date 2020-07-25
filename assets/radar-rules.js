1c522b6c9f6ada99f0a0d8c0f96f6a14b33d69c8
hoilc <hoilc@foxmail.com>
2020-07-26 02:56:15 +0800

    add radar rule

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -2077,4 +2077,15 @@
             },
         ],
     },
+    'picuki.com': {
+        _name: 'Picuki',
+        www: [
+            {
+                title: '用户',
+                docs: 'https://docs.rsshub.app/social-media.html#picuki-yong-hu',
+                source: '/profile/:id',
+                target: '/picuki/profile/:id',
+            },
+        ],
+    },
 });
