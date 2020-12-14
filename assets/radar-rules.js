4a1d5cdbaa5e0c9e6b184b6ed6567fdda0d16f01
GitHub Action <action@github.com>
2020-12-14 11:29:57 +0000

    style: auto format

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -2455,7 +2455,7 @@
                 docs: 'https://docs.rsshub.app/bbs.html#saraba1st',
                 source: '/2b/:id',
                 target: (params) => {
-                    const id = params.id.includes("thread") ? params.id.split("-")[1] : '';
+                    const id = params.id.includes('thread') ? params.id.split('-')[1] : '';
                     return id ? `/saraba1st/thread/${id}` : '';
                 },
             },
