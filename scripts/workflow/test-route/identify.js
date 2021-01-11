07bf1797fea512d75ad36ba4d7c368100d0f2334
NeverBehave <gayhub@never.pet>
2021-01-10 19:31:51 -0500

    fix: style and token used on exit

--- scripts/workflow/test-route/identify.js
+++ scripts/workflow/test-route/identify.js
@@ -1,14 +1,14 @@
 const noFound = 'Auto: Route No Found';
 
 
-module.exports = ({github, context, core}, body, number) => {
+module.exports = async ({github, context, core}, body, number) => {
     core.debug(`body: ${body}`);
-    const m = body.match(/```routes\r\n((.|\r\n)*)```/);
-    core.debug(`match: ${m}`);
+    const m = body.match(/```routes\r\n((.|\r\n)*)```/)
+    core.debug(`match: ${m}`)
     let res = null;
 
-    const removeLabel = () => {
-        github.issues.removeLabel({
+    const removeLabel = async () => {
+        return github.issues.removeLabel({
             issue_number: number,
             owner: context.repo.owner,
             repo: context.repo.repo,
@@ -18,12 +18,12 @@ module.exports = ({github, context, core}, body, number) => {
 
     if (m && m[1]) {
         res = m[1].trim().split("\r\n");
-        core.info(`routes detected: ${res}`);
+        core.info(`routes detected: ${res}`)
 
         if (res.length > 0 && res[0] === "NOROUTE") {
-            core.info("PR stated no route, passing");
-            removeLabel();
-            github.issues.addLabels({
+            core.info("PR stated no route, passing")
+            await removeLabel();
+            await github.issues.addLabels({
                 issue_number: number,
                 owner: context.repo.owner,
                 repo: context.repo.repo,
@@ -32,20 +32,20 @@ module.exports = ({github, context, core}, body, number) => {
 
             return;
         } else if (res.length > 0) {
-            core.exportVariable('TEST_CONTINUE', true);
-            removeLabel();
-            return res;
+            core.exportVariable('TEST_CONTINUE', true)
+            await removeLabel()
+            return res
         }
     }
 
-    core.info("seems no route found, failing");
+    core.info("seems no route found, failing")
 
-    github.issues.addLabels({
+    await github.issues.addLabels({
         issue_number: number,
         owner: context.repo.owner,
         repo: context.repo.repo,
         labels: [noFound]
-    });
+    }).catch((e) => { core.warning(e) })
 
-    throw "Please follow the PR rules: failed to detect route";
+    throw "Please follow the PR rules: failed to detect route"
 };
