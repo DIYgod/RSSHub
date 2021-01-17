594b4c8fe65093113e82a0db5adad1bc4053b185
Fatpandac <30423976+Fatpandac@users.noreply.github.com>
2021-01-18 04:49:50 +0800

    add 重庆文理学院 (#6714)
    * add 重庆文理学院
    
    * add 重庆文理学院
    
    * Update radar-rules.js

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -2607,4 +2607,31 @@
             },
         ],
     },
+    'cqwu.net': {
+        _name: '重庆文理学院',
+        www: [
+            {
+                title: '通知',
+                docs: 'https://docs.rsshub.app/university.html#chong-qing-wen-li-xue-yuan',
+                source: '/:type',
+                target: (params) => {
+                    if (params.type === 'channel_7721.html')
+                    {
+                        return '/cqwu/news/notify';
+                    }
+                },
+            },
+            {
+                title: '学术活动',
+                docs: 'https://docs.rsshub.app/university.html#chong-qing-wen-li-xue-yuan',
+                source: '/:type',
+                target: (params) => {
+                    if (params.type === 'channel_7722.html')
+                    {
+                        return '/cqwu/news/academiceve';
+                    }
+                },
+            },
+        ]
+    },
 });
