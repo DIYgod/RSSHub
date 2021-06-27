61d9e2dd4e44e0555285e0a34edad08d8513b609
Howard Yin <37261065+yindaheng98@users.noreply.github.com>
2021-06-27 08:02:22 +0800

    fix(radar)(route): Pixiv search & Radar rules (#7700)

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -140,24 +140,26 @@
             {
                 title: '用户收藏',
                 docs: 'https://docs.rsshub.app/social-media.html#pixiv',
-                source: '/bookmark.php',
-                target: (params, url) => `/pixiv/user/bookmarks/${new URL(url).searchParams.get('id')}`,
+                source: '/users/:id/bookmarks/artworks',
+                target: '/pixiv/user/bookmarks/:id',
             },
             {
                 title: '用户动态',
                 docs: 'https://docs.rsshub.app/social-media.html#pixiv',
-                source: '/member.php',
-                target: (params, url) => `/pixiv/user/${new URL(url).searchParams.get('id')}`,
+                source: '/users/:id',
+                target: '/pixiv/user/:id',
             },
             {
                 title: '排行榜',
                 docs: 'https://docs.rsshub.app/social-media.html#pixiv',
                 source: '/ranking.php',
+                target: (params, url) => `/pixiv/ranking/${new URL(url).searchParams.get('mode') || 'daily'}`,
             },
             {
                 title: '关键词',
                 docs: 'https://docs.rsshub.app/social-media.html#pixiv',
-                source: '/search.php',
+                source: ['/tags/:keyword', '/tags/:keyword/:type?'],
+                target: (params, url) => `/pixiv/search/:keyword/${new URL(url).searchParams.get('order')}/${new URL(url).searchParams.get('mode')}`,
             },
             {
                 title: '关注的新作品',
