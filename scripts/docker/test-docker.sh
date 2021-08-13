f3dfc7ce70f811ca3646740d16588eeee99acde1
NeverBehave <gayhub@never.pet>
2021-08-13 05:01:01 -0700

    chore(test): test running docker after build

---
+++ scripts/docker/test-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Try running the docker and get the output
+# then try getting homepage in 3 mins
+
+docker run -d -p 1200:1200 rsshub
+
+RETRY=0
+curl -m 10 localhost:1200
+while [[ $? -ne 0 ]] && [[ $RETRY -lt 60 ]]; do
+    sleep 5
+    ((RETRY++))
+    curl -m 10 localhost:1200
+done
+
+if [[ $RETRY -gt 60 ]]
+then
+    echo "Unable to run docker, aborted"
+    exit 1
+else
+    echo "Successfully acquire homepage, passing"
+    exit 0
+fi
