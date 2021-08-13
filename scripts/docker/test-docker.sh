cfe690bbb0f0136143432d30f8fccb4fab3b500d
NeverBehave <gayhub@never.pet>
2021-08-13 05:42:51 -0700

    add count times

--- scripts/docker/test-docker.sh
+++ scripts/docker/test-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MAX_RETRIES=60
 # Try running the docker and get the output
 # then try getting homepage in 3 mins
 
@@ -10,15 +11,16 @@ if [[ $? -ne 0 ]]
     exit 1
 fi
 
-RETRY=0
+RETRY=1
 curl -m 10 localhost:1200
-while [[ $? -ne 0 ]] && [[ $RETRY -lt 60 ]]; do
+while [[ $? -ne 0 ]] && [[ $RETRY -lt $MAX_RETRIES ]]; do
     sleep 5
     ((RETRY++))
+    echo "RETRY: ${RETRY}"
     curl -m 10 localhost:1200
 done
 
-if [[ $RETRY -gt 60 ]]
+if [[ $RETRY -gt $MAX_RETRIES ]]
 then
     echo "Unable to run, aborted"
     exit 1
