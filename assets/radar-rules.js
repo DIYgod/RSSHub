040975a1245739be140dd01a430426c0fac34323
legr4ndk <51554406+legr4ndk@users.noreply.github.com>
2021-12-27 10:28:45 +0800

    trun to V2

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -240,10 +240,6 @@
             },
         ],
     },
-    'computer.hdu.edu.cn': {
-        _name: '杭州电子科技大学计算机学院',
-        it: [{ title: '计算机学院 - 通知公告', docs: 'https://docs.rsshub.app/university.html#hang-zhou-dian-zi-ke-ji-da-xue', source: '/6738/list.htm', target: '/hdu/cs' }],
-    },
     'swufe.edu.cn': {
         _name: '西南财经大学',
         it: [
