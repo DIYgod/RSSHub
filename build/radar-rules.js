1509ffd6df0e03e049dff311f08ba6b47476976f
DIYgod <DIYgod@users.noreply.github.com>
2022-01-22 20:13:59 +0000

    deploy: 9a6cefac5ce33d2c97619b6d432eeef4eb771aea

--- build/radar-rules.js
+++ build/radar-rules.js
@@ -202,6 +202,127 @@
                     const id = new URL(url).searchParams.get('id');
                     return `/ajmide/${id}`;
                 } } ] },
+  "appledaily.com":{ _name:"苹果新闻网",
+    tw:[ { title:"首頁",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'home') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"焦点",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'recommend') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"最新",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'new') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"热门",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'hot') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"生活",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'life') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"娱乐",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'entertainment') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"社会",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'local') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"财经地产",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'property') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"国际",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'international') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"政治",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'politics') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"3C车城",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'gadget') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"吃喝玩乐",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'supplement') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"体育",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'sports') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"苹评理",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'forum') {
+                        return '/appledaily/:channel';
+                    }
+                } },
+      { title:"微视频",
+        docs:"https://docs.rsshub.app/traditional-media.html#ping-guo-xin-wen-wang",
+        source:[ "/realtime/:channel" ],
+        target:(params) => {
+                    if (params.channel === 'micromovie') {
+                        return '/appledaily/:channel';
+                    }
+                } } ] },
   "blog.leanstack.com":{ _name:"Ash Maurya's blog",
     ".":[ { title:"Ash Maurya's blog",
         docs:"https://docs.rsshub.app/en/blog.html#ash-maurya",
