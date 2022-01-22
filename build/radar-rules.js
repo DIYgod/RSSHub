e9d9d5247018f1ffb06c5dc35577d9da5cf3aa64
DIYgod <DIYgod@users.noreply.github.com>
2022-01-22 19:50:19 +0000

    deploy: 05f0a4b870b338e789a35d3f70dd5fe9a7631bd0

--- build/radar-rules.js
+++ build/radar-rules.js
@@ -173,6 +173,23 @@
         docs:"https://docs.rsshub.app/picture.html#cool-18",
         source:"/",
         target:"/cool18/:id?/:type?/:keyword?" } ] },
+  "ey.gov.tw":{ _name:"行政院消费者保护会",
+    cpc:[ { title:"新闻稿",
+        docs:"https://docs.rsshub.app/government.html#tai-wan-xing-zheng-yuan-xiao-fei-zhe-bao-hu-hui",
+        source:"/Page/:type",
+        target:(params) => {
+                    if (params.type === 'A3412E2A5A7B398F') {
+                        return '/cycey/xwg';
+                    }
+                } },
+      { title:"消费资讯",
+        docs:"https://docs.rsshub.app/government.html#tai-wan-xing-zheng-yuan-xiao-fei-zhe-bao-hu-hui",
+        source:"/Page/:type",
+        target:(params) => {
+                    if (params.type === 'E414CC218269CCE8') {
+                        return '/cycey/xfzx';
+                    }
+                } } ] },
   "cqwu.net":{ _name:"重庆文理学院",
     www:[ { title:"通知",
         docs:"https://docs.rsshub.app/university.html#chong-qing-wen-li-xue-yuan",
