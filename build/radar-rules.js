414d4c7a7cd19d8ebf5ca2b33b02646842748a93
DIYgod <DIYgod@users.noreply.github.com>
2022-01-22 20:06:01 +0000

    deploy: d95705b4db8b6db28ea90c27454524f48cf87c4f

--- build/radar-rules.js
+++ build/radar-rules.js
@@ -198,6 +198,21 @@
         docs:"https://docs.rsshub.app/en/blog.html#ash-maurya",
         source:[ "/" ],
         target:"/ash-maurya" } ] },
+  "atcoder.jp":{ _name:"AtCoder",
+    ".":[ { title:"Posts",
+        docs:"https://docs.rsshub.app/programming.html#atcoder-posts",
+        source:[ "/posts",
+          "/" ],
+        target:(params, url) => `/atcoder/post/${new URL(url).searchParams.get('lang') ?? 'en'}/${new URL(url).searchParams.get('keyword') ?? ''}` },
+      { title:"Contests",
+        docs:"https://docs.rsshub.app/programming.html#atcoder-contests",
+        source:[ "/contests/archive",
+          "/contests",
+          "" ],
+        target:(params, url) =>
+                    `/atcoder/content/${new URL(url).searchParams.get('lang') ?? 'en'}/${new URL(url).searchParams.get('ratedType') ?? '0'}/${new URL(url).searchParams.get('category') ?? '0'}/${
+                        new URL(url).searchParams.get('keyword') ?? ''
+                    }` } ] },
   "bandcamp.com":{ _name:"Bandcamp",
     ".":[ { title:"Upcoming Live Streams",
         docs:"https://docs.rsshub.app/multimedia.html#bandcamp-upcoming-live-streams",
