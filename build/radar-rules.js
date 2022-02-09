d8b6d06c891c75ed0520afefccadd671ba2406d4
TonyRL <TonyRL@users.noreply.github.com>
2022-02-09 13:01:20 +0000

    deploy: 8ea8033e9b33deaba35f3b3ba055157a6181ea6c

--- build/radar-rules.js
+++ build/radar-rules.js
@@ -3,6 +3,24 @@
         docs:"https://docs.rsshub.app/bbs.html#2047-fen-lei",
         source:[ "/" ],
         target:"/2047/:category?/:sort?" } ] },
+  "12306.cn":{ _name:"12306",
+    kyfw:[ { title:"售票信息",
+        docs:"https://docs.rsshub.app/travel.html#_12306-shou-shu-piao-piao-xin-shen-xi",
+        source:[ "/",
+          "/otn/leftTicket/init" ],
+        target:(params, url) => {
+                    const searchParams = new URL(url).searchParams;
+                    const from = searchParams.get('fs').split(',')[0];
+                    const to = searchParams.get('ts').split(',')[0];
+                    const date = searchParams.get('date');
+
+                    return `/12306/${date}/${from}/${to}`;
+                } } ],
+    www:[ { title:"最新动态",
+        docs:"https://docs.rsshub.app/travel.html#_12306-zui-cuo-xin-dong-tai",
+        source:[ "/",
+          "/mormhweb/1/:id/index_fl.html" ],
+        target:"/12306/zxdt/:id" } ] },
   "18comic.org":{ _name:"18comic 禁漫天堂",
     ".":[ { title:"成人 A 漫",
         docs:"https://docs.rsshub.app/anime.html#18comic-jin-man-tian-tang-cheng-ren-a-man",
