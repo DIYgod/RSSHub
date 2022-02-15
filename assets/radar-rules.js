c655bc911b774d54c33c293253727076e9d78163
zhuty <zhuty@sci99.com>
2022-02-15 08:59:14 +0800

    remove old doc

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -1252,13 +1252,4 @@
             },
         ],
     },
-    'audiobar.cn': {
-        _name: '音频应用',
-        www: [
-            {
-                title: '最新主题',
-                docs: 'https://docs.rsshub.app/bbs.html#audiobar',
-            },
-        ],
-    },
 });
