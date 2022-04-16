f2b8141af756d1b7aab81407bea02c4b011fa3e2
linkai <linkai@mochasoft.com.cn>
2022-04-16 13:18:53 +0800

    fix(route) : merge from FledgeXu

--- assets/radar-rules.js
+++ assets/radar-rules.js
@@ -480,7 +480,7 @@
                 docs: 'https://docs.rsshub.app/picture.html#google-xiang-ce',
                 source: '/share/*',
                 target: (params, url, document) => {
-                    const id = document && document.querySelector('html').innerHTML.match(/photos.app.goo.gl\/(.*?)"/)[1];
+                    const id = document && document.querySelector('html').innerHTML.match(/photos.app.goo.gl\/(.*?)'/)[1];
                     return id ? `/google/album/${id}` : '';
                 },
             },
@@ -1151,7 +1151,8 @@
     },
     'chinacef.cn': {
         _name: '首席经济学家论坛',
-        www: [
+        '.': [
+            { title: '最新文章列表', docs: 'https://docs.rsshub.app/finance.html#shou-xi-jing-ji-xue-jia-lun-tan', source: ['/'], target: '/chinacef' },
             { title: '专家文章', docs: 'https://docs.rsshub.app/finance.html#shou-xi-jing-ji-xue-jia-lun-tan-zhuan-jia', source: ['/index.php/experts/zjmain/experts_id/:experts_id'], target: '/chinacef/:experts_id' },
             { title: '金融热点', docs: 'https://docs.rsshub.app/finance.html#shou-xi-jing-ji-xue-jia-lun-tan-jin-rong-re-dian', source: ['/index.php/index/index'], target: '/chinacef/portal/hot' },
         ],
