7bcb400c6c764d7debf4c7f7c499e7fa2fdf8f41
Tony <TonyRL@users.noreply.github.com>
2023-04-27 01:16:11 +0400

    style(eslint): add eslint-plugin-n (#12393)

--- test/utils/rand-user-agent.js
+++ test/utils/rand-user-agent.js
@@ -9,7 +9,7 @@ describe('rand-user-agent', () => {
         const uaArr = Array(100)
             .fill()
             .map(() => randUserAgent({ browser: 'chrome', os: 'windows' }));
-        const match = uaArr.find((e) => (e.includes('Chrome-Lighthouse') || e.includes('HeadlessChrome') ? true : false));
+        const match = uaArr.find((e) => !!(e.includes('Chrome-Lighthouse') || e.includes('HeadlessChrome')));
         expect(match).toBeFalsy();
     });
 
