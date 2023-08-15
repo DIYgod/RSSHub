341bf3972af0eb4d74bb551c72cd0df33232ed12
TonyRL <TonyRL@users.noreply.github.com>
2023-08-15 22:05:28 +0000

    Revert "fix: remove unused variable"
    This reverts commit 52bec57a1ec8c54166793b31cf972131555dc6bc.
    What is React

--- website/src/theme/SearchBar/index.jsx
+++ website/src/theme/SearchBar/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import 'meilisearch-docsearch/css';
