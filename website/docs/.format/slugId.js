33cbd2e9f75d239ac5367a82be750f02438ebea7
Tony <TonyRL@users.noreply.github.com>
2023-09-10 21:45:55 +0000

    docs: slugify h4

--- website/docs/.format/slugId.js
+++ website/docs/.format/slugId.js
@@ -1,24 +1,29 @@
-const file = require('./file');
 const { pinyin } = require('pinyin-pro');
 
-const slugify = (s) =>
-    encodeURIComponent(
+const slugify = (s) => {
+    s = s?.replace(/[#&'()+,/:[\]|’“”、「」・（）．：｜]/g, '-');
+    return encodeURIComponent(
         pinyin(s, {
             nonZh: 'consecutive',
             toneType: 'none',
             type: 'array',
+            v: true,
         })
             .join(' ')
             .trim()
             .toLowerCase()
             .replace(/\s+/g, '-')
+            .replace(/-+/g, '-')
+            .replace(/(^-|-$)/g, '')
     );
+};
 
 module.exports = {
-    rules: (list) => list.filter((e) => e.type === file.ROUTE_TYPE && e.lang === file.LANG_CN),
+    rules: (list) => list.filter((e) => e),
     handler: async (data) => {
         const content = data.split('\n');
         let lastH2 = '';
+        let lastH3 = '';
 
         for (let i = 0; i < content.length; i++) {
             if (content[i].startsWith('## ')) {
@@ -26,9 +31,13 @@ module.exports = {
 
                 content[i] = `## ${lastH2} {#${slugify(lastH2)}}`;
             } else if (content[i].startsWith('### ')) {
-                const title = content[i].match(`### (([^{])*)`)?.[1].trim();
+                lastH3 = content[i].match(`### (([^{])*)`)?.[1].trim();
 
-                content[i] = `### ${title} {#${slugify(lastH2)}-${slugify(title)}}`;
+                content[i] = `### ${lastH3} {#${slugify(lastH2)}-${slugify(lastH3)}}`;
+            } else if (content[i].startsWith('#### ')) {
+                const title = content[i].match(`#### (([^{])*)`)?.[1].trim();
+
+                content[i] = `#### ${title} {#${slugify(lastH2)}-${slugify(lastH3)}-${slugify(title)}}`;
             }
         }
 
