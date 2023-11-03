e6c4c6b3bca6092ad2bc4998eba70ce8d093753a
TonyRL <TonyRL@users.noreply.github.com>
2023-11-03 14:45:57 +0000

    docs: fix search build

--- website/src/theme/SearchBar/index.jsx
+++ website/src/theme/SearchBar/index.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { docsearch } from 'meilisearch-docsearch';
 
 import 'meilisearch-docsearch/css';
-import './variables.css';
+import './style.css';
 
 export default function SearchBarWrapper() {
   const { siteConfig } = useDocusaurusContext();
   useEffect(() => {
+    const { docsearch } = require('meilisearch-docsearch');
     const docSearchOptions = siteConfig.customFields['meilisearch-docsearch'];
-    docsearch(docSearchOptions)
+
+    return () => docsearch(docSearchOptions)
   }, [])
 
   return (
