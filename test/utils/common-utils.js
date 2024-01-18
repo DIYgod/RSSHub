95081cd0046a4818ffa9f94cd9f8a195011f43b1
Tony <TonyRL@users.noreply.github.com>
2024-01-18 13:47:26 +0800

    style: fix no-useless-undefined again

--- test/utils/common-utils.js
+++ test/utils/common-utils.js
@@ -8,7 +8,7 @@ describe('common-utils', () => {
     it('convertDateToISO8601', () => {
         expect(utils.convertDateToISO8601('')).toBe('');
         expect(utils.convertDateToISO8601(null)).toBe(null);
-        expect(utils.convertDateToISO8601()).toBe(undefined);
+        expect(utils.convertDateToISO8601(undefined)).toBe(undefined);
 
         const date = new Date('2019-01-01');
         const expected = date.toISOString();
@@ -28,7 +28,7 @@ describe('common-utils', () => {
     it('collapseWhitespace', () => {
         expect(utils.collapseWhitespace('')).toBe('');
         expect(utils.collapseWhitespace(null)).toBe(null);
-        expect(utils.collapseWhitespace()).toBe(undefined);
+        expect(utils.collapseWhitespace(undefined)).toBe(undefined);
         expect(utils.collapseWhitespace('   \n\n\n    ')).toBe('');
         expect(utils.collapseWhitespace('a string already collapsed')).toBe('a string already collapsed');
         expect(utils.collapseWhitespace(' \n  a lot of     whitespaces   and \n\n\n\n linebreaks   \n\n ')).toBe('a lot of whitespaces and linebreaks');
