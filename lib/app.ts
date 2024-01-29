00a74bcb097207231a5d877dbcf15988c6f00831
DIYgod <i@diygod.me>
2024-01-29 22:39:24 +0800

    feat: app.ts

---
+++ lib/app.ts
@@ -0,0 +1,52 @@
+import '@/utils/request-wrapper'
+
+import { Handler, Hono } from 'hono'
+
+import cache from '@/middleware/cache'
+import template from '@/middleware/template'
+import onerror from '@/middleware/onerror'
+import accessControl from '@/middleware/access-control'
+import debug from '@/middleware/debug'
+import header from '@/middleware/header'
+import antiHotlink from '@/middleware/anti-hotlink'
+import parameter from '@/middleware/parameter'
+import logger from '@/utils/logger'
+
+import routes from '@/routes'
+import index from '@/v3/index'
+import { errorHandler } from '@/errors'
+
+process.on('uncaughtException', (e) => {
+    logger.error('uncaughtException: ' + e);
+});
+
+const app = new Hono()
+
+app.use('*', onerror);
+app.use('*', accessControl);
+app.use('*', debug);
+app.use('*', template);
+app.use('*', header);
+app.use('*', antiHotlink);
+app.use('*', parameter);
+app.use('*', cache);
+
+for (const name in routes) {
+  const subApp = app.basePath(`/${name}`)
+  routes[name]({
+    get: (path, handler) => {
+      const wrapedHandler: Handler = async (ctx, ...args) => {
+        if (!ctx.get('data')) {
+          await handler(ctx, ...args)
+        }
+      }
+      subApp.get(path, wrapedHandler)
+    },
+  })
+}
+
+app.get('/', index)
+
+app.onError(errorHandler)
+
+export default app
--- lib/index.ts
+++ lib/index.ts
@@ -1,55 +1,7 @@
-import '@/utils/request-wrapper'
-
 import { serve } from '@hono/node-server'
-import { Handler, Hono } from 'hono'
-
-import cache from '@/middleware/cache'
-import template from '@/middleware/template'
-import onerror from '@/middleware/onerror'
-import accessControl from '@/middleware/access-control'
-import debug from '@/middleware/debug'
-import header from '@/middleware/header'
-import antiHotlink from '@/middleware/anti-hotlink'
-import parameter from '@/middleware/parameter'
 import logger from '@/utils/logger'
-
-import routes from '@/routes'
-import index from '@/v3/index'
 import { config } from '@/config'
-import { errorHandler } from '@/errors'
-
-process.on('uncaughtException', (e) => {
-    logger.error('uncaughtException: ' + e);
-});
-
-const app = new Hono()
-
-app.use('*', onerror);
-app.use('*', accessControl);
-app.use('*', debug);
-app.use('*', template);
-app.use('*', header);
-app.use('*', antiHotlink);
-app.use('*', parameter);
-app.use('*', cache);
-
-for (const name in routes) {
-  const subApp = app.basePath(`/${name}`)
-  routes[name]({
-    get: (path, handler) => {
-      const wrapedHandler: Handler = async (ctx, ...args) => {
-        if (!ctx.get('data')) {
-          await handler(ctx, ...args)
-        }
-      }
-      subApp.get(path, wrapedHandler)
-    },
-  })
-}
-
-app.get('/', index)
-
-app.onError(errorHandler)
+import app from '@/app'
 
 const port = config.connect.port
 
@@ -60,3 +12,5 @@ serve({
   fetch: app.fetch,
   port
 })
+
+export default app
