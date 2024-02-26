7b1238f968d8362ff60e43ab6906b215376668d6
DIYgod <i@diygod.me>
2024-02-26 19:46:59 +0800

    test: filter-engine

--- lib/middleware/filter-engine.test.ts
+++ lib/middleware/filter-engine.test.ts
@@ -1,5 +1,6 @@
-const supertest = require('supertest');
-jest.mock('request-promise-native');
+import { describe, expect, it, afterAll, jest, afterEach } from '@jest/globals';
+import supertest from 'supertest';
+
 jest.setTimeout(50000);
 
 afterAll(() => {
@@ -13,35 +14,29 @@ afterEach(() => {
 
 describe('filter-engine', () => {
     it(`filter RE2 engine ReDoS attack`, async () => {
-        const server = require('../../lib/index');
-        const request = supertest(server);
+        const request = supertest((await import('@/index')).default);
 
         const response = await request.get('/test/1?filter=abc(%3F%3Ddef)');
         expect(response.status).toBe(404);
         expect(response.text).toMatch(/RE2JSSyntaxException/);
-        server.close();
     });
 
     it(`filter Regexp engine backward compatibility`, async () => {
         process.env.FILTER_REGEX_ENGINE = 'regexp';
 
-        const server = require('../../lib/index');
-        const request = supertest(server);
+        const request = supertest((await import('@/index')).default);
 
         const response = await request.get('/test/1?filter=abc(%3F%3Ddef)');
         expect(response.status).toBe(200);
-        server.close();
     });
 
     it(`filter Regexp engine test config`, async () => {
         process.env.FILTER_REGEX_ENGINE = 'somethingelse';
 
-        const server = require('../../lib/index');
-        const request = supertest(server);
+        const request = supertest((await import('@/index')).default);
 
         const response = await request.get('/test/1?filter=abc(%3F%3Ddef)');
         expect(response.status).toBe(404);
         expect(response.text).toMatch(/somethingelse/);
-        server.close();
     });
 });
