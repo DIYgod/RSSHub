5f7b20f137f2f3547f49503f66e943ce9cf9e3d7
Karasu <karasu.shing@gmail.com>
2024-03-04 23:01:35 +0800

    fix(route): 兼容外链及视频页面特殊dom结构

--- lib/routes/cast/index.ts
+++ lib/routes/cast/index.ts
@@ -10,6 +10,39 @@ interface ResponseData<T> extends Response {
     data: T;
 }
 
+async function parsePage(html: string) {
+    return await Promise.all(
+        load(html)('li')
+            .toArray()
+            .map((el) => {
+                const title = load(el)('a');
+                let articleUrl = title.attr('href');
+
+                if (articleUrl?.startsWith('http')) {
+                    return {
+                        title: title.text(),
+                        link: title.attr('href'),
+                        description: '',
+                    };
+                }
+                articleUrl = `${baseUrl}${title.attr('href')}`;
+
+                return cache.tryGet(articleUrl, async () => {
+                    const res = (await got.get(articleUrl!)) as ResponseData<string>;
+                    const article = load(res.data);
+                    const pubDate = timezone(parseDate(article('meta[name=PubDate]').attr('content')!, 'YYYY-MM-DD HH:mm'), +8);
+
+                    return {
+                        title: title.text(),
+                        pubDate,
+                        description: article('#zoom').html(),
+                        link: `${baseUrl}${title.attr('href')}`,
+                    };
+                });
+            })
+    );
+}
+
 export default async (ctx) => {
     const { column, subColumn, category } = ctx.req.param();
     const { limit = 20 } = ctx.req.query();
@@ -21,43 +54,30 @@ export default async (ctx) => {
 
     const $ = load(indexData);
 
-    const buildUnitScript = $('script').filter((_, el) => $(el).attr('parsetype') === 'bulidstatic');
-    const queryData = JSON.parse(buildUnitScript.attr('querydata')?.replace(/'/g, '"') ?? '{}');
-    queryData.paramJson = `{"pageNo":1,"pageSize":${limit}}`;
+    let items: any[] = [];
+
+    // 新闻-视频首页特殊处理
+    if (column === 'xw' && subColumn === 'SP' && !category) {
+        items = await parsePage(indexData);
+    } else {
+        const buildUnitScript = $('script').filter((_, el) => $(el).attr('parsetype') === 'bulidstatic');
+        const queryUrl = `${baseUrl}/${buildUnitScript.attr('url')}`;
+        const queryData = JSON.parse(buildUnitScript.attr('querydata')?.replace(/'/g, '"') ?? '{}');
+        queryData.paramJson = `{"pageNo":1,"pageSize":${limit}}`;
+
+        const { data } = (await got.get(queryUrl, {
+            searchParams: new URLSearchParams(queryData),
+        })) as ResponseData<{ data: { html: string } }>;
+
+        items = await parsePage(data.data.html);
+    }
+
     const pageTitle = $('head title').text();
-
-    const queryUrl = `${baseUrl}/${buildUnitScript.attr('url')}`;
-
-    const { data } = (await got.get(queryUrl, {
-        searchParams: new URLSearchParams(queryData),
-    })) as ResponseData<{ data: { html: string } }>;
 
     ctx.set('data', {
         title: pageTitle,
         link,
         image: 'https://www.cast.org.cn/favicon.ico',
-        item: await Promise.all(
-            $(data.data.html)
-                .find('li')
-                .toArray()
-                .map((el) => {
-                    const item = $(el);
-                    const title = item.find('a');
-                    const articleUrl = `${baseUrl}${title.attr('href')}`;
-
-                    return cache.tryGet(articleUrl, async () => {
-                        const res = (await got.get(articleUrl)) as ResponseData<string>;
-                        const article = load(res.data);
-                        const pubDate = timezone(parseDate(article('meta[name=PubDate]').attr('content')!, 'YYYY-MM-DD HH:mm'), +8);
-
-                        return {
-                            title: title.text(),
-                            pubDate,
-                            description: article('#zoom').html(),
-                            link: `${baseUrl}${title.attr('href')}`,
-                        };
-                    });
-                })
-        ),
+        item: items,
     });
 };
