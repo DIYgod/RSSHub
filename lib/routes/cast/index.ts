20952b87f96f6b5925cb0753ab3669dd0b44d7e5
Karasu <karasu.shing@gmail.com>
2024-03-04 17:10:12 +0800

    fix(route): cast

--- lib/routes/cast/index.ts
+++ lib/routes/cast/index.ts
@@ -1,76 +1,63 @@
-// @ts-nocheck
 import cache from '@/utils/cache';
 import got from '@/utils/got';
 import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
 import timezone from '@/utils/timezone';
+import { Response } from 'got';
 const baseUrl = 'https://www.cast.org.cn';
 
+interface ResponseData<T> extends Response {
+    data: T;
+}
+
 export default async (ctx) => {
-    const { column = 457 } = ctx.req.param();
-    const { limit = 10 } = ctx.req.query();
-    const link = `${baseUrl}/col/col${column}/index.html`;
-    const { data: response } = await got.post(`${baseUrl}/module/web/jpage/dataproxy.jsp`, {
-        searchParams: {
-            startrecord: 1,
-            endrecord: limit,
-            perpage: limit,
-        },
-        form: {
-            col: 1,
-            appid: 1,
-            webid: 1,
-            path: '/',
-            columnid: column,
-            sourceContentType: 1,
-            unitid: 335,
-            webname: '中国科学技术协会',
-            permissiontype: 0,
-        },
-    });
+    const { column, subColumn, category } = ctx.req.param();
+    const { limit = 20 } = ctx.req.query();
+    let link = `${baseUrl}/${column}/${subColumn}`;
+    if (category) {
+        link += `/${category}/index.html`;
+    }
+    const { data: indexData } = (await got.get(link)) as ResponseData<string>;
 
-    const $ = load(response, {
-        xml: {
-            xmlMode: true,
-        },
-    });
+    const $ = load(indexData);
 
-    const pageTitle = await cache.tryGet(link, async () => {
-        const { data: response } = await got(link);
-        const $ = load(response);
-        return $('head title').text();
-    });
+    const buildUnitScript = $('script').filter((_, el) => $(el).attr('parsetype') === 'bulidstatic');
+    const queryData = JSON.parse(buildUnitScript.attr('querydata')?.replace(/'/g, '"') ?? '{}');
+    queryData.paramJson = `{"pageNo":1,"pageSize":${limit}}`;
+    const pageTitle = $('head title').text();
 
-    const list = $('record')
-        .toArray()
-        .map((item) => {
-            item = load($(item).html(), null, false);
-            const a = item('a').first();
-            return {
-                title: a.text(),
-                pubDate: parseDate(item('.list-data').text().trim(), 'DDYYYY/MM'),
-                link: `${baseUrl}${a.attr('href')}`,
-            };
-        });
+    const queryUrl = `${baseUrl}/${buildUnitScript.attr('url')}`;
 
-    const items = await Promise.all(
-        list.map((item) =>
-            cache.tryGet(item.link, async () => {
-                const { data: response } = await got(item.link);
-                const $ = load(response);
-
-                item.description = $('#zoom').html();
-                item.pubDate = timezone(parseDate($('meta[name=PubDate]').attr('content'), 'YYYY-MM-DD HH:mm'), +8);
-
-                return item;
-            })
-        )
-    );
+    const { data } = (await got.get(queryUrl, {
+        searchParams: new URLSearchParams(queryData),
+    })) as ResponseData<{ data: { html: string } }>;
 
     ctx.set('data', {
         title: pageTitle,
         link,
         image: 'https://www.cast.org.cn/favicon.ico',
-        item: items,
+        item: await Promise.all(
+            $(data.data.html)
+                .find('li')
+                .toArray()
+                .map((el) => {
+                    const item = $(el);
+                    const title = item.find('a');
+                    const articleUrl = `${baseUrl}${title.attr('href')}`;
+
+                    return cache.tryGet(articleUrl, async () => {
+                        const res = (await got.get(articleUrl)) as ResponseData<string>;
+                        const article = load(res.data);
+                        const pubDate = timezone(parseDate(article('meta[name=PubDate]').attr('content')!, 'YYYY-MM-DD HH:mm'), +8);
+
+                        return {
+                            title: title.text(),
+                            pubDate,
+                            description: article('#zoom').html(),
+                            link: `${baseUrl}${title.attr('href')}`,
+                        };
+                    });
+                })
+        ),
     });
 };
--- lib/routes/cast/radar.ts
+++ lib/routes/cast/radar.ts
@@ -5,8 +5,8 @@ export default {
             {
                 title: '通用',
                 docs: 'https://docs.rsshub.app/routes/government#zhong-guo-ke-xue-ji-shu-xie-hui',
-                source: ['/col/:column/index.html'],
-                target: (params) => `/cast/${params.column.replace('col', '')}`,
+                source: ['/:column/:subColumn/:category/index.html', '/:column/:subColumn/index.html'],
+                target: (params) => (params.category ? `/cast/${params.column}/${params.subColumn}/${params.category}` : `/cast/${params.column}/${params.subColumn}`),
             },
         ],
     },
