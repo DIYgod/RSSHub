a42947231104a9ec3436fc52cedb31740c9a7069
Yufan You <ouuansteve@gmail.com>
2024-03-05 17:27:51 +0800

    Merge pull request from GHSA-3p3p-cgj7-vgw3
    * fix: SSRF in /m4/:id?/:category*
    
    * fix: allow /mastodon/acct/ to any domain when MASTODON_API_HOST is set
    
    * fix: add missing import for isValidHost

--- lib/routes/mastodon/acct.ts
+++ lib/routes/mastodon/acct.ts
@@ -1,14 +1,8 @@
 const utils = require('./utils');
-import { config } from '@/config';
 
 export default async (ctx) => {
     const acct = ctx.req.param('acct');
     const only_media = ctx.req.param('only_media') ? 'true' : 'false';
-    const acctSite = acct.split('@').filter(Boolean)[1];
-
-    if (!config.feature.allow_user_supply_unsafe_domain && !utils.allowSiteList.includes(acctSite)) {
-        throw new Error(`This RSS is disabled unless 'ALLOW_USER_SUPPLY_UNSAFE_DOMAIN' is set to 'true'.`);
-    }
 
     const { site, account_id } = await utils.getAccountIdByAcct(acct);
 
