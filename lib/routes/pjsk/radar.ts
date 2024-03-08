efe4e2b8ab4d4b52699faf27a8416ff6d5637707
randompasser <33059426+randompasser@users.noreply.github.com>
2024-03-08 20:44:26 +0800

    feat(route): add radar.ts for pjsk

---
+++ lib/routes/pjsk/radar.ts
@@ -0,0 +1,13 @@
+export default {
+    'sega.jp': {
+        _name: 'Project Sekai / プロセカ',
+        pjsekai: [
+            {
+                title: 'Game News',
+                docs: 'https://docs.rsshub.app/routes/game#shi-jie-ji-hua-duo-cai-wu-tai-projectsekai-%E3%83%97%E3%83%AD%E3%82%BB%E3%82%AB-news',
+                source: ['/news/index.html'],
+                target: '/pjsk/news',
+            },
+        ],
+    },
+};
