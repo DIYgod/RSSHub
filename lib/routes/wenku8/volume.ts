5b573365b24c985feb63d7ccccebfa2fc0b39edf
Tony <TonyRL@users.noreply.github.com>
2024-03-08 21:31:00 +0800

    fix: add chapter link

--- lib/routes/wenku8/volume.ts
+++ lib/routes/wenku8/volume.ts
@@ -9,15 +9,20 @@ export default async (ctx) => {
     const $ = load(await get(link));
     const vid = $('.vcss').last().parent().next().find('a')[0].attribs.href.replace('.htm', '');
     const volumeUrl = `https://dl.wenku8.com/packtxt.php?aid=${aid}&vid=${vid}&charset=gbk`;
+    const lastestChapters = $('.vcss')
+        .last()
+        .parent()
+        .nextAll()
+        .find('a')
+        .toArray()
+        .map((a) => ({ link: a.attribs.href }));
 
     ctx.set('data', {
         title: `轻小说文库 ${$('#title').text()} 最新卷`,
         link,
         item: await cache.tryGet(volumeUrl, async () =>
             [...(await get(volumeUrl)).matchAll(/\s{2}(\S.*)\r?\n([\S\s]+?)\r?\n\r?\n/g)]
-                .filter((chapter) => chapter[2].trim())
-                .reverse()
-                .map((chapter) => ({
+                .map((chapter, index) => ({
                     title: chapter[1],
                     description: chapter[2]
                         .split('\r\n')
@@ -25,7 +30,10 @@ export default async (ctx) => {
                         .map((line) => `<p>${line.trim()}</p>`)
                         .join(''),
                     guid: Buffer.from(`${vid}${chapter[1]}`).toString('base64'),
+                    link: lastestChapters[index]?.link,
                 }))
+                .filter((chapter) => chapter.description)
+                .toReversed()
         ),
     });
 };
