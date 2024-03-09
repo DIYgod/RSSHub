eb98be90925353aac96f1e37d4d740c71d4a7984
怎么学都学不会的何同学 <1467602180@qq.com>
2024-03-09 00:58:52 +0000

    feat: 印记中文周刊增加完整文章抓取

--- lib/routes/docschina/weekly.ts
+++ lib/routes/docschina/weekly.ts
@@ -1,5 +1,6 @@
 import got from '@/utils/got';
 import { load } from 'cheerio';
+import cache from '@/utils/cache';
 
 export default async (ctx) => {
     const { category = 'js' } = ctx.req.param();
@@ -7,7 +8,6 @@ export default async (ctx) => {
     const baseURL = 'https://docschina.org';
     const path = `/news/weekly/${category}`;
     const { data: res } = await got(`${baseURL}${path}`);
-
     // @ts-ignore
     const $ = load(res);
 
@@ -15,15 +15,28 @@ export default async (ctx) => {
     const dataEl = $('#__NEXT_DATA__');
     const dataText = dataEl.text();
     const data = JSON.parse(dataText);
+
+    const items = await Promise.all(
+        data?.props?.pageProps?.data?.map((item) => {
+            const link = `${baseURL}${path}/${item.issue}`;
+            return cache.tryGet(link, async () => {
+                const { data: response } = await got(link);
+                // @ts-ignore
+                const $ = load(response);
+                return {
+                    title: item.title,
+                    description: $('#__next > main > div > div').first().html(),
+                    link: `${baseURL}${path}/${item.issue}`,
+                    author: item.editors?.join(','),
+                    itunes_item_image: item.imageUrl,
+                };
+            });
+        })
+    );
+
     ctx.set('data', {
         title,
         link: baseURL + path,
-        item: data?.props?.pageProps?.data?.map((item) => ({
-            title: item.title,
-            description: item.description,
-            link: `${baseURL}${path}/${item.issue}`,
-            author: item.editors?.join(','),
-            itunes_item_image: item.imageUrl,
-        })),
+        item: items,
     });
 };
