a9ae562d388b1431f271fd966702a86b64bfcd05
DIYgod <i@diygod.me>
2024-03-09 16:52:18 +0800

    feat: build markdown docs from routes

--- scripts/workflow/build-routes.ts
+++ scripts/workflow/build-routes.ts
@@ -17,6 +17,7 @@ const radar: {
             | string;
     };
 } = {};
+const docs = {};
 
 for (const namespace in namespaces) {
     for (const path in namespaces[namespace].routes) {
@@ -49,9 +50,79 @@ for (const namespace in namespaces) {
                 });
             }
         }
+        const categories = data.categories || namespaces[namespace].categories || ['other'];
+        for (const category of categories) {
+            if (!docs[category]) {
+                docs[category] = {};
+            }
+            if (!docs[category][namespace]) {
+                docs[category][namespace] = {
+                    routes: {},
+                };
+            }
+            docs[category][namespace].name = namespaces[namespace].name;
+            docs[category][namespace].description = namespaces[namespace].description;
+            docs[category][namespace].routes[realPath] = data;
+        }
     }
 }
 
 fs.writeFileSync(path.join(__dirname, '../../assets/build/radar-rules.json'), JSON.stringify(radar, null, 2));
 fs.writeFileSync(path.join(__dirname, '../../assets/build/maintainers.json'), JSON.stringify(maintainers, null, 2));
 fs.writeFileSync(path.join(__dirname, '../../assets/build/routes.json'), JSON.stringify(namespaces, null, 2));
+
+const pinyinCompare = new Intl.Collator('zh-Hans-CN-u-co-pinyin').compare;
+const isASCII = (str) => /^[\u0000-\u007F]*$/.test(str);
+
+const md = {};
+for (const category in docs) {
+    md[category] = `# ${category}\n\n`;
+
+    const namespaces = Object.keys(docs[category]).sort((a, b) => {
+        const aname = docs[category][a].name;
+        const bname = docs[category][b].name;
+        const ia = isASCII(aname);
+        const ib = isASCII(bname);
+        if (ia && ib) {
+            return aname.toLowerCase() < bname.toLowerCase() ? -1 : 1;
+        } else if (ia || ib) {
+            return ia > ib ? -1 : 1;
+        } else {
+            return pinyinCompare(aname, bname);
+        }
+    });
+    for (const namespace of namespaces) {
+        md[category] += `## ${docs[category][namespace].name}\n\n`;
+        if (docs[category][namespace].description) {
+            md[category] += `${docs[category][namespace].description}\n\n`;
+        }
+
+        const realPaths = Object.keys(docs[category][namespace].routes).sort((a, b) => {
+            const aname = docs[category][namespace].routes[a].name;
+            const bname = docs[category][namespace].routes[b].name;
+            const ia = isASCII(aname);
+            const ib = isASCII(bname);
+            if (ia && ib) {
+                return aname.toLowerCase() < bname.toLowerCase() ? -1 : 1;
+            } else if (ia || ib) {
+                return ia > ib ? -1 : 1;
+            } else {
+                return pinyinCompare(aname, bname);
+            }
+        });
+
+        for (const realPath of realPaths) {
+            const data = docs[category][namespace].routes[realPath];
+            md[category] += `### ${data.name}\n\n`;
+            md[category] += `<Route data={${JSON.stringify(data)}} />\n\n`;
+            if (data.description) {
+                md[category] += `${data.description}\n\n`;
+            }
+        }
+    }
+}
+
+fs.writeFileSync(path.join(__dirname, '../../assets/build/docs.json'), JSON.stringify(docs, null, 2));
+for (const category in md) {
+    fs.writeFileSync(path.join(__dirname, `../../website/docs/routes/${category}.mdx`), md[category]);
+}
