fdb77bfa3f76ea90432f853debaa97088bdad3fb
zcw159357 <zcw159357@gmail.com>
2024-03-16 21:22:03 +0800

    迁移小米相关路由

--- lib/routes/miui/index.ts
+++ lib/routes/miui/index.ts
@@ -1,6 +1,5 @@
 import { Route } from '@/types';
 import got from '@/utils/got';
-import cache from '@/utils/cache';
 import queryString from 'query-string';
 
 export const route: Route = {
@@ -8,17 +7,16 @@ export const route: Route = {
     categories: ['program-update'],
     example: '/miui/aries',
     parameters: { device: 'the device `codename` eg. `aries` for Mi 2S', type: 'type', region: 'Region, default to `cn`' },
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
     name: 'New firmware',
     maintainers: ['Indexyz'],
-    description: "  | stable  | development |\n  | ------- | ----------- |\n  | release | dev         |\n\n  | region | region |\n  | ------ | ------ |\n  | China  | cn     |\n  | Global | global |",
+    description: `  | stable  | development |
+    | ------- | ----------- |
+    | release | dev         |
+  
+    | region | region |
+    | ------ | ------ |
+    | China  | cn     |
+    | Global | global |`,
     handler,
 };
 
@@ -29,7 +27,6 @@ async function handler(ctx) {
     const releaseType = type === 'release' ? 'F' : 'X';
     const localeTypeName = type === 'release' ? '稳定版' : '开发版';
     const regionName = region === 'global' ? 'global' : 'cn';
-    const cacheName = `RSSHubMIUIUpdate|${device}|${releaseType}|${regionName}`;
 
     const response = await got({
         method: 'get',
@@ -44,36 +41,15 @@ async function handler(ctx) {
     });
 
     const responseData = response.data;
-    let oldPosts = [];
-    try {
-        oldPosts = JSON.parse(await cache.get(cacheName));
-    } catch {
-        // no need handle here: parseError
-    }
-
-    if (oldPosts === null) {
-        oldPosts = [];
-    }
-
-    let item = oldPosts;
-
-    if (oldPosts.length === 0 || oldPosts[0].description !== responseData.LatestFullRom.filename) {
-        item = [
-            {
-                title: `${device} 有新的 ${localeTypeName}本: ${responseData.LatestFullRom.version}`,
-                guid: responseData.LatestFullRom.md5,
-                description: responseData.LatestFullRom.filename,
-                link: responseData.LatestFullRom.descriptionUrl,
-            },
-            ...oldPosts,
-        ];
-
-        cache.set(cacheName, item.slice(0, cacheLength));
-    }
 
     return {
         title: `MIUI 更新 - ${device} - ${type === 'release' ? '稳定版' : '开发版'}`,
         link: 'http://www.miui.com/download.html',
-        item,
+        item: [{
+            title: `${device} 有新的 ${localeTypeName}本: ${responseData.LatestFullRom.version}`,
+            guid: responseData.LatestFullRom.md5,
+            description: responseData.LatestFullRom.filename,
+            link: responseData.LatestFullRom.descriptionUrl,
+        }],
     };
 };
