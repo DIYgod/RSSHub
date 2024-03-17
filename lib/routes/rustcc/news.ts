13f23ffce03881ea37fc45f8da339bf0a270af63
zhenlohuang <zhenlohuang@gmail.com>
2024-03-17 20:08:05 +0800

    update code per comments

--- lib/routes/rustcc/news.ts
+++ lib/routes/rustcc/news.ts
@@ -4,24 +4,15 @@ import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
 import timezone from '@/utils/timezone';
 
-const base_url = 'https://rustcc.cn';
+const baseUrl = 'https://rustcc.cn';
 
 export const route: Route = {
     path: '/news',
     categories: ['programming'],
     example: '/rustcc/news',
-    parameters: {},
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
-    radar: {
+    radar: [{
         source: ['rustcc.cn/'],
-    },
+    }],
     name: '新闻/聚合',
     maintainers: ['zhenlohuang'],
     handler,
@@ -29,12 +20,12 @@ export const route: Route = {
 };
 
 async function handler() {
-    const news_url = 'https://rustcc.cn/section?id=f4703117-7e6b-4caf-aa22-a3ad3db6898f';
+    const newsUrl = 'https://rustcc.cn/section?id=f4703117-7e6b-4caf-aa22-a3ad3db6898f';
 
     const response = await got({
-        url: news_url,
+        url: newsUrl,
         headers: {
-            Referer: base_url,
+            Referer: baseUrl,
         },
     });
 
@@ -43,9 +34,9 @@ async function handler() {
 
     return {
         title: 'Rust语言中文社区 | 新闻/聚合',
-        link: news_url,
+        link: newsUrl,
         description: `获取Rust语言中文社区的新闻/聚合`,
-        item: await Promise.all(list.map((item) => getFeedItem(item))),
+        item: list.map((item) => getFeedItem(item)),
     };
 }
 
@@ -55,7 +46,7 @@ function getFeedItem(item) {
 
     return {
         title: title.text(),
-        link: `${base_url}${title.attr('href')}`,
+        link: `${baseUrl}${title.attr('href')}`,
         description: $('.info .tags').text(),
         pubDate: timezone(parseDate($('.info .timestamp').text(), 'YYYY-MM-DD hh:mm'), +8),
     };
