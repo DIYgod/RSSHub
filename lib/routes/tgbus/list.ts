5e9ed44c56ac956a597350be77037415c198af1a
Xzonn <Xzonn@outlook.com>
2024-03-18 10:52:25 +0800

    feat(route): Update TGBUS (电玩巴士)

--- lib/routes/tgbus/list.ts
+++ lib/routes/tgbus/list.ts
@@ -6,32 +6,39 @@ import { parseDate } from '@/utils/parse-date';
 import timezone from '@/utils/timezone';
 import { parseArticle } from './utils';
 
-export const route: Route = {
-    path: '/news/',
-    categories: ['game'],
-    example: '/tgbus/news/',
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
-    radar: [
-        {
-            source: ['www.tgbus.com/list/news/'],
-            target: '/news/',
-        },
-    ],
-    name: '首页要闻',
-    maintainers: ['Xzonn'],
-    handler,
+type Category = 'news' | 'review' | 'video' | 'special' | 'hardware';
+
+const categories: Record<Category, string> = {
+    news: '最新资讯',
+    review: '游戏评测',
+    video: '游戏视频',
+    special: '巴士首页特稿',
+    hardware: '硬件资讯',
 };
 
-async function handler() {
-    const url = 'https://www.tgbus.com/list/news/';
-    const res = await got(url);
+export const route: Route = {
+    path: '/list/:category',
+    categories: ['game'],
+    example: '/tgbus/list/news',
+    radar: [
+        {
+            source: ['www.tgbus.com/list/:category/'],
+            target: '/list/:category',
+        },
+    ],
+    name: '文章列表',
+    maintainers: ['Xzonn'],
+    handler,
+    description: `| 最新资讯 | 游戏评测 | 游戏视频 | 巴士首页特稿 | 硬件资讯 |
+    | -------- | -------- | -------- | ------------ | -------- |
+    | news     | review   | video    | special      | hardware |`,
+};
+
+async function handler(ctx) {
+    const category = ctx.req.param('category') as Category;
+    const listUrl = `https://www.tgbus.com/list/${category}/`;
+
+    const res = await got(listUrl);
     const $ = load(res.data as unknown as string);
     const list = $('div.special-infocard')
         .toArray()
@@ -41,7 +48,7 @@ async function handler() {
             return {
                 title: element.find('div.title').text(),
                 link: `https://www.tgbus.com${a.attr('href')}`,
-                description: element.find('div.title').text(),
+                description: element.find('div.content').text(),
                 pubDate: timezone(parseDate(element.find('div.info span:nth-child(3)').text().trim()), 8),
             };
         });
@@ -49,9 +56,9 @@ async function handler() {
     const out = await Promise.all(list.map((item) => parseArticle(item, cache.tryGet)));
 
     return {
-        title: '电玩巴士首页要闻',
+        title: `${categories[category]} - 电玩巴士`,
         description: $('meta[name="description"]').attr('content'),
-        link: url,
+        link: listUrl,
         item: out,
     };
 }
