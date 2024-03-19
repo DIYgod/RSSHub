2c6cdb857d9faf64d12056644e5ae4411bca9963
HaodongZou <zouhaodong@outlook.com>
2024-03-20 01:21:19 +0800

    fix(route): 删除无用代码

--- lib/routes/gov/lf/jyj/namespace.ts
+++ lib/routes/gov/lf/jyj/namespace.ts
@@ -2,10 +2,9 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: '廊坊市教育局',
-    url: 'jyj.lf.gov.cn/?cat=57',
+    url: 'jyj.lf.gov.cn',
     description: `廊坊市教育局公示公告`,
 
-    
     zh: {
         name: '廊坊市教育局',
     },
--- lib/routes/gov/lf/jyj/public-notice.ts
+++ lib/routes/gov/lf/jyj/public-notice.ts
@@ -8,7 +8,7 @@ export const route: Route = {
     path: '/lf/jyj/:cat',
     categories: ['government'],
     example: '/lf/jyj/57',
-    parameters: { cat: '分类'},
+    parameters: { cat: '分类' },
     features: {
         requireConfig: false,
         requirePuppeteer: false,
@@ -26,7 +26,7 @@ async function handler(ctx) {
     const cat = ctx.req.param('cat');
     const link = `http://jyj.lf.gov.cn/?cat=${cat}/`;
 
-    const data =  await buildData({
+    const data = await buildData({
         link,
         url: link,
         title: `%title%`,
@@ -38,24 +38,20 @@ async function handler(ctx) {
             item: '.list > ul > .lf_list_li',
             title: `$('p > a').text()`,
             link: `$('p > a').attr('href')`,
-            // pubDate: `$('h4 > .date').text().trim()`,
-            pubDate: `$('span.lf_list_time').text().trim()`
+            pubDate: `$('span.lf_list_time').text().trim()`,
         },
     });
 
     await Promise.all(
         data.item.map((item) =>
             cache.tryGet(item.link, async () => {
-                const { data: resonse } = await got(item.link);
-                const $ = load(resonse);
-                item.description = $('.detail > div').first().html();
+                const { data: response } = await got(item.link);
+                const $ = load(response);
+                item.description = $('.detail > div').html();
                 return item;
             })
         )
     );
 
-    ctx.set('data', data);
-
     return data;
-    
 }
