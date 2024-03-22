97e4eb3a6bcae535e399790af3f6c09ea825c4e5
DIYgod <i@diygod.me>
2024-03-22 22:20:45 +0800

    chore: upgrade got

--- lib/utils/got.ts
+++ lib/utils/got.ts
@@ -25,7 +25,6 @@ const custom: {
     delete: GotRequestFunction;
 } & GotRequestFunction &
     Got = got.extend({
-    get: got.get,
     retry: {
         limit: config.requestRetry,
         statusCodes: [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 421, 422, 423, 424, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511, 521, 522, 524],
