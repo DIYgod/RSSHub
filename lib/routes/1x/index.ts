377ec5c586e478df53d6ece0050a327969e86dbf
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-03-23 23:03:01 +0800

    fix radar

--- lib/routes/1x/index.ts
+++ lib/routes/1x/index.ts
@@ -7,39 +7,7 @@ import { load } from 'cheerio';
 import { art } from '@/utils/render';
 import * as path from 'node:path';
 
-export const route: Route = {
-    path: '/:category{.+}?',
-    name: '1x.com',
-    url: '1x.com',
-    maintainers: ['nczitzk'],
-    handler,
-    example: '/1x/:category?',
-    parameters: { category: 'Category, Latest Awarded by default' },
-    description: `::: tip
-Fill in the field in the path with the part of the corresponding page URL after \`https://1x.com/gallery/\` or \`https://1x.com/photo/\`. Here are the examples:
-
-If you subscribe to Abstract Awarded, you should fill in the path with the part \`abstract/awarded\` from the page URL \`https://1x.com/gallery/abstract/awarded\`. In this case, the route will be \`/1x/abstract/awarded\`.
-    
-If you subscribe to Wildlife Published, you should fill in the path with the part \`wildlife/published\` from the page URL \`https://1x.com/gallery/wildlife/published\`. In this case, the route will be \`/1x/wildlife/published\`.
-    :::`,
-    categories: ['design', 'picture'],
-
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportRadar: true,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
-    radar: {
-        source: ['/gallery/:category*', '/photos/:category*'],
-        target: '/1x/:category',
-    },
-};
-
-export async function handler(ctx) {
+export const handler = async (ctx) => {
     const { category = 'latest/awarded' } = ctx.req.param();
     const limit = ctx.req.query('limit') ? Number.parseInt(ctx.req.query('limit'), 10) : 30;
 
@@ -116,4 +84,38 @@ export async function handler(ctx) {
         author: $('meta[property="og:site_name"]').prop('content'),
         language,
     };
-}
+};
+
+export const route: Route = {
+    path: '/:category{.+}?',
+    name: 'Gallery',
+    url: '1x.com',
+    maintainers: ['nczitzk'],
+    handler,
+    example: '/1x/:category?',
+    parameters: { category: 'Category, Latest Awarded by default' },
+    description: `::: tip
+Fill in the field in the path with the part of the corresponding page URL after \`https://1x.com/gallery/\` or \`https://1x.com/photo/\`. Here are the examples:
+
+If you subscribe to [Abstract Awarded](https://1x.com/gallery/abstract/awarded), you should fill in the path with the part \`abstract/awarded\` from the page URL \`https://1x.com/gallery/abstract/awarded\`. In this case, the route will be [\`/1x/abstract/awarded\`](https://rsshub.app/1x/abstract/awarded).
+    
+If you subscribe to [Wildlife Published](https://1x.com/gallery/wildlife/published), you should fill in the path with the part \`wildlife/published\` from the page URL \`https://1x.com/gallery/wildlife/published\`. In this case, the route will be [\`/1x/wildlife/published\`](https://rsshub.app/1x/wildlife/published).
+:::`,
+    categories: ['design', 'picture'],
+
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportRadar: true,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['/gallery/:category*', '/photos/:category*'],
+            target: '/1x/:category',
+        },
+    ],
+};
