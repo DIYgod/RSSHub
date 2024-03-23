1faeee586948f76109dd9421c678d221de12a2fd
fashioncj <276333112@qq.com>
2024-03-24 00:27:18 +0800

    feat(route): recover route blogread/newest

--- lib/routes/blogread/index.ts
+++ lib/routes/blogread/index.ts
@@ -1,8 +1,23 @@
-const got = require('@/utils/got');
+import { Route } from '@/types';
+import got from '@/utils/got';
 const cheerio = require('cheerio');
 
-module.exports = async (ctx) => {
-    const url = 'http://blogread.cn/news/newest.php';
+export const route: Route = {
+    path: '/newest',
+    categories: ['programming'],
+    example: '/newest',
+    radar: [
+        {
+            source: ['blogread.cn/news/newest.php'],
+        },
+    ],
+    name: '最新文章',
+    maintainers: ['fashioncj'],
+    handler,
+};
+
+async function handler() {
+    const url = 'https://blogread.cn/news/newest.php';
     const response = await got({
         method: 'get',
         url,
@@ -12,19 +27,19 @@ module.exports = async (ctx) => {
         .map((index, elem) => {
             elem = $(elem);
             const $link = elem.find('dt a');
-
             return {
                 title: $link.text(),
                 description: elem.find('dd').eq(0).text(),
                 link: $link.attr('href'),
                 author: elem.find('.small a').eq(0).text(),
+                pubDate: elem.find('dd').eq(1).text().split('\n')[2],
             };
         })
         .get();
 
-    ctx.state.data = {
+    return {
         title: '技术头条',
         link: url,
         item: resultItem,
     };
-};
+}
