432260f580e61f9969ed108798f9f64de25c5508
Mg Pig <w2xzzpig@hotmail.com>
2024-03-24 14:44:40 +0800

    feat(route): gamer520 change to use wordpress api

--- lib/routes/gamer520/index.ts
+++ lib/routes/gamer520/index.ts
@@ -1,81 +1,133 @@
 import { Data, DataItem, Route } from '@/types';
 import got from '@/utils/got';
 import { parseDate } from '@/utils/parse-date';
-import { load } from 'cheerio';
 import { Context } from 'hono';
 
 export const route: Route = {
-    path: '/:category?/:order?/:price?',
+    path: '/:category?/:order?',
     categories: ['game'],
     example: '/gamer520/switchyouxi',
     parameters: {
         category: '分类，见下表',
-        order: '排序，见下表',
-        price: '价格, 全部:0;免费1;付费:2;',
+        order: '排序，发布日期: date; 修改日期: modified',
     },
     features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
+        antiCrawler: true,
     },
-    name: '全球游戏交流中心',
+    name: '文章',
     maintainers: ['xzzpig'],
     handler,
     url: 'www.gamer520.com/',
     description: `分类
 
-  | Switch游戏下载 | 金手指 | 3A巨作 | switch主题 | PC游戏 | PC游戏下载 | 模拟器大全 | 听我所听 |
-  | --- | --- | --- | --- | --- | --- | --- | --- |
-  | switchyouxi | jinshouzhi | 3ajuzuo | zhuti | pcgame | pcgame | zhangji | ting |
-  
-  排序
-  
-  | 发布日期 | 修改日期 | 评论数量 | 随机 | 热度 |
-  | --- | --- | --- | --- | --- |
-  | date | modified | comment_count | rand | hot |`,
+  | 所有 | Switch游戏下载 | 金手指 | 3A巨作 | switch主题 | PC游戏 |
+  | --- | --- | --- | --- | --- | --- |
+  | all | switchyouxi | jinshouzhi | 3ajuzuo | zhuti | pcgame |`,
 };
 
+// (await (await fetch('https://www.gamer520.com/wp-json/wp/v2/categories')).json()).map((category) => ({ slug: category.slug, id: category.id, name: category.name, link: category.link }));
+const CATEGORY_ARRAY = [
+    {
+        slug: 'shen',
+        id: 1,
+        name: '[神の贴]',
+        link: 'https://www.gamer520.com/shen'
+    },
+    {
+        slug: '3ajuzuo',
+        id: 4,
+        name: '3A巨作',
+        link: 'https://www.gamer520.com/3ajuzuo'
+    },
+    {
+        slug: 'cth',
+        id: 1481,
+        name: 'CTH',
+        link: 'https://www.gamer520.com/cth'
+    },
+    {
+        slug: 'offline-game-trainer',
+        id: 1483,
+        name: 'PC单机游戏修改器',
+        link: 'https://www.gamer520.com/offline-game-trainer'
+    },
+    {
+        slug: 'pcsmall',
+        id: 1477,
+        name: 'PC小仓库[持续更新]',
+        link: 'https://www.gamer520.com/pcsmall'
+    },
+    {
+        slug: 'pcgame',
+        id: 1471,
+        name: 'PC游戏下载',
+        link: 'https://www.gamer520.com/pcgame'
+    },
+    {
+        slug: 'zhuti',
+        id: 1476,
+        name: 'switch主题',
+        link: 'https://www.gamer520.com/zhuti'
+    },
+    {
+        slug: 'switchyouxi',
+        id: 2,
+        name: 'Switch游戏下载',
+        link: 'https://www.gamer520.com/switchyouxi'
+    },
+    {
+        slug: 'zhongwen',
+        id: 1461,
+        name: '中文',
+        link: 'https://www.gamer520.com/zhongwen'
+    },
+    {
+        slug: 'jinshouzhi',
+        id: 3,
+        name: '加藤之指',
+        link: 'https://www.gamer520.com/jinshouzhi'
+    }
+];
+
+interface Post {
+    id: number,
+    guid: { rendered: string },
+    title: { rendered: string },
+    date_gmt: string,
+    modified_gmt: string,
+    categories?: number[],
+    content: { rendered: string },
+}
+
 async function handler(ctx?: Context): Promise<Data> {
-    const category = ctx?.req.param('category') ?? 'switchyouxi';
+    const category = ctx?.req.param('category') ?? 'all';
     const order = ctx?.req.param('order');
-    const price = ctx?.req.param('price');
+    const categoryId = CATEGORY_ARRAY.find((c) => c.slug === category)?.id;
 
-    const currentUrl = new URL(`https://www.gamer520.com/${category}`);
-    if (order !== undefined) {
-        currentUrl.searchParams.set('order', order);
-    }
-    if (price !== undefined) {
-        currentUrl.searchParams.set('cao_type', price);
-    }
+    const baseUrl = 'https://www.gamer520.com';
 
-    const response = await got(currentUrl);
-    const $ = load(response.data as any);
+    const { data } = await got(`${baseUrl}/wp-json/wp/v2/posts`, {
+        searchParams: {
+            categories: categoryId,
+            orderby: order,
+            per_page: ctx?.req.query('limit') ? Number.parseInt(ctx?.req.query('limit') as string) : undefined,
+        },
+    }) as unknown as { data: Post[] };
 
-    const selector = `main article`;
-    const items: DataItem[] = $(selector)
-        .toArray()
-        .map((item) => {
-            const $item = $(item);
-            const $titleItem = $item.find('.entry-title a');
-            return {
-                title: $titleItem.text().trim(),
-                link: $titleItem.attr('href')!,
-                pubDate: parseDate($item.find('time').attr('datetime')?.trim() ?? ''),
-                category: $item
-                    .find('a[rel="category"]')
-                    .toArray()
-                    .map((a) => $(a).text().trim()),
-                description: $item.html() ?? '',
-            };
-        });
+    const items: DataItem[] = data
+        .map((item) => ({
+            guid: `gamer520:${item.id}`,
+            title: item.title.rendered,
+            link: item.guid.rendered,
+            pubDate: parseDate(item.date_gmt),
+            updated: parseDate(item.modified_gmt),
+            category: item.categories?.map((c) => CATEGORY_ARRAY.find((ca) => ca.id === c)?.name ?? '').filter((c) => c !== '') ?? [],
+            description: item.content.rendered,
+        }));
 
     return {
-        title: '全球游戏交流中心-' + $('title').text(),
-        link: currentUrl.toString(),
-        allowEmpty: true,
+        title: '全球游戏交流中心-' + (CATEGORY_ARRAY.find((c) => c.slug === category)?.name ?? '所有'),
+        link: CATEGORY_ARRAY.find((c) => c.slug === category)?.link ?? baseUrl,
         item: items,
     };
 }
