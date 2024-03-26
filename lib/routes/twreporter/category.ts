e588a1ee3c42dacfa1fa5e92423548addec29c74
Enoch Ma <emdoe@users.noreply.github.com>
2024-03-26 14:42:54 +0100

    simplification
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/twreporter/category.ts
+++ lib/routes/twreporter/category.ts
@@ -37,17 +37,13 @@ async function handler(ctx) {
 
     const regexp = /^window\.__REDUX_STATE__=(.*);$/gm;
     const raw = $('script[charset="UTF-8"]').text().replaceAll(regexp, '$1');
-    const list = JSON.parse(raw);
+    const posts = JSON.parse(raw).entities.posts;
 
-    const ids = list.entities.posts.allIds;
-    const links = [];
-    const titles = [];
-    const category = list.entities.posts.byId[ids[0]].category_set[0].category.name;
-
-    for (const id of ids) {
-        links.push(baseURL + '/a/' + list.entities.posts.byId[id].slug);
-        titles.push(list.entities.posts.byId[id].title);
-    }
+    const list = posts.allIds.map((id) => ({
+        link: baseURL + '/a/' + posts.byId[id].slug,
+        title: posts.byId[id].title,
+    }));
+    const category = posts.byId[posts.allIds[0]].category_set[0].category.name;
 
     const out = await Promise.all(
         links.map((item, index) => {
