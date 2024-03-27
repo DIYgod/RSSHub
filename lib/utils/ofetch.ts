23104cd1f69c2f16297cf1cdb1315c44888b6976
DIYgod <i@diygod.me>
2024-03-28 02:42:24 +0800

    feat: use undici

--- lib/utils/ofetch.ts
+++ lib/utils/ofetch.ts
@@ -1,14 +1,11 @@
-import { ofetch } from 'ofetch';
+import { createFetch } from 'ofetch';
 import { config } from '@/config';
 import logger from '@/utils/logger';
 
-const rofetch = ofetch.create({
+const rofetch = createFetch().create({
     retry: config.requestRetry,
     retryDelay: 1000,
     timeout: config.requestTimeout,
-    headers: {
-        'user-agent': config.ua,
-    },
     onRequestError({ request, error }) {
         logger.error(`Request ${request} fail: ${error}`);
     },
