90bf686fdcc7d5949fe6644a8e27b5c1bbee2602
JimenezLi <jimenezli@foxmail.com>
2024-03-29 13:15:35 +0800

    feat(core): replace git-rev-sync with async-git

--- lib/utils/git-hash.ts
+++ lib/utils/git-hash.ts
@@ -1,11 +1,11 @@
-import gitRevSync from 'git-rev-sync';
+import asyncGit from 'async-git';
 
 let gitHash = process.env.HEROKU_SLUG_COMMIT?.slice(0, 8) || process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 8);
 let gitDate: Date | undefined;
 if (!gitHash) {
     try {
-        gitHash = gitRevSync.short(undefined, 8);
-        gitDate = gitRevSync.date();
+        gitHash = (await asyncGit.sha).slice(0, 8);
+        gitDate = await asyncGit.date;
     } catch {
         gitHash = 'unknown';
     }
