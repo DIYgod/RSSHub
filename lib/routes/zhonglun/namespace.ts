688263f7de98afa3e5a4a4fabe5a9c1d2846601b
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-03-31 22:19:03 +0800

    feat(route): add 中伦律师事务所中伦研究专业文章

---
+++ lib/routes/zhonglun/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '中伦律师事务所',
+    url: 'zhonglun.com',
+    categories: ['new-media'],
+    description: '',
+};
