3d3286db6d69031e3eb82bfd74af9edbcb3833f4
JimenezLi <75196426+JimenezLi@users.noreply.github.com>
2024-04-02 01:28:35 +0800

    fix(core): fix Git Hash unknown (#15041)
    * fix: replace git-rev-sync with node function
    
    * fix: replace git-rev-sync in routes-deprecated

--- lib/utils/git-hash.ts
+++ lib/utils/git-hash.ts
@@ -1,11 +1,11 @@
-import gitRevSync from 'git-rev-sync';
+import { execSync } from 'child_process';
 
 let gitHash = process.env.HEROKU_SLUG_COMMIT?.slice(0, 8) || process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 8);
 let gitDate: Date | undefined;
 if (!gitHash) {
     try {
-        gitHash = gitRevSync.short(undefined, 8);
-        gitDate = gitRevSync.date();
+        gitHash = execSync('git rev-parse HEAD').toString().trim().slice(0, 8);
+        gitDate = new Date(execSync('git log -1 --format=%cd').toString().trim());
     } catch {
         gitHash = 'unknown';
     }
