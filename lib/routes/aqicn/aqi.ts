666b1b01ad9e03a2abf2d74d12f9c1499b765bfb
ladeng07 <2312936963@qq.com>
2024-04-03 17:06:47 +0800

    update api.ts

--- lib/routes/aqicn/aqi.ts
+++ lib/routes/aqicn/aqi.ts
@@ -1,4 +1,5 @@
 import got from '@/utils/got';
+import { parseDate } from '@/utils/parse-date';
 
 export const route: Route = {
     path: '/:city/:pollution?',
@@ -27,10 +28,10 @@ export const route: Route = {
 |   so2    |  SO2     |
 |   co     |  CO      |
 
-举例: [https://rsshub.app/aqicn/beijing/pm25,pm10](https://rsshub.app/aqicn/beijing/pm25.pm10)
+举例: [https://rsshub.app/aqicn/beijing/pm25,pm10](https://rsshub.app/aqicn/beijing/pm25,pm10)
 
 1. 显示单个污染成分，例如「pm25」, [https://rsshub.app/aqicn/beijing/pm25](https://rsshub.app/aqicn/beijing/pm25)
-2. 逗号分隔显示多个污染成分，例如「pm25,pm10」，[https://rsshub.app/aqicn/beijing/pm25,pm10](https://rsshub.app/aqicn/beijing/pm25.pm10)
+2. 逗号分隔显示多个污染成分，例如「pm25,pm10」，[https://rsshub.app/aqicn/beijing/pm25,pm10](https://rsshub.app/aqicn/beijing/pm25,pm10)
 3. 城市子站 ID 获取方法：右键显示网页源代码，搜索 "idx" （带双冒号），后面的 ID 就是子站的 ID，如你给的链接 ID 是 4258，RSS 地址就是 [https://rsshub.app/aqicn/4258](https://rsshub.app/aqicn/4258)
 `,
 };
@@ -72,8 +73,8 @@ async function handler(ctx) {
             {
                 title: `${data.namena}实时空气质量(AQI)${data.utimecn}`,
                 description: `${data.infocn}<br>风力:<b>${data.cwind[0]}</b>级<br>AQI:<b>${data.aqi}</b><br>${pollutionDetailed}<img src="${data.wgt}">`,
-                pubDate: new Date(data.time * 1000 + 8 * 3600 * 1000).toUTCString(),
-                guid: data.time,
+                pubDate: parseDate(data.time * 1000),
+                guid: `${data.time}-${city}-${pollution}`,
                 link: `https://aqicn.org/city/${data.ids.path}`,
             },
         ],
