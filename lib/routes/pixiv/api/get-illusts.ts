85004ded6aa72552b1c56c0d8e37c8471bc74116
windytiany <98391694+windytiany@users.noreply.github.com>
2024-04-04 22:54:35 +0800

    Update get-illusts.ts

--- lib/routes/pixiv/api/get-illusts.ts
+++ lib/routes/pixiv/api/get-illusts.ts
@@ -8,15 +8,30 @@ import queryString from 'query-string';
  * @param {string} token pixiv oauth token
  * @returns {Promise<got.AxiosResponse<{illusts: illust[]}>>}
  */
-export default function getIllusts(user_id, token) {
-    return got('https://app-api.pixiv.net/v1/user/illusts', {
-        headers: {
-            ...maskHeader,
-            Authorization: 'Bearer ' + token,
-        },
-        searchParams: queryString.stringify({
-            user_id,
-            filter: 'for_ios',
-        }),
-    });
+export async function getIllusts(user_id, token, limit) {
+    let offset = 0;
+    const illusts = new Array<any>();
+    let response;
+    let illustsAmount = 0;
+
+    do {
+        response = await got('https://app-api.pixiv.net/v1/user/illusts', {
+            headers: {
+                ...maskHeader,
+                Authorization: 'Bearer ' + token,
+            },
+            searchParams: queryString.stringify({
+                user_id,
+                filter: 'for_ios',
+                offset,
+            }),
+        });
+        illustsAmount = response.data.illusts.length;
+        for (const element of response.data.illusts) {
+            illusts.push(element);
+        }
+        offset += illustsAmount;
+    } while (illustsAmount === 30 && offset < limit);
+
+    return illusts;
 }
