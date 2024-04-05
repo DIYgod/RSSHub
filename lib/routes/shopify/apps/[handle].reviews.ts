cfd46071381c3db2d162143aa26d63d92d3a5cc9
Tony <TonyRL@users.noreply.github.com>
2024-04-06 00:27:44 +0800

    fix: cr

--- lib/routes/shopify/apps/[handle].reviews.ts
+++ lib/routes/shopify/apps/[handle].reviews.ts
@@ -11,69 +11,68 @@ export const route: Route = {
     name: 'App reviews',
     maintainers: ['PrintNow'],
     handler,
+    radar: [
+        {
+            source: ['apps.shopify.com/:handle'],
+        },
+    ],
 };
 
-async function handler(ctx?: Context): Promise<Data> {
-    if (!ctx) {
-        throw new Error('未传入 ctx');
-    }
-
+async function handler(ctx: Context): Promise<Data> {
     const { handle = '', page = '1' } = ctx.req.param();
-    const response = got.get(`${baseURL}/${handle}/reviews`, {
+    const response = await got.get(`${baseURL}/${handle}/reviews`, {
         searchParams: {
             sort_by: 'newest',
             page,
         },
-        responseType: 'text',
         headers: {
             accept: 'text/html, application/xhtml+xml',
             'accept-language': 'en-US;q=0.9',
             referer: baseURL,
             dnt: '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         },
     });
 
-    const htmlContent = await response.text();
+    const htmlContent = response.data;
     const $ = load(htmlContent);
 
-    const items = $(`div[data-merchant-review]`)
-        .map((index, item) => {
+    const items = $('div[data-merchant-review]')
+        .toArray()
+        .map((item) => {
             const $item = $(item);
 
             const reviewID = $item.attr('data-review-content-id');
 
-            const $review1 = $item.find(`div:nth-child(1)`);
-            const $review2 = $item.find(`div:nth-child(2)`);
+            const $review1 = $item.find('div:nth-child(1)');
+            const $review2 = $item.find('div:nth-child(2)');
 
-            const description = $item.find(`div[data-truncate-review] div[data-truncate-content-copy] p`).html() || '';
-            const author = $review2.find(`div.tw-text-fg-primary`).text().trim();
+            const description = $item.find('div[data-truncate-review] div[data-truncate-content-copy] p').html() || '';
+            const author = $review2.find('div.tw-text-fg-primary').text().trim();
 
             const result: DataItem = {
                 guid: reviewID,
                 title: description,
                 author,
-                pubDate: new Date($review1.find(`div[aria-label] + div`).text().trim()),
+                pubDate: new Date($review1.find('div[aria-label] + div').text().trim()),
 
                 // 评论内容
                 description,
 
                 _extra: {
-                    ratting_value: Number($review1.find(`div[role="img"]`).attr(`aria-label`)?.substring(0, 1)),
-                    location: $review2.find(`div.tw-text-fg-primary + div`).text().trim(),
+                    ratting_value: Number($review1.find('div[role="img"]').attr('aria-label')?.substring(0, 1)),
+                    location: $review2.find('div.tw-text-fg-primary + div').text().trim(),
                     author,
                 },
             };
 
             return result;
-        })
-        .toArray();
+        });
 
     return {
         title: `Reviews handle:${handle} page:${page} – Shopify App Store`,
         link: `${baseURL}/${handle}/reviews`,
         allowEmpty: true,
-        language: `en-US`,
+        language: 'en-US',
         item: items,
     };
 }
--- lib/routes/shopify/apps/search.ts
+++ lib/routes/shopify/apps/search.ts
@@ -8,48 +8,51 @@ export const route: Route = {
     path: '/apps/search/:q',
     example: '/shopify/apps/search/flow',
     parameters: { q: '需要搜索的 App' },
-    name: 'Shopify App store search',
+    name: 'App store search',
     maintainers: ['PrintNow'],
     handler,
+    radar: [
+        {
+            source: ['apps.shopify.com/search'],
+            target: (_params, url) => {
+                const { searchParams } = new URL(url).searchParams;
+                return searchParams.has('q') ? `/shopify/apps/search/${searchParams.get('q')}` : null;
+            },
+        },
+    ],
 };
 
-async function handler(ctx?: Context): Promise<Data> {
-    if (!ctx) {
-        throw new Error('未传入 ctx');
-    }
-
+async function handler(ctx: Context): Promise<Data> {
     const { q = '' } = ctx.req.param();
-    const response = got.get(`${baseURL}/search`, {
+    const response = await got.get(`${baseURL}/search`, {
         searchParams: {
             q,
         },
-        responseType: 'text',
         headers: {
             accept: 'text/html, application/xhtml+xml',
             'accept-language': 'en-US;q=0.9',
             'turbo-frame': 'search_page',
             referer: baseURL,
             dnt: '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         },
     });
 
-    const htmlContent = await response.text();
+    const htmlContent = await response.data;
     const $ = load(htmlContent);
 
-    const items = $(`.search-results-component div[data-controller="app-card"]`)
+    const items = $('.search-results-component div[data-controller="app-card"]')
         .toArray()
         .map((item) => {
-            const handle = $(item).attr(`data-app-card-handle-value`);
+            const handle = $(item).attr('data-app-card-handle-value');
 
-            // const appInfo = $(item).find(`div.tw-transition-colors.tw-text-fg-primary + div.tw-self-stretch`);
-            const appInfo = $(item).find(`div.tw-self-stretch`).clone();
+            // const appInfo = $(item).find('div.tw-transition-colors.tw-text-fg-primary + div.tw-self-stretch');
+            const appInfo = $(item).find('div.tw-self-stretch').clone();
 
             const rattingMatch = appInfo
-                .find(`span`)
+                .find('span')
                 .text()
                 .match(/\d\.\d/);
-            const rattingCountMatch = appInfo.find(`span + span.tw-sr-only`).text().match(/\d+/);
+            const rattingCountMatch = appInfo.find('span + span.tw-sr-only').text().match(/\d+/);
 
             const result: DataItem = {
                 title: $(item).attr('data-app-card-name-value') ?? '',
@@ -73,7 +76,7 @@ async function handler(ctx?: Context): Promise<Data> {
         link: `https://apps.shopify.com/search?q=${q}`,
         // description: `Search results for "${q}" – Shopify App Store`,
         allowEmpty: true,
-        language: `en-US`,
+        language: 'en-US',
         item: items,
     };
 }
