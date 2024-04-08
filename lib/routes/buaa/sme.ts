96cac8b8856673c9706047039470f9ca542bead4
Mean <390602272@qq.com>
2024-04-08 14:29:02 +0800

    feat: 适配新版路由规范

--- lib/routes/buaa/sme.ts
+++ lib/routes/buaa/sme.ts
@@ -9,9 +9,40 @@ const BASE_URL = 'http://www.sme.buaa.edu.cn';
 
 export const route: Route = {
     path: '/sme/:path{.+}?',
-    name: 'Unknown',
-    maintainers: [],
+    name: '集成电路科学与工程学院',
+    url: 'www.sme.buaa.edu.cn',
+    maintainers: ['MeanZhang'],
     handler,
+    example: '/buaa/sme/tzgg',
+    parameters: {
+        path: '版块路径，默认为 `tzgg`（通知公告）',
+    },
+    description: `:::tip
+
+版块路径（\`path\`）应填写板块 URL 中 \`http://www.sme.buaa.edu.cn/\` 和 \`.htm\` 之间的字段。
+
+示例：
+
+1. [通知公告](http://www.sme.buaa.edu.cn/tzgg.htm) 页面的 URL 为 \`http://www.sme.buaa.edu.cn/tzgg.htm\`，对应的路径参数为 \`tzgg\`，完整路由为 \`/buaa/sme/tzgg\`；
+2. [就业信息](http://www.sme.buaa.edu.cn/zsjy/jyxx.htm) 页面的 URL 为 \`http://www.sme.buaa.edu.cn/zsjy/jyxx.htm\`，对应的路径参数为 \`zsjy/jyxx\`，完整路由为 \`/buaa/sme/zsjy/jyxx\`。
+
+:::
+
+:::warning
+
+部分页面（如[学院介绍](http://www.sme.buaa.edu.cn/xygk/xyjs.htm)、[微纳中心](http://www.sme.buaa.edu.cn/wnzx.htm)、[院学生会](http://www.sme.buaa.edu.cn/xsgz/yxsh.htm)）存在无内容、内容跳转至外站等情况，因此可能出现解析失败的现象。
+
+:::`,
+    categories: ['university'],
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportRadar: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
 };
 
 async function handler(ctx) {
