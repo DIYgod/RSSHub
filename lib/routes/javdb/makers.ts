87828ede631ed62aede79869fbdc41437688761d
zhaoyi.wu <zhaoyi.wu@amh-group.com>
2024-04-12 09:58:03 +0800

    feat: Update JavDB routes to include optional JAVDB_SESSION in requireConfig

--- lib/routes/javdb/makers.ts
+++ lib/routes/javdb/makers.ts
@@ -7,7 +7,13 @@ export const route: Route = {
     example: '/javdb/makers/7R',
     parameters: { id: '编号，可在片商页 URL 中找到', filter: '过滤，见下表，默认为 `全部`' },
     features: {
-        requireConfig: false,
+        requireConfig: [
+            {
+                name: 'JAVDB_SESSION',
+                description: 'JavDB登陆后的session值，可在控制台的cookie下查找 `_jdb_session` 的值，即可获取',
+                optional: true,
+            },
+        ],
         requirePuppeteer: false,
         antiCrawler: true,
         supportBT: false,
--- lib/routes/javdb/rankings.ts
+++ lib/routes/javdb/rankings.ts
@@ -7,7 +7,13 @@ export const route: Route = {
     example: '/javdb/rankings',
     parameters: { category: '分类，见下表，默认为 `有碼`', time: '时间，见下表，默认为 `日榜`' },
     features: {
-        requireConfig: false,
+        requireConfig: [
+            {
+                name: 'JAVDB_SESSION',
+                description: 'JavDB登陆后的session值，可在控制台的cookie下查找 `_jdb_session` 的值，即可获取',
+                optional: true,
+            },
+        ],
         requirePuppeteer: false,
         antiCrawler: true,
         supportBT: false,
