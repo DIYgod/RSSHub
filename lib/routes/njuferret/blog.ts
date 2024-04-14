4377b19a6351c4f17d411a9a45aa56db32ccd04a
Sunny <76834004+tyl0622@users.noreply.github.com>
2024-04-14 03:32:15 +0000

    Update lib/routes/njuferret/blog.ts

--- lib/routes/njuferret/blog.ts
+++ lib/routes/njuferret/blog.ts
@@ -7,56 +7,35 @@ export const route: Route = {
     path: '/blog',
     categories: ['blog'],
     example: '/njuferret/blog',
-    parameters: { user: 'Github.io blog name' },
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
     radar: [
         {
             source: ['njuferret.github.io'],
         },
     ],
     name: 'Blogs',
-    maintainers: [],
+    maintainers: ['tyl0622'],
     handler,
 };
 
 async function handler() {
-    const promises = [];
-    let pageUrl;
     const baseUrl = 'https://njuferret.github.io';
-    for (let page = 1; page <= 7; page++) {
-        pageUrl = page === 1 ? baseUrl : `${baseUrl}/page/${page}`;
-        promises.push(ofetch(pageUrl));
-    }
+    const response = await ofetch(baseUrl);
+    const $ = load(response);
 
-    const responses = await Promise.all(promises);
-    let items = [];
-    for (const response of responses) {
-        const $ = load(response);
-
-        const pageItem = $('div.post-block')
-            .toArray()
-            .map((item) => {
-                item = $(item);
-                const a = item.find('a').first();
-                return {
-                    title: a.text(),
-                    link: `${baseUrl}${a.attr('href')}`,
-                    pubDate: parseDate(item.find('time').attr('datetime')),
-                };
-            });
-        items = [...items, ...pageItem];
-    }
+    const items = $('div.post-block')
+        .toArray()
+        .map((item) => {
+            item = $(item);
+            const a = item.find('a').first();
+            return {
+                title: a.text(),
+                link: `${baseUrl}${a.attr('href')}`,
+                pubDate: parseDate(item.find('time').attr('datetime')),
+            };
+        });
 
     return {
         title: 'njuferret - blog',
-        // link: baseUrl,
         item: items,
     };
 }
