dbd3826e80ef63472a51f072f38dc5cbf0959364
Sunny <76834004+tyl0622@users.noreply.github.com>
2024-04-14 17:00:07 +0800

    feat(route): Add route for njuferret.github.io (#15219)
    * Add route for njuferret.github.io
    
    * Improve: use spread operator
    
    * Update lib/routes/njuferret/blog.ts

---
+++ lib/routes/njuferret/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'njuferret',
+    url: 'njuferret.github.io',
+    description: '',
+    zh: {
+        name: '有点博客',
+    },
+};
