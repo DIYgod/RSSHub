cf8638f75589e8af6bf079bbdfec1f4b1190e914
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-04-15 08:16:48 +0800

    feat(route): add Access Briefing

---
+++ lib/routes/accessbriefing/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Access Briefing',
+    url: 'accessbriefing.com',
+    categories: ['new-media'],
+    description: '',
+};
