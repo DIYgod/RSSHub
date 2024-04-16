fc81efca4b727479d7739e909d7ba51176e8e407
Jaya Johnson <gujiakai28@gmail.com>
2024-04-16 05:26:52 +0000

    feat: Enhance link resolution and content selection
    - Update utils/index.ts to check for non-empty redirect URLs before assignment.
    - Modify yjs.ts and ai.ts to select content using a higher-level ID '#vsb_content' for more comprehensive data extraction.
    - Ensure all relative URLs within content are transformed into absolute URLs to improve link accessibility and reliability in RSS feeds.

--- lib/routes/gxmzu/utils/index.ts
+++ lib/routes/gxmzu/utils/index.ts
@@ -26,7 +26,7 @@ async function getNoticeList(ctx, url, host, titleSelector, dateSelector, conten
                     item.description = '该通知无法直接预览，请点击原文链接↑查看';
                 } else {
                     const response = await got({ url: item.link, https: { rejectUnauthorized: false } });
-                    if (response.redirectUrls.length) {
+                    if (response.redirectUrls && response.redirectUrls.length > 0) {
                         item.link = response.redirectUrls[0];
                         item.description = '该通知无法直接预览，请点击原文链接↑查看';
                     } else {
@@ -34,7 +34,19 @@ async function getNoticeList(ctx, url, host, titleSelector, dateSelector, conten
                         item.title = $(contentSelector.title).text();
                         const hasEmbeddedPDFScript = $('script:contains("showVsbpdfIframe")').length > 0;
 
-                        item.description = hasEmbeddedPDFScript ? '该通知无法直接预览，请点击原文链接↑查看' : $(contentSelector.content).html();
+                        if (hasEmbeddedPDFScript) {
+                            item.description = '该通知无法直接预览，请点击原文链接↑查看';
+                        } else {
+                            const $content = load($(contentSelector.content).html());
+                            $content('a').each(function () {
+                                const a = $(this);
+                                const href = a.attr('href');
+                                if (href && !href.startsWith('http')) {
+                                    a.attr('href', new URL(href, host).href);
+                                }
+                            });
+                            item.description = $content.html();
+                        }
                         const preDate = $(contentSelector.date).text().replaceAll(/年|月/g, '-').replaceAll('日', '');
                         item.pubDate = timezone(parseDate(preDate), +8);
                     }
