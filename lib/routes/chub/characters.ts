0825dfb53998878d30d8c7d6c0f1e431f363c0ff
flameleaf <18636550+flameleaf@users.noreply.github.com>
2024-04-19 16:19:34 -0700

    Create characters.ts

---
+++ lib/routes/chub/characters.ts
@@ -0,0 +1,52 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/characters',
+    categories: ['new-media'],
+    example: '/chub/characters',
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: 'Characters',
+    maintainers: ['flameleaf'],
+    handler,
+};
+
+
+async function handler(ctx) {
+    const host = `https://www.chub.ai/characters`;
+    const url = `https://api.chub.ai/api/characters/search?query=&first=200&page=1&sort=last_activity_at&asc=false&include_forks=false&nsfw=true&nsfl=true&nsfw_only=false&require_images=false&require_example_dialogues=false&require_alternate_greetings=false&require_custom_prompt=false&exclude_mine=false&venus=true&chub=true&min_tokens=50&require_expressions=false&require_lore=false&mine_first=false&require_lore_embedded=false&require_lore_linked=false&inclusive_or=false&recommended_verified=false`;
+    const headers = { Accept: 'application/json' };
+    const response = await got({
+        method: 'get',
+        url,
+        headers,
+    });
+    const data = response.data;
+    const nodes = data.nodes;
+
+    return {
+        allowEmpty: true,
+        title: `Chub`,
+        link: host,
+        item: nodes
+            .map((item) => ({
+                    title: item.name,
+                    description: `${item.tagline}<br><br>${item.description}<br><br>${item.topics}`,
+                    pubDate: parseDate(item.lastActivityAt),
+                    link: `${host}/${item.fullPath}`,
+                    author: `${item.fullPath.split('/',1)}`,
+                    enclosure_url: `${item.avatar_url}`,
+                    enclosure_type: `image/webp`,
+            })),
+    };
+}
+
+
