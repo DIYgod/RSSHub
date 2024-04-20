5c244c26b311fdfba2ab69e659c7fbd65e947655
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-04-20 18:26:42 +0800

    feat(route): add 中国汽车流通协会汽车市场研究分会

---
+++ lib/routes/cpcaauto/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'cpcaauto.com',
+    url: '中国汽车流通协会汽车市场研究分会',
+    categories: ['new-media'],
+    description: '',
+};
