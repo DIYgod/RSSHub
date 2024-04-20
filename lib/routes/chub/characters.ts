31c5e54fd9bffd378de00cb61fea3c7c961c77b5
flameleaf <18636550+flameleaf@users.noreply.github.com>
2024-04-19 22:00:57 -0700

    remove uncessary ctx

--- lib/routes/chub/characters.ts
+++ lib/routes/chub/characters.ts
@@ -20,7 +20,7 @@ export const route: Route = {
 };
 
 
-async function handler(ctx) {
+async function handler() {
     const host = `https://www.chub.ai/characters`;
     const url = `https://api.chub.ai/api/characters/search?query=&first=200&page=1&sort=last_activity_at&asc=false&include_forks=false&nsfw=true&nsfl=true&nsfw_only=false&require_images=false&require_example_dialogues=false&require_alternate_greetings=false&require_custom_prompt=false&exclude_mine=false&venus=true&chub=true&min_tokens=50&require_expressions=false&require_lore=false&mine_first=false&require_lore_embedded=false&require_lore_linked=false&inclusive_or=false&recommended_verified=false`;
     const headers = { Accept: 'application/json' };
