fef2744248cb861b855b60032a5bbe704feaf750
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-04-22 20:47:10 +0800

    feat(route): add 网猴线报

---
+++ lib/routes/iehou/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '网猴线报',
+    url: 'iehou.com',
+    categories: ['new-media'],
+    description: '',
+};
