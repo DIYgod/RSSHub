fb0b142b5a2e15ca72ada33353dcb28ba2ed80c4
Bangwu <119404476+markbang@users.noreply.github.com>
2024-04-24 18:44:37 +0800

    feat(route): add jwc notice (#15357)
    * add jwc
    
    * fix Move function definitions to the highest possible scope.
    Warning
    
    * Fix code non-standard format
    
    * Fix code non-standard format

--- lib/routes/ecnu/namespace.ts
+++ lib/routes/ecnu/namespace.ts
@@ -2,5 +2,5 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: 'East China Normal University 华东师范大学',
-    url: 'acm.ecnu.edu.cn',
+    url: 'ecnu.edu.cn',
 };
