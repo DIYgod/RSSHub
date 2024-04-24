79f9f0aecb14c9d8bdde019ad8748a7e24f8fa0f
lihaoyu <lihaoyu@unicoresoft.com>
2024-04-24 19:33:19 +0800

    refactor: 浙江省人民政府-统一搜索结果-代码规范修正、description字段补充

--- lib/routes/gov/zj/search.ts
+++ lib/routes/gov/zj/search.ts
@@ -9,10 +9,10 @@ export const route: Route = {
     categories: ['government'],
     example: '/gov/zj/search',
     parameters: {
-        websiteid: '搜索范围-全省、各市各区',
-        word: '搜索关键词',
-        cateid: '信息分类',
-        sortType: '排序类型',
+        websiteid: '搜索范围-全省、各市各区、详细信息点击源网站https://www.zj.gov.cn/请求中寻找',
+        word: '搜索关键词-默认：人才',
+        cateid: '信息分类-默认：658（全部）',
+        sortType: '排序类型-默认：2（按时间）',
     },
     features: {
         requireConfig: false,
@@ -29,45 +29,58 @@ export const route: Route = {
     name: '浙江省人民政府-全省政府网站统一搜索',
     url: 'search.zj.gov.cn/jsearchfront/search.do',
     maintainers: ['HaoyuLee'],
-    handler,
+    description: `
+        | 行政区域         | websiteid |
+        | ------------ | -- |
+        | 宁波市本级     | 330201000000000  |
+
+        | 搜索关键词         | word    |
+
+        | 信息分类         | cateid    |
+
+        | 排序类型         | sortType    |
+        | ------------ | -- |
+        | 按相关度     | 1  |
+        | 按时间     | 2  |
+    `,
+    async handler(ctx) {
+        const { websiteid = '330201000000000', word = '人才', cateid = 658, sortType = 2 } = ctx.req.param();
+        const {
+            data: { result: list },
+        } = await got.post('https://search.zj.gov.cn/jsearchfront/interfaces/cateSearch.do', {
+            form: {
+                websiteid,
+                pg: '30',
+                p: '1',
+                cateid,
+                word,
+                checkError: 1,
+                isContains: 0,
+                q: word,
+                begin: dayjs().subtract(1, 'week').format('YYYYMMDD'),
+                end: dayjs().format('YYYYMMDD'),
+                timetype: 2,
+                pos: 'title,content,keyword',
+                sortType,
+            },
+        });
+        const items =
+            list?.map((item: string) => {
+                const $ = load(item);
+                const title = $('.titleWrapper>a');
+                const footer = $('.sourceTime>span');
+                return {
+                    title: title.text().trim() || '',
+                    link: title.attr('href') || '',
+                    pubDate: parseDate(footer.eq(1).text().trim().replace('时间:', '')) || '',
+                    author: footer.eq(0).text().trim().replace('来源:', '') || '',
+                    description: $('.newsDescribe>a').text() || '',
+                };
+            }) || [];
+        return {
+            title: `浙江省人民政府-全省政府网站统一搜索`,
+            link: `search.zj.gov.cn/jsearchfront/search.do`,
+            item: items,
+        };
+    },
 };
-async function handler(ctx) {
-    const { websiteid = '330201000000000', word = '人才', cateid = 658, sortType = 2 } = ctx.req.param();
-    const {
-        data: { result: list },
-    } = await got.post('https://search.zj.gov.cn/jsearchfront/interfaces/cateSearch.do', {
-        form: {
-            websiteid,
-            pg: '30',
-            p: '1',
-            cateid,
-            word,
-            checkError: 1,
-            isContains: 0,
-            q: word,
-            begin: dayjs().subtract(1, 'week').format('YYYYMMDD'),
-            end: dayjs().format('YYYYMMDD'),
-            timetype: 2,
-            pos: 'title,content,keyword',
-            sortType,
-        },
-    });
-    const items =
-        list?.map((item: string) => {
-            const $ = load(item);
-            const title = $('.titleWrapper>a');
-            const footer = $('.sourceTime>span');
-            return {
-                title: title.text().trim() || '',
-                link: title.attr('href') || '',
-                pubDate: parseDate(footer.eq(1).text().trim().replace('时间:', '')) || '',
-                author: footer.eq(0).text().trim().replace('来源:', '') || '',
-                description: $('.newsDescribe>a').text() || '',
-            };
-        }) || [];
-    return {
-        title: `浙江省人民政府-全省政府网站统一搜索`,
-        link: `search.zj.gov.cn/jsearchfront/search.do`,
-        item: items,
-    };
-}
