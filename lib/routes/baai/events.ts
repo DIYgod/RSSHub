bdc52ef588af2752b4d2c9b6bce32783373db146
Tony <TonyRL@users.noreply.github.com>
2024-04-25 03:14:02 +0800

    fix(route): remove `got.all` usage in baai

--- lib/routes/baai/events.ts
+++ lib/routes/baai/events.ts
@@ -1,6 +1,6 @@
 import { Route } from '@/types';
 import cache from '@/utils/cache';
-import got from '@/utils/got';
+import ofetch from '@/utils/ofetch';
 
 import { baseUrl, apiHost, parseEventDetail, parseItem } from './utils';
 
@@ -8,15 +8,6 @@ export const route: Route = {
     path: '/hub/events',
     categories: ['programming'],
     example: '/baai/hub/events',
-    parameters: {},
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
     radar: [
         {
             source: ['hub.baai.ac.cn/events', 'hub.baai.ac.cn/'],
@@ -28,18 +19,16 @@ export const route: Route = {
     url: 'hub.baai.ac.cn/events',
 };
 
-async function handler(ctx) {
-    const responses = await got.all(
-        Array.from(
-            {
-                // first 2 pages
-                length: (ctx.req.query('limit') ? Number.parseInt(ctx.req.query('limit'), 10) : 16) / 8,
-            },
-            (_, v) => `${apiHost}/api/v1/events?page=${v + 1}`
-        ).map((url) => got.post(url))
-    );
+async function handler() {
+    const response = await ofetch(`${apiHost}/api/v1/events`, {
+        method: 'POST',
+        body: {
+            page: 1,
+            tag_id: '',
+        },
+    });
 
-    const list = responses.flatMap((response) => response.data.data).map((item) => parseItem(item));
+    const list = response.data.map((item) => parseItem(item));
 
     const items = await Promise.all(
         list.map((item) =>
