99e657b320782eb60c1be960117a3d87687fc524
flameleaf <18636550+flameleaf@users.noreply.github.com>
2024-04-25 16:45:17 -0700

    replaced got with ofetch + query

--- lib/routes/chub/characters.ts
+++ lib/routes/chub/characters.ts
@@ -1,5 +1,5 @@
 import { Route } from '@/types';
-import got from '@/utils/got';
+import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
 
 export const route: Route = {
@@ -13,28 +13,49 @@ export const route: Route = {
 
 
 async function handler() {
-    const host = 'https://www.chub.ai/characters';
-    const url = `https://api.chub.ai/api/characters/search?query=&first=200&page=1&sort=last_activity_at&asc=false&include_forks=false&nsfw=true&nsfl=true&nsfw_only=false&require_images=false&require_example_dialogues=false&require_alternate_greetings=false&require_custom_prompt=false&exclude_mine=false&venus=true&chub=true&min_tokens=50&require_expressions=false&require_lore=false&mine_first=false&require_lore_embedded=false&require_lore_linked=false&inclusive_or=false&recommended_verified=false`;
-    const headers = { Accept: 'application/json' };
-    const response = await got({
-        method: 'get',
-        url,
-        headers,
+    const hostURL = 'https://www.chub.ai/characters';
+    const apiURL = 'https://api.chub.ai/api/characters/search?query=';
+    const data = await ofetch(`${apiURL}`, {
+        headers: { Accept: 'application/json' },
+        query: {
+            first: 200,
+            page: 1,
+            sort: 'last_activity_at',
+            asc: 'false',
+            include_forks: 'false',
+            nsfw: 'true',
+            nsfl: 'true',
+            nsfw_only: 'false',
+            require_images: 'false',
+            require_example_dialogues: 'false',
+            require_alternate_greetings: 'false',
+            require_custom_prompt: 'false',
+            exclude_mine: 'false',
+            venus: 'true',
+            chub: 'true',
+            min_tokens: 50,
+            require_expressions: 'false',
+            require_lore: 'false',
+            mine_first: 'false',
+            require_lore_embedded: 'false',
+            require_lore_linked: 'false',
+            inclusive_or: 'false',
+            recommended_verified: 'false',
+        },
     });
-    const data = response.data;
     const nodes = data.nodes;
 
     return {
         allowEmpty: true,
         title: 'Chub',
-        link: host,
+        link: hostURL,
         item: nodes
             .map((item) => ({
                     title: item.name,
                     description: `${item.tagline}<br><br>${item.description}`,
                     pubDate: parseDate(item.createdAt),
                     updated: parseDate(item.lastActivityAt),
-                    link: `${host}/${item.fullPath}`,
+                    link: `${hostURL}/${item.fullPath}`,
                     author: String(item.fullPath.split('/', 1)),
                     enclosure_url: item.avatar_url,
                     enclosure_type: `image/webp`,
