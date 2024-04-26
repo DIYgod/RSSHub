fe078fef2f3a6d59e61173f8392b805af032289b
root <root@debian.st>
2024-04-27 01:40:14 +0800

    feat(route): add olevod.one

---
+++ lib/routes/olevodone/vodlist.ts
@@ -0,0 +1,57 @@
+import { Route } from '@/types';
+import { load } from 'cheerio';
+import ofetch from '@/utils/ofetch';
+
+export const route: Route = {
+    path: '/vodlist',
+    categories: ['social-media'],
+    example: '/olevodone/vodlist',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['www.olevod.one'],
+            target: '/vodlist',
+        },
+    ],
+    name: '最新视频',
+    maintainers: ['st'],
+    handler,
+};
+
+async function handler() {
+    const urlBase = 'https://www.olevod.one';
+    const title = '欧乐影院 最新视频';
+
+    const response = await ofetch(urlBase);
+    const $ = load(response);
+
+    const items = $('.cbox1 .vodlist_thumb.lazyload')
+        .toArray()
+        .map((item) => {
+            const tmp = $(item);
+            const href = urlBase + tmp.attr('href');
+            const title = tmp.attr('title');
+            const image = urlBase + tmp.attr('data-original');
+
+            return {
+                title: `${title} ${tmp.find('.pic_text.text_right').text()}`,
+                link: href,
+                image,
+                description: `豆瓣评分 ${tmp.find('.text_right.text_dy').text()}`,
+            };
+        });
+
+    return {
+        title,
+        link: urlBase,
+        item: items,
+    };
+}
