6a6ddddb888e60f9c0df4f3ca39c7372ca847629
dzx-dzx <dzx_dzx@hotmail.com>
2024-04-27 20:28:46 +0800

    feat(route): Copernicium

---
+++ lib/routes/copernicium/index.ts
@@ -0,0 +1,61 @@
+import { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+
+import cache from '@/utils/cache';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/:category',
+    categories: ['blog'],
+    radar: [{ source: ['www.copernicium.tw'] }],
+    name: '分类',
+    maintainers: [''],
+    handler,
+};
+
+async function handler(ctx) {
+    const CATEGORY_TO_ARG_MAP = new Map([
+        ['环球视角', '4_1'],
+        ['人文叙述', '4_3'],
+        ['观点评论', '4_5'],
+    ]);
+    const reqArgs = {
+        args: {
+            _jcp: CATEGORY_TO_ARG_MAP.get(ctx.req.param().category),
+            m31pageno: 1,
+        },
+        type: 0,
+    };
+    const res = await ofetch(`https://www.copernicium.tw/nr.jsp`, {
+        query: { _reqArgs: reqArgs },
+    });
+    const $ = load(res);
+    const list = $('.J_newsResultLine a.mixNewsStyleTitle')
+        .toArray()
+        .map((e) => {
+            e = $(e);
+            return {
+                title: e.text(),
+                link: e.attr('href'),
+            };
+        });
+    const items = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link, async () => {
+                const detailResponse = await ofetch(item.link);
+                const content = load(detailResponse);
+                return {
+                    pubDate: parseDate(content('span.newsInfo').text().substring(5)),
+                    description: content('.richContent').html(),
+                    ...item,
+                };
+            })
+        )
+    );
+    return {
+        title: `日新说 - ${ctx.req.param().category}`,
+        link: 'www.copernicium.tw',
+        item: items,
+    };
+}
---
+++ lib/routes/copernicium/namespace.ts
@@ -0,0 +1,6 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '日新说',
+    url: 'www.copernicium.tw',
+};
