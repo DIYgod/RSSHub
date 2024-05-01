0a506a9e67e503922cf7feb0eabc39d5118f3f06
Tony <TonyRL@users.noreply.github.com>
2024-05-02 03:44:06 +0800

    feat(route): wellcee (#15433)

---
+++ lib/routes/wellcee/types.ts
@@ -0,0 +1,58 @@
+export interface City {
+    id: string;
+    icon: string;
+    city: string;
+    enCityName: string;
+    cityName: string;
+    chCityName: string;
+    twCityName: string;
+    statics: {
+        total_text: string;
+        online_text: string;
+        total_count: number;
+        online_count: number;
+    };
+    district: District[];
+}
+
+export interface District {
+    id: string;
+    longitude: number;
+    latitude: number;
+    name: string;
+    business: {
+        id: string;
+        name: string;
+    }[];
+}
+
+export interface House {
+    id: string;
+    imgs: string[];
+    ev: number;
+    city: string;
+    district: string;
+    tagsText: string;
+    tags: string[];
+    typeTags: string[];
+    rent: string;
+    video: string;
+    personalDesc: string;
+    address: string;
+    longitude: number;
+    latitude: number;
+    collectNum: number;
+    isCollect: boolean;
+    userInfo: {
+        id: string;
+        name: string;
+        gender: number;
+        avatar: string;
+        language: string[];
+        loginTime: string;
+        tenant_status: number;
+        landlord_status: number;
+    };
+    loginTime: number;
+    dailyRent: string;
+}
