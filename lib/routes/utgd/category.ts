4f2f0f55bc01edf7fc564ba49f6a886a0ec5cfdb
liqun1024 <liqun1024@foxmail.com>
2024-05-02 21:28:19 +0800

    Update utgd.net API

--- lib/routes/utgd/category.ts
+++ lib/routes/utgd/category.ts
@@ -14,9 +14,9 @@ const md = MarkdownIt({
 });
 
 export const route: Route = {
-    path: '/:category?',
+    path: '/category/:category?',
     categories: ['new-media'],
-    example: '/utgd/method',
+    example: '/utgd/category/method',
     parameters: { category: '分类，可在对应分类页的 URL 中找到，默认为方法' },
     features: {
         requireConfig: false,
@@ -29,7 +29,7 @@ export const route: Route = {
     radar: [
         {
             source: ['utgd.net/category/s/:category', 'utgd.net/'],
-            target: '/:category',
+            target: '/category/:category',
         },
     ],
     name: '分类',
@@ -42,10 +42,11 @@ export const route: Route = {
 
 async function handler(ctx) {
     const category = ctx.req.param('category') ?? 'method';
-    const limit = ctx.req.query('limit') ? Number.parseInt(ctx.req.query('limit')) : 20;
+    const pagesize = ctx.req.query('limit') ? Number.parseInt(ctx.req.query('limit')) : 20;
 
     const rootUrl = 'https://utgd.net';
-    const apiUrl = `${rootUrl}/api/v2/pages/`;
+    const baseApiUrl = 'https://api.utgd.net'
+    const apiUrl = `${baseApiUrl}/api/v2/categories`;
     const currentUrl = `${rootUrl}/category/s/${category}`;
     const slugUrl = `${rootUrl}/api/v2/category/slug/${category}/`;
 
@@ -58,35 +59,32 @@ async function handler(ctx) {
 
     response = await got({
         method: 'get',
-        url: apiUrl,
+        url: `${apiUrl}/${data.id}/related_articles/`,
         searchParams: {
-            type: 'article.Article',
-            fields: `article_category(category_name),article_tag(tag_name),title,article_image,article_author,article_description,article_published_time`,
-            article_category: data.id,
-            order: '-article_published_time',
-            limit,
+            page: '1',
+            pagesize,
         },
     });
 
-    const items = await Promise.all(
-        response.data.items.map((item) =>
-            cache.tryGet(`untag-${item.id}`, async () => {
+    const results = await Promise.all(
+        response.data.results.map((result) =>
+            cache.tryGet(`untag-${result.id}`, async () => {
                 const authorResponse = await got({
                     method: 'get',
-                    url: `${rootUrl}/api/v2/user/profile/${item.article_author.id}/`,
+                    url: `${rootUrl}/api/v2/user/profile/${result.article_author_id}/`,
                 });
 
                 return {
-                    title: item.title,
-                    link: `${rootUrl}/article/${item.id}`,
+                    title: result.title,
+                    link: `${rootUrl}/article/${result.id}`,
                     description: art(path.join(__dirname, 'templates/description.art'), {
-                        membership: item.article_for_membership,
-                        image: item.article_image,
-                        description: md.render(item.article_description),
+                        membership: result.article_for_membership,
+                        image: result.article_image,
+                        description: md.render(result.article_description),
                     }),
                     author: authorResponse.data.display_name,
-                    pubDate: timezone(parseDate(item.article_published_time), +8),
-                    category: [...item.article_category.map((c) => c.category_name), ...item.article_tag.map((t) => t.tag_name)],
+                    pubDate: timezone(parseDate(result.article_published_time), +8),
+                    category: [...result.article_category.map((c) => c.category_name), ...result.article_tag.map((t) => t.tag_name)],
                 };
             })
         )
@@ -95,7 +93,7 @@ async function handler(ctx) {
     return {
         title: `UNTAG - ${data.category_name}`,
         link: currentUrl,
-        item: items,
+        item: results,
         image: data.category_image,
         description: data.category_description,
     };
