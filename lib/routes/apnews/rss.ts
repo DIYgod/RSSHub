65e0fa350dd885d15b8e053f8e484b5ed87584e9
Andvari <31068367+dzx-dzx@users.noreply.github.com>
2024-05-03 01:22:10 +0800

    Update rss.ts

--- lib/routes/apnews/rss.ts
+++ lib/routes/apnews/rss.ts
@@ -6,7 +6,7 @@ const HOME_PAGE = 'https://apnews.com';
 export const route: Route = {
     path: '/rss/:rss?',
     categories: ['traditional-media'],
-    example: '/apnews/topics/apf-topnews',
+    example: '/apnews/rss/business',
     parameters: { rss: 'Route name from the first segment of the corresponding site, or `index` for the front page(default).' },
     features: {
         requireConfig: false,
