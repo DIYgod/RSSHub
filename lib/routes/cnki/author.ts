ec87b6ea8a0d866459e1304691f5087f8c15877b
Tony <TonyRL@users.noreply.github.com>
2024-05-07 05:10:35 +0800

    fix: typo

--- lib/routes/cnki/author.ts
+++ lib/routes/cnki/author.ts
@@ -2,7 +2,7 @@ import { Route } from '@/types';
 import cache from '@/utils/cache';
 import got from '@/utils/got';
 import { load } from 'cheerio';
-import utils from './utils';
+import { ProcessItem } from './utils';
 
 const rootUrl = 'https://kns.cnki.net';
 
@@ -70,7 +70,7 @@ async function handler(ctx) {
         };
     });
 
-    const items = await Promise.all(list.map((item) => cache.tryGet(item.link, () => utils.ProcessItem(item))));
+    const items = await Promise.all(list.map((item) => cache.tryGet(item.link, () => ProcessItem(item))));
 
     return {
         title: `知网 ${authorName} ${companyName}`,
