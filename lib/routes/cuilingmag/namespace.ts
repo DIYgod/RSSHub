5e8ea80a8ed9b1844c0efe79dc6fcd523c13ada5
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2024-05-12 19:07:53 +0800

    feat(route): add 萃嶺网 (#15562)
    * feat(route): add 萃嶺网
    
    * fix typo
    
    * fix typo

---
+++ lib/routes/cuilingmag/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '萃嶺网',
+    url: 'cuilingmag.com',
+    categories: ['new-media'],
+    description: '',
+};
