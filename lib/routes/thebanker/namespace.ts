ca337574b7f52248429bc0c337be8a05b920c345
Benjamin <web-service@zhangyanze.com>
2024-05-14 10:25:13 +0800

    Test thebanker

---
+++ lib/routes/thebanker/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The Banker',
+    url: 'www.thebanker.com',
+    categories: ['finance'],
+    description: '',
+};
