dd88055277c88fe975c21149da0d254b0fe5d44c
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2024-05-16 23:58:47 +0800

    feat(route): add 穿越火线CFHD专区资讯中心 (#15601)
    * feat(route): add 穿越火线CFHD专区资讯中心
    
    * fix: remove enclosure_url

--- lib/routes/qq/namespace.ts
+++ lib/routes/qq/namespace.ts
@@ -1,6 +1,8 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: '腾讯新闻较真查证平台',
-    url: 'ac.qq.com',
+    name: '腾讯网',
+    url: 'qq.com',
+    categories: ['new-media'],
+    description: '',
 };
