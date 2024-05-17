c91e0c46363038d1df6a363fc603a55f95fef598
Antesam <antespam@pm.me>
2024-05-17 10:35:40 +0100

    Update pornstar.ts
    Fix pornstar URL - it is different to how models URL structured

--- lib/routes/pornhub/pornstar.ts
+++ lib/routes/pornhub/pornstar.ts
@@ -36,7 +36,7 @@ export const route: Route = {
 
 async function handler(ctx) {
     const { language = 'www', username, sort = 'mr' } = ctx.req.param();
-    const link = `https://${language}.pornhub.com/pornstar/${username}/videos?o=${sort}`;
+    const link = `https://${language}.pornhub.com/pornstar/${username}?o=${sort}`;
     if (!isValidHost(language)) {
         throw new InvalidParameterError('Invalid language');
     }
