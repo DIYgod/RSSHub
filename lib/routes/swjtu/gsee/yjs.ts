a023bc4e991e15227d67c6c22d67b09984071884
E1nzbern <42884262+E1nzbern@users.noreply.github.com>
2024-05-19 18:36:34 +0800

    feat(route): Add 北京交通大学研究生院通知和西南交通大学地球科学与工程学院研究生通知路由 (#15608)
    * feat: 根据V2规范重新编写了北京交通大学研究生院的路由规则
    
    * fix: 修复标题错误
    
    * feat: 添加西南交通大学地球科学与工程学院研究生教育路由规则
    
    * fix: 修复example编写错误
    
    * style: 变量命名风格修改为camelCase；删除多余的heading；修改description缩进为2空格；
    refactor: 删除弃用的路由；删除不必要的cache参数传递；
    
    ---------
    
    Co-authored-by: pull[bot] <39814207+pull[bot]@users.noreply.github.com>

---
+++ lib/routes/swjtu/gsee/yjs.ts
@@ -0,0 +1,76 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+
+const rootURL = 'https://gsee.swjtu.edu.cn';
+const urlAddr = `${rootURL}/xwzx/tzgg.htm`;
+
+const getItem = (item) => {
+    const newsInfo = item.find('dt');
+    const newsDate = item
+        .find('dd')
+        .text()
+        .match(/\d{4}(-|\/|.)\d{1,2}\1\d{1,2}/)[0];
+
+    const infoTitle = newsInfo.text();
+    const link = rootURL + newsInfo.find('a').last().attr('href').slice(2);
+    return cache.tryGet(link, async () => {
+        const resp = await ofetch(link);
+        const $$ = load(resp);
+        const infoText = $$('.article').html();
+
+        return {
+            title: infoTitle,
+            pubDate: parseDate(newsDate),
+            link,
+            description: infoText,
+        };
+    }) as any;
+};
+
+export const route: Route = {
+    path: '/gsee/yjs',
+    categories: ['university'],
+    example: '/swjtu/gsee/yjs',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: true,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['gsee.swjtu.edu.cn/'],
+        },
+    ],
+    name: '地球科学与工程学院',
+    maintainers: ['E1nzbern'],
+    handler,
+    description: `研究生教育通知公告`,
+};
+
+async function handler() {
+    const resp = await ofetch(urlAddr);
+    const $ = load(resp);
+
+    const list = $('dl');
+
+    const items = await Promise.all(
+        list.toArray().map((i) => {
+            const item = $(i);
+            return getItem(item);
+        })
+    );
+
+    return {
+        title: '西南交大地学学院-研究生通知',
+        link: urlAddr,
+        item: items,
+        allowEmpty: true,
+    };
+}
