86ac2d8ab55d446cbff933f0e1b5eba347dfbae6
coder_wen <xianghua_we@163.com>
2024-05-22 13:48:51 +0800

    style: reg i

--- lib/routes/6v123/latest-movies.ts
+++ lib/routes/6v123/latest-movies.ts
@@ -28,7 +28,7 @@ export const route: Route = {
 };
 
 async function handler(ctx) {
-    const item = await processItems(ctx, baseURL, [/第*集/, /第*季/, /(EP|ep)\d+/, /([Ss])\d+/, /更新/]);
+    const item = await processItems(ctx, baseURL, [/第*集/, /第*季/, /(ep)\d+/i, /(s)\d+/i, /更新/]);
 
     return {
         title: '6v电影-最新电影',
