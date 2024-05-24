adf0338bf2c5675607801ceda2c9d2fa42288513
Kieran Wynne <keewynne90@gmail.com>
2024-05-24 21:03:34 +0100

    feat: implements new route for academia.edu

---
+++ lib/routes/academiaedu/topics.ts
@@ -0,0 +1,43 @@
+import { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+import { load } from 'cheerio';
+
+export const route: Route = {
+    path: '/:interest',
+    radar: [
+        {
+            source: ['academia.edu/Documents/in/:interest'],
+            target: '/:interest',
+        },
+    ],
+    name: 'interest',
+    maintainers: ['k33k0'],
+    handler,
+    url: 'academia.edu',
+};
+
+async function handler(ctx) {
+    let items = null
+    const interest = ctx.req.param('interest');
+    const response = await ofetch(`https://www.academia.edu/Documents/in/${interest}/MostRecent`);
+    const $ = load(response);
+    const list = $('.works > .u-borderBottom1')
+        .toArray()
+        .map((item) => {
+            return {
+                title: $(item).find('.title > a').first().text(),
+                link: $(item).find('.title > a').first().attr('href'),
+                // pubDate
+                author:$(item).find('span[itemprop=author] > a').text(),
+                description: $(item).find('.summarized').text()
+            }
+        })
+        return {
+            // channel title
+            title: `academia.edu | ${interest} documents`,
+            // channel link
+            link: `https://academia.edu/Documents/in/${interest}/MostRecent`,
+            // each feed item
+            item: list,
+        };
+}
