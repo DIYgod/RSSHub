efae802645eeba3aba5bc20e8ee01a1f40cbea25
HPDell <huyg0180110559@outlook.com>
2024-05-25 13:09:44 +0100

    fix: image links

--- lib/routes/typst/universe.ts
+++ lib/routes/typst/universe.ts
@@ -3,6 +3,7 @@ import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
 import { load } from 'cheerio';
 import markdownit from 'markdown-it';
+import { Token, Options, Renderer } from 'markdown-it/index.js';
 import vm from 'node:vm';
 
 interface Package {
@@ -24,6 +25,48 @@ interface Package {
 
 interface Context {
     an: { exports: Array<Package> };
+}
+
+const GITHUBRAW_BASE = 'https://raw.githubusercontent.com';
+const PKG_GITHUB_BASE = `${GITHUBRAW_BASE}/typst/packages/main/packages/preview`;
+
+function fixImageSrc(src, env) {
+    if (src.indexOf('://') > 0) {
+        if (src.startsWith('https://typst.app/universe/package')) {
+            src = src.replaceAll('https://typst.app/universe/package', `${PKG_GITHUB_BASE}/${env.name}/${env.version}`);
+        } else if (src.startsWith('https://github.com')) {
+            src = src.replace('https://github.com', GITHUBRAW_BASE);
+        }
+    } else {
+        const suffix = src.indexOf('/') === 0 ? '' : '/';
+        const package_base = `${PKG_GITHUB_BASE}/${env.name}/${env.version}${suffix}`;
+        const url = new URL(src, package_base);
+        src = url.toString();
+    }
+    return src;
+}
+
+function rendererImage(tokens: Token[], idx: number, options: Options, env: Package, self: Renderer): string {
+    const token = tokens[idx];
+    if (token.attrs === null) {
+        return '';
+    }
+    const src = token.attrGet('src');
+    if (src === null) {
+        return '';
+    }
+    token.attrs[token.attrIndex('src')][1] = fixImageSrc(src, env);
+    return self.renderToken(tokens, idx, options);
+}
+
+function rendererHtmlImage(tokens: Token[], idx: number, _: Options, env: Package): string {
+    const token = tokens[idx];
+    const $ = load(token.content, {}, false);
+    $('img').each((i, el) => {
+        const src = el.attribs.src;
+        el.attribs.src = fixImageSrc(src, env);
+    });
+    return $.html();
 }
 
 export const route: Route = {
@@ -58,6 +101,9 @@ export const route: Route = {
                 displayErrors: true,
             });
             const md = markdownit('commonmark');
+            md.renderer.rules.image = rendererImage;
+            md.renderer.rules.html_inline = rendererHtmlImage;
+            md.renderer.rules.html_block = rendererHtmlImage;
             const items = context.an.exports.sort((a, b) => a.updatedAt - b.updatedAt);
             const groups = new Map(items.map((it) => [it.name, it]));
             const pkgs = [...groups.values()].map((item) => ({
