cb468a4b6709c28d7840d2974396916b70069fdb
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2024-05-25 20:45:30 +0800

    fix(core): restore LISTEN_INADDR_ANY functionality
    Somehow the functionality is lost during Hono migration [1]. Let's add
    it back.
    
    [1] https://github.com/DIYgod/RSSHub/commit/8e0154552ce3a5c2f9b88fa45447eb809d5ec252

--- lib/index.ts
+++ lib/index.ts
@@ -5,17 +5,21 @@ import { config } from '@/config';
 import app from '@/app';
 
 const port = config.connect.port;
+const listenInaddrAny = Number.parseInt(config.listenInaddrAny);
 const hostIPList = getLocalhostAddress();
 
 logger.info(`🎉 RSSHub is running on port ${port}! Cheers!`);
 logger.info('💖 Can you help keep this open source project alive? Please sponsor 👉 https://docs.rsshub.app/sponsor');
 logger.info(`🔗 Local: 👉 http://localhost:${port}`);
-for (const ip of hostIPList) {
-    logger.info(`🔗 Network: 👉 http://${ip}:${port}`);
+if (listenInaddrAny) {
+    for (const ip of hostIPList) {
+        logger.info(`🔗 Network: 👉 http://${ip}:${port}`);
+    }
 }
 
 const server = serve({
     fetch: app.fetch,
+    hostname: listenInaddrAny ? null : '127.0.0.1',
     port,
 });
 
