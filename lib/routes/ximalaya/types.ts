41159c6568d5c71ab6e5da21d3087867121a136b
Tony <TonyRL@users.noreply.github.com>
2024-05-26 15:02:39 +0000

    chore: add types

---
+++ lib/routes/ximalaya/types.ts
@@ -0,0 +1,62 @@
+interface TrackData {
+    trackId: number;
+    trackRecordId: number;
+    uid: number;
+    playUrl64: string;
+    playUrl32: string;
+    playPathAacv164: string;
+    playPathAacv224: string;
+    playPathHq: string;
+    title: string;
+    duration: number;
+    albumId: number;
+    albumTitle: string;
+    isPaid: boolean;
+    isFree: boolean;
+    isVideo: boolean;
+    isDraft: boolean;
+    isAuthorized: boolean;
+    priceTypeId: number;
+    sampleDuration: number;
+    priceTypeEnum: number;
+    type: number;
+    relatedId: number;
+    orderNo: number;
+    isHoldCopyright: boolean;
+    vipFirstStatus: number;
+    paidType: number;
+    isSample: boolean;
+    ximiFirstStatus: number;
+    coverLarge: string;
+    permissionSource: string;
+    playtimes: number;
+    labelType: number;
+    processState: number;
+    createdAt: number;
+    coverSmall: string;
+    coverMiddle: string;
+    nickname: string;
+    smallLogo: string;
+    userSource: number;
+    opType: number;
+    isPublic: boolean;
+    likes: number;
+    comments: number;
+    shares: number;
+    status: number;
+    videoCover: string;
+    intro: string;
+    labelList: string[];
+    isTrailer: number;
+}
+
+export interface TrackInfoResponse {
+    msg: string;
+    ret: number;
+    data: TrackData[];
+}
+
+export interface RichIntro {
+    ret: number;
+    richIntro: string;
+}
