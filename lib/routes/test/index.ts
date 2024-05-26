61e0ae13d95292059d441ecd3e38f1e6193176b3
DIYgod <i@diygod.me>
2024-05-26 17:22:13 +0800

    Revert "feat: split test routes"
    This reverts commit c265a0616afc52821a2b9be62853dff84437c00e.

--- lib/routes/test/index.ts
+++ lib/routes/test/index.ts
@@ -1,8 +1,10 @@
 import { Route, DataItem } from '@/types';
 import { config } from '@/config';
+import got from '@/utils/got';
 import wait from '@/utils/wait';
 import cache from '@/utils/cache';
 import { fetchArticle } from '@/utils/wechat-mp';
+import ConfigNotFoundError from '@/errors/types/config-not-found';
 import InvalidParameterError from '@/errors/types/invalid-parameter';
 
 let cacheIndex = 0;
@@ -15,6 +17,21 @@ export const route: Route = {
 };
 
 async function handler(ctx) {
+    if (ctx.req.param('id') === 'error') {
+        throw new Error('Error test');
+    }
+    if (ctx.req.param('id') === 'httperror') {
+        await got({
+            method: 'get',
+            url: 'https://httpbingo.org/status/404',
+        });
+    }
+    if (ctx.req.param('id') === 'config-not-found-error') {
+        throw new ConfigNotFoundError('Test config not found error');
+    }
+    if (ctx.req.param('id') === 'invalid-parameter-error') {
+        throw new InvalidParameterError('Test invalid parameter error');
+    }
     let item: DataItem[] = [];
     switch (ctx.req.param('id')) {
         case 'filter':
