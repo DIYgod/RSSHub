847184d0bba00b90c48cc6184d3935eecb49c480
LLLLLFish <l13457497196@gmail.com>
2024-06-09 18:58:10 +0800

    update3

--- lib/routes/bing/daily-wallpaper.ts
+++ lib/routes/bing/daily-wallpaper.ts
@@ -6,7 +6,10 @@ import timezone from '@/utils/timezone';
 
 export const route: Route = {
     path: '/:routeParams?',
-    parameters: { type: '类型', story: '故事' },
+    parameters: {
+        type: '返回的壁纸格式,取值为: UHD/1920x1080/1920x1200/768x1366/1080x1920/1080x1920_logo;默认值与输入的值不在范围内时设为1920x1080;1920x1200与1080x1920_logo会带有水印',
+        story: '是否开启壁纸对应故事, 取值为: 1/0;默认值为0即不开启故事输出',
+    },
     radar: [
         {
             source: ['cn.bing.com/'],
@@ -18,10 +21,6 @@ export const route: Route = {
     handler,
     url: 'cn.bing.com/',
     example: '/bing/type=UHD&story=1',
-    description: `
-    type 壁纸的类型 取值为: UHD/1920x1080/1920x1200/768x1366/1080x1920/1080x1920_logo;默认值与输入的值不在范围内时设为1920x1080;1920x1200与1080x1920_logo会带有水印；
-    story 壁纸对应的故事 取值为 1/0;默认值为0,即不输出壁纸对应的故事;
-    `,
 };
 
 async function handler(ctx) {
@@ -34,20 +33,20 @@ async function handler(ctx) {
         type = '1920x1080';
     }
     const story = routeParams.get('story') === '1';
-    const api_url = `https://www.bing.com/hp/api/model`;
+    const api_url = `https://cn.bing.com/hp/api/model`;
     const resp = await got({
         method: 'get',
         url: api_url,
     });
     return {
         title: 'Bing每日壁纸',
-        link: 'https://www.bing.com/',
+        link: 'https://cn.bing.com/',
         description: 'Bing每日壁纸',
         item: resp.data.MediaContents.map((item) => {
             const ssd = item.Ssd;
             const key = `bing_${ssd}_${lang}_${type}_${story}`;
             return cache.tryGet(key, () => {
-                const link = `https://www.bing.com${item.ImageContent.Image.Url.match(/\/th\?id=[^_]+_[^_]+/)[0].replace(/(_\d+x\d+\.webp)$/i, '')}_${type}.jpg`;
+                const link = `https://cn.bing.com${item.ImageContent.Image.Url.match(/\/th\?id=[^_]+_[^_]+/)[0].replace(/(_\d+x\d+\.webp)$/i, '')}_${type}.jpg`;
                 let description = `<img src="${link}" alt="Article Cover Image" style="display: block; margin: 0 auto;"><br>`;
                 if (story) {
                     description += `<b>${item.ImageContent.Headline}</b><br><br>`;
@@ -57,7 +56,7 @@ async function handler(ctx) {
                 return {
                     title: item.ImageContent.Title,
                     description,
-                    link: `https://www.bing.com${item.ImageContent.BackstageUrl}`,
+                    link: `https://cn.bing.com${item.ImageContent.BackstageUrl}`,
                     author: String(item.ImageContent.Copyright),
                     pubDate: timezone(parseDate(ssd, 'YYYYMMDD_HHmm'), -8),
                 };
