18a3d10afacef0691908182c6461cdee7914cb39
Benjamin <web-service@zhangyanze.com>
2024-06-12 04:27:05 +0800

    增添The Banker

---
+++ lib/routes/thebanker/index.ts
@@ -0,0 +1,53 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import { load } from 'cheerio';
+import got from '@/utils/got';
+
+export const route: Route = {
+    path: '/news',
+    categories: ['finance'],
+    example: '/thebanker/news',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: 'The Banker - News in Brief',
+    maintainers: ['username'],
+    handler,
+    description: 'RSS feed for The Banker website news briefs.',
+};
+
+async function handler(ctx) {
+    const url = 'https://www.thebanker.com/News-in-Brief';
+    const response = await got(url);
+    const $ = load(response.data);
+    const selectors = ['div#layout-content-wrapper > div > div:nth-of-type(2) > div > div > div > div > div:nth-of-type(2)'];
+    const list = [];
+
+    for (const selector of selectors) {
+        $(selector).each((_, e) => {
+            const title = $(e).find('div:nth-child(2) > a[data-cy="card-title-link"] > h3').text();
+            const link = $(e).find('div:nth-child(2) > a[data-cy="card-title-link"]').attr('href');
+            const description = $(e).find('div:nth-child(2) > a:nth-child(2) > p').text();
+
+            if (link) {
+                list.push({ title, link, description });
+            }
+        });
+    }
+
+    const result = await Promise.all(
+        list.map(item => cache.tryGet(item.link, async () => item))
+    );
+
+    return {
+        title: 'The Banker - News in Brief',
+        link: 'https://www.thebanker.com/News-in-Brief',
+        item: result,
+    };
+}
