4a24dad0a0bd64bf26a8f04b0583f5b918d039fd
guicaiyue <2941382431@qq.com>
2024-06-12 16:47:55 +0000

    update:支持知闻AI资讯

---
+++ lib/routes/informedainews/namespace.ts
@@ -0,0 +1,18 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Informed AI News',
+    url: 'informedainews.com',
+    description: `
+:::tip
+informed AI RSS feeds:
+
+-   World News Daily: \`https://informedainews.com/docs/world-news-daily\`
+-   Tech Enthusiast Weekly: \`https://informedainews.com/docs/tech-enthusiast-daily\`
+-   AI Enthusiast Weekly: \`https://informedainews.com/docs/ai-enthusiast-daily\`
+:::`,
+
+    zh: {
+        name: '知闻AI',
+    },
+};
