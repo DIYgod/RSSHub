044c62526ccd31f048ed37317f7be4615529da06
chaizhijun <353325487@qq.com>
2024-06-13 15:49:26 +0800

    feat(route): 新增抖店开放平台

---
+++ lib/routes/jinritemai/docs.ts
@@ -0,0 +1,76 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import got from '@/utils/got';
+import dayjs from 'dayjs';
+import timezone from '@/utils/timezone';
+
+const typeMap = {
+    '5': '全部公告',
+    '19': '产品发布',
+    '21': '规则变更',
+    '20': '维护公告',
+    '22': '其他公告',
+};
+
+/**
+ *
+ * @param ctx {import('koa').Context}
+ */
+export const route: Route = {
+    path: '/docs/:dirId?',
+    categories: ['programming'],
+    example: '/jinritemai/docs/19',
+    parameters: { categoryId: '公告分类, 可在页面URL获取 默认为全部' },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '平台公告',
+    maintainers: ['zhijunchai'],
+    handler,
+    description: `| 类型    | type    |
+  | --------- | ---------- |
+  | 全部公告    | 5    |
+  | 产品发布    | 19   |
+  | 规则变更    | 21   |
+  | 维护公告    | 20   |
+  | 其他公告    | 22   |`,
+};
+
+async function handler(ctx) {
+    const dirId = ctx.req.param('dirId') ? ctx.req.param('dirId') : '5';
+    const limit = ctx.req.param('limit') ? Number.parseInt(ctx.req.param('limit')) : 10;
+    const url = `https://op.jinritemai.com/doc/external/open/queryDocArticleList?pageIndex=0&pageSize=${limit}&status=1&dirId=${dirId}&orderType=3`;
+    const response = await got({ method: 'get', url });
+
+    const list = response.data.data.articles.map((item) => ({
+        title: item.title,
+        id: item.id,
+        dirName: item.dirName,
+        link: `https://op.jinritemai.com/docs/notice-docs/${dirId}/${item.id}`,
+        pubDate: timezone(dayjs.unix(item.createTime).format('YYYY-MM-DD HH:mm:ss'), +8),
+    }));
+
+    const result = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link, async () => {
+                const itemResponse = await got({
+                    method: 'get',
+                    url: `https://op.jinritemai.com/doc/external/open/queryDocArticleDetail?articleId=${item.id}&onlyView=false`,
+                });
+                item.description = itemResponse.data.data.article.content;
+                return item;
+            })
+        )
+    );
+
+    return {
+        title: `抖店开放平台 - ${typeMap[dirId] ?? '平台公告'}`,
+        link: `https://op.jinritemai.com/docs/notice-docs/${dirId}`,
+        item: result,
+    };
+}
