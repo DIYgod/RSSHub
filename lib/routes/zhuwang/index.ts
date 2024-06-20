3831dca52d3fe2332c6c1c9c99cb46d6761e56ba
ueiu <bautuoyoul@gmail.com>
2024-06-21 01:49:57 +0800

    fix(route): 「全国今日生猪价格」 接口地址变更

--- lib/routes/zhuwang/index.ts
+++ lib/routes/zhuwang/index.ts
@@ -26,7 +26,7 @@ export const route: Route = {
 };
 
 async function handler() {
-    const baseUrl = 'https://zhujia.zhuwang.cc/';
+    const baseUrl = 'https://zhujia.zhuwang.com.cn/';
     const now = new Date();
     const date = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
     const response = await got(`${baseUrl}/api/chartData`, {
@@ -65,7 +65,7 @@ async function handler() {
     return {
         title: `全国今日生猪价格`,
         desription: '中国养猪网猪价频道是中国猪价权威平台,提供每日猪评,猪价和行情分析,并且预测猪价和分析每天的猪价排行。',
-        link: baseUrl,
+        link: 'https://www.zhuwang.com.cn/',
         item: priceItems,
     };
 }
