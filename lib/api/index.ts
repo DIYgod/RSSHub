0ba17e15bdc810906141d5cad391239e855aacc4
DIYgod <i@diygod.me>
2024-06-20 17:42:05 +0800

    feat: category one api

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -3,6 +3,7 @@ import { route as namespaceAllRoute, handler as namespaceAllHandler } from '@/ap
 import { route as namespaceOneRoute, handler as namespaceOneHandler } from '@/api/namespace/one';
 import { route as radarRulesAllRoute, handler as radarRulesAllHandler } from '@/api/radar/rules/all';
 import { route as radarRulesOneRoute, handler as radarRulesOneHandler } from '@/api/radar/rules/one';
+import { route as categoryOneRoute, handler as categoryOneHandler } from '@/api/category/one';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { apiReference } from '@scalar/hono-api-reference';
 
@@ -12,6 +13,7 @@ app.openapi(namespaceAllRoute, namespaceAllHandler);
 app.openapi(namespaceOneRoute, namespaceOneHandler);
 app.openapi(radarRulesAllRoute, radarRulesAllHandler);
 app.openapi(radarRulesOneRoute, radarRulesOneHandler);
+app.openapi(categoryOneRoute, categoryOneHandler);
 
 const docs = app.getOpenAPI31Document({
     openapi: '3.1.0',
