e7a4653a5daa18a6d9a76b0f4ae14e8db439af32
Hossein Margani <hossein@margani.dev>
2024-06-23 19:25:09 +0100

    Refactor the code
    And fix the issues with the return of got

--- lib/routes/mcr.microsoft/mcr.ts
+++ lib/routes/mcr.microsoft/mcr.ts
@@ -26,27 +26,38 @@ export const route: Route = {
 
 async function handler(ctx) {
     const product = ctx.req.path.replace('/mcr.microsoft/product/', '');
-    const details = await got({
+    const { data: details } = await got({
         method: 'get',
         url: `https://mcr.microsoft.com/api/v1/catalog/${product}/details`,
     });
-    const tags = await got({
+    const { data: tags } = await got({
         method: 'get',
         url: `https://mcr.microsoft.com/api/v1/catalog/${product}/tags`,
     });
 
     return {
         title: `${details.name} - Microsoft Artifact Registry`,
-        description: `${details.shortDescription}\n\n ${details.readme}`,
+        description: String(details.shortDescription),
         image: `https://mcr.microsoft.com${details.imagePath}`,
         link: `https://mcr.microsoft.com/en-us/product/${product}`,
-        item: tags.map((tag: any) => ({
-            title: `${details.name} - ${tag.name}`,
-            author: details.publisher,
-            description: `Digest: \`${tag.digest}\`\n\nLast modified date: ${new Date(tag.lastModifiedDate).toDateString()}\n\n${tag.architecture ? 'Architecture: ' + tag.architecture + '\n\n' : ''}${tag.operatingSystem ? 'Operating system: ' + tag.operatingSystem + '\n\n' : ''}`,
-            pubDate: new Date(tag.lastModifiedDate),
-            guid: `mcr::${product}::${tag.name}::${tag.digest}`,
-            link: `https://mcr.microsoft.com/en-us/product/${product}/tags/${tag.name}/${tag.digest}`,
-        })),
+        item: tags.map((tag: any) => {
+            const descriptionItems = [`Digest: \`${tag.digest}\``, `Last modified date: ${new Date(tag.lastModifiedDate).toDateString()}`];
+
+            if (tag.architecture) {
+                descriptionItems.push(`Architecture: ${tag.architecture}`);
+            }
+            if (tag.operatingSystem) {
+                descriptionItems.push(`Operating system: ${tag.operatingSystem}`);
+            }
+
+            return {
+                title: `${details.name} - ${tag.name}`,
+                author: details.publisher,
+                description: descriptionItems.join(', '),
+                pubDate: new Date(tag.lastModifiedDate),
+                guid: `mcr::${product}::${tag.name}::${tag.digest}`,
+                link: `https://mcr.microsoft.com/en-us/product/${product}/tags/${tag.name}/${tag.digest}`,
+            };
+        }),
     };
 }
