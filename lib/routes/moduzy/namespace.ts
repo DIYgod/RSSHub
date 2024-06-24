c423a7cba90c2a35f03c5b8b20436d62f8043557
Hualiang <78242797+hualiong@users.noreply.github.com>
2024-06-25 03:29:06 +0800

    docs(route): 纠正文档错误 (#15983)
    * docs(route): 纠正文档错误
    
    * fix

--- lib/routes/moduzy/namespace.ts
+++ lib/routes/moduzy/namespace.ts
@@ -5,7 +5,7 @@ export const namespace: Namespace = {
     url: 'www.moduzy.com',
     description: `
 :::tip
-哔嘀影视有多个备用域名，路由默认使用域名 \`moduzy.net\` 确保在中国大陆内的实例也能访问。如果你想直接访问源站可以通过以下域名：
+魔都资源网有多个备用域名，路由默认使用域名 \`moduzy.net\` 确保在中国大陆内的实例也能访问。如果你想直接访问源站可以通过以下域名：
 
 - moduzy.com（主域名，大陆内无法访问）
 - moduzy.cc
