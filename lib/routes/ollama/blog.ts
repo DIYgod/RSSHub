3a10980a2c8c469d06d557866ddc158d5bd10a65
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-06-26 11:59:52 -0700

    remove fullText

--- lib/routes/ollama/blog.ts
+++ lib/routes/ollama/blog.ts
@@ -2,7 +2,6 @@ import { Route } from '@/types';
 import ofetch from '@/utils/ofetch';
 import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
-import cache from '@/utils/cache';
 
 export const route: Route = {
     path: '/blog',
@@ -24,31 +23,14 @@ async function handler() {
     const response = await ofetch(`${baseUrl}/blog`);
     const $ = load(response);
 
-    const list = $('a.group.border-b.py-10')
+    const items = $('a.group.border-b.py-10')
         .toArray()
-        .map((item) => {
-            const $item = $(item);
-            return {
-                title: $item.children('h2').first().text(),
-                link: baseUrl + $item.attr('href'),
-                pubDate: parseDate($item.children('h3').first().text()),
-                description: $item.children('p').first().text(),
-            };
-        });
-
-    const items = await Promise.all(
-        list.map((item) =>
-            cache.tryGet(item.link, async () => {
-                const response = await ofetch(item.link);
-                const $ = load(response);
-
-                // Extract the full text
-                const fullText = $('main').text();
-                return { ...item, fullText };
-            })
-        )
-    );
-
+        .map((item) => ({
+                title: $(item).children('h2').first().text(),
+                link: baseUrl + $(item).attr('href'),
+                pubDate: parseDate($(item).children('h3').first().text()),
+                description: $(item).children('p').first().text(),
+            }));
     return {
         title: 'ollama blog',
         link: 'https://ollama.com/blog',
