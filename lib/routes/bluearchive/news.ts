bcc5b49543a7863e70e13d2b1f50a351905fad6e
equt <17521736+equt@users.noreply.github.com>
2024-06-27 22:09:23 +0800

    fix: correct fetch typing
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/bluearchive/news.ts
+++ lib/routes/bluearchive/news.ts
@@ -41,7 +41,7 @@ const handler: Route['handler'] = async (ctx) => {
 const ja: Route['handler'] = async (ctx) => {
     const { type = '0' } = ctx.req.param();
 
-    const data = await ofetch<{ data: { rows: { id: string; content: string; summary: string; publishTime: number }[] } }, 'json'>('https://api-web.bluearchive.jp/api/news/list', {
+    const data = await ofetch<{ data: { rows: { id: number; content: string; summary: string; publishTime: number }[] } }, 'json'>('https://api-web.bluearchive.jp/api/news/list', {
         params: {
             typeId: type,
             pageNum: 16,
