69c3ed2ddba6b9184bd40cb589902d424fb85b34
equt <17521736+equt@users.noreply.github.com>
2024-07-01 21:37:56 +0800

    fix: remove unnecessary cache according to #16038

--- lib/routes/nextjs/blog.ts
+++ lib/routes/nextjs/blog.ts
@@ -1,38 +1,35 @@
 import type { DataItem, Route } from '@/types';
-import cache from '@/utils/cache';
 import { parseDate } from '@/utils/parse-date';
 import { load } from 'cheerio';
 import ofetch from '@/utils/ofetch';
 
 const handler: Route['handler'] = async () => {
-    const item = (await cache.tryGet('nextjs:blog', async () => {
-        const data = await ofetch('https://nextjs.org/blog');
+    const data = await ofetch('https://nextjs.org/blog');
 
-        const $ = load(data);
+    const $ = load(data);
 
-        return await Promise.all(
-            $('article')
-                .toArray()
-                .slice(0, 20)
-                .map<Promise<DataItem>>(async (item) => {
-                    const $ = load(item);
+    const item = await Promise.all(
+        $('article')
+            .toArray()
+            .slice(0, 20)
+            .map<Promise<DataItem>>(async (item) => {
+                const $ = load(item);
 
-                    const h = $('a[href^="/blog"]');
-                    const title = h.text().trim();
-                    const link = `https://nextjs.org${h.attr('href')}`;
-                    const date = $('p').first().text(); // not reliable, but works for now
+                const h = $('a[href^="/blog"]');
+                const title = h.text().trim();
+                const link = `https://nextjs.org${h.attr('href')}`;
+                const date = $('p').first().text(); // not reliable, but works for now
 
-                    const data = await ofetch(link);
+                const data = await ofetch(link);
 
-                    return {
-                        title,
-                        link,
-                        description: load(data)('div.prose').html() ?? '',
-                        pubDate: parseDate(date.replace(/st|nd|rd|th/, '')),
-                    };
-                })
-        );
-    })) as DataItem[];
+                return {
+                    title,
+                    link,
+                    description: load(data)('div.prose').html() ?? '',
+                    pubDate: parseDate(date.replace(/st|nd|rd|th/, '')),
+                };
+            })
+    );
 
     return {
         title: 'Next.js Blog',
