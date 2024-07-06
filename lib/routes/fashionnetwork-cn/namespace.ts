6fcf712f457f8da9169ffb008a3e352843264412
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-07-06 23:36:01 +0800

    feat(route): add FashionNetwork中国新闻

---
+++ lib/routes/fashionnetwork-cn/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'FashionNetwork 中国',
+    url: 'fashionnetwork.cn',
+    categories: ['new-media'],
+    description: '',
+};
