e2bcb18d86660a2d8ee224c29c0bc8cffd2b0f19
WilliamGates <3852641+williamgateszhao@users.noreply.github.com>
2024-07-08 10:12:05 +0800

    new route /iplaysoft/index

---
+++ lib/routes/iplaysoft/index.ts
@@ -0,0 +1,69 @@
+import cache from '@/utils/cache';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+import { DataItem, Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+import parser from '@/utils/rss-parser';
+
+export const route: Route = {
+    path: '/index',
+    categories: ['new-media'],
+    example: '/iplaysoft/index',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['www.iplaysoft.com/'],
+        },
+    ],
+    name: '全部文章',
+    maintainers: ['williamgateszhao'],
+    handler,
+};
+
+async function handler(ctx) {
+    const feed = await parser.parseURL(`https://www.iplaysoft.com/feed/atom`);
+    const filteredItems = feed.items.filter((item) => item?.link && item?.pubDate && new URL(item.link).hostname.match(/.*\.iplaysoft\.com$/)) as DataItem[];
+    const items: DataItem[] = await Promise.all(
+        filteredItems.map(
+            (item) =>
+                cache.tryGet(item.link as string, async () => {
+                    const response = await ofetch(item.link as string);
+                    const $ = load(response);
+                    const content = $('.entry-content');
+                    content
+                        .find('div')
+                        .filter((_index, element) => {
+                            const style = $(element).attr('style');
+                            return style !== undefined && style.includes('overflow:hidden');
+                        })
+                        .remove();
+                    return {
+                        title: item.title,
+                        description: content.html(),
+                        link: item.link,
+                        author: item.author,
+                        pubDate: parseDate(item.pubDate as string),
+                    } as DataItem;
+                }) as Promise<DataItem>
+        )
+    );
+
+    ctx.set('json', {
+        items,
+    });
+
+    return {
+        title: '异次元软件世界',
+        link: 'www.iplaysoft.com',
+        language: 'zh-CN',
+        item: items,
+    };
+}
