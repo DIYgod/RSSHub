99847616016c6ddd8a44d55e24b666026bc35b4d
kuhahku <kuhahku@outlook.com>
2024-07-10 06:50:50 +0800

    feat(route): add j-test.com route

---
+++ lib/routes/j-test/news.ts
@@ -0,0 +1,67 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+import timezone from '@/utils/timezone';
+
+export const route: Route = {
+    path: '/news',
+    name: '公告',
+    url: 'www.j-test.com',
+    maintainers: ['kuhahku'],
+    example: '/j-test/news',
+    parameters: {},
+    categories: ['study'],
+    features: {
+        supportRadar: true,
+    },
+    radar: [
+        {
+            source: ['www.j-test.com'],
+            target: '/news',
+        },
+    ],
+    handler,
+    description: '',
+};
+
+async function handler() {
+    const base = 'http://www.j-test.com';
+    const response = await ofetch(base);
+    const $ = load(response);
+
+    const list = $('#content1 > .center > .col_box1 > .col_body1 > ul > li')
+        .get()
+        .map((item) => {
+            const [title, time] = $(item).text().toString().trim().replaceAll(']', '').split(' [');
+            const link = `${base}${$(item).children('a').attr('href')}`;
+            const pubDate = timezone(parseDate(time), +8);
+            return {
+                title,
+                link,
+                pubDate,
+                description: '',
+            };
+        });
+
+    const items = await Promise.all(
+        list.map(async (item) => {
+            const res = await cache.get(item.link);
+            if (res) {
+                return JSON.parse(res);
+            }
+            const response = await ofetch(item.link);
+            const $ = load(response);
+            item.description = $('.content > table').html() ?? '';
+            cache.set(item.link, JSON.stringify(item));
+            return item;
+        })
+    );
+
+    return {
+        title: '实用日本语鉴定考试 (J-Test) 公告',
+        link: 'http://www.j-test.com',
+        item: items,
+    };
+}
