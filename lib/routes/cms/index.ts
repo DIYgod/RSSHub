8934e2cfcba53bcd0296ff38c00b9ab0258f1e7a
周华梁 <642615676@qq.com>
2024-07-09 23:11:37 +0800

    限制可选域名

--- lib/routes/cms/index.ts
+++ lib/routes/cms/index.ts
@@ -22,7 +22,7 @@ export const route: Route = {
         supportScihub: false,
     },
     parameters: {
-        domain: '采集站域名，通常采集站点会提供CMS的视频采集接口',
+        domain: '采集站域名，可选值如下表',
         type: '类别ID，不同采集站点有不同的类别规则和ID，默认为 0，代表全部类别',
         size: '每次获取的数据条数，上限 100 条，默认 30 条',
     },
@@ -31,9 +31,19 @@ export const route: Route = {
     description: `
 :::tip
 每个采集站提供的影视类别ID是不同的，即参数中的 \`type\` 是不同的。**可以先访问一次站点提供的采集接口，然后从返回结果中的 \`class\` 字段中的 \`type_id\`获取相应的类别ID**
-:::`,
+:::
+
+| 站名 | 域名 | 站名 | 域名 | 站名 | 域名 |
+| --- | --- | --- | --- | --- | --- |
+| 魔都资源网 | moduzy.net | 华为吧影视资源站 | hw8.live | 360资源站 | 360zy.com |
+| jkun爱坤联盟资源网 | ikunzyapi.com | 奥斯卡资源站 | aosikazy.com | 飞速资源采集网 | www.feisuzyapi.com |
+| 森林资源网 | slapibf.com | 天空资源采集网 | api.tiankongapi.com | 百度云资源 | api.apibdzy.com |
+| 红牛资源站 | www.hongniuzy2.com | 乐视资源网 | leshiapi.com | 暴风资源 | bfzyapi.com |`,
     handler: async (ctx) => {
         const { domain, type = '0', size = '30' } = ctx.req.param();
+        if (!list.has(domain)) {
+            throw new Error('非法域名！');
+        }
 
         const res = await ofetch<Result>(`https://${domain}/api.php/provide/vod`, {
             parseResponse: JSON.parse,
@@ -58,3 +68,5 @@ export const route: Route = {
         };
     },
 };
+
+const list = new Set(['moduzy.net', 'hw8.live', '360zy.com', 'ikunzyapi.com', 'aosikazy.com', 'www.feisuzyapi.com', 'slapibf.com', 'api.tiankongapi.com', 'api.apibdzy.com', 'www.hongniuzy2.com', 'leshiapi.com', 'bfzyapi.com']);
