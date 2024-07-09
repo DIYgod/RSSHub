92076e8c90c6a1cae49cd594ba11f8d85bd8ec04
kuhahku <kuhahku@outlook.com>
2024-07-09 23:16:51 +0800

    feat(route/neea): update routes and improve description

--- lib/routes/neea/namespace.ts
+++ lib/routes/neea/namespace.ts
@@ -1,6 +1,6 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: 'NEEA 中国教育考试网',
-    url: 'jlpt.neea.cn',
+    name: '中国教育考试网',
+    url: 'www.neea.edu.cn',
 };
