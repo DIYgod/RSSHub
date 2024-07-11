84c202a2b7ed37a0c274baea9db9fd4b7d50bd1e
dzx-dzx <dzx_dzx@hotmail.com>
2024-07-11 11:14:27 +0800

    .

--- lib/routes/mirrormedia/category.ts
+++ lib/routes/mirrormedia/category.ts
@@ -5,22 +5,24 @@ import ofetch from '@/utils/ofetch';
 import { getArticle } from './utils';
 
 export const route: Route = {
-    path: '/category/:category',
+    path: ['/category/:category', '/section/:section'],
     categories: ['traditional-media'],
     example: '/mirrormedia/category/political',
-    parameters: { category: '分类名' },
+    parameters: { category: '分类名', section: '子板名' },
     name: '分类',
     maintainers: ['dzx-dzx'],
     radar: [
         {
-            source: ['mirrormedia.mg/category/:category'],
+            source: ['mirrormedia.mg/category/:category', 'mirrormedia.mg/section/:section'],
         },
     ],
     handler,
 };
 
 async function handler(ctx) {
-    const { category } = ctx.req.param('category');
+    const { category, section } = ctx.req.param();
+    const categoryFilter = category ? { categories: { some: { slug: { equals: category } } } } : {};
+    const sectionFilter = section ? { sections: { some: { slug: { equals: section } } } } : {};
     const rootUrl = 'https://www.mirrormedia.mg';
 
     const response = await ofetch('https://adam-weekly-api-server-prod-ufaummkd5q-de.a.run.app/content/graphql', {
@@ -32,7 +34,7 @@ async function handler(ctx) {
                 orderBy: { publishedDate: 'desc' },
                 filter: {
                     state: { equals: 'published' },
-                    categories: { some: { slug: { equals: category } } },
+                    ...categoryFilter, ...sectionFilter,
                 },
             },
             query: `fragment section on Section {\n  id\n  name\n  slug\n  state\n  __typename\n}\n\nfragment category on Category {\n  id\n  name\n  slug\n  state\n  __typename\n}\n\nfragment listingPost on Post {\n  id\n  slug\n  title\n  brief\n  publishedDate\n  state\n  sections(where: {state: {equals: "active"}}) {\n    ...section\n    __typename\n  }\n  categories(where: {state: {equals: "active"}}) {\n    ...category\n    __typename\n  }\n  isFeatured\n  __typename\n}\n\nquery ($take: Int, $skip: Int, $orderBy: [PostOrderByInput!]!, $filter: PostWhereInput!) {\n  postsCount(where: $filter)\n  posts(take: $take, skip: $skip, orderBy: $orderBy, where: $filter) {\n    ...listingPost\n    __typename\n  }\n}`,
