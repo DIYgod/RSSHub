2c04940a6fe39094b523a5b4bbc839b17bb44ad9
Andvari <31068367+dzx-dzx@users.noreply.github.com>
2024-07-14 23:12:03 +0800

    feat(route/mirrormedia): Add route for category. (#16138)
    * feat(route/mirrormedia): Add route for category.
    
    * Fix pubDate.
    
    * .
    
    * Update filter.ts
    
    * .
    
    * .
    
    * .

--- lib/routes/mirrormedia/index.ts
+++ lib/routes/mirrormedia/index.ts
@@ -1,9 +1,8 @@
 import { Route } from '@/types';
 
-import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
-import cache from '@/utils/cache';
 import ofetch from '@/utils/ofetch';
+import { getArticle } from './utils';
 
 export const route: Route = {
     path: '/',
@@ -28,25 +27,13 @@ async function handler(ctx) {
     const items = [...response.choices.map((e) => ({ __from: 'choices', ...e })), ...response.latest.map((e) => ({ __from: 'latest', ...e }))]
         .map((e) => ({
             title: e.title,
-            pubDate: parseDate(e.publishDate),
+            pubDate: parseDate(e.publishedDate),
             category: [...(e.sections ?? []).map((_) => _.name), e.__from],
             link: `${rootUrl}/${e.style === '' ? 'external' : 'story'}/${e.slug}`,
         }))
         .slice(0, ctx.req.query('limit') ? Number.parseInt(ctx.req.query('limit'), 10) : 20);
 
-    const list = await Promise.all(
-        items.map((item) =>
-            cache.tryGet(item.link, async () => {
-                const detailResponse = await ofetch(item.link);
-
-                const content = load(detailResponse);
-
-                item.description = content("div[data-contents='true']").html();
-
-                return item;
-            })
-        )
-    );
+    const list = await Promise.all(items.map((item) => getArticle(item)));
 
     return {
         title: '鏡週刊 Mirror Media',
