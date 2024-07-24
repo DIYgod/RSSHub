aa70f25bc9f5946099c3cede210a4cba21bfb159
Jinkin <nadph513@outlook.com>
2024-07-24 09:44:02 +0800

    Update namespcae.ts

--- lib/routes/chlinlearn/namespcae.ts
+++ lib/routes/chlinlearn/namespcae.ts
@@ -1,6 +1,6 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: '值得一读技术博客',
+    name: 'chlinlearn的技术博客',
     url: 'https://daily-blog.chlinlearn.top/',
 };
