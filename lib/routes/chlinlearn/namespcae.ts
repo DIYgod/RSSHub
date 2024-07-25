63081ba7097b9892c4ef4d9050494f2e67eaaea1
Tony <TonyRL@users.noreply.github.com>
2024-07-25 17:51:53 +0800

    Update lib/routes/chlinlearn/namespcae.ts

--- lib/routes/chlinlearn/namespcae.ts
+++ lib/routes/chlinlearn/namespcae.ts
@@ -2,5 +2,5 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: 'chlinlearn的技术博客',
-    url: 'https://daily-blog.chlinlearn.top/',
+    url: 'daily-blog.chlinlearn.top',
 };
