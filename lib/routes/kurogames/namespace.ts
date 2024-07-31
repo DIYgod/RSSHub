ccd5d6d0d4c60739ddf0941269b3d55cd8f60ff2
enpitsulin <lldslm@vip.qq.com>
2024-07-31 22:49:09 +0800

    fix: namespace url
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/kurogames/namespace.ts
+++ lib/routes/kurogames/namespace.ts
@@ -2,6 +2,6 @@ import { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: '库洛游戏',
-    url: 'https://www.kurogames.com/',
+    url: ''www.kurogames.com',
     categories: ['game'],
 };
