4c7ac817a3e13e7eb12445d9074503c975a9b24c
xtex <xtexchooser@duck.com>
2024-08-04 16:21:25 +0800

    fix(fediverse/timeline): Resolve outbox items as JSON-LD references

--- lib/routes/fediverse/timeline.ts
+++ lib/routes/fediverse/timeline.ts
@@ -62,25 +62,34 @@ async function handler(ctx) {
 
     const items = firstOutbox.orderedItems;
 
+    const itemResolvers = [] as Promise<any>[];
+
+    for (const item of items) {
+        if (typeof item.object === 'string') {
+            itemResolvers.push(
+                (async (item: any) => {
+                    item.object = await ofetch(item.object, requestOptions);
+                    return item;
+                })(item)
+            );
+        } else {
+            itemResolvers.push(Promise.resolve(item));
+        }
+    }
+
+    const resolvedItems = await Promise.all(itemResolvers);
+
     return {
         title: `${self.name || self.preferredUsername} (Fediverse@${account})`,
         description: self.summary,
         image: self.icon?.url || self.image?.url,
         link,
-        item: items.map((item) => {
-            const object =
-                typeof item.object === 'string'
-                    ? {
-                          content: item.object,
-                      }
-                    : item.object;
-            return {
-                title: object.content,
-                description: `${object.content}\n${object.attachment?.map((attachment) => `<img src="${attachment.url}" width="${attachment.width}" height="${attachment.height}" />`).join('\n') || ''}`,
+        item: resolvedItems.map((item) => ({
+                title: item.object.content,
+                description: `${item.object.content}\n${item.object.attachment?.map((attachment) => `<img src="${attachment.url}" width="${attachment.width}" height="${attachment.height}" />`).join('\n') || ''}`,
                 link: item.url,
                 pubDate: parseDate(item.published),
                 guid: item.id,
-            };
-        }),
+            })),
     };
 }
