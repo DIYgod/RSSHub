28612f1b01aed5afe8f0b9aa84cf4ec7d4ca9de2
dzx-dzx <dzx_dzx@hotmail.com>
2024-08-15 21:21:47 +0800

    feat(route/infzm): Support hot articles list.

--- lib/routes/infzm/hot.ts
+++ lib/routes/infzm/hot.ts
@@ -4,24 +4,18 @@ import got from '@/utils/got';
 import { fetchArticles } from './utils';
 
 export const route: Route = {
-    path: '/:id',
-    parameters: { id: '南方周末频道 id, 可在该频道的 URL 中找到（即 https://www.infzm.com/contents?term_id=:id)' },
+    path: '/hot',
+    parameters: {},
     categories: ['traditional-media'],
-    example: '/infzm/1',
+    example: '/infzm/hot',
     radar: [
         {
-            source: ['infzm.com/contents'],
-            target: (_, url) => (url ? `/infzm/${url.match(/contents\?term_id=(\d*)/)?.[1]}` : ''),
+            source: ['infzm.com/'],
         },
     ],
-    name: '频道',
+    name: '热门文章',
     maintainers: ['KarasuShin', 'ranpox', 'xyqfer'],
     handler,
-    description: `下面给出部分参考：
-
-    | 推荐 | 新闻 | 观点 | 文化 | 人物 | 影像 | 专题 | 生活 | 视频 |
-    | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- |
-    | 1    | 2    | 3    | 4    | 7    | 8    | 6    | 5    | 131  |`,
 };
 
 export const baseUrl = 'https://www.infzm.com/contents';
@@ -31,16 +25,16 @@ async function handler(ctx): Promise<Data> {
     const link = `${baseUrl}?term_id=${id}`;
     const { data } = await got<ContentsResponse>({
         method: 'get',
-        url: `${baseUrl}?term_id=${id}&page=1&format=json`,
+        url: `https://www.infzm.com/hot_contents`,
         headers: {
             Referer: link,
         },
     });
 
-    const resultItem = await fetchArticles(data.data.contents);
+    const resultItem = await fetchArticles(data.data.hot_contents);
 
     return {
-        title: `南方周末-${data.data.current_term.title}`,
+        title: `南方周末-热门文章`,
         link,
         image: 'https://www.infzm.com/favicon.ico',
         item: resultItem as DataItem[],
