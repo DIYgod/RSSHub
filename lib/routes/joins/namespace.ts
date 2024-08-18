7447f446e0bf478723c11c177838d2e8bd0fa7ef
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2024-08-19 00:53:46 +0800

    feat(route): add 中央日报中文网 (#16474)

---
+++ lib/routes/joins/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '中央日报',
+    url: 'joins.com',
+    categories: ['traditional-media'],
+    description: '',
+};
