807b0d95ab6bbf07c05f4851bb62ff4cf619aad0
hinus <64959125+cxheng315@users.noreply.github.com>
2024-08-23 21:28:36 +0800

    Update lib/routes/galxe/index.ts
    remove Promise.all()
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/galxe/index.ts
+++ lib/routes/galxe/index.ts
@@ -77,14 +77,13 @@ async function handler(ctx) {
 
     const space = response.data.space;
 
-    const items = await Promise.all(
+    const items = 
         space.campaigns.list.map((campaign) => ({
             title: campaign.name,
             link: `https://app.galxe.com/quest/${alias}/${campaign.id}`,
             description: campaign.description,
             pubDate: campaign.startTime ? parseDate(campaign.startTime * 1000) : null,
-        }))
-    );
+        }));
 
     return {
         title: space.name,
