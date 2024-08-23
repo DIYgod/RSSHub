6d1a19a9bdd43cbebe2154ee454d987b62c904ff
DIYgod <i@diygod.me>
2024-08-23 16:08:35 +0800

    feat: puppeteer timeout

--- lib/utils/puppeteer.ts
+++ lib/utils/puppeteer.ts
@@ -54,9 +54,12 @@ const outPuppeteer = async (
                     }
                   : options
           ));
-    setTimeout(() => {
-        browser.close();
-    }, 30000);
+    setTimeout(
+        () => {
+            browser.close();
+        },
+        5 * 60 * 1000
+    );
 
     return browser;
 };
