115e26aaa173e84fc75e65d6d70832bc17fe6716
zihao <zihao626@gmail.com>
2024-08-25 23:08:28 +0800

    Update lib/routes/imhcg/namespace.ts
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/imhcg/namespace.ts
+++ lib/routes/imhcg/namespace.ts
@@ -3,5 +3,5 @@ import type { Namespace } from '@/types';
 export const namespace: Namespace = {
     name: 'imhcg的信息站',
     url: 'infos.imhcg.cn',
-    description: `包含多种技术和新闻信息的网站`,
+    description: '包含多种技术和新闻信息的网站',
 };
