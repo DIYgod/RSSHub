8792a4f93f9ba1a60e14fde4b80a893a25fa9a98
colin4k <colin4k@gmail.com>
2024-08-31 15:59:23 +0800

    fix some little bugs requested by reviewee.

--- lib/routes/bankless/article.ts
+++ lib/routes/bankless/article.ts
@@ -3,7 +3,8 @@ import ofetch from '@/utils/ofetch';
 import cache from '@/utils/cache';
 import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
-const baseUrl = 'https://www.bankless.com/read';
+const baseUrl =
+    'https://phantomjscloud.com/api/browser/v2/ak-17y47-kr42k-dmnbg-ehnya-27v91/?request=%7B%22url%22%3A%22https%3A%2F%2Fwww.bankless.com%2Fread%22%2C%22renderType%22%3A%22html%22%2C%22requestSettings%22%3A%7B%22userAgent%22%3A%22Mozilla%2F5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_5%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F100.0.4896.81%20Safari%2F537.36%22%7D%7D';
 export const route: Route = {
     path: '/article',
     categories: ['finance'],
@@ -13,8 +14,8 @@ export const route: Route = {
             source: ['bankless.com/read'],
         },
     ],
-    name: 'Bankless articles',
-    maintainers: ['Colin'],
+    name: 'Articles',
+    maintainers: ['colin4k'],
     handler,
 };
 async function handler() {
@@ -39,7 +40,10 @@ async function handler() {
                 const $ = load(response);
                 const urlList = $('#article').first();
                 const $u = $(urlList);
-                item.description = $u.find('#focalAnchor.contents').text();
+                item.description = $u.find('#focalAnchor.contents').html();
+                const src = $u.find('img').prop('src');
+                const alt = $u.find('img').prop('alt');
+                item.image = { src, alt };
                 item.pubDate = parseDate($u.find('#intro .meta.wow.fadeInUp').children('span')[1].childNodes[0].data);
                 item.author = $u.find('#intro .meta.wow.fadeInUp .authorName').text();
                 return item;
@@ -48,9 +52,9 @@ async function handler() {
     );
     // console.log(items);
     return {
-        title: `Bankless Articles`,
+        title: 'Bankless Articles',
         link: baseUrl,
-        description: `Bankless is a global community to help you on your crypto journey.`,
+        description: 'Bankless is a global community to help you on your crypto journey.',
         item: items,
     };
 }
