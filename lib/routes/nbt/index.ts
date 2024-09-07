22384c0b4314100d0aba8e79c604de26dc983484
longyan <longyan0524@qq.com>
2024-09-07 18:17:30 +0800

    feat(Route) Add NingboTech University route.

---
+++ lib/routes/nbt/index.ts
@@ -0,0 +1,72 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import { load } from 'cheerio';
+import got from '@/utils/got';
+
+const host = 'https://www.nbt.edu.cn/';
+const typeMap = {
+    importance: '/cs_list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1013',
+    news: '/lgxw/mtlg.htm',
+    notice: '/cs_list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1015',
+};
+
+const titleMap = {
+    importance: '理工要闻',
+    news: '媒体理工',
+    notice: '理工快讯',
+};
+
+export const route: Route = {
+    path: '/:type',
+    categories: ['university'],
+    example: '/nbt/importance',
+    parameters: { type: '通知类型，默认为理工要问' },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '理工要问',
+    maintainers: ['A-normal'],
+    handler,
+    description: `| 理工要闻 | 媒体理工 | 理工快讯 |
+  | ---------- | ----- | ------ |
+  | importance | news  | notice |`,
+};
+
+async function handler(ctx) {
+    const type = ctx.req.param('type') ?? 'notice';
+    const link = host + typeMap[type];
+
+    const title = '浙大宁波理工学院' + titleMap[type];
+    const response = await got.get(link);
+    const $ = load(response.data);
+    const list = $('div[class="page_list2"] ul').find('li');
+
+    const items = await Promise.all(
+        list.map(async (i, item) => {
+            const info = $(item).find('a').attr('href');
+            const pageUrl = host + info;
+            const { desc } = await cache.tryGet(pageUrl, async () => {
+                const page = await got.get(pageUrl);
+                const $ = load(page.data);
+                return {
+                    desc: $('form[name="_newscontent_fromname"]').html(),
+                };
+            });
+            return {
+                title: $(item).find('a').text(),
+                link: pageUrl,
+                description: desc,
+            };
+        })
+    );
+    return {
+        title,
+        link,
+        item: items,
+    };
+}
---
+++ lib/routes/nbt/sjxy/index.ts
@@ -0,0 +1,70 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import { load } from 'cheerio';
+import got from '@/utils/got';
+
+const host = 'https://sjxy.nbt.edu.cn';
+const typeMap = {
+    notice: '/index/tzgg.htm',
+    news: '/index/xwdt.htm',
+};
+
+const titleMap = {
+    notice: '通知公告',
+    news: '新闻动态',
+};
+
+export const route: Route = {
+    path: '/sjxy/:type/',
+    categories: ['university'],
+    example: '/nbt/sjxy/notice',
+    parameters: { type: '通知类型，默认为通知公告' },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '通知公告',
+    maintainers: ['A-normal'],
+    handler,
+    description: `| 通知公告 | 新闻动态 |
+    | ---------- | ----- |
+    | notice     | news  |`,
+};
+
+async function handler(ctx) {
+    const type = ctx.req.param('type') ?? 'notice';
+    const link = host + typeMap[type];
+
+    const title = '浙大宁波理工学院 计算机与数据工程学院' + titleMap[type];
+    const response = await got.get(link);
+    const $ = load(response.data);
+    const list = $('div[class="lm_list"] ul').find('li');
+
+    const items = await Promise.all(
+        list.map(async (i, item) => {
+            const info = $(item).find('a').attr('href');
+            const pageUrl = host + info?.substring(2, info.length);
+            const { desc } = await cache.tryGet(pageUrl, async () => {
+                const page = await got.get(pageUrl);
+                const $ = load(page.data);
+                return {
+                    desc: $('.c-content').html(),
+                };
+            });
+            return {
+                title: $(item).find('a').text(),
+                link: pageUrl,
+                description: desc,
+            };
+        })
+    );
+    return {
+        title,
+        link,
+        item: items,
+    };
+}
