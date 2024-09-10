023cc968062f9cd709b370f558caea44434f08a8
jiangsong216 <yalijiangsong@163.com>
2024-09-10 09:36:00 +0800

    feat(route): add route for easynomad

--- lib/routes/easynomad/namespace.ts
+++ lib/routes/easynomad/namespace.ts
@@ -3,6 +3,4 @@ import type { Namespace } from '@/types';
 export const namespace: Namespace = {
     name: '轻松游牧-远程工作聚集地',
     url: 'easynomad.cn',
-    // categories: ['blog'],
-    // description: '',
 };
