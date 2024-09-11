d4e8bd715c1e1e414d3da80ebed52206bc270492
CH563 <wonderful563@qq.com>
2024-09-11 12:07:22 +0800

    feat(route): add 太平洋科技

---
+++ lib/routes/pconline/focus.ts
@@ -0,0 +1,135 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import got from '@/utils/got';
+import { parseDate } from '@/utils/parse-date';
+
+const rootUrl = 'https://www.pconline.com.cn';
+interface Item {
+    id: string;
+    title: string;
+    channelName: string;
+    wap_3g_url: string;
+    bigImage: string;
+    cover: string;
+    largeImage: string;
+    authorname: string;
+    authorImg: string;
+    pc_pubDate: string;
+    artType: string;
+    summary: string;
+    url: string;
+}
+
+const categories = {
+    all: {
+        title: '全部',
+        path: '',
+    },
+    tech: {
+        title: '科技',
+        path: 'tech/',
+    },
+    finance: {
+        title: '财经',
+        path: 'finance/',
+    },
+    life: {
+        title: '生活',
+        path: 'life/',
+    },
+    company: {
+        title: '公司',
+        path: 'company/',
+    },
+    character: {
+        title: '人物',
+        path: 'character/',
+    },
+};
+
+export const handler = async (ctx) => {
+    const { category = 'all' } = ctx.req.param();
+    const cate = categories[category] || categories.all;
+    const currentUrl = `${rootUrl}/3g/other/focus/${cate.path}index.html`;
+
+    let item = [];
+
+    try {
+        let dataString = await cache.get(currentUrl);
+        if (!dataString) {
+            const response = await got({
+                method: 'get',
+                url: currentUrl,
+            });
+
+            const resString = response.data
+                .replace(/Module\.callback\((.*)\)/s, '$1')
+                .split('\n')
+                .filter((e) => e.indexOf('"tags":') !== 0)
+                .join('\n')
+                .replaceAll('\'', '"');
+            const tinyData = resString.replaceAll(/[\n\r]/g, '');
+            dataString = tinyData.replaceAll(',}', '}');
+        }
+        const data = JSON.parse(dataString || '');
+        const { articleList } = data;
+        item = articleList.map((item: Item) => ({
+                id: item.id,
+                title: item.title,
+                author: [
+                    {
+                        name: item.authorname,
+                        avatar: item.authorImg,
+                    },
+                ],
+                pubDate: parseDate(item.pc_pubDate),
+                link: item.url,
+                description: item.summary,
+                category: item.channelName,
+                image: item.cover,
+            }));
+        cache.set(currentUrl, JSON.stringify(item));
+    } catch {
+        // console.error(error);
+    }
+
+    return {
+        title: `太平洋科技-${cate.title}`,
+        link: currentUrl,
+        item,
+    };
+};
+
+export const route: Route = {
+    path: '/focus/:category?',
+    categories: ['new-media'],
+    example: '/pconline/focus',
+    parameters: {
+        category: {
+            description: '科技新闻的类别，获取最新的一页，分别：all, tech, finance, life, company, character',
+            default: 'all',
+        },
+    },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['pconline.com.cn/focus/', 'pconline.com.cn/'],
+            target: '/focus',
+        },
+    ],
+    name: '科技新闻',
+    maintainers: ['CH563'],
+    handler,
+    description: `::: tip
+| 全部 | 科技 | 财经 | 生活 | 公司 | 人物 |
+| --- | --- | --- | --- | --- | --- |
+| all | tech | finance | life | company | character |
+:::`,
+};
