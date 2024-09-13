a1bf4e52f8143110eac9d901fb0fb2d48f5ff91a
sd0ric4 <1286518974@qq.com>
2024-09-14 00:06:24 +0800

    feat(github-advisor): add GitHub Advisory Database RSS route and namespace

---
+++ lib/routes/github-advisor/namespace.ts
@@ -0,0 +1,72 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'github-advisory-database-rss',
+    url: 'github.com/advisories',
+    description: `
+:::tip
+GitHub Advisory Database RSS feeds
+These RSS Feeds is a collection of [GitHub Advisory Database](https://github.com/advisories).
+-   OPML Feeds(All ecosystems): \`https://azu.github.io/github-advisory-database-rss\`
+-   COMPOSER: \`https://azu.github.io/github-advisory-database-rss/composer.json\` 
+-   GO: \`https://azu.github.io/github-advisory-database-rss/go.json\` 
+-   MAVEN: \`https://azu.github.io/github-advisory-database-rss/maven.json\` 
+-   NPM: \`https://azu.github.io/github-advisory-database-rss/npm.json\` 
+-   NUGET: \`https://azu.github.io/github-advisory-database-rss/nuget.json\` 
+-   PIP: \`https://azu.github.io/github-advisory-database-rss/pip.json\` 
+-   PUB: \`https://azu.github.io/github-advisory-database-rss/pub.json\` 
+-   RUBYGEMS: \`https://azu.github.io/github-advisory-database-rss/rubygems.json\` 
+-   RUST: \`https://azu.github.io/github-advisory-database-rss/rust.json\` 
+-   ERLANG: \`https://azu.github.io/github-advisory-database-rss/erlang.json\` 
+-   ACTIONS: \`https://azu.github.io/github-advisory-database-rss/actions.json\` 
+-   SWIFT: \`https://azu.github.io/github-advisory-database-rss/swift.json\` 
+:::`,
+
+    zh: {
+        name: 'GitHub 安全威胁数据库 RSS',
+        url: 'github.com/advisories',
+        description: `
+:::tip
+GitHub 安全威胁数据库 RSS
+这些 RSS 源是 [GitHub 安全威胁数据库](https://github.com/advisories) 的集合。
+-   OPML 源(所有生态系统): \`https://azu.github.io/github-advisory-database-rss\`
+-   COMPOSER: \`https://azu.github.io/github-advisory-database-rss/composer.json\`
+-   GO: \`https://azu.github.io/github-advisory-database-rss/go.json\`
+-   MAVEN: \`https://azu.github.io/github-advisory-database-rss/maven.json\`
+-   NPM: \`https://azu.github.io/github-advisory-database-rss/npm.json\`
+-   NUGET: \`https://azu.github.io/github-advisory-database-rss/nuget.json\`
+-   PIP: \`https://azu.github.io/github-advisory-database-rss/pip.json\`
+-   PUB: \`https://azu.github.io/github-advisory-database-rss/pub.json\`
+-   RUBYGEMS: \`https://azu.github.io/github-advisory-database-rss/rubygems.json\`
+-   RUST: \`https://azu.github.io/github-advisory-database-rss/rust.json\`
+-   ERLANG: \`https://azu.github.io/github-advisory-database-rss/erlang.json\`
+-   ACTIONS: \`https://azu.github.io/github-advisory-database-rss/actions.json\`
+-   SWIFT: \`https://azu.github.io/github-advisory-database-rss/swift.json\`
+:::`,
+    },
+    ja: {
+        name: 'GitHub アドバイザリ データベース RSS',
+        url: 'github.com/advisories',
+        description: `
+:::tip
+GitHub アドバイザリ データベース RSS フィード
+これらの RSS フィードは [GitHub アドバイザリ データベース](https://github.com/advisories) のコレクションです。
+-   OPML フィード(すべてのエコシステム): \`https://azu.github.io/github-advisory-database-rss\`
+-   COMPOSER: \`https://azu.github.io/github-advisory-database-rss/composer.json\`
+-   GO: \`https://azu.github.io/github-advisory-database-rss/go.json\`
+-   MAVEN: \`https://azu.github.io/github-advisory-database-rss/maven.json\`
+-   NPM: \`https://azu.github.io/github-advisory-database-rss/npm.json\`
+-   NUGET: \`https://azu.github.io/github-advisory-database-rss/nuget.json\`
+-   PIP: \`https://azu.github.io/github-advisory-database-rss/pip.json\`
+-   PUB: \`https://azu.github.io/github-advisory-database-rss/pub.json\`
+-   RUBYGEMS: \`https://azu.github.io/github-advisory-database-rss/rubygems.json\`
+-   RUST: \`https://azu.github.io/github-advisory-database-rss/rust.json\`
+-   ERLANG: \`https://azu.github.io/github-advisory-database-rss/erlang.json\`
+-   ACTIONS: \`https://azu.github.io/github-advisory-database-rss/actions.json\`
+-   SWIFT: \`https://azu.github.io/github-advisory-database-rss/swift.json\`
+:::`,
+    },
+    'zh-TW': {
+        name: 'GitHub 安全威脅資料庫 RSS',
+    },
+};
