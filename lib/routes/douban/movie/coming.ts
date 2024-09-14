1fd7d4e97a8d8274f1212e67198d04dfba6d8997
reonokiy <reonokiy@gmail.com>
2024-09-15 01:24:49 +0800

    切换到art模板，添加radar

--- lib/routes/douban/movie/coming.ts
+++ lib/routes/douban/movie/coming.ts
@@ -1,5 +1,10 @@
 import { Route } from '@/types';
 import got from '@/utils/got';
+import { getCurrentPath } from '@/utils/helpers';
+import { art } from '@/utils/render';
+import path from 'node:path';
+
+const __dirname = getCurrentPath(import.meta.url);
 
 export const route: Route = {
     path: '/movie/coming',
@@ -15,15 +20,22 @@ export const route: Route = {
     },
     name: '电影即将上映',
     maintainers: ['reonokiy'],
+    radar: [
+        {
+            title: '豆瓣电影-即将上映',
+            source: ['movie.douban.com/coming'],
+            target: '/movie/coming',
+        },
+    ],
     handler,
 };
+const renderDescription = (info: { title?: string; cover_url?: string; pubdate?: string[]; intro?: string; directors?: string[]; actors?: string[]; genres: string[]; wish_count?: number | string }) =>
+    art(path.join(__dirname, '../templates/movie_coming.art'), info);
 
 async function handler(ctx) {
-    const url = 'https://m.douban.com/rexxar/api/v2/movie/coming_soon';
-
     const response = await got({
         method: 'get',
-        url,
+        url: 'https://m.douban.com/rexxar/api/v2/movie/coming_soon',
         headers: {
             Referer: 'https://m.douban.com/movie/',
         },
@@ -34,29 +46,23 @@ async function handler(ctx) {
     return {
         title: '豆瓣电影-即将上映',
         link: 'https://movie.douban.com/coming',
-        item: response.data?.subjects?.map((item) => {
-            const description = `<img src="${item?.cover_url}" alt="${item?.title}" referrerpolicy="no-referrer"><br>
-              <h2>电影信息</h2>
-              <ul>
-              <li>导演：${item?.directors?.map((d) => d?.name).join(', ')}</li>
-              <li>演员：${item?.actors?.map((a) => a?.name).join(', ')}</li>
-              <li>类型：${item?.genres?.join(' / ')}</li>
-              <li>上映日期：${item?.pubdate?.join(' / ')}</li>
-              <li>想看：${item?.wish_count}</li>
-              </ul>
-              <h2>剧情简介</h2>
-              <p>${item?.intro}</p>
-              `;
-
-            return {
+        item: response.data?.subjects?.map((item) => ({
+            title: item?.title,
+            link: item?.url,
+            guid: item?.url,
+            description: renderDescription({
                 title: item?.title,
-                link: item?.url,
-                guid: item?.url,
-                description,
-                category: item?.genres,
-                itunes_item_image: item?.cover_url,
-                upvotes: item?.wish_count,
-            };
-        }),
+                intro: item?.intro,
+                pubdate: item?.pubdate,
+                cover_url: item?.cover_url,
+                directors: item?.directors?.map((d) => d?.name),
+                actors: item?.actors?.map((a) => a?.name),
+                genres: item?.genres,
+                wish_count: item?.wish_count,
+            }),
+            category: item?.genres,
+            itunes_item_image: item?.cover_url,
+            upvotes: item?.wish_count,
+        })),
     };
 }
