cf87dd2a5697683bf5fada2dc44deb3e62125954
howfool <wang@plasma.ac.cn>
2024-09-17 00:56:17 +0800

    add route of '国家发展改革委 - 政府信息公开'

---
+++ lib/routes/gov/zfxxgk/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '国家发展改革委 - 政府信息公开',
+    url: 'zfxxgk.ndrc.gov.cn/',
+    categories: ['government'],
+    description: '',
+};
