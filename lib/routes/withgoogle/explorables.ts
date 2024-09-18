710d1014e71e7ece551561b68678468fd358b973
cesar <cesaryuan@qq.com>
2024-09-18 09:48:53 +0800

    fix: maintainers and cache scope

--- lib/routes/withgoogle/explorables.ts
+++ lib/routes/withgoogle/explorables.ts
@@ -1,4 +1,4 @@
-import { Route } from '@/types';
+import type { Route, DataItem } from '@/types';
 import cache from '@/utils/cache';
 import ofetch from '@/utils/ofetch';
 import { load } from 'cheerio';
@@ -7,7 +7,7 @@ export const route: Route = {
     name: 'PAIR - AI Exploreables',
     url: 'pair.withgoogle.com/explorables',
     path: '/explorables',
-    maintainers: ['cesar'],
+    maintainers: ['cesaryuan'],
     example: '/withgoogle/explorables',
     radar: [
         {
@@ -26,24 +26,21 @@ export const route: Route = {
                 .map(async (_, el) => {
                     const title = $(el).find('h3').text();
                     const image = $(el).find('img').attr('src');
-                    const link = $(el).find('a').attr('href');
-                    let description;
-                    if (link) {
-                        description = await cache.tryGet(link, async () => {
-                            const response = await ofetch(baseUrl + link);
-                            const $item = load(response);
-                            return $item('body').html() || '';
-                        });
-                    }
-                    if (!description || description.trim() === '') {
-                        description = $('p').text();
-                    }
-                    return {
-                        title,
-                        link: baseUrl + link,
-                        description,
-                        image,
-                    };
+                    const link = baseUrl + $(el).find('a').attr('href');
+                    return (await cache.tryGet(link, async () => {
+                        const response = await ofetch(link);
+                        const $item = load(response);
+                        let description = $item('body').html();
+                        if (!description || description.trim() === '') {
+                            description = $('p').text();
+                        }
+                        return {
+                            title,
+                            link,
+                            description,
+                            image,
+                        };
+                    })) as DataItem;
                 })
                 .toArray()
         );
