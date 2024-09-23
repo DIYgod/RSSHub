e0d606211bf673ed23b9c582e4a3c3eed8b14bd1
valuex <gm2wie@gmail.com>
2024-09-23 22:58:46 +0900

    Update scienceatechnology.ts

--- lib/routes/nikkei/scienceatechnology.ts
+++ lib/routes/nikkei/scienceatechnology.ts
@@ -50,8 +50,8 @@ async function handler() {
     const items = await Promise.all(
         lists.map((item) =>
             cache.tryGet(item.link, async () => {
-                let thisUrl = item.link;
-                let trueLink = thisUrl.includes("http") ? thisUrl : base_link + thisUrl;
+                const thisUrl = item.link;
+                const trueLink = thisUrl.includes("http") ? thisUrl : base_link + thisUrl;
                 const response = await got.get(trueLink);
                 const $ = load(response.data);
                 item.description = $('#contentDiv').html();
