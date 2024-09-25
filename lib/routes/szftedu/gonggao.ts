3071a1f431d734955bfaa255bc3f168664cbfc8a
valuex <gm2wie@gmail.com>
2024-09-25 21:33:09 +0900

    Update gonggao.ts

--- lib/routes/szftedu/gonggao.ts
+++ lib/routes/szftedu/gonggao.ts
@@ -46,7 +46,7 @@ async function handler() {
                 const trueLink = thisUrl.includes("http") ? thisUrl : host + thisUrl.substring(1);
                 const response = await got(trueLink);
                 const $ = load(response.data);
-                item.description = $('body').html();
+                item.description = $('#page-content').html();
                 item.pubDate = timezone(parseDate($('.item').first().text().replace('发布时间：', '')), 8);
                 return item;
             })
