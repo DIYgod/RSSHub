f21d19b98b2cb87e14cb05cc68cbb62a1017350c
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2024-09-26 23:17:48 +0900

    Fix and update LoveLive NEWS route

--- lib/routes/lovelive-anime/news.ts
+++ lib/routes/lovelive-anime/news.ts
@@ -10,10 +10,14 @@ import { art } from '@/utils/render';
 const renderDescription = (desc) => art(path.join(__dirname, 'templates/description.art'), desc);
 
 export const route: Route = {
-    path: '/news/:option?',
+    path: '/news/:abbr?/:category?/:option?',
     categories: ['anime'],
     example: '/lovelive-anime/news',
-    parameters: { option: 'Crawl full text when `option` is `detail`.' },
+    parameters: {
+        abbr: 'The path to the Love Live series of sub-projects on the official website is detailed in the table below, `abbr` is `detail` when crawling the full text',
+        category: 'The official website lists the Topics category, `category` is `detail` when crawling the full text, other categories see the following table for details',
+        option: 'Crawl full text when `option` is `detail`.',
+    },
     features: {
         requireConfig: false,
         requirePuppeteer: false,
@@ -29,15 +33,36 @@ export const route: Route = {
         },
     ],
     name: 'Love Live! Official Website Latest NEWS',
-    maintainers: ['axojhf'],
+    maintainers: ['axojhf', 'zhaoweizhong'],
     handler,
     url: 'www.lovelive-anime.jp/',
+    description: `| Sub-project Name (not full name) | Lovelive!   | Lovelive! Sunshine!! | Lovelive! Nijigasaki High School Idol Club | Lovelive! Superstar!! | 蓮ノ空女学院 | 幻日のヨハネ | ラブライブ！スクールアイドルミュージカル |
+  | -------------------------------- | ----------- | -------------------- | ------------------------------------------ | --------------------- | ------------ | ---------------------------------------- |
+  | \`abbr\`parameter                  | lovelive | sunshine           | nijigasaki                                 | superstar              | hasunosora | yohane       | musical                                  |
+
+  | Category Name       | 全てのニュース        | 音楽商品 | アニメ映像商品 | キャスト映像商品 | 劇場    | アニメ放送 / 配信 | キャスト配信 / ラジオ | ライブ / イベント | ブック | グッズ | ゲーム | メディア | ご当地情報 | キャンペーン | その他 |
+  | ------------------- | --------------------- | -------- | -------------- | ---------------- | ------- | ----------------- | --------------------- | ----------------- | ------ | ------ | ------ | -------- | ---------- | ------ | ------------ |
+  | \`category\`parameter | <u>*No parameter*</u> | music    | anime\_movie   | cast\_movie      | theater | onair             | radio                 | event             | books  | goods  | game   | media    | local      | campaign  | other   |`,
 };
 
 async function handler(ctx) {
-    const rootUrl = 'https://www.lovelive-anime.jp/news/';
+    let url = 'https://www.lovelive-anime.jp/news/';
+    const abbr = ctx.req.param('abbr');
+    const category = ctx.req.param('category');
+    const option = ctx.req.param('option');
 
-    const response = await got(rootUrl);
+    const isDetail = abbr === 'detail' || category === 'detail' || option === 'detail';
+
+    const params = new URLSearchParams();
+    if (abbr && abbr !== 'detail') {
+        params.append('series', abbr);
+        if (category && category !== 'detail') {
+            params.append('subcategory', category);
+        }
+    }
+    url += `?${params.toString()}`;
+
+    const response = await got(url);
 
     const $ = load(response.data);
 
@@ -60,7 +85,7 @@ async function handler(ctx) {
 
     let items = pageFace;
 
-    if (ctx.req.param('option') === 'detail') {
+    if (isDetail) {
         items = await Promise.all(
             pageFace.map((item) =>
                 cache.tryGet(item.link, async () => {
