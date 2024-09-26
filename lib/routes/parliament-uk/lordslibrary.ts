5f50540e615b25ce23893a2871db9d0a8ddefd1e
Yansy <yansiyu.old.dog@hotmail.com>
2024-09-26 17:40:42 +0800

    feat(route): add route UK Parliament Lords Library Research by Topic

---
+++ lib/routes/parliament-uk/lordslibrary.ts
@@ -0,0 +1,54 @@
+import { load } from 'cheerio';
+import { Route } from '@/types';
+import puppeteer from '@/utils/puppeteer';
+import timezone from '@/utils/timezone';
+
+export const route: Route = {
+    path: '/lordslibrary/type/:tag?',
+    categories: ['government'],
+    example: '/parliament-uk/lordslibrary/type/research-briefing',
+    parameters: { tag: 'term tag, string, example: [research-briefing|buisness|economy]' },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: 'UK Parliament - loadslibrary',
+    maintainers: ['AntiKnot'],
+    handler,
+};
+
+async function handler(ctx) {
+    const { tag } = ctx.req.param();
+    const baseUrl = 'https://lordslibrary.parliament.uk/type';
+    const url = `${baseUrl}/${tag}/`;
+    const browser = await puppeteer();
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+        request.resourceType() === 'document' || request.resourceType() === 'script' ? request.continue() : request.abort();
+    });
+    await page.goto(url, {
+        waitUntil: 'domcontentloaded',
+    });
+
+    const html = await page.evaluate(() => document.documentElement.innerHTML);
+    await page.close();
+    const $ = load(html);
+    const items = $('article.card--horizontal')
+        .map((_, article) => ({
+            title: $(article).find('.card__text a').text().trim(),
+            link: $(article).find('.card__text a').attr('href'),
+            description: $(article).find('p').last().text().trim(),
+            pubDate: timezone($(article).find('.card__date time').attr('datetime')),
+        }))
+        .toArray();
+    return {
+        title: `parliament - lordslibrary - ${tag}`,
+        link: baseUrl,
+        item: items,
+    };
+}
