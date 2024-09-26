b52e12cd168d14aa747fd441c68c12052a9da646
nczitzk <42264778+nczitzk@users.noreply.github.com>
2024-09-26 14:08:25 +0800

    feat(route): add AIbase

---
+++ lib/routes/aibase/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'AIBase',
+    url: 'aibase.com',
+    categories: ['new-media'],
+    description: '',
+};
