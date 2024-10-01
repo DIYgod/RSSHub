16e44b7d22c7af62de9880b12e5c05cd3c6a20c7
pandada8 <pandada8@gmail.com>
2024-10-02 02:38:39 +0800

    fix(route/ceph): remove unnecessary caching

--- lib/routes/ceph/blog.ts
+++ lib/routes/ceph/blog.ts
@@ -34,45 +34,39 @@ export const route: Route = {
 async function handler(ctx: Context): Promise<Data> {
     const { category } = ctx.req.param();
     const url = category ? `https://ceph.io/en/news/blog/category/${category}/` : 'https://ceph.io/en/news/blog/';
-    return await cache.tryGet(
-        url,
-        async () => {
-            const response = await got.get(url);
-            const data = response.data;
-            const $ = load(data);
-            const list = $('#main .section li')
-                .toArray()
-                .map((e) => {
-                    const element = $(e);
-                    const title = element.find('a').text().trim();
-                    const pubDate = parseDate(element.find('time').attr('datetime'));
-                    return {
-                        title,
-                        link: new URL(element.find('a').attr('href'), 'https://ceph.io').href,
-                        pubDate,
-                    };
-                });
-
-            const result = await Promise.all(
-                list.map((item) =>
-                    cache.tryGet(item.link, async () => {
-                        const itemReponse = await got.get(item.link);
-                        const data = itemReponse.data;
-                        const item$ = load(data);
-
-                        item.author = item$('#main section > div:nth-child(1) span').text().trim();
-                        item.description = item$('#main section > div:nth-child(2) > div').html();
-                        return item;
-                    })
-                )
-            );
-
+    const response = await got.get(url);
+    const data = response.data;
+    const $ = load(data);
+    const list = $('#main .section li')
+        .toArray()
+        .map((e) => {
+            const element = $(e);
+            const title = element.find('a').text().trim();
+            const pubDate = parseDate(element.find('time').attr('datetime'));
             return {
-                title: 'Ceph Blog',
-                link: url,
-                item: result,
+                title,
+                link: new URL(element.find('a').attr('href'), 'https://ceph.io').href,
+                pubDate,
             };
-        },
-        3600
+        });
+
+    const result = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link, async () => {
+                const itemReponse = await got.get(item.link);
+                const data = itemReponse.data;
+                const item$ = load(data);
+
+                item.author = item$('#main section > div:nth-child(1) span').text().trim();
+                item.description = item$('#main section > div:nth-child(2) > div').html();
+                return item;
+            })
+        )
     );
+
+    return {
+        title: 'Ceph Blog',
+        link: url,
+        item: result,
+    };
 }
