98b7703e95f31072ab2f19cf27e8eafa80ede916
pseudoyu <pseudoyu@connect.hku.hk>
2024-10-09 11:17:02 +0700

    fix(route): use apnews api instead of official rss (down now)

--- lib/routes/apnews/api.ts
+++ lib/routes/apnews/api.ts
@@ -6,7 +6,7 @@ import { parseDate } from '@/utils/parse-date';
 
 export const route: Route = {
     path: '/api/:tags?',
-    categories: ['traditional-media'],
+    categories: ['traditional-media', 'popular'],
     example: '/apnews/api/business',
     view: ViewType.Articles,
     parameters: {
