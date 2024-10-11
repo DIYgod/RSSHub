7adc7e03931f29caf75c5dfa1cec854d6c26aba2
Ron <ap66.ron@gmail.com>
2024-10-11 14:35:35 +0800

    feat: 添加koa路由
     ## Involved Issue / 该 PR 相关 Issue
    
    Close #
    
    ## Example for the Proposed Route(s) / 路由地址示例
    
    ## New RSS Route Checklist / 新 RSS 路由检查表
    
    - [x] New Route / 新的路由
    - [x] Follows [Script Standard](https://docs.rsshub.app/joinus/advanced/script-standard) / 跟随 [路由规范](https://docs.rsshub.app/zh/joinus/advanced/script-standard)
    - [x] Documentation / 文档说明
    - [ ] Full text / 全文获取
    - [ ] Use cache / 使用缓存
    - [ ] Anti-bot or rate limit / 反爬/频率限制
    - [ ] If yes, do your code reflect this sign? / 如果有, 是否有对应的措施?
    - [ ] [Date and time](https://docs.rsshub.app/joinus/advanced/pub-date) / [日期和时间](https://docs.rsshub.app/zh/joinus/advanced/pub-date)
    - [ ] Parsed / 可以解析
    - [ ] Correct time zone / 时区正确
    - [ ] New package added / 添加了新的包
    - [x]
    
    ## Note / 说明
    
    - 新增路由暂时用于提供經香港天文台分析的全球5.0級或以上及本地有感的地震資訊。
    - 后期会扩展新增新增更多其它自然公益资讯。

---
+++ lib/routes/hko/app.ts
@@ -0,0 +1,97 @@
+import { Route } from '@/types';
+import { load } from 'cheerio';
+import logger from '@/utils/logger';
+import puppeteer from '@/utils/puppeteer';
+
+export const route: Route = {
+    path: '/earthquake',
+    name: 'hko earthquake',
+    maintainers: ['after9'],
+    handler,
+    example: 'https://rsshub.app/hko/earthquake',
+    categories: ['forecast'],
+    description: `来自香港天文台的全球5级以上地震记录`,
+};
+
+async function handler() {
+    const title = '来自香港天文台的全球5级以上地震记录';
+    const link = 'https://www.hko.gov.hk/tc/gts/equake/quake-info.htm';
+    const description = '提供經天文台分析的全球5.0級或以上及本地有感的地震資訊。';
+
+    // 导入 puppeteer 工具类并初始化浏览器实例
+    const browser = await puppeteer();
+    // 打开一个新标签页
+    const page = await browser.newPage();
+    // 访问目标链接
+    const linkUrl = 'https://www.hko.gov.hk/tc/gts/equake/quake-info.htm';
+    // ofetch 请求会被自动记录，
+    // 但 puppeteer 请求不会
+    // 所以我们需要手动记录它们
+    logger.http(`Requesting ${linkUrl}`);
+    await page.goto(linkUrl, {
+        // 指定页面等待载入的时间
+        waitUntil: 'domcontentloaded',
+    });
+    // 获取页面的 HTML 内容
+    const response = await page.content();
+    // 关闭标签页
+    await page.close();
+
+    const $ = load(response);
+
+    const items = $('#tbody_list > tr')
+        // 使用“toArray()”方法将选择的所有 DOM 元素以数组的形式返回。
+        .toArray()
+        // 使用“map()”方法遍历数组，并从每个元素中解析需要的数据。
+        .map((item, index, array) => {
+            item = $(item);
+            const posHtml = item.find('td.td_region a').html();
+            const degree = item.find('td.td_mag a').text();
+            const pubDate = item.find('td.td_region a span').text();
+            const position = posHtml.split('<br>')[1].replace('.', '').trim();
+            return index === array.length - 1
+                ? null
+                : {
+                      title: `[震級:${degree}] [地點:${position}]`,
+                      pubDate: formatDate(pubDate),
+                  };
+        })
+        .filter((item) => item !== undefined);
+    return {
+        title,
+        link,
+        description,
+        item: items,
+    };
+}
+
+function formatDate(originalTimeStr) {
+    // 检查输入是否有效
+    if (typeof originalTimeStr !== 'string' || originalTimeStr.trim() === '') {
+        throw new Error('Invalid input: Please provide a valid date string.');
+    }
+
+    // 将 HKT 转换为 UTC
+    const parts = originalTimeStr.split(' '); // 分割日期、时间和时区
+
+    if (parts.length !== 3) {
+        throw new Error("Invalid format: Expected format is 'DD-MM-YYYY HH:mm TZ'.");
+    }
+
+    const [datePart, timePart] = parts;
+
+    // 解析日期和时间
+    const [day, month, year] = datePart.split('-');
+    const [hours, minutes] = timePart.split(':');
+
+    // 检查解析结果
+    if ([day, month, year, hours, minutes].includes(undefined)) {
+        throw new Error('Invalid date, time format. Please ensure they are in proper formats.');
+    }
+
+    // 创建一个 UTC 时间
+    const utcDate = new Date(Date.UTC(year, month - 1, day, hours - 8, minutes)); // 减去 8 小时以转换为 UTC
+
+    // 使用 toUTCString 方法将 Date 对象转换为所需的格式
+    return utcDate.toUTCString();
+}
