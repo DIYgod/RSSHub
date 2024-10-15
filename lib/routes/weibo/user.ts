2fb25639a60a29803250fd7c92cce15b7523766a
Nano <nanoapezlk@gmail.com>
2024-10-15 08:56:37 +0800

    chore(route/weibo): add maintainer

--- lib/routes/weibo/user.ts
+++ lib/routes/weibo/user.ts
@@ -35,7 +35,7 @@ export const route: Route = {
         },
     ],
     name: '博主',
-    maintainers: ['DIYgod', 'iplusx', 'Rongronggg9'],
+    maintainers: ['DIYgod', 'iplusx', 'Rongronggg9', 'Konano'],
     handler,
     description: `:::warning
   部分博主仅登录可见，未提供 Cookie 的情况下不支持订阅，可以通过打开 \`https://m.weibo.cn/u/:uid\` 验证
