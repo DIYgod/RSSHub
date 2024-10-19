670cefd1b5f5f33e49764230938d4951af05ebe8
xizeyoupan <44920131+xizeyoupan@users.noreply.github.com>
2024-10-20 04:01:52 +0800

    docs: add require config for coolapk (#17197)

--- lib/routes/coolapk/dyh.ts
+++ lib/routes/coolapk/dyh.ts
@@ -9,7 +9,13 @@ export const route: Route = {
     example: '/coolapk/dyh/1524',
     parameters: { dyhId: '看看号ID' },
     features: {
-        requireConfig: false,
+        requireConfig: [
+            {
+                name: 'ALLOW_USER_HOTLINK_TEMPLATE',
+                optional: true,
+                description: '设置为`true`并添加`image_hotlink_template`参数来代理图片',
+            },
+        ],
         requirePuppeteer: false,
         antiCrawler: false,
         supportBT: false,
