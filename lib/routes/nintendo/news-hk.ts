dd85a2bbe705b3067a152ea1cdd9e9c034165a9e
benzking <kingdom.wei@gmail.com>
2024-10-23 17:59:14 +0800

    fix(route): nintendo.com.hk

--- lib/routes/nintendo/news-hk.ts
+++ lib/routes/nintendo/news-hk.ts
@@ -1,13 +1,13 @@
 import { Route } from '@/types';
 import cache from '@/utils/cache';
 import got from '@/utils/got';
-import util from './utils';
+import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
 
 export const route: Route = {
-    path: '/news/hk',
+    path: '/news/jp',
     categories: ['game'],
-    example: '/nintendo/news/hk',
+    example: '/nintendo/news/jp',
     parameters: {},
     features: {
         requireConfig: false,
@@ -19,31 +19,50 @@ export const route: Route = {
     },
     radar: [
         {
-            source: ['nintendo.com.hk/topics', 'nintendo.com.hk/'],
+            source: ['nintendo.com/jp'],
         },
     ],
-    name: 'News（Hong Kong）',
+    name: 'News（JP）',
     maintainers: ['benzking'],
     handler,
-    url: 'nintendo.com.hk/topics',
+    url: 'nintendo.com/jp',
 };
 
 async function handler(ctx) {
-    const response = await got('https://www.nintendo.com.hk/api/top/topics_pickup');
-    const data = response.data.slice(0, ctx.req.query('limit') ? Number(ctx.req.query('limit')) : 30);
-
+    const response = await got('https://www.nintendo.com/jp/topics/c/api/json_list?key=newtopics');
+    console.log(response);
+    const data = response.data.slice(0, 10);
+    console.log(data);
+    const list = data.map((item) => ({
+        // 文章标题
+        title: item.title,
+        // 文章链接
+        link: item.topic_url,
+        // 文章发布日期
+        pubDate: parseDate(item.release_date, 'YYYY/M/D HH:mm:ss'), // "release_date": "2024/10/18 17:00:00"
+        itunes_item_image:item.thumbnail.large.medium,
+        category:item.categorylarge.name,
+    }));
+    console.log(list);
     // 获取新闻正文
-    const result = await util.ProcessNews(data, cache);
-
+    const items = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link, async () => {
+                const { data: response } = await got(item.link);
+                const $ = load(response);
+                // 选择类名为“comment-body”的第一个元素
+                item.description = $('div.topics-articleBody').first().html();
+                // 上面每个列表项的每个属性都在此重用，
+                // 并增加了一个新属性“description”
+                return item;
+            })
+        )
+    );
+    console.log(items);
     return {
-        title: 'Nintendo（香港）主页资讯',
-        link: 'https://www.nintendo.com.hk/topics/',
-        description: 'Nintendo 香港有限公司官网刊登的资讯',
-        item: result.map((item) => ({
-            title: item.title,
-            description: item.content,
-            link: `https://www.nintendo.com.hk${item.url}`,
-            pubDate: parseDate(item.release_date, 'YYYY.MM.DD'),
-        })),
+        title: 'Nintendo（日本）主页资讯',
+        link: 'https://www.nintendo.com/jp/topics',
+        description: 'Nintendo JP',
+        item:items,
     };
 }
