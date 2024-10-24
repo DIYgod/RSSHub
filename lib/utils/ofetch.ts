a7199e92668b7d805e39908fadd670beab3f91e7
bugyaluwang <jiabowang@moego.pet>
2024-10-24 15:51:28 +0800

    chore: add node-network-devtools to enhance development experience

--- lib/utils/ofetch.ts
+++ lib/utils/ofetch.ts
@@ -1,6 +1,9 @@
 import { createFetch } from 'ofetch';
 import { config } from '@/config';
 import logger from '@/utils/logger';
+import { register } from 'node-network-devtools';
+
+process.env.NODE_ENV === 'dev' && register();
 
 const rofetch = createFetch().create({
     retryStatusCodes: [400, 408, 409, 425, 429, 500, 502, 503, 504],
@@ -11,12 +14,12 @@ const rofetch = createFetch().create({
         if (options.retry) {
             logger.warn(`Request ${request} with error ${response.status} remaining retry attempts: ${options.retry}`);
             if (!options.headers) {
-                options.headers = {};
+                options.headers = {} as Headers;
             }
             if (options.headers instanceof Headers) {
                 options.headers.set('x-prefer-proxy', '1');
             } else {
-                options.headers['x-prefer-proxy'] = '1';
+                (options.headers as Headers)['x-prefer-proxy'] = '1';
             }
         }
     },
