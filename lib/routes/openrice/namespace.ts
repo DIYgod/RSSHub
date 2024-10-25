6910dd3c33c69e7d84bac7b1332c1837192fa2be
After9 <ap66.ron@gmail.com>
2024-10-25 12:03:24 +0800

    feat: add route for openrice.com (#17276)
    * feat: add route for openrice.com
    
    * fix: Optimize the code according to the recommendations of review
    
    * fix: Remove the initial assignment of variable urlPath

---
+++ lib/routes/openrice/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Openrice開飯喇',
+    url: 'www.openrice.com',
+    categories: ['shopping'],
+    description: '美食網站Openrice相关資訊',
+};
