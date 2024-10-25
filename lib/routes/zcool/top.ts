bc14ba0bed4dd05c6bf564f8e968247451120348
pseudoyu <pseudoyu@connect.hku.hk>
2024-10-25 10:51:57 +0800

    feat(route): add more design popular routes

--- lib/routes/zcool/top.ts
+++ lib/routes/zcool/top.ts
@@ -1,4 +1,4 @@
-import { Route } from '@/types';
+import { Route, ViewType } from '@/types';
 import cache from '@/utils/cache';
 import got from '@/utils/got';
 import { load } from 'cheerio';
@@ -8,9 +8,18 @@ const baseUrl = 'https://www.zcool.com.cn';
 
 export const route: Route = {
     path: '/top/:type',
-    categories: ['design'],
+    categories: ['design', 'popular'],
+    view: ViewType.Pictures,
     example: '/zcool/top/design',
-    parameters: { type: '推荐类型,详见下面的表格' },
+    parameters: {
+        type: {
+            description: '推荐类型',
+            options: [
+                { value: 'design', label: '作品榜单' },
+                { value: 'article', label: '文章榜单' },
+            ],
+        },
+    },
     features: {
         requireConfig: false,
         requirePuppeteer: false,
@@ -22,11 +31,6 @@ export const route: Route = {
     name: '作品总榜单',
     maintainers: ['yuuow'],
     handler,
-    description: `榜单类型
-
-  | design   | article  |
-  | -------- | -------- |
-  | 作品榜单 | 文章榜单 |`,
 };
 
 async function handler(ctx) {
