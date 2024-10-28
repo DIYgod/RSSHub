2bf7aa3607a996f7a4db2885fa8a269b4fe33b49
Eric Zhu <eric@zhu.email>
2024-10-28 13:04:28 +0800

    feat(twitter): support LoginEnterAlternateIdentifierSubtask task and better login flow controll

--- lib/routes/twitter/api/mobile-api/login.ts
+++ lib/routes/twitter/api/mobile-api/login.ts
@@ -10,6 +10,8 @@ import logger from '@/utils/logger';
 import cache from '@/utils/cache';
 import { RateLimiterMemory, RateLimiterRedis, RateLimiterQueue } from 'rate-limiter-flexible';
 
+const ENDPOINT = 'https://api.x.com/1.1/onboarding/task.json';
+
 const NAMESPACE = 'd41d092b-b007-48f7-9129-e9538d2d8fe9';
 
 let authentication = null;
@@ -41,12 +43,70 @@ const loginLimiter = cache.clients.redisClient
 
 const loginLimiterQueue = new RateLimiterQueue(loginLimiter);
 
-async function login({ username, password, authenticationSecret }) {
+const postTask = async (flowToken: string, subtaskId: string, subtaskInput: any) => {
+    const task = await got.post(ENDPOINT, {
+        headers,
+        json: {
+            flow_token: flowToken,
+            subtask_inputs: [Object.assign({ subtask_id: subtaskId }, subtaskInput)],
+        },
+    });
+    logger.debug(`Twitter login flow task finished: ${subtaskId}.`);
+    return task;
+};
+
+async function login({ username, password, authenticationSecret, phoneOrEmail }) {
     return (await cache.tryGet(
         `twitter:authentication:${username}`,
         async () => {
             try {
                 await loginLimiterQueue.removeTokens(1);
+
+                const flowTasks = {
+                    async LoginEnterUserIdentifier(flowToken: string) {
+                        return await postTask(flowToken, 'LoginEnterUserIdentifier', {
+                            enter_text: {
+                                suggestion_id: null,
+                                text: username,
+                                link: 'next_link',
+                            },
+                        });
+                    },
+                    async LoginEnterPassword(flowToken: string) {
+                        return await postTask(flowToken, 'LoginEnterPassword', {
+                            enter_password: {
+                                password,
+                                link: 'next_link',
+                            },
+                        });
+                    },
+                    async LoginEnterAlternateIdentifierSubtask(flowToken: string) {
+                        return await postTask(flowToken, 'LoginEnterAlternateIdentifierSubtask', {
+                            enter_text: {
+                                suggestion_id: null,
+                                text: phoneOrEmail,
+                                link: 'next_link',
+                            },
+                        });
+                    },
+                    async AccountDuplicationCheck(flowToken: string) {
+                        return await postTask(flowToken, 'AccountDuplicationCheck', {
+                            check_logged_in_account: {
+                                link: 'AccountDuplicationCheck_false',
+                            },
+                        });
+                    },
+                    async LoginTwoFactorAuthChallenge(flowToken: string) {
+                        const token = authenticator.generate(authenticationSecret);
+                        return await postTask(flowToken, 'LoginTwoFactorAuthChallenge', {
+                            enter_text: {
+                                suggestion_id: null,
+                                text: token,
+                                link: 'next_link',
+                            },
+                        });
+                    },
+                };
 
                 logger.debug('Twitter login start.');
                 const android_id = uuidv5(username, NAMESPACE);
@@ -61,12 +121,13 @@ async function login({ username, password, authenticationSecret }) {
                     },
                     method: 'POST',
                 });
-                logger.debug('Twitter login 1 finished: guest token.');
+                logger.debug('Twitter login guest token.');
 
                 headers['x-guest-token'] = guestToken.data.guest_token;
 
-                const task1 = await ofetch.raw(
-                    'https://api.x.com/1.1/onboarding/task.json?' +
+                let task: any = await ofetch.raw(
+                    ENDPOINT +
+                        '?' +
                         new URLSearchParams({
                             flow_name: 'login',
                             api_version: '1',
@@ -95,112 +156,41 @@ async function login({ username, password, authenticationSecret }) {
                         },
                     }
                 );
-                logger.debug('Twitter login 2 finished: login flow.');
+                logger.debug('Twitter login flow start.');
 
-                headers.att = task1.headers.get('att');
+                // @ts-ignore
+                headers.att = task.headers.get('att');
 
-                const task2 = await got.post('https://api.x.com/1.1/onboarding/task.json', {
-                    headers,
-                    json: {
-                        flow_token: task1._data.flow_token,
-                        subtask_inputs: [
-                            {
-                                enter_text: {
-                                    suggestion_id: null,
-                                    text: username,
-                                    link: 'next_link',
-                                },
-                                subtask_id: 'LoginEnterUserIdentifier',
-                            },
-                        ],
-                    },
-                });
-                logger.debug('Twitter login 3 finished: LoginEnterUserIdentifier.');
-
-                const task3 = await got.post('https://api.x.com/1.1/onboarding/task.json', {
-                    headers,
-                    json: {
-                        flow_token: task2.data.flow_token,
-                        subtask_inputs: [
-                            {
-                                enter_password: {
-                                    password,
-                                    link: 'next_link',
-                                },
-                                subtask_id: 'LoginEnterPassword',
-                            },
-                        ],
-                    },
-                });
-                logger.debug('Twitter login 4 finished: LoginEnterPassword.');
-                for (const subtask of task3.data?.subtasks || []) {
+                const runTask = async (subtaskId: string, flowToken: string) => {
+                    if (!(subtaskId in flowTasks)) {
+                        logger.error(`Twitter login flow failed: unknown subtask: ${subtaskId}`);
+                        return null;
+                    }
+                    task = await flowTasks[subtaskId](flowToken);
+                    const subtask = task.data.subtasks.shift();
                     if (subtask.open_account) {
+                        logger.debug('Twitter login success.');
                         return subtask.open_account;
                     }
-                }
+                    subtaskId = subtask.subtask_id;
+                    flowToken = task.data.flow_token;
+                    return await runTask(subtaskId, flowToken);
+                };
 
-                const task4 = await got.post('https://api.x.com/1.1/onboarding/task.json', {
-                    headers,
-                    json: {
-                        flow_token: task3.data.flow_token,
-                        subtask_inputs: [
-                            {
-                                check_logged_in_account: {
-                                    link: 'AccountDuplicationCheck_false',
-                                },
-                                subtask_id: 'AccountDuplicationCheck',
-                            },
-                        ],
-                    },
-                });
-                logger.debug('Twitter login 5 finished: AccountDuplicationCheck.');
+                const subtaskId = task._data.subtasks.shift().subtask_id;
+                const flowToken = task._data.flow_token;
+                authentication = await runTask(subtaskId, flowToken);
 
-                for await (const subtask of task4.data?.subtasks || []) {
-                    if (subtask.open_account) {
-                        authentication = subtask.open_account;
-                        break;
-                    } else if (authenticationSecret && subtask.subtask_id === 'LoginTwoFactorAuthChallenge') {
-                        const token = authenticator.generate(authenticationSecret);
-
-                        const task5 = await got.post('https://api.x.com/1.1/onboarding/task.json', {
-                            headers,
-                            json: {
-                                flow_token: task4.data.flow_token,
-                                subtask_inputs: [
-                                    {
-                                        enter_text: {
-                                            suggestion_id: null,
-                                            text: token,
-                                            link: 'next_link',
-                                        },
-                                        subtask_id: 'LoginTwoFactorAuthChallenge',
-                                    },
-                                ],
-                            },
-                        });
-                        logger.debug('Twitter login 6 finished: LoginTwoFactorAuthChallenge.');
-
-                        for (const subtask of task5.data?.subtasks || []) {
-                            if (subtask.open_account) {
-                                authentication = subtask.open_account;
-                                break;
-                            }
-                        }
-                        break;
-                    } else {
-                        logger.error(`Twitter login 6 failed: unknown subtask: ${subtask.subtask_id}`);
-                    }
-                }
                 if (authentication) {
-                    logger.debug('Twitter login success.');
+                    logger.debug('Twitter login success.', authentication);
                 } else {
-                    logger.error(`Twitter login failed. ${JSON.stringify(task4.data?.subtasks, null, 2)}`);
+                    logger.error(`Twitter login failed. ${JSON.stringify(task.data?.subtasks, null, 2)}`);
                 }
-
-                return authentication;
             } catch (error) {
                 logger.error(`Twitter username ${username} login failed:`, error);
             }
+
+            return authentication as unknown as string | Record<string, any>;
         },
         60 * 60 * 24 * 30, // 30 days
         false
