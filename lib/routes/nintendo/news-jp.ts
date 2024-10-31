ad416b9dcfc06b32c6944595ad1dbdf6af0c8f73
benzking <kingdom.wei@gmail.com>
2024-10-31 11:12:52 +0800

    Update news-jp.ts

--- lib/routes/nintendo/news-jp.ts
+++ lib/routes/nintendo/news-jp.ts
@@ -30,20 +30,20 @@ export const route: Route = {
 
 async function handler(ctx) {
     const response = await got('https://www.nintendo.com/jp/topics/c/api/json_list?key=newtopics');
-    console.log(response);
+    //  console.log(response);
     const data = response.data.slice(0, 10);
-    console.log(data);
+    //  console.log(data);
     const list = data.map((item) => ({
         // 文章标题
-        title: item.title,
+        title: item.title, 
         // 文章链接
-        link: item.topic_url,
+        link: item.topic_url, 
         // 文章发布日期
-        pubDate: parseDate(item.release_date, 'YYYY/M/D HH:mm:ss'), // "release_date": "2024/10/18 17:00:00"
-        itunes_item_image:item.thumbnail.large.medium,
-        category:item.categorylarge.name,
+        pubDate: parseDate(item.release_date, 'YYYY/M/D HH:mm:ss'),  // "release_date": "2024/10/18 17:00:00"
+        itunes_item_image:item.thumbnail.large.medium, 
+        category:item.categorylarge.name, 
     }));
-    console.log(list);
+    //  console.log(list);
     // 获取新闻正文
     const items = await Promise.all(
         list.map((item) =>
@@ -58,11 +58,11 @@ async function handler(ctx) {
             })
         )
     );
-    console.log(items);
+    //  console.log(items);
     return {
-        title: 'Nintendo（日本）主页资讯',
-        link: 'https://www.nintendo.com/jp/topics',
-        description: 'Nintendo JP',
-        item:items,
+        title: 'Nintendo（日本）主页资讯', 
+        link: 'https://www.nintendo.com/jp/topics', 
+        description: 'Nintendo JP', 
+        item:items, 
     };
 }
