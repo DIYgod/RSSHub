396f88529fdcc7ba683f393e0b111fe58817c388
canonnizq <canonnizq@gmail.com>
2024-11-01 20:57:01 +0800

    fix linter errors

--- lib/routes/meta/blog.ts
+++ lib/routes/meta/blog.ts
@@ -17,7 +17,7 @@ async function handler() {
     const baseUrl = 'https://ai.meta.com';
 
     const response = await ofetch(`${baseUrl}/blog/`);
-    const $ = load(response);   
+    const $ = load(response);
 
     const items = $('div._ams_')
         .toArray().map((item) => ({
@@ -26,7 +26,7 @@ async function handler() {
             title: $(item).find('a._amt1 p._amt2').text(),
             description: $(item).find('div._4ik4._4ik5 p._amt3').text(),
             pubDate: parseDate($(item).find('p._amt4').text())
-        }));
+    }));
 
     return {
         title: 'AI at Meta Blog',
