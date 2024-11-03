8a7b1f7845094c5ba04911753fade0239ac0e0b1
Andvari <31068367+dzx-dzx@users.noreply.github.com>
2024-11-03 18:03:20 +0800

    feat(route/caixin): Add full text fetch (#17231)
    * feat(route/caixin): Add full text fetch
    
    * .
    
    * Update blog.ts
    
    * Update utils.ts
    
    * Update utils.ts
    
    * Update utils-fulltext.ts
    
    * Update utils-fulltext.ts

--- lib/routes/caixin/utils.ts
+++ lib/routes/caixin/utils.ts
@@ -6,44 +6,44 @@ import { load } from 'cheerio';
 import { art } from '@/utils/render';
 import path from 'node:path';
 
-const parseArticle = (item, tryGet) =>
-    /\.blog\.caixin\.com$/.test(new URL(item.link).hostname)
-        ? parseBlogArticle(item, tryGet)
-        : tryGet(item.link, async () => {
-              const { data: response } = await got(item.link);
+const parseArticle = async (item) => {
+    if (/\.blog\.caixin\.com$/.test(new URL(item.link).hostname)) {
+        return parseBlogArticle(item);
+    } else {
+        const { data: response } = await got(item.link);
 
-              const $ = load(response);
+        const $ = load(response);
 
-              item.description = art(path.join(__dirname, 'templates/article.art'), {
-                  item,
-                  $,
-              });
+        item.description = art(path.join(__dirname, 'templates/article.art'), {
+            item,
+            $,
+        });
 
-              if (item.audio) {
-                  item.itunes_item_image = item.audio_image_url;
-                  item.enclosure_url = item.audio;
-                  item.enclosure_type = 'audio/mpeg';
-              }
-
-              return item;
-          });
-
-const parseBlogArticle = (item, tryGet) =>
-    tryGet(item.link, async () => {
-        const response = await got(item.link);
-        const $ = load(response.data);
-        const article = $('#the_content').removeAttr('style');
-        article.find('img').removeAttr('style');
-        article
-            .find('p')
-            // Non-breaking space U+00A0, `&nbsp;` in html
-            // element.children[0].data === $(element, article).text()
-            .filter((_, element) => element.children[0].data === String.fromCharCode(160))
-            .remove();
-
-        item.description = article.html();
+        if (item.audio) {
+            item.itunes_item_image = item.audio_image_url;
+            item.enclosure_url = item.audio;
+            item.enclosure_type = 'audio/mpeg';
+        }
 
         return item;
-    });
+    }
+};
+
+const parseBlogArticle = async (item) => {
+    const response = await got(item.link);
+    const $ = load(response.data);
+    const article = $('#the_content').removeAttr('style');
+    article.find('img').removeAttr('style');
+    article
+        .find('p')
+        // Non-breaking space U+00A0, `&nbsp;` in html
+        // element.children[0].data === $(element, article).text()
+        .filter((_, element) => element.children[0].data === String.fromCodePoint(160))
+        .remove();
+
+    item.description = article.html();
+
+    return item;
+};
 
 export { parseArticle, parseBlogArticle };
