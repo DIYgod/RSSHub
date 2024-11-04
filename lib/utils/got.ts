4ffe081eb29512f269b2cb2d5a6c0bb702286e34
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-11-05 00:27:55 +0800

    fix(bilibili): video reply (#17440)
    * fix(bilibili): video reply
    
    * fix
    
    * update

--- lib/utils/got.ts
+++ lib/utils/got.ts
@@ -1,6 +1,10 @@
 import { destr } from 'destr';
 import ofetch from '@/utils/ofetch';
 
+function isObject(o) {
+    return o !== null && typeof o === 'object' && Array.isArray(o) === false;
+}
+
 const getFakeGot = (defaultOptions?: any) => {
     const fakeGot = (request, options?: any) => {
         if (!(typeof request === 'string' || request instanceof Request) && request.url) {
@@ -35,7 +39,15 @@ const getFakeGot = (defaultOptions?: any) => {
             delete options.form;
         }
         if (options?.searchParams) {
-            request += '?' + new URLSearchParams(options.searchParams).toString();
+            request +=
+                '?' +
+                new URLSearchParams(
+                    isObject(options.searchParams)
+                        ? Object.entries(options.searchParams)
+                              .filter(([, value]) => value !== undefined)
+                              .map(([key, value]) => [key, String(value)])
+                        : options.searchParams
+                ).toString();
             delete options.searchParams;
         }
 
