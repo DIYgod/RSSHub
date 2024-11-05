5f841a59b63748a9c29823dad521fcaefee36930
catyyy <zongyejian@gmail.com>
2024-11-05 19:32:39 +0900

    基础功能实现

---
+++ lib/routes/isct/namespace.ts
@@ -0,0 +1,14 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Institute of Science Tokyo',
+    url: 'isct.ac.jp',
+    description: `
+:::tip
+
+:::`,
+
+    ja: {
+        name: '東京科学大学',
+    },
+};
