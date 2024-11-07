c76b8062ba52f1afc2f8a0034162d578d723bd0d
Chikit-L <529158751@qq.com>
2024-11-07 09:38:18 +0800

    feat(route): add 中国地质科学院研究生院

--- lib/routes/cags/namespace.ts
+++ lib/routes/cags/namespace.ts
@@ -1,22 +1,9 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: 'CAGS Edu',
-    url: 'https://edu.cags.ac.cn',
-    description: `
-    :::tip
-    中国地质科学院研究生院网站的部分通知公告可以通过以下 RSS 路由订阅：
-
-    -   通知公告：\`/tzgg\`
-    -   要闻简讯：\`/ywjx\`
-    -   博士生招生：\`/zs-bss\`
-    -   硕士生招生：\`/zs-sss\`
-
-    可以根据不同的需求来订阅对应页面的最新公告信息。
-    :::
-    `,
-
+    name: 'Chines',
+    url: 'http://cags.cgs.gov.cn/',
     zh: {
-        name: '中国地质科学院研究生院',
+        name: '中国地质科学院',
     },
 };
