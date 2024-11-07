958709c398c7c65ac10711f15173e4b52d961d2c
JianLinWei1 <319720040@qq.com>
2024-11-07 16:22:22 +0800

    feat(route):新增新浪黑猫投诉热点投诉

--- lib/routes/sina/tousu.ts
+++ lib/routes/sina/tousu.ts
@@ -1,6 +1,10 @@
+/*
+ * @Descripttion: description
+ * @Author: -0V
+ * @Date: 2024-11-01 10:59:51
+ * @LastEditTime: 2024-11-07 16:11:14
+ */
 import { Route } from '@/types';
-import ofetch from '@/utils/ofetch';
-import { parseDate } from '@/utils/parse-date';
 
 import logger from '@/utils/logger';
 import puppeteer from '@/utils/puppeteer';
@@ -18,48 +22,36 @@ export const route: Route = {
         },
     ],
     handler,
+};
 
-}
-
-
-async function handler(ctx) {
-
+async function handler() {
     const browser = await puppeteer();
-
     const page = await browser.newPage();
-
     const link = 'https://tousu.sina.cn';
-
     logger.http(`Requesting ${link}`);
     await page.goto(link, {
-
         waitUntil: 'networkidle2',
     });
-
     const response = await page.content();
-
     page.close();
     const $ = load(response);
     const blackcatCons = $('div.blackcat-con').toArray();
     const items = blackcatCons.map((item) => {
-        const title = $(item).find('h1.blackcat-hot').text()
-        const link = $(item).find('a.box').attr('href')
-        const description = $(item).find('p').text() + $(item).find('ul.list').text()
-        const author = $(item).find('span.name').text()
+        const title = $(item).find('h1.blackcat-hot').text();
+        const link = $(item).find('a.box').attr('href');
+        const description = $(item).find('p').text() + $(item).find('ul.list').text();
+        const author = $(item).find('span.name').text();
         return {
-            title: title,
-            link: link,
-            description: description,
-            author: author,
-        }
-    })
+            title,
+            link,
+            description,
+            author,
+        };
+    });
     return {
         title: `黑猫投诉-新浪旗下消费者服务平台`,
         link: `https://tousu.sina.cn`,
         description: `feedId:74988831961543680+userId:74002328552935424`,
         item: items,
     };
-
-
-
 }
