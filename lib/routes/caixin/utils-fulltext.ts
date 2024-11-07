ad0eb079fcb92631629d10052a8b77b80f548777
Andvari <31068367+dzx-dzx@users.noreply.github.com>
2024-11-07 23:23:21 +0800

    fix(route/caixin): filter out articles that does not have ID (#17495)

--- lib/routes/caixin/utils-fulltext.ts
+++ lib/routes/caixin/utils-fulltext.ts
@@ -12,6 +12,10 @@ export async function getFulltext(url: string) {
     if (!config.caixin.cookie) {
         return;
     }
+    if (!/(\d+)\.html/.test(url)) {
+        return;
+    }
+    const articleID = url.match(/(\d+)\.html/)[1];
 
     const nonce = crypto.randomUUID().replaceAll('-', '').toUpperCase();
 
@@ -20,7 +24,6 @@ export async function getFulltext(url: string) {
         .find((e) => e.includes('SA_USER_UID'))
         ?.split('=')[1]; //
 
-    const articleID = url.match(/(\d+)\.html/)[1];
     const rawString = `id=${articleID}&uid=${userID}&${nonce}=nonce`;
 
     const sig = new KJUR.crypto.Signature({ alg: 'SHA256withRSA' });
