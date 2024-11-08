89dcf5ed355891ee14134d199db69e3079e1e379
Chikit-L <57130625+Chikit-L@users.noreply.github.com>
2024-11-08 23:00:38 +0800

    feat(route): add 中国地质科学院研究生院 (#17458)
    * 新增 中国地质科学院研究生院 路由
    
    * feat(route): add 中国地质科学院研究生院
    
    * Update namespace.ts
    
    * feat(route): add 中国地质科学院研究生院

---
+++ lib/routes/cags/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Chinese Academy of Geological Sciences',
+    url: 'cags.cgs.gov.cn',
+    zh: {
+        name: '中国地质科学院',
+    },
+};
