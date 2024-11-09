f0b94f3d2a5a733f2572615be77655b37052dace
Ke Xu <kexu567@gmail.com>
2024-11-09 20:21:06 +0800

    Add RSS for Tongji SEM

---
+++ lib/routes/tongji/sem/_utils.ts
@@ -0,0 +1,69 @@
+import got from '@/utils/got';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+
+export async function getNotifByPage(pageNumber: number) {
+    const pageUrl: string = `https://sem.tongji.edu.cn/semch/category/frontpage/notice/page/${pageNumber}`;
+
+    try {
+        const response = await got.get(pageUrl, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.101.76 Safari/537.36',
+            },
+        });
+
+        const html = response.body;
+        const $ = load(html);
+
+        const notifListElements = $('#page-wrap > div.maim_pages > div > div.leftmain_page > div > ul > li');
+
+        return notifListElements.toArray().map((Element) => {
+            const aTagFirst = $(Element).find('a.bt');
+            const aTagSecond = $(Element).find('a.time');
+
+            const title = aTagFirst.attr('title');
+            const href = aTagFirst.attr('href');
+            const time = aTagSecond.text().trim();
+
+            return {
+                title,
+                link: href,
+                pubDate: parseDate(time, 'YYYY-MM-DD'),
+            };
+        });
+    } catch {
+        // console.error(error);
+    }
+    return [];
+}
+
+export async function getLastPageNumber() {
+    try {
+        const response = await got.get('https://sem.tongji.edu.cn/semch/category/frontpage/notice', {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.101.76 Safari/537.36',
+            },
+        });
+        const html = response.body;
+        const $ = load(html);
+
+        const lastPageElement = $('#page-wrap > div.maim_pages > div > div.leftmain_page > div > div > a.extend');
+        const lastPageUrl: string | undefined = lastPageElement.attr('href');
+
+        // console.log(lastPageUrl);
+
+        if (lastPageUrl) {
+            const lastPageNumber = lastPageUrl.match(/page\/(\d+)/)?.[1];
+            if (lastPageNumber) {
+                // console.log(`Last page number: ${lastPageNumber}`);
+                return Number.parseInt(lastPageNumber);
+            } else {
+                // console.error('Failed to extract last page number.');
+            }
+        }
+    } catch {
+        // console.error(error);
+    }
+
+    return -1;
+}
---
+++ lib/routes/tongji/sem/notice.ts
@@ -0,0 +1,40 @@
+// Warning: The author still knows nothing about javascript!
+import { Route } from '@/types';
+
+export const route: Route = {
+    path: '/sem',
+    categories: ['university'],
+    example: '/tongji/sem',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '经济与管理学院通知',
+    maintainers: ['sitdownkevin'],
+    url: 'https://sem.tongji.edu.cn/semch/category/frontpage/notice',
+    handler,
+    description: ``,
+};
+
+import { getNotifByPage } from './_utils';
+
+async function handler() {
+    const promises = [];
+    for (let i = 1; i <= 20; i++) {
+        promises.push(getNotifByPage(i));
+    }
+
+    const results = await Promise.all(promises);
+
+    // feed the data
+    return {
+        title: '同济大学经济与管理学院',
+        link: 'https://bksy.tongji.edu.cn/30359/list.htm',
+        item: results.flat(),
+    };
+}
