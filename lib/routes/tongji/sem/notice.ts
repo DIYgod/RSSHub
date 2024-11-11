95ed539459d43a9ab5047faa3ebf749a7c473152
XU Ke <kexu567@gmail.com>
2024-11-12 01:30:52 +0800

    feat(/tongji/sem): Add new routes for /tongji/sem/* (#17543)
    * Add description for articles
    
    * Update route /tongji/sem
    
    Add notice, enrollment, academic-paper, news, events, focus, collegerecruitment
    
    * Update notice.ts

--- lib/routes/tongji/sem/notice.ts
+++ lib/routes/tongji/sem/notice.ts
@@ -4,10 +4,10 @@ import { getNotifByPage, getArticle } from './_utils';
 import cache from '@/utils/cache';
 
 export const route: Route = {
-    path: '/sem',
+    path: '/sem/:type?',
     categories: ['university'],
-    example: '/tongji/sem',
-    parameters: {},
+    example: '/tongji/sem/notice',
+    parameters: { type: '通知类型，默认为 `notice`' },
     features: {
         requireConfig: false,
         requirePuppeteer: false,
@@ -18,22 +18,38 @@ export const route: Route = {
     },
     name: '经济与管理学院通知',
     maintainers: ['sitdownkevin'],
-    url: 'sem.tongji.edu.cn/semch/category/frontpage/notice',
+    url: 'sem.tongji.edu.cn/semch',
     handler,
-    description: ``,
+    description: `| 学院通知 | 招生通知 | 学术观点 | 新闻 | 活动 | 视点 | 教师与行政人员招聘 |
+| -------- | -------------- | ------------------ | ---- | ---------- | --------- | ------------------ |
+| notice   | enrollment     | academic-paper     | news | events     | focus     | collegerecruitment |
+`,
 };
 
-async function handler() {
-    const results: { title: string; link: string; pubDate: Date }[] = await getNotifByPage();
+async function handler(ctx) {
+    const type = ctx.req.param('type') || 'notice';
+    const subType = new Set(['enrollment', 'academic-paper', 'news', 'events', 'focus', 'collegerecruitment']);
+    const subTypeName = {
+        notice: '学院通知',
+        enrollment: '招生通知',
+        'academic-paper': '学术观点',
+        news: '新闻',
+        events: '活动',
+        focus: '视点',
+        collegerecruitment: '教师与行政人员招聘',
+    };
+
+    const url = `https://sem.tongji.edu.cn/semch/category/frontpage/${subType.has(type) ? type : 'notice'}`;
+
+    const results: { title: string; link: string; pubDate: Date }[] = await getNotifByPage(url);
+
     const resultsWithContent = await Promise.all(results.map((item) => cache.tryGet(item.link, () => getArticle(item))));
 
     // feed the data to rss
     return {
         title: '同济大学经济与管理学院',
-        description: '同济大学经济与管理学院官网通知',
-        language: 'zh-cn',
-        image: 'https://sem.tongji.edu.cn/semch/wp-content/themes/wood-themes-cn/images/pages/page_banner.gif',
-        logo: 'https://tongji.edu.cn/images/badge.png',
+        description: String(subType.has(type) ? subTypeName[type] : '学院通知'),
+        image: 'https://tongji.edu.cn/images/badge.png',
         link: 'https://sem.tongji.edu.cn/semch',
         item: resultsWithContent,
     };
