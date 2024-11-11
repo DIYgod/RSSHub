d44f3b89e9712537708ac192afb310c90e3c8d46
Ke Xu <kexu567@gmail.com>
2024-11-11 10:15:43 +0800

    Add description for articles

--- lib/routes/tongji/sem/notice.ts
+++ lib/routes/tongji/sem/notice.ts
@@ -1,6 +1,7 @@
 // Warning: The author still knows nothing about javascript!
 import { Route } from '@/types';
-import { getNotifByPage } from './_utils';
+import { getNotifByPage, getArticle } from './_utils';
+import cache from '@/utils/cache';
 
 export const route: Route = {
     path: '/sem',
@@ -23,12 +24,17 @@ export const route: Route = {
 };
 
 async function handler() {
-    const results = await getNotifByPage();
+    const results: { title: string; link: string; pubDate: Date }[] = await getNotifByPage();
+    const resultsWithContent = await Promise.all(results.map((item) => cache.tryGet(item.link, () => getArticle(item))));
 
     // feed the data to rss
     return {
         title: '同济大学经济与管理学院',
-        link: 'https://sem.tongji.edu.cn/semch/category/frontpage/notice',
-        item: results,
+        description: '同济大学经济与管理学院官网通知',
+        language: 'zh-cn',
+        image: 'https://sem.tongji.edu.cn/semch/wp-content/themes/wood-themes-cn/images/pages/page_banner.gif',
+        logo: 'https://tongji.edu.cn/images/badge.png',
+        link: 'https://sem.tongji.edu.cn/semch',
+        item: resultsWithContent,
     };
 }
