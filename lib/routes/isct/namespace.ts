489630a1b9bbb718acfb708b0297f55eadc6179a
Cat_yyy <zongyejian@gmail.com>
2024-11-13 11:04:09 +0900

    feat(route): add Science Tokyo News 東京科学大学ニュース (#17550)
    * chore(deps): bump telegram from 2.26.2 to 2.26.6 (#17384)
    
    * chore(deps): bump telegram from 2.26.2 to 2.26.6
    
    Bumps [telegram](https://github.com/gram-js/gramjs) from 2.26.2 to 2.26.6.
    - [Release notes](https://github.com/gram-js/gramjs/releases)
    - [Commits](https://github.com/gram-js/gramjs/commits)
    
    ---
    updated-dependencies:
    - dependency-name: telegram
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: fix pnpm install
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * 基础功能实现
    
    * Update lib/routes/isct/namespace.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * Update lib/routes/isct/news.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ lib/routes/isct/namespace.ts
@@ -0,0 +1,11 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Institute of Science Tokyo',
+    url: 'isct.ac.jp',
+    lang: 'ja',
+
+    ja: {
+        name: '東京科学大学',
+    },
+};
