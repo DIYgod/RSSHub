7118d69afea5832bbe82a09f81917fd199b283d5
Tony <TonyRL@users.noreply.github.com>
2024-11-16 01:06:54 +0800

    fix(bilibili): use default ua

--- lib/routes/bilibili/article.ts
+++ lib/routes/bilibili/article.ts
@@ -57,7 +57,6 @@ async function handler(ctx) {
                             headers: {
                                 Referer: `https://space.bilibili.com/${uid}/article`,
                                 Cookie: cookie,
-                                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
                             },
                         })
                     ).data
