01615ad6f333d8338892240c3eac3358aee829d3
Derekmini <mg.wang.now@gmail.com>
2024-11-23 14:22:23 +0800

    update

--- lib/routes/cnki/author.ts
+++ lib/routes/cnki/author.ts
@@ -32,6 +32,84 @@ async function handler(ctx) {
     const host = 'https://kns.cnki.net';
     const link = `${host}/kns8s/AdvSearch?classid=WD0FTY92`;
 
+    const params = new URLSearchParams();
+    params.append('boolSearch', 'true');
+    params.append(
+        'QueryJson',
+        JSON.stringify({
+            Platform: '',
+            Resource: 'CROSSDB',
+            Classid: 'WD0FTY92',
+            Products: '',
+            QNode: {
+                QGroup: [
+                    {
+                        Key: 'Subject',
+                        Title: '',
+                        Logic: 0,
+                        Items: [],
+                        ChildItems: [
+                            {
+                                Key: 'input[data-tipid=gradetxt-1]',
+                                Title: '作者',
+                                Logic: 0,
+                                Items: [
+                                    {
+                                        Key: 'input[data-tipid=gradetxt-1]',
+                                        Title: '作者',
+                                        Logic: 0,
+                                        Field: 'AU',
+                                        Operator: 'DEFAULT',
+                                        Value: name,
+                                        Value2: '',
+                                    },
+                                ],
+                                ChildItems: [],
+                            },
+                            {
+                                Key: 'input[data-tipid=gradetxt-2]',
+                                Title: '作者单位',
+                                Logic: 0,
+                                Items: [
+                                    {
+                                        Key: 'input[data-tipid=gradetxt-2]',
+                                        Title: '作者单位',
+                                        Logic: 0,
+                                        Field: 'AF',
+                                        Operator: 'FUZZY',
+                                        Value: company,
+                                        Value2: '',
+                                    },
+                                ],
+                                ChildItems: [],
+                            },
+                        ],
+                    },
+                    {
+                        Key: 'ControlGroup',
+                        Title: '',
+                        Logic: 0,
+                        Items: [],
+                        ChildItems: [],
+                    },
+                ],
+            },
+            ExScope: '0',
+            SearchType: 3,
+            Rlang: 'CHINESE',
+            KuaKuCode: 'YSTT4HG0,LSTPFY1C,JUP3MUPD,MPMFIG1A,EMRPGLPA,WQ0UVIAA,BLZOG7CK,PWFIRAGL,NN3FJMUV,NLBO1Z6R',
+        })
+    );
+    params.append('pageNum', '1');
+    params.append('pageSize', '20');
+    params.append('sortField', 'PT');
+    params.append('sortType', 'desc');
+    params.append('dstyle', 'listmode');
+    params.append('productStr', 'YSTT4HG0,LSTPFY1C,RMJLXHZ3,JQIRZIYA,JUP3MUPD,1UR4K4HZ,BPBAFJ5S,R79MZMCB,MPMFIG1A,EMRPGLPA,J708GVCE,ML4DRIDX,WQ0UVIAA,NB3BWEHK,XVLO76FD,HR1YT1Z9,BLZOG7CK,PWFIRAGL,NN3FJMUV,NLBO1Z6R,');
+    params.append('aside', `（作者：${name}(精确)）AND（作者单位：${company}(模糊)）`);
+    params.append('searchFrom', '资源范围：总库;  时间范围：更新时间：不限;');
+    params.append('CurPage', '1');
+
     const response = await ofetch(`${host}/kns8s/brief/grid`, {
         method: 'POST',
         headers: {
