b6dfa40585a754832bfd7283f7b9fefcbbcf7802
Tony <TonyRL@users.noreply.github.com>
2024-11-28 04:38:03 +0800

    fix(route): taptap

--- lib/routes/taptap/namespace.ts
+++ lib/routes/taptap/namespace.ts
@@ -1,8 +1,8 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: 'TapTap 中国',
-    url: 'taptap.com',
+    name: 'TapTap',
+    url: 'www.taptap.io',
     description: `:::warning
 由于区域限制，需要在有国内 IP 的机器上自建才能正常获取 RSS。\
 而对于《TapTap 国际版》则需要部署在具有海外出口的 IP 上才可正常获取 RSS。
