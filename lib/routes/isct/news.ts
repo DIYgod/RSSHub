b0215fe965f6e5808094436a61f7cd713fce1b0c
catyyy <zongyejian@gmail.com>
2024-11-29 00:55:26 +0900

    fix: follow camelCase fix: /isct/news/en

--- lib/routes/isct/news.ts
+++ lib/routes/isct/news.ts
@@ -40,14 +40,14 @@ export const route: Route = {
     maintainers: ['catyyy'],
     handler: async (ctx) => {
         const { lang = 'ja' } = ctx.req.param();
-        const get_media_list = await ofetch(`https://www.isct.ac.jp/expansion/get_media_list_json.php?lang_cd=${lang}`);
-        const get_tag_list = await ofetch(`https://www.isct.ac.jp/expansion/get_tag_list_json.php?lang_cd=${lang}`);
+        const mediaResponse = await ofetch(`https://www.isct.ac.jp/expansion/get_media_list_json.php?lang_cd=${lang}`);
+        const tagResponse = await ofetch(`https://www.isct.ac.jp/expansion/get_tag_list_json.php?lang_cd=${lang}`);
 
-        const media_data = JSON.parse(decode(get_media_list));
-        const tag_data = JSON.parse(decode(get_tag_list));
+        const mediaData = JSON.parse(decode(mediaResponse));
+        const tagData = JSON.parse(decode(tagResponse));
 
-        const itemsArray: MediaItem[] = Object.values(media_data);
-        const tagArray: TagItem[] = Object.values(tag_data);
+        const itemsArray: MediaItem[] = Object.values(mediaData);
+        const tagArray: TagItem[] = Object.values(tagData);
 
         const tagIdNameMapping: { [key: string]: string } = {};
 
@@ -67,7 +67,7 @@ export const route: Route = {
             // 如果有的话，文章作者
             // author: item.user.login,
             // 如果有的话，文章分类
-            category: [tagIdNameMapping[Number.parseInt(item.MEDIA_TYPES.replaceAll('"', ''), 10)]],
+            category: item.MEDIA_TYPES ? [tagIdNameMapping[Number.parseInt(item.MEDIA_TYPES.replaceAll('"', ''), 10)]] : [],
         }));
         return {
             // 源标题
