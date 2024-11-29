76149e8205b198c6b26c15038a9c086e6f837a33
Gerald <1979986188@qq.com>
2024-11-30 01:12:29 +0800

    fix: change description

--- lib/routes/foodtalks/namespace.ts
+++ lib/routes/foodtalks/namespace.ts
@@ -5,6 +5,5 @@ export const namespace: Namespace = {
     url: 'www.foodtalks.cn',
     categories: ['new-media'],
     lang: 'zh-CN',
-    description:
-        'FoodTalks全球食品资讯网是一个提供食品饮料行业新闻、资讯、分析和商业资源的领先在线平台。它涵盖行业趋势、市场动态、产品创新、投融资信息以及企业新闻，连接行业内的专业人士、企业和消费者。feedId:85338156873565184+userId:83135917569925120',
+    description: 'FoodTalks全球食品资讯网是一个提供食品饮料行业新闻、资讯、分析和商业资源的领先在线平台。它涵盖行业趋势、市场动态、产品创新、投融资信息以及企业新闻，连接行业内的专业人士、企业和消费者。',
 };
