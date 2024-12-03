1a88923072bf0de31022d9d9a66a50e90e3d47ef
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2024-12-04 04:44:36 +0800

    feat(route): add Jones Lang LaSalle Trends & Insights (#17788)

---
+++ lib/routes/joneslanglasalle/namespace.ts
@@ -0,0 +1,13 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Jones Lang LaSalle',
+    url: 'joneslanglasalle.com.cn',
+    categories: ['new-media'],
+    description: 'JLL is a global real estate services firm in commercial property and investment management, providing services for real estate owners, occupiers and investors.',
+    lang: 'zh-CN',
+    zh: {
+        name: '仲量联行JLL',
+        description: '仲量联行JLL是全球领先的房地产专业服务和投资管理公司，为企业、房地产业主、投资者及政府提供各类资产的施工、租赁、管理、投资咨询服务。仲量联行也致力于高质量城市发展、打造理想空间、提供可持续的房地产解决方案。',
+    },
+};
