f71160f288223414086b325d3861d06f37e5ff06
iusx <rhyme.qing@gmail.com>
2024-12-10 12:23:45 +0800

    Update namespace.ts

--- lib/routes/isct/namespace.ts
+++ lib/routes/isct/namespace.ts
@@ -8,7 +8,7 @@ export const namespace: Namespace = {
 支持通过 category 参数筛选新闻类别。详情请查看 [指南](https://docs.rsshub.app/zh/guide/parameters#%E5%86%85%E5%AE%B9%E8%BF%87%E6%BB%A4) 。
     
 You can filter news by category through the category parameter. For more information, please refer to the [guide](https://docs.rsshub.app/guide/parameters#filtering).
-    :::`,
+:::`,
     ja: {
         name: '東京科学大学',
     },
