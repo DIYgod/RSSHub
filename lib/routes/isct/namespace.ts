f1c793aca558c51e10fe646d6bc41759db41ca00
iusx <rhyme.qing@gmail.com>
2024-12-10 20:55:52 +0800

    fix(route/isct): TIP overflow layout (#17838)
    * Update namespace.ts
    
    * Update namespace.ts

--- lib/routes/isct/namespace.ts
+++ lib/routes/isct/namespace.ts
@@ -4,10 +4,10 @@ export const namespace: Namespace = {
     name: 'Institute of Science Tokyo',
     url: 'isct.ac.jp',
     lang: 'ja',
-    description: `
-:::tip
-    支持通过category参数筛选新闻类别。详情请查看[指南](https://docs.rsshub.app/zh/guide/parameters#%E5%86%85%E5%AE%B9%E8%BF%87%E6%BB%A4)。
-    You can filter news by category through the category parameter. For more information, please refer to the [guide](https://docs.rsshub.app/guide/parameters#filtering).
+    description: `:::tip
+支持通过 category 参数筛选新闻类别。详情请查看 [指南](https://docs.rsshub.app/zh/guide/parameters#%E5%86%85%E5%AE%B9%E8%BF%87%E6%BB%A4) 。
+    
+You can filter news by category through the category parameter. For more information, please refer to the [guide](https://docs.rsshub.app/guide/parameters#filtering).
 :::`,
     ja: {
         name: '東京科学大学',
