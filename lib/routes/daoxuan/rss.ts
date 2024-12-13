c0bccc6eba388ae9333327c36b20850c0ca51f55
DaoXuan <116349858+dx2331lxz@users.noreply.github.com>
2024-12-13 15:34:47 +0000

    /daoxuan

--- lib/routes/daoxuan/rss.ts
+++ lib/routes/daoxuan/rss.ts
@@ -1,34 +1,43 @@
-// import { Route } from '@/types';
-// import ofetch from '@/utils/ofetch';
-// import { load } from 'cheerio';
-// import { parseDate } from '@/utils/parse-date';
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
 
-// export const route: Route = {
-//     path: '/',
-//     categories: ['blog'],
-//     example: '/daoxuan',
-//     radar: [
-//         {
-//             source: ['daoxuan.cc/'],
-//         },
-//     ],
-//     name: '全部文章',
-//     maintainers: ['dx2331lxz'],
-//     url: 'daoxuan.cc/',
-//     handler: (ctx) => {
-//         const url = 'https://daoxuan.cc/archives/';
-//         const response = await ofetch(url);
-//         const $ = load(response);
-//         const items = $('div.article-sort-item')
-//             .toArray()
-//             .map((item) => {
-//                 item = $(item);
-//                 const a = item.find('a').first();
-//                 return {
-//                     title: a.attr('title'),
-//                     link: `https://daoxuan.cc${a.attr('href')}`,
-//                     pubDate: parseDate(),
-//                 };
-//             });
-//     },
-// };
+export const route: Route = {
+    path: '/',
+    categories: ['blog'],
+    example: '/daoxuan',
+    radar: [
+        {
+            source: ['daoxuan.cc/'],
+        },
+    ],
+    name: '推荐阅读文章',
+    maintainers: ['dx2331lxz'],
+    url: 'daoxuan.cc/',
+    handler,
+};
+
+async function handler() {
+    const url = 'https://daoxuan.cc/';
+    const response = await got({ method: 'get', url });
+    const $ = load(response.data);
+    const items = $('div.recent-post-item')
+        .toArray()
+        .map((item) => {
+            item = $(item);
+            const a = item.find('a.article-title').first();
+            const timeElement = item.find('time').first();
+            return {
+                title: a.attr('title'),
+                link: `https://daoxuan.cc${a.attr('href')}`,
+                pubDate: parseDate(timeElement.attr('datetime')),
+                description: a.attr('title'),
+            };
+        });
+    return {
+        title: '道宣的窝',
+        link: url,
+        item: items,
+    };
+}
