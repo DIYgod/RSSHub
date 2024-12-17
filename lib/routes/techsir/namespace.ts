2eabd0e4db30e065351b823a54dc742b79558d36
Bubu <43925055+p3psi-boo@users.noreply.github.com>
2024-12-17 22:14:03 +0800

    feat(route): add techsir.com (#17840)

---
+++ lib/routes/techsir/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'TechSir',
+    url: 'techsir.com',
+    description: '科技先生 TechSir.Com 是新酷科技创新与创业媒体',
+    lang: 'zh-CN',
+};
