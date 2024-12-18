3dbed6d31baa34789a35e0efb8e1abeb90721500
Cedar <github@fylsen.com>
2024-12-19 07:19:41 +0800

    Update lib/routes/jimmyspa/namespace.ts
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/jimmyspa/namespace.ts
+++ lib/routes/jimmyspa/namespace.ts
@@ -1,7 +1,7 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: '幾米JIMMY S.P.A. Official Website',
+    name: '幾米 JIMMY S.P.A. Official Website',
     url: 'www.jimmyspa.com',
     lang: 'zh-TW',
 };
