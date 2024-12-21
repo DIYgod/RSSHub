a8504e8779352e2b13570384d1b3f635184f524d
Andvari <31068367+dzx-dzx@users.noreply.github.com>
2024-12-21 11:29:41 +0800

    Update lib/routes/copernicium/index.ts
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/copernicium/index.ts
+++ lib/routes/copernicium/index.ts
@@ -35,11 +35,11 @@ async function handler(ctx) {
             },
             type: 0,
         };
-        res = await ofetch(`http://www.copernicium.tw/nr.jsp`, {
+        res = await ofetch('http://www.copernicium.tw/nr.jsp', {
             query: { _reqArgs: reqArgs },
         });
     } else {
-        res = await ofetch(`http://www.copernicium.tw/sys-nr/`, {
+        res = await ofetch('http://www.copernicium.tw/sys-nr/', {
             query: { _reqArgs: { args: {}, type: 15 } },
         });
     }
