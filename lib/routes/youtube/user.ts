5fa210c6817bbc75e56223fafe80f2d925a0178d
Tsuyumi <SnowU1310828@gmail.com>
2024-12-26 10:55:42 +0800

    fix(youtube): add 'www.youtube.com/:username' to radar source

--- lib/routes/youtube/user.ts
+++ lib/routes/youtube/user.ts
@@ -29,7 +29,7 @@ export const route: Route = {
     },
     radar: [
         {
-            source: ['www.youtube.com/user/:username'],
+            source: ['www.youtube.com/user/:username', 'www.youtube.com/:username'],
             target: '/user/:username',
         },
     ],
