f08b5270d482f9d94827655460457ac6df450be2
rjnishant530 <singhnisant530@gmail.com>
2024-12-31 23:36:49 +0530

    fix: finology

--- lib/routes/finology/utils.ts
+++ lib/routes/finology/utils.ts
@@ -4,14 +4,24 @@ import { parseDate } from '@/utils/parse-date';
 import timezone from '@/utils/timezone';
 import { ofetch } from 'ofetch';
 import { DataItem } from '@/types';
+import { ofetch } from 'ofetch';
+import { DataItem } from '@/types';
 
 const getItems = async (url: string, extra: { date: boolean; selector: string }) => {
+const getItems = async (url: string, extra: { date: boolean; selector: string }) => {
     const mainUrl = 'https://insider.finology.in';
+    const response = await ofetch(url);
     const response = await ofetch(url);
     const $ = load(response);
     const listItems = $(extra.selector)
         .toArray()
         .map((item) => {
+            const $item = $(item);
+            const title = $item.find('p.text-m-height').text();
+            const link = $item.find('a').attr('href');
+            const pubDate = extra.date ? timezone(parseDate($item.find('div.text-light p').first().text()), 0) : '';
+            const itunes_item_image = $item.find('img').attr('src');
+            const category = [$item.find('p.pt025').text()];
             const $item = $(item);
             const title = $item.find('p.text-m-height').text();
             const link = $item.find('a').attr('href');
@@ -38,6 +48,20 @@ const getItems = async (url: string, extra: { date: boolean; selector: string })
                     const $ = load(response);
                     const div = $('div.w60.flex.flex-wrap-badge');
                     item.author = div.find('div a p').text();
+                    item.updated = extra.date ? parseDate(div.find('p:contains("Updated on") span').text()) : '';
+                    item.description =
+                        $('div#main-wrapper div#insiderhead')
+                            .find('div.flex.flex-col.w100.align-center')
+                            .children('div.m-position-r')
+                            .remove()
+                            .end()
+                            .find('a[href="https://quest.finology.in/"]')
+                            .remove()
+                            .end()
+                            .find('div.blur-wall-wrap')
+                            .remove()
+                            .end()
+                            .html() ?? '';
                     item.updated = extra.date ? parseDate(div.find('p:contains("Updated on") span').text()) : '';
                     item.description =
                         $('div#main-wrapper div#insiderhead')
