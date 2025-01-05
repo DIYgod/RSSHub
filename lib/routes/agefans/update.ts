dabb3b4d29627b598d73f578ed435dee7f6e14cd
zhoudd <328704089@qq.com>
2025-01-06 00:44:35 +0800

    fix: Fixed the issue of being blocked by the website due to excessive concurrent requests, which resulted in failure to retrieve information.

--- lib/routes/agefans/update.ts
+++ lib/routes/agefans/update.ts
@@ -46,26 +46,27 @@ async function handler() {
             };
         });
 
-    const items = await Promise.all(
-        list.map((item) =>
-            cache.tryGet(item.link, async () => {
-                const detailResponse = await got(item.link);
-                const content = load(detailResponse.data);
+    const items:any[] = []
+    for (const item of list) {
+        const _item = await cache.tryGet(item.link, async () => {
+            const detailResponse = await got(item.link);
+            const content = load(detailResponse.data);
 
-                content('img').each((_, ele) => {
-                    if (ele.attribs['data-original']) {
-                        ele.attribs.src = ele.attribs['data-original'];
-                        delete ele.attribs['data-original'];
-                    }
-                });
-                content('.video_detail_collect').remove();
+            content('img').each((_, ele) => {
+                if (ele.attribs['data-original']) {
+                    ele.attribs.src = ele.attribs['data-original'];
+                    delete ele.attribs['data-original'];
+                }
+            });
+            content('.video_detail_collect').remove();
 
-                item.description = content('.video_detail_left').html();
+            item.description = content('.video_detail_left').html();
 
-                return item;
-            })
-        )
-    );
+            return item;
+        })
+        items.push(_item)
+    }
+
 
     return {
         title: $('title').text(),
