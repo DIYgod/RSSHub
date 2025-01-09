56692e418b68785159e81825c148e88607d97ab4
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2025-01-09 23:27:32 +0800

    feat(route): add difficulty limit to osu latest ranked

--- lib/routes/osu/beatmaps/latest-rank.ts
+++ lib/routes/osu/beatmaps/latest-rank.ts
@@ -2,11 +2,13 @@ import { Data, DataItem, Route } from '@/types';
 import got from '@/utils/got';
 import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
+import cache from '@/utils/cache';
+import { Context } from 'hono';
 
 export const route: Route = {
-    path: '/latestranked',
+    path: '/latest-ranked/:routeParams?',
     categories: ['game'],
-    example: '/osu/latestranked',
+    example: '/osu/latest-ranked/includedMode=osu&includedMode=mania&difficultyLimit=>3&difficultyLimit<=7',
     features: {
         requireConfig: false,
         requirePuppeteer: false,
@@ -15,8 +17,36 @@ export const route: Route = {
         supportPodcast: false,
         supportScihub: false,
     },
+    parameters: {
+        includeMode: {
+            description: 'Determine included mode of beatmaps. Contains this parameter several times to specify multiple mode',
+            default: undefined,
+            options: [
+                {
+                    value: 'osu',
+                    label: 'Osu!',
+                },
+                {
+                    value: 'mania',
+                    label: 'Osu!Mania',
+                },
+                {
+                    value: 'fruits',
+                    label: 'Osu!Catch',
+                },
+                {
+                    value: 'taiko',
+                    label: 'Osu!Taiko',
+                },
+            ],
+        },
+        difficultyLimit: {
+            description:
+                'Lower/upper limit difficulties of the beatmap in the beatmapset item, E.g.: `L3.50`, `U5.0`. Note that the beatmapset will be included as long as one single beatmap in that set satisfy the requirements. `U` and `L` could each be specified once, `difficultyLimit=U3&difficultyLimit=U5` is not allowed.',
+        },
+    },
     name: 'Latest Ranked Beatmap',
-    maintainers: ['JimenezLi'],
+    maintainers: ['nfnfgo'],
     handler,
 };
 
@@ -115,26 +145,75 @@ interface BeatmapInfo {
     pack_tags: string[];
 }
 
-async function handler(): Promise<Data> {
-    const link = `https://osu.ppy.sh/beatmapsets`;
+async function handler(ctx: Context): Promise<Data> {
+    const includedModesStr = ctx.req.param('routeParams');
+    const searchParams = new URL(`https://osu.ppy.sh?${includedModesStr}`).searchParams;
+    const includedModes = searchParams.getAll('includedMode');
+    const difficultyLimits = searchParams.getAll('difficultyLimit');
 
-    const response = await got.get(link);
-    const $ = load(response.data);
+    // fetch beatmap JSON info from website
+    let beatmapsetList = (await cache.tryGet('https://osu.ppy.sh/beatmapsets:JSON', async () => {
+        const link = `https://osu.ppy.sh/beatmapsets`;
 
-    const beatmapInfo = JSON.parse($('#json-beatmaps').html() ?? '{"beatmapsets": undefined}');
+        const response = await got.get(link);
+        const $ = load(response.data);
 
-    const beatmapList: BeatmapInfo[] = beatmapInfo.beatmapsets;
+        const beatmapInfo = JSON.parse($('#json-beatmaps').html() ?? '{"beatmapsets": undefined}');
 
-    if (beatmapList === undefined) {
-        throw new Error('Could not retrieve latest ranked beatmap info from osu! website');
+        const beatmapList: BeatmapInfo[] = beatmapInfo.beatmapsets;
+
+        // Failed to fetch, raise error
+        if (beatmapList === undefined) {
+            throw new Error('Failed to retrieve JSON beatmap info from osu! website');
+        }
+
+        return beatmapList;
+    })) as BeatmapInfo[];
+
+    // Sort beatmap by difficultyRate.desc
+    // This step is necessary even if difficultyLimit not enabled, since we want the beatmap
+    // in RSS description sorted when displayed
+    for (const item of beatmapsetList) {
+        item.beatmaps.sort((a, b) => a.difficulty_rating - b.difficulty_rating);
     }
 
-    const rssItems: DataItem[] = beatmapList.map((item) => {
-        // Format publication date using parseDate utility
-        const pubDate = parseDate(item.ranked_date);
+    // filter beatmapset types
+    // Note:
+    // One Osu beatmapset could actually contains several beatmaps with different game mode.
+    // Here for simplicity we just use the mode of first beatmap in this set for filtering criteria.
+    if (includedModes?.length && includedModes?.length > 0) {
+        beatmapsetList = beatmapsetList.filter((bm) => includedModes.includes(bm.beatmaps[0].mode));
+    }
 
-        // Sort beatmaps by difficulty rating, low to high
-        const sortedBeatmaps = item.beatmaps.sort((a, b) => a.difficulty_rating - b.difficulty_rating);
+    let upperLimit = 99; // Osu! will never have maps with 99+ star rating right?
+    let lowerLimit = 0;
+    if (difficultyLimits && difficultyLimits.length > 0 && difficultyLimits.length < 2) {
+        for (const dfLimit of difficultyLimits) {
+            if (dfLimit.startsWith('U')) {
+                upperLimit = Number.parseFloat(dfLimit.substring(1));
+            } else if (dfLimit.startsWith('L')) {
+                lowerLimit = Number.parseFloat(dfLimit.substring(1));
+            }
+        }
+
+        const difficultyRateFilterFunc = (item: BeatmapInfo): boolean => {
+            if (item.beatmaps.at(0)!.difficulty_rating > upperLimit) {
+                return false;
+            }
+            if (item.beatmaps.at(-1)!.difficulty_rating < lowerLimit) {
+                return false;
+            }
+            return true;
+        };
+
+        beatmapsetList = beatmapsetList.filter((item) => difficultyRateFilterFunc(item));
+    }
+
+    // Construct beatmap feed items
+    const rssItems: DataItem[] = beatmapsetList.map((item) => {
+        // Format publication date using parseDate utility
+        // Here it make sense to consider the ranked date as the pubDate of this item since this is ranked map RSS
+        const pubDate = parseDate(item.ranked_date);
 
         // Select the best resolution cover (2x if available)
         const coverImage = item.covers['cover@2x'] || item.covers.cover;
@@ -162,17 +241,20 @@ async function handler(): Promise<Data> {
                     <th>Difficulty Rating</th>
                     <th>Drain</th>
                 </tr>
-                ${sortedBeatmaps
-                    .map(
-                        (beatmap) => `
+                ${
+                    // Sort beatmap by beatmap difficulty rating desc
+                    item.beatmaps
+                        .map(
+                            (beatmap) => `
                     <tr>
                         <td><a href="${beatmap.url}" target="_blank">${beatmap.version}</a></td>
                         <td>${beatmap.difficulty_rating}</td>
                         <td>${beatmap.drain}</td>
                     </tr>
                 `
-                    )
-                    .join('')}
+                        )
+                        .join('')
+                }
             </table>
         `;
 
@@ -181,7 +263,7 @@ async function handler(): Promise<Data> {
             description,
             pubDate,
             link: `https://osu.ppy.sh/beatmapsets/${item.id}`,
-            category: ['osu!'],
+            category: ['osu!', 'game'],
             author: [{ name: item.creator }],
             image: coverImage,
             banner: bannerImage,
