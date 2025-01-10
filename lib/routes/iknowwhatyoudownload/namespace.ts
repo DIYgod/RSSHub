2d49aa3d011d5a7c74ce03854ff8aa8dc1cb50fe
Bubu <43925055+p3psi-boo@users.noreply.github.com>
2025-01-11 01:01:49 +0800

    feat(route): add iknowwhatyoudownload daily stats (#17977)

---
+++ lib/routes/iknowwhatyoudownload/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'I Know What You Download',
+    url: 'iknowwhatyoudownload.com',
+    description: '',
+    lang: 'en',
+};
