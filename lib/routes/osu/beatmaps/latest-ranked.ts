57f163f5491484c98bacb9dec1145ef84249c13d
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2025-01-13 09:47:07 +0800

    fix(route): Revert previous commit, put desc back to ts file.
    This reverts commit c6b8dc7af17ad2111c4204dc93ec554ac06786d9.

--- lib/routes/osu/beatmaps/latest-ranked.ts
+++ lib/routes/osu/beatmaps/latest-ranked.ts
@@ -1,6 +1,5 @@
 import { Data, DataItem, Route } from '@/types';
 import path from 'node:path';
-import { readFileSync } from 'node:fs';
 import got from '@/utils/got';
 import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
@@ -8,8 +7,52 @@ import cache from '@/utils/cache';
 import { config } from '@/config';
 import { art } from '@/utils/render';
 import { getCurrentPath } from '@/utils/helpers';
-
 const __dirname = getCurrentPath(import.meta.url);
+
+const actualParametersDescTable = `
+| Name              | Default  | Description                                                                                                                                                                                                                                          |
+| ----------------- | -------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| \`includeMode\`     | All mode | Could be \`osu\`, \`mania\`, \`fruits\` or \`taiko\`. Specify included game mode of beatmaps. Including this paramseter multiple times to specify multiple game modes, e.g.: \`includeMode=osu&includeMode=mania\`. Subscribe to all game modes if not specified |
+| \`difficultyLimit\` | None     | Lower/upper limit of star rating of the beatmaps in the beatmapset item, e.g.:\`difficultyLimit=U6\`. Checkout tips in descriptions for detailed explaination and examples.                                                                            |
+| \`modeInTitle\`     | \`true\`   | \`true\` or \`false\` Add mode info into feed title.
+`;
+
+const descriptionDoc: string = `
+Subscribe to the new beatmaps on https://osu.ppy.sh/beatmapsets.
+
+### Parameter Description
+
+Parameters allows you to:
+
+- Filter game mode
+- Limit beatmap difficulty
+- Show/hide game mode in feed title
+
+Below is a table of all allowed parameters passed to \`routeParams\`
+
+${actualParametersDescTable}
+
+This actual parameters should be passed as \`routeParams\` in URL Query String format without \`?\`, e.g.:
+
+    /osu/latest-ranked/modeInTitle=true&includeMode=osu
+
+:::tip
+You could make use of \`difficultyLimit\` paramters to create a "high difficulty/low difficulty only" only feed.
+
+For example, if you only wants to play low star rating beatmap like 1 or 2 star, you could subscribe to:
+
+    /osu/latest-ranked/difficultyLimit=U2
+
+This will filter out all beatmapsets that do not provide at least one beatmap with star rating<=\`2.00\`.
+
+Similarly, you could use lower bound to filter out beatmapsets which don't have at least one beatmap
+with star rating higher than a certain threshold.
+
+    /osu/latest-ranked/difficultyLimit=L6
+
+Now all beatmapsets that don't provided at least one beatmap with star rating higher than \`6.00\` will be filtered.
+:::
+`;
 
 export const route: Route = {
     path: '/latest-ranked/:routeParams?',
@@ -31,7 +74,7 @@ export const route: Route = {
         },
     },
     name: 'Latest Ranked Beatmap',
-    description: readFileSync(path.join(__dirname, 'docs/latest-ranked.md')).toString(),
+    description: descriptionDoc,
     maintainers: ['nfnfgo'],
     radar: [
         {
