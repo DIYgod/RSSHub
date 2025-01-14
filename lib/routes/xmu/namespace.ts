0a049a9df43a7ac064a9bd16ce5ba66fcd78294f
yangqian <1175960101@qq.com>
2025-01-15 01:06:05 +0800

    add 厦门大学经济学院科研动态

---
+++ lib/routes/xmu/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'XMU SOE',
+    url: 'soe.xmu.edu.cn',
+    zh: {
+        name: '厦门大学经济学院',
+    },
+};
