5285493addab116da5bbe3eef705fdecad8f497a
cnk <cscnk52@outlook.com>
2025-01-20 03:11:10 +0800

    fix(typo|routes/hrbust): fix docs and description (#18165)

--- lib/routes/hrbust/news.ts
+++ lib/routes/hrbust/news.ts
@@ -5,8 +5,6 @@ import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
 import timezone from '@/utils/timezone';
 
-const rootUrl = 'https://news.hrbust.edu.cn/';
-
 export const route: Route = {
     path: '/news/:category?',
     name: '新闻网',
@@ -14,10 +12,10 @@ export const route: Route = {
     maintainers: ['cscnk52'],
     handler,
     example: '/hrbust/news',
-    parameters: { category: '栏目标识，默认为理工要闻' },
-    description: `| 理工要闻 | 新闻导读 | 综合新闻   | 教学科研 | 院处动态 | 学术科创 | 交流合作 | 招生就业 | 党建思政 | 在线播放 | 理工校报 | 媒体理工 | 讲座论坛 |
-|------|------|--------|------|------|------|------|------|------|------|------|------|------|
-| lgyw | xwdd | zhenew | jxky | ycdt | xskc | jlhz | zsjy | djsz | zxbf | lgxb | mtlg | jzlt |`,
+    parameters: { category: '栏目标识，默认为 lgyw（理工要闻）' },
+    description: `| 理工要闻 | 新闻导读 | 综合新闻 | 教学科研 | 院处动态 | 学术科创 | 交流合作 | 招生就业 | 党建思政 | 在线播放 | 理工校报 | 媒体理工 | 讲座论坛 | 人才招聘 |
+|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|
+| lgyw     | xwdd     | zhenew   | jxky     | ycdt     | xskc     | jlhz     | zsjy     | djsz     | zxbf     | lgxb     | mtlg     | jzlt     | rczp     |`,
     categories: ['university'],
     features: {
         supportRadar: true,
@@ -36,6 +34,8 @@ export const route: Route = {
 };
 
 async function handler(ctx) {
+    const rootUrl = 'https://news.hrbust.edu.cn/';
+
     const { category = 'lgyw' } = ctx.req.param();
 
     const response = await got(`${rootUrl}${category}.htm`);
@@ -62,7 +62,7 @@ async function handler(ctx) {
         list.map((item) =>
             cache.tryGet(item.link, async () => {
                 if (!item.link.startsWith(rootUrl)) {
-                    item.description = '本文需跳转，请点击标题后阅读';
+                    item.description = '本文需跳转，请点击原文链接后阅读';
                     return item;
                 }
 
