9f5329521bc8639555c7b8eb142ecddc6dbf1376
cnk <cscnk52@outlook.com>
2025-01-22 13:04:40 +0800

    feat(routes/hrbust/news): add support for more column (#18180)
    * feat(routes/hrbust/news): add support for more column
    
    - support author
    - support attachment
    
    * fix(routes/hrbust/news): use camelCase name

--- lib/routes/hrbust/news.ts
+++ lib/routes/hrbust/news.ts
@@ -13,11 +13,17 @@ export const route: Route = {
     handler,
     example: '/hrbust/news',
     parameters: { category: '栏目标识，默认为 lgyw（理工要闻）' },
-    description: `| 理工要闻 | 新闻导读 | 综合新闻 | 教学科研 | 院处动态 | 学术科创 | 交流合作 | 招生就业 | 党建思政 | 在线播放 | 理工校报 | 媒体理工 | 讲座论坛 | 人才招聘 |
-|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|
-| lgyw     | xwdd     | zhenew   | jxky     | ycdt     | xskc     | jlhz     | zsjy     | djsz     | zxbf     | lgxb     | mtlg     | jzlt     | rczp     |`,
+    description: `| 理工要闻 | 新闻导读 | 图文报道 | 综合新闻 | 教学科研 | 院处动态 | 学术科创 | 交流合作 | 学生天地 | 招生就业 | 党建思政 | 在线播放 | 理工人物 | 理工校报 | 媒体理工 | 讲座论坛 | 人才招聘 | 学科建设 |
+|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|
+| lgyw     | xwdd     | twbd     | zhenew   | jxky     | ycdt     | xskc     | jlhz     | xstd     | zsjy     | djsz     | zxbf     | lgrw     | lgxb     | mtlg     | jzlt     | rczp     | xkjs     |`,
     categories: ['university'],
     features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
         supportRadar: true,
     },
     radar: [
@@ -44,7 +50,7 @@ async function handler(ctx) {
 
     const bigTitle = $('title').text().split('-')[0].trim();
 
-    const list = $('div.main-liebiao-con-left-bottom li[id^=line_u10]')
+    const list = $('li[id^=line_u10]')
         .toArray()
         .map((item) => {
             const element = $(item);
@@ -75,7 +81,17 @@ async function handler(ctx) {
                     item.pubDate = pubTime;
                 }
 
-                item.description = content('div.v_news_content').html() || '解析正文失败';
+                const author = content('p.xinxi span:contains("作者：")').text().replace('作者：', '').trim();
+                item.author = author || null;
+
+                const newsContent = content('div.v_news_content') || '解析正文失败';
+                const listAttachments = content('ul[style="list-style-type:none;"] a');
+                let listAttachmentsHtml = '';
+                listAttachments.each((_, a_element) => {
+                    listAttachmentsHtml += '<br />' + content(a_element).prop('outerHTML');
+                });
+
+                item.description = newsContent + listAttachmentsHtml;
                 return item;
             })
         )
