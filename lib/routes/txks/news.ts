49aa57e5a3c12017e7b205024993cd2c89848a3a
PrinOrange <prinorange@outlook.com>
2025-01-22 20:08:50 +0800

    fix: fix news-router name

--- lib/routes/txks/news.ts
+++ lib/routes/txks/news.ts
@@ -82,7 +82,7 @@ const handler: Route['handler'] = async () => {
 
 export const route: Route = {
     path: '/news',
-    name: '全国通信专业技术人员职业水平考试动态',
+    name: '通信考试动态',
     description: '**注意：** 官方网站限制了国外网络请求，可能需要通过部署在中国大陆内的 RSSHub 实例访问。',
     maintainers: ['PrinOrange'],
     handler,
