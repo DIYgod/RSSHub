9a9654983efcb88a73975f50f32fe6b0981b3dcb
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-01-30 00:32:38 +0800

    feat(route): add 机核网文章 (#18238)
    * feat(route): add 机核网文章
    
    * fix: missing reference
    
    * fix typo

--- lib/routes/gcores/articles.ts
+++ lib/routes/gcores/articles.ts
@@ -10,23 +10,23 @@ export const __dirname = getCurrentPath(import.meta.url);
 export const handler = async (ctx: Context): Promise<Data> => {
     const limit: number = Number.parseInt(ctx.req.query('limit') ?? '30', 10);
 
-    const targetUrl: string = new URL('news', baseUrl).href;
-    const apiUrl: string = new URL('gapi/v1/articles', baseUrl).href;
+    const targetUrl: string = new URL('articles', baseUrl).href;
+    const apiUrl: string = new URL(`gapi/v1/articles`, baseUrl).href;
 
     const query = {
-        'filter[is-news]': 1,
+        'filter[is-news]': 0,
     };
 
     return await processItems(limit, query, apiUrl, targetUrl);
 };
 
 export const route: Route = {
-    path: '/news',
-    name: '资讯',
+    path: '/articles',
+    name: '文章',
     url: 'www.gcores.com',
     maintainers: ['nczitzk'],
     handler,
-    example: '/gcores/news',
+    example: '/gcores/articles',
     parameters: undefined,
     description: undefined,
     categories: ['game'],
@@ -41,8 +41,8 @@ export const route: Route = {
     },
     radar: [
         {
-            source: ['www.gcores.com/news'],
-            target: '/gcores/news',
+            source: ['www.gcores.com/articles'],
+            target: '/gcores/articles',
         },
     ],
     view: ViewType.Articles,
