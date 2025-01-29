e4eb2069afb27da79c85aa5f5e3794ccbfeae222
Aleksandr Bogdanov <syn@syn.im>
2025-01-29 20:00:38 +0100

    improve text representation of channel content

--- lib/routes/telegram/tglib/channel.ts
+++ lib/routes/telegram/tglib/channel.ts
@@ -27,8 +27,8 @@ export function getMediaLink(src: string, m: Api.TypeMessageMedia) {
         const vid = (doc.attributes.find((t) => t instanceof Api.DocumentAttributeVideo) ?? { w: 1080, h: 720 }) as {w: number, h: number};
         return `<video controls preload="metadata" poster="${src}?thumb" width="${vid.w / 2}" height="${vid.h / 2}"><source src="${src}" type="${mime}"></video>`;
     }
-    if (mime.startsWith('audio/')) {
-        return `<audio src="${src}"></audio>`;
+    if (doc && mime.startsWith('audio/')) {
+        return `<div>${getAudioTitle(m)}</div><div><audio src="${src}"></audio></div>`;
     }
 
     if (doc && mime.startsWith('application/')) {
@@ -37,7 +37,7 @@ export function getMediaLink(src: string, m: Api.TypeMessageMedia) {
             linkText = ''; // remove filename, it's only an animated sticker
         }
         if ((doc.thumbs?.length ?? 0) > 0) {
-            linkText = `<img src="${src}?thumb" alt=""/><br/>${linkText}`;
+            linkText = `<div><img src="${src}?thumb" alt=""/></div><div>${linkText}</div>`;
         }
         return `<a href="${src}" target="_blank">${linkText}</a>`;
     }
@@ -45,8 +45,8 @@ export function getMediaLink(src: string, m: Api.TypeMessageMedia) {
         return getGeoLink(m.geo);
     }
     if (m instanceof Api.MessageMediaPoll) {
-        return `<h4>${m.poll.quiz ? 'Quiz' : 'Poll'}: ${m.poll.question}</h4><br />
-        <ul>${m.poll.answers.map((a) => `<li>${a.text}</li>`).join('')}</ul>`;
+        return `<h4>${m.poll.quiz ? 'Quiz' : 'Poll'}: ${m.poll.question}</h4>
+        <div><ul>${m.poll.answers.map((a) => `<li>${a.text}</li>`).join('')}</ul></div>`;
     }
     if (m instanceof Api.MessageMediaWebPage) {
         return ''; // a link without a document attach, usually is in the message text, so we can skip here
@@ -55,6 +55,13 @@ export function getMediaLink(src: string, m: Api.TypeMessageMedia) {
         return `Contact: <a href="tel:${m.phoneNumber}" target="_blank">${m.firstName} ${m.lastName} ${m.phoneNumber}</a>`;
         // TODO: download vCard as media ?
     }
+    if (m instanceof Api.MessageMediaInvoice) {
+        let description = m.description;
+        if (m.photo?.url) {
+            description = `<img src="${m.photo?.url}" /><br />${description}`;
+        }
+        return `<h4>${m.test ? 'TEST ' : ''}Invoice: ${m.title}</h4><div>${description}</div>`;
+    }
 
     return m.className;
 }
@@ -62,6 +69,35 @@ export function getMediaLink(src: string, m: Api.TypeMessageMedia) {
 function humanFileSize(size: number) {
     const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
     return (size / Math.pow(1024, i)).toFixed(2) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+}
+
+export function getAudioTitle(x: Api.TypeMessageMedia) {
+    if (x instanceof Api.MessageMediaDocument && x.document instanceof Api.Document) {
+        const attr = x.document.attributes.find((x) => x instanceof Api.DocumentAttributeAudio);
+        if (attr) {
+            return `${attr.performer} - ${attr.title} (${humanDuration(attr.duration)})`;
+        }
+    }
+    return getFilename(x);
+}
+
+export function humanDuration(seconds: number) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    // Format time components with leading zeros if necessary
+    const paddedMinutes = String(minutes).padStart(2, '0');
+    const paddedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    // Construct the time string conditionally
+    if (hours > 0) {
+        return `${hours}:${paddedMinutes}:${paddedSeconds}`; // Show hours, minutes, and seconds
+    } else if (minutes > 0) {
+        return `${minutes}:${paddedSeconds}`; // Show minutes and seconds
+    } else {
+        return `0:${paddedSeconds}`; // Show only seconds
+    }
 }
 
 export default async function handler(ctx: Context) {
@@ -76,22 +112,31 @@ export default async function handler(ctx: Context) {
     let i = 0;
     const item: DataItem[] = [];
     for (const message of messages) {
-        let text = message.text;
+        let text = message.text; // must not be HTML
 
         if (message.fwdFrom?.fromId) {
             const fwdFrom = await client.getEntity(message.fwdFrom.fromId);
-            text = `<b>Forwarded from: ${getDisplayName(fwdFrom)}</b>: ${text}`;
+            text = `Forwarded From: ${getDisplayName(fwdFrom)}: ${text}`;
         }
         const media = await unwrapMedia(message.media, message.peerId);
         if (message.media instanceof Api.MessageMediaStory && media) { // if successfully loaded the story
             const storyFrom = await client.getEntity(message.media.peer);
-            text = `<b>Story from: ${getDisplayName(storyFrom)}</b>: ${text}`;
+            text = `Story From: ${getDisplayName(storyFrom)}: ${text}`;
         }
         if (media) {
             // messages that have no text are shown as if they're one post
             // because in TG only 1 attachment per message is possible
             const src = `${new URL(ctx.req.url).origin}/telegram/media/${username}/${getPeerId(message.peerId)}_${message.id}`;
             attachments.push(getMediaLink(src, media));
+        }
+        if (message.replyMarkup instanceof Api.ReplyInlineMarkup) {
+            for (const buttonRow of message.replyMarkup.rows) {
+                for (const button of buttonRow.buttons) {
+                    if (button instanceof Api.KeyboardButtonUrl) {
+                        attachments.push(`<div><a href="${button.url}" target="_blank">${button.text}</a></div>`);
+                    }
+                }
+            }
         }
         if (text !== '' || ++i === messages.length - 1) {
             let description = attachments.join('<br/>\n');
