befbcf22a57c5a315bfedb2b347edadeb8bbab94
sk22 <sk22@mailbox.org>
2025-02-02 18:27:16 +0100

    feat(route): Add wohnnet.at real estate search engine (#18243)
    * Add wohnnet.at real estate search engine
    
    * fix string concatenation in single line
    
    * add leading slash to path
    
    ---------

---
+++ lib/routes/wohnnet/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'wohnnet.at',
+    url: 'wohnnet.at',
+    description: 'Austrian search engine for real estate',
+    lang: 'de',
+};
