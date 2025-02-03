0680531b85e622d92b0604b32ee59ea9b4e89ee7
Ryan Yao <hello@ryanyao.design>
2025-02-02 23:36:53 -0800

    wainao-reads

--- lib/routes/wainao/namespace.ts
+++ lib/routes/wainao/namespace.ts
@@ -2,7 +2,7 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: '歪脑',
-    url: 'www.wainao.me',
+    url: 'wainao.me',
     description: '歪脑是为讲中文的年轻一代度身定制的新闻杂志。',
     lang: 'zh-CN',
 };
