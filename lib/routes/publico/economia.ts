eac36dbc09c2c4447ad37de6760c0ff9c8bb9055
Adrián Rico <37882755+adrianrico97@users.noreply.github.com>
2025-02-06 05:40:17 +0100

    feat(route): add publico.es (#18242)
    * Add new route: Spanish newspaper publico.es
    
    * Add new route: Spanish newspaper publico.es (II)
    
    * Add new route: Spanish newspaper publico.es (III)
    
    * Add new route: Spanish newspaper publico.es (fix)
    
    * Add new route: Spanish newspaper publico.es (fixes II)

---
+++ lib/routes/publico/economia.ts
@@ -0,0 +1,55 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { load } from 'cheerio';
+
+import getItems from './items-processor';
+
+export const route: Route = {
+    path: '/economia/:subsection?',
+    parameters: {
+        subsection: {
+            description: "Filter by subsection. Check the subsections available on the newspaper's website.",
+        },
+    },
+    categories: ['traditional-media'],
+    example: '/publico/economia',
+    features: {
+        requireConfig: false,
+        requirePuppeteer: true,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['publico.es/economia'],
+            target: '/economia',
+        },
+    ],
+    name: 'Economia',
+    maintainers: ['adrianrico97'],
+    handler,
+};
+
+async function handler(ctx) {
+    const { subsection } = ctx.req.param();
+
+    const rootUrl = 'https://www.publico.es';
+    const currentUrl = subsection ? `${rootUrl}/economia/${subsection}` : `${rootUrl}/economia`;
+
+    const response = await got({
+        method: 'get',
+        url: currentUrl,
+    });
+
+    const $ = load(response.data);
+    const title = $('.article-section h1').text();
+    const items = getItems($);
+
+    return {
+        title: `${title} | Público`,
+        link: currentUrl,
+        item: items,
+    };
+}
---
+++ lib/routes/publico/sociedad.ts
@@ -0,0 +1,55 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { load } from 'cheerio';
+
+import getItems from './items-processor';
+
+export const route: Route = {
+    path: '/sociedad/:subsection?',
+    parameters: {
+        subsection: {
+            description: "Filter by subsection. Check the subsections available on the newspaper's website.",
+        },
+    },
+    categories: ['traditional-media'],
+    example: '/publico/sociedad',
+    features: {
+        requireConfig: false,
+        requirePuppeteer: true,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['publico.es/sociedad'],
+            target: '/sociedad',
+        },
+    ],
+    name: 'Sociedad',
+    maintainers: ['adrianrico97'],
+    handler,
+};
+
+async function handler(ctx) {
+    const { subsection } = ctx.req.param();
+
+    const rootUrl = 'https://www.publico.es';
+    const currentUrl = subsection ? `${rootUrl}/sociedad/${subsection}` : `${rootUrl}/sociedad`;
+
+    const response = await got({
+        method: 'get',
+        url: currentUrl,
+    });
+
+    const $ = load(response.data);
+
+    const items = getItems($);
+
+    return {
+        title: 'Medio Ambiente | Sociedad | Público',
+        link: currentUrl,
+        item: items,
+    };
+}
