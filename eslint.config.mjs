74d31842fce8314eb72274812ec45983cc426960
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2025-02-08 02:00:53 +0800

    fix(route/bilibili): fix manga updates
    Closes https://github.com/DIYgod/RSSHub/issues/18246

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,8 @@ export default [{
         'assets/build/radar-rules.js',
         'lib/routes-deprecated',
         'lib/router.js',
+        // A copy of https://github.com/golang/go/blob/master/lib/wasm/wasm_exec.js
+        'lib/routes/bilibili/wasm-exec.js',
         '**/babel.config.js',
         'scripts/docker/minify-docker.js',
     ],
