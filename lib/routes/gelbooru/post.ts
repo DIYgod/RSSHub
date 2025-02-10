a871d0f4824bbb3dd394bcd6bc9caccbafaed20f
undefined <33629016+magicFeirl@users.noreply.github.com>
2025-02-10 08:46:31 +0000

    feat(route): add image quality option for Gelbooru

--- lib/routes/gelbooru/post.ts
+++ lib/routes/gelbooru/post.ts
@@ -31,32 +31,35 @@ export const route: Route = {
     description: `
 - 默认查询: \`/gelbooru/post\` 功能等同查询 Gelbooru 网站最新的投稿
 - 单标签查询: \`/gelbooru/post/1girl\` 查询 \`1girl\` 的最新投稿
-- 多标签查询: \`\`/gelbooru/post/1girl school_uniform rating:general\`\`
+- 多标签查询: \`/gelbooru/post/1girl school_uniform rating:general\`
 - 更多例子: 请参考 Gelbooru 官方 wiki https://gelbooru.com/index.php?page=wiki&s=&s=view&id=25921
 
 **可选的 URL 参数**
 - apiKey & userId: API Access Credentials 如果需要认证，从 https://gelbooru.com/index.php?page=account&s=options 获取
 - limit 页面返回数据量，默认 40，可选 1 ~ 100
+- imgQuality \`sample\` or \`orig\` 图片质量，默认为 sample
 
-e.g.: \`/gelbooru/post?limit=20&apiKey=xx&userId=xx\`
+e.g.: \`/gelbooru/post?limit=20&apiKey=xx&userId=xx&imgQuality=orig\`
 `,
     handler,
 };
 
-function generatePostDescription(post, link) {
+function generatePostDescription(post, link, quality: 'sample' | 'orig') {
     const { id, source, owner, file_url: fileUrl, tags, score } = post;
     const isURL = URL.canParse(source);
     const sourceHtml = isURL ? `<a href=${source}>Source</a>` : `<span>${source}</span>`;
     const sourceHost = isURL ? new URL(source).host : source;
     const postHtml = `<a href="${link}">#${id}</a>`;
     const uploadByHtml = `Upload by: <a href="https://gelbooru.com/index.php?page=post&s=list&tags=user:${owner}">${owner}</a>`;
+    const imgQualityMap = { sample: 'sample_url', orig: 'file_url' };
+    const contentURL = post[imgQualityMap[quality]] || fileUrl;
 
     // 只有两个视频后缀
     const videoExtList = ['mp4', 'webm'];
     const fileExt = fileUrl.slice(fileUrl.lastIndexOf('.') + 1);
 
     // 默认是图片，但是也考虑视频的情况
-    let contentHtml = `<img src="${fileUrl}" />`;
+    let contentHtml = `<img src="${contentURL}" />`;
     if (videoExtList.includes(fileExt)) {
         contentHtml = `<video controls="true" muted src="${fileUrl}"></video>`;
     }
@@ -74,7 +77,7 @@ function generatePostDescription(post, link) {
 
 async function handler(ctx: Context) {
     const tags = (decodeURIComponent(ctx.req.param('tags')) || '').trim();
-    const { apiKey = '', userId = '', limit = 40 }: { apiKey?: string; userId?: string; limit?: number } = ctx.req.query();
+    const { apiKey = '', userId = '', limit = 40, quality = 'sample' }: { apiKey?: string; userId?: string; limit?: number; quality?: 'sample' | 'orig' } = ctx.req.query();
 
     const response = await got({
         url: 'https://gelbooru.com/index.php',
@@ -104,7 +107,7 @@ async function handler(ctx: Context) {
             link: `https://gelbooru.com/index.php?page=post&s=view&id=${post.id}`,
             author: post.owner,
             pubDate: parseDate(post.created_at),
-            description: generatePostDescription(post, `https://gelbooru.com/index.php?page=post&s=view&id=${post.id}`),
+            description: generatePostDescription(post, `https://gelbooru.com/index.php?page=post&s=view&id=${post.id}`, quality),
             upvotes: post.score,
             updated: parseDate(post.change),
             media: {
