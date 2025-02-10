87d59691f5e82a94a49407272fda7d3919917bde
AiraNadih <128119996+AiraNadih@users.noreply.github.com>
2025-02-11 00:53:47 +0800

    feat(route): add route "4KHD" (#18308)

---
+++ lib/routes/4khd/latest.ts
@@ -0,0 +1,41 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { SUB_NAME_PREFIX, SUB_URL } from './const';
+import loadArticle from './article';
+import { WPPost } from './types';
+
+export const route: Route = {
+    path: '/',
+    categories: ['picture'],
+    example: '/4khd',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['www.4khd.com/'],
+            target: '',
+        },
+    ],
+    name: 'Latest',
+    maintainers: ['AiraNadih'],
+    handler,
+    url: 'www.4khd.com/',
+};
+
+async function handler(ctx) {
+    const limit = Number.parseInt(ctx.req.query('limit')) || 20;
+    const { data: posts } = await got(`${SUB_URL}wp-json/wp/v2/posts?per_page=${limit}`);
+
+    return {
+        title: `${SUB_NAME_PREFIX} - Latest`,
+        link: SUB_URL,
+        item: posts.map((post) => loadArticle(post as WPPost)),
+    };
+}
---
+++ lib/routes/4khd/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '4KHD',
+    url: 'www.4khd.net',
+    description: '4KHD - HD Beautiful Girls',
+    lang: 'en',
+};
