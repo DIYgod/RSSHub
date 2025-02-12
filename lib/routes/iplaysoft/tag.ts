87b27c2ba483cfda7199183213d4aaaed541a32a
cnk <cscnk52@outlook.com>
2025-02-12 08:54:12 +0800

    feat(routes/iplaysoft): use wp-api, add tag category routes (#18332)
    * feat(routes/iplaysoft): use wp-api, add tag category routes
    
    * fix(routes/iplaysoft): change categories to 'program-update'
    
    * fix(routes/iplaysoft): fix index url
    
    * fix(routes/iplaysoft): use date_gmt
    
    ---------

--- lib/routes/iplaysoft/tag.ts
+++ lib/routes/iplaysoft/tag.ts
@@ -1,29 +1,33 @@
 import { Data, DataItem, Route, ViewType } from '@/types';
-import { fetchNewsItems } from './utils';
+import { fetchNewsItems, fetchTag } from './utils';
 
-export const handler = async (): Promise<Data> => {
+export const handler = async (ctx): Promise<Data> => {
+    const slug = ctx.req.param('slug');
+
+    const { id, name } = await fetchTag(slug);
+
     const rootUrl = 'https://www.iplaysoft.com/';
-    const postApiUrl = `${rootUrl}wp-json/wp/v2/posts?_embed`;
+    const postApiUrl = `${rootUrl}wp-json/wp/v2/posts?_embed&tags=${id}`;
 
     const items: DataItem[] = await fetchNewsItems(postApiUrl);
 
     return {
-        title: '异次元软件世界',
+        title: `${name} - 异次元软件世界`,
         description: '软件改变生活',
         language: 'zh-CN',
-        link: rootUrl,
+        link: `${rootUrl}tag/${slug}`,
         item: items,
     };
 };
 
 export const route: Route = {
-    path: '/',
-    name: '首页',
+    path: '/tag/:slug',
+    name: '标签',
     url: 'www.iplaysoft.com',
-    maintainers: ['williamgateszhao', 'cscnk52'],
+    maintainers: ['cscnk52'],
     handler,
-    example: '/iplaysoft',
-    parameters: undefined,
+    example: '/iplaysoft/tag/windows',
+    parameters: { slug: '标签名称' },
     description: undefined,
     categories: ['program-update'],
     features: {
@@ -37,8 +41,8 @@ export const route: Route = {
     },
     radar: [
         {
-            source: ['www.iplaysoft.com'],
-            target: '/',
+            source: ['www.iplaysoft.com/tag/:slug'],
+            target: '/tag/:slug',
         },
     ],
     view: ViewType.Articles,
