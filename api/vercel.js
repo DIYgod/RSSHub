56abcdbe4518a50cb3e0ac8b0a9faae09ad02948
Ray Weitzenberg <ray@mechaneyes.com>
2025-02-19 22:36:35 -0500

    puppeteer verceljsons + package.json

--- api/vercel.js
+++ api/vercel.js
@@ -5,7 +5,16 @@ moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
 const config = require('../lib/config');
 config.set({
     NO_LOGFILES: true,
+    PUPPETEER_WS_ENDPOINT: '',
+    CHROMIUM_EXECUTABLE_PATH: '/tmp/chromium',
+    CHROME_BIN: '/tmp/chromium',
 });
+
+// Configure puppeteer for Vercel
+if (process.env.VERCEL) {
+    process.env.PUPPETEER_BROWSER = 'chrome-aws-lambda';
+    process.env.PUPPETEER_EXECUTABLE_PATH = '/tmp/chromium';
+}
 
 const app = require('../lib/app');
 
