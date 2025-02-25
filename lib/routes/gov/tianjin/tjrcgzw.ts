ac15cdd4e392fd03455345094925885cfe2693dc
李浩宇 <37102468+HaoyuLee@users.noreply.github.com>
2025-02-25 16:07:07 +0800

    feat: add route ‘/gov/tianjin/tjftz-notice/channelId' 、‘/gov/tianjin/tjrcgzw-notice/:cate/:subCate' (#18434)
    * feat: 宁波市人力资源保障局-某分类-公告列表
    
    * feat: 宁波国资委-某分类-公告列表
    
    * Update lib/routes/gov/zj/ningborsjnotice.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * Update lib/routes/gov/zj/ningbogzw-notice.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * Update lib/routes/gov/zj/ningbogzw-notice.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * Update lib/routes/gov/zj/ningborsjnotice.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * fix: description content indent update
    
    * fix: route ‘/gov/zj/search/:websiteid?/:word/:cateid?’ result.item deduplication
    
    * fix: route ‘/gov/zj/search/:websiteid?/:word/:cateid?’ result.item deduplication
    
    * feat: add route ‘/gov/tianjin/tjftz-notice/:channelId'
    
    * feat: add route ‘/gov/tianjin/tjrcgzw-notice/:cate/:subCate'
    
    * Update lib/routes/gov/tianjin/tjrcgzw.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * Update lib/routes/gov/tianjin/tjrcgzw.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * fix: fix lint issues
    
    ---------

---
+++ lib/routes/gov/tianjin/tjrcgzw.ts
@@ -0,0 +1,51 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+export const route: Route = {
+    path: '/tianjin/tjrcgzw-notice/:cate/:subCate',
+    categories: ['government'],
+    example: '/gov/tianjin/tjrcgzw-notice/rczc/sjrczc/',
+    parameters: {
+        channelId: '公告分类id、详细信息点击源网站https://hrss.tj.gov.cn/ztzl/ztzl1/tjrcgzw/请求中寻找',
+    },
+    radar: [
+        {
+            source: ['hrss.tj.gov.cn/ztzl/ztzl1/tjrcgzw/'],
+            target: '/tianjin/tjrcgzw-notice/:cate/:subCate',
+        },
+    ],
+    name: '天津人才工作网-公告',
+    url: 'hrss.tj.gov.cn/ztzl/ztzl1/tjrcgzw/',
+    maintainers: ['HaoyuLee'],
+    async handler(ctx) {
+        const { cate, subCate } = ctx.req.param();
+        const url = `https://hrss.tj.gov.cn/ztzl/ztzl1/tjrcgzw/${cate}/${subCate}/`;
+        const { data: response } = await got(url);
+        const noticeCate = load(response)('.routeBlockAuto').text().trim();
+        const item = load(response)('ul.listUlBox01>li')
+            .toArray()
+            .map((el) => {
+                const $ = load(el);
+                const title = $('a').text().trim();
+                const href = $('a').attr('href') || '';
+                const date = $('span').text().trim();
+                const link = href!.includes('http') ? href : new URL(href, url).href;
+                return {
+                    title: `天津人才工作网:${title}`,
+                    link,
+                    pubDate: parseDate(date),
+                    author: '天津人才工作网',
+                    description: `
+                        <h4>${noticeCate}</h4>
+                        <a href="${link}">${title}</a>
+                    `,
+                };
+            });
+        return {
+            title: '天津人才工作网-公告',
+            link: url,
+            item,
+        };
+    },
+};
