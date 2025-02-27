539010a7da4dc312b00587b31d59b4b454e5e187
AzureG03 <155449067+AzureG03@users.noreply.github.com>
2025-02-27 11:02:06 +0800

    feat(route): add 西南交通大学 (#18463)
    * feat(route): add 西南交通大学计算机学院
    
    * fix: 修正西南交通大学URL
    
    * feat(route): add 西南交通大学体育学院
    
    * fix(route): 西南交通大学体育学院

--- lib/routes/swjtu/scai.ts
+++ lib/routes/swjtu/scai.ts
@@ -5,13 +5,12 @@ import { parseDate } from '@/utils/parse-date';
 import ofetch from '@/utils/ofetch';
 
 const rootURL = 'https://scai.swjtu.edu.cn';
-const pageURL = `${rootURL}/web/page-module.html?mid=B730BEB095B31840`;
 
 export const route: Route = {
-    path: '/scai/bks',
+    path: '/scai/:type',
     categories: ['university'],
     example: '/swjtu/scai/bks',
-    parameters: {},
+    parameters: { type: '通知类型' },
     features: {
         requireConfig: false,
         requirePuppeteer: false,
@@ -26,9 +25,30 @@ export const route: Route = {
         },
     ],
     name: '计算机与人工智能学院',
-    description: '本科生教育',
+    description: `
+| 分区              | 参数         |
+| ----------------- | ----------- |
+| 本科生教育         | bks         |
+| 研究生教育         | yjs         |
+| 学生工作           | xsgz        |
+`,
     maintainers: ['AzureG03', 'SuperJeason'],
     handler,
+};
+
+const partition = {
+    bks: {
+        title: '本科生教育',
+        url: `${rootURL}/web/page-module.html?mid=B730BEB095B31840`,
+    },
+    yjs: {
+        title: '研究生教育',
+        url: `${rootURL}/web/page-module.html?mid=6A69B0E32021446B`,
+    },
+    xsgz: {
+        title: '学生工作',
+        url: `${rootURL}/web/page-module.html?mid=F3D3909EB1861B5D`,
+    },
 };
 
 const getItem = (item, cache) => {
@@ -39,22 +59,14 @@ const getItem = (item, cache) => {
         const res = await ofetch(link);
         const $ = load(res);
 
-        // 尝试获取日期
         let dateText = $('div.news-info span:nth-of-type(2)').text();
+        // 转教务通知时的时间获取方法
         if (!dateText) {
             dateText = $('div.news-top-bar span:nth-of-type(1)').text();
         }
-        if (!dateText) {
-            //   console.error('Date not found for', link);
-            return null; // 或者返回一个默认值
-        }
-
-        const dateMatch = dateText.match(/\d{4}(-|\/|.)\d{1,2}\1\d{1,2}/);
-        if (!dateMatch) {
-            //   console.error('Invalid date format for', link);
-            return null; // 或者返回一个默认值
-        }
-        const pubDate = parseDate(dateMatch[0]);
+        // 'date' may be undefined. and 'parseDate' will return current time.
+        const date = dateText.match(/\d{4}(-|\/|.)\d{1,2}\1\d{1,2}/)?.[0];
+        const pubDate = parseDate(date);
         const description = $('div.content-main').html();
 
         return {
@@ -66,8 +78,10 @@ const getItem = (item, cache) => {
     });
 };
 
-async function handler() {
-    const res = await ofetch(pageURL);
+async function handler(ctx) {
+    const { type } = ctx.req.param();
+    const url = partition[type].url;
+    const res = await ofetch(url);
 
     const $ = load(res);
     const $list = $('div.list-top-item, div.item-wrapper');
@@ -80,8 +94,8 @@ async function handler() {
     );
 
     return {
-        title: '西南交大计算机学院-本科生教育',
-        link: pageURL,
+        title: `西南交大计院-${partition[type].title}`,
+        link: url,
         item: items,
         allowEmpty: true,
     };
