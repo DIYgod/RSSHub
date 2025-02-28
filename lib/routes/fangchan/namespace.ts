f4da409088ea604e9acccf0b6b58c7b279d5df89
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-02-28 09:46:22 +0800

    feat(route): add 中房网 (#18464)

---
+++ lib/routes/fangchan/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '中房网',
+    url: 'fangchan.com',
+    categories: ['new-media'],
+    description: '',
+    lang: 'zh-CN',
+};
