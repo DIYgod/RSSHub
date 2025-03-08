25f5381b7ea24a159be9fae106d8526adcfeade7
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-03-08 23:47:58 +0800

    feat(route): add 龙腾网网站翻译 (#18522)
    * feat(route): add 龙腾网网站翻译
    
    * fix: remove deprecated file

---
+++ lib/routes/ltaaa/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '龙腾网',
+    url: 'ltaaa.cn',
+    categories: ['new-media'],
+    description: '',
+    lang: 'zh-CN',
+};
