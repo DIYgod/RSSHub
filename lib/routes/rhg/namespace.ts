d0151e7253f7511993b29fbe2273710113557c1e
杨阳 <1002522146@qq.com>
2025-03-09 11:03:40 +0800

    feat(route): add 荣鼎咨询

---
+++ lib/routes/rhg/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '荣鼎咨询',
+    url: 'rhg.com',
+    description: '荣鼎咨询研究报告',
+    zh: {
+        name: '给他哈不',
+    },
+};
