ff72bcb6e4321e91e5440167d4a5a1fc03d40f64
MY_Liee <leftongyu@gmail.com>
2025-03-12 01:03:19 +0800

    fix(route/xjtu): remove unnecessary filter

--- lib/routes/xjtu/yz.ts
+++ lib/routes/xjtu/yz.ts
@@ -51,23 +51,21 @@ async function handler(ctx) {
                 pubDate: timezone(parseDate(item.find('span.date.fr').text()), +8),
             } as DataItem;
         });
-    const items = (
-        await Promise.all(
-            list.map((item) =>
-                cache.tryGet(item.link!, async () => {
-                    try {
-                        const res = await ofetch(item.link!);
-                        const content = load(res);
-                        item.description = content('#vsb_content').html()! + (content('form ul').length > 0 ? content('form ul').html() : '');
-                        return item;
-                    } catch (error) {
-                        logger.error(`Fetch failed for ${item.link}:`, error);
-                        return item;
-                    }
-                })
-            )
+    const items = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link!, async () => {
+                try {
+                    const res = await ofetch(item.link!);
+                    const content = load(res);
+                    item.description = content('#vsb_content').html()! + (content('form ul').length > 0 ? content('form ul').html() : '');
+                    return item;
+                } catch (error) {
+                    logger.error(`Fetch failed for ${item.link}:`, error);
+                    return item;
+                }
+            })
         )
-    ).filter((item) => item !== null);
+    );
     return {
         title: '西安交通大学研究生招生信息网',
         link: 'https://yz.xjtu.edu.cn',
