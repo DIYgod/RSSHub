74ccf014e3b86be3de5899f4c866715e8b4d36cf
CanonNi <canonnizq@gmail.com>
2025-03-15 21:59:06 +0800

    feat(route): add cherrytimes

---
+++ lib/routes/cherrytimes/market.ts
@@ -0,0 +1,38 @@
+import { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+import { load } from 'cheerio';
+
+export const route: Route = {
+    path: '/market',
+    categories: ['new-media'],
+    example: '/cherrytimes/market',
+    name: 'Market',
+    maintainers: ['canonnizq'],
+
+    handler: async (ctx) => {
+        let items = [];
+
+        for (let i = 1; i <= 10; i++) {
+            const response = await ofetch(`https://cherrytimes.it/en/tag/markets?page=${i}`);
+            const $ = load(response);
+
+            items = items.concat($('div.post-container')
+                .toArray().map((item) => {
+                    item = $(item);
+                    const a = item.find('a').eq(2);
+                    return {
+                        title: a.text(),
+                        link: a.attr('href'),
+                        description: item.find('p.excerpt').text(),
+                        category: item.find('a').last().text()
+                    }
+                }))
+        }
+
+        return {
+            title: 'Market',
+            link: 'https://cherrytimes.it/en/tag/markets',
+            item: items
+        }
+    },
+};
