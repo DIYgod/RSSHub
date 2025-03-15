9a42cf8a2e8aa45dd7003efb829575e9a118a468
CanonNi <canonnizq@gmail.com>
2025-03-15 22:11:30 +0800

    fix lint errors

--- lib/routes/cherrytimes/market.ts
+++ lib/routes/cherrytimes/market.ts
@@ -19,20 +19,20 @@ export const route: Route = {
             items = items.concat($('div.post-container')
                 .toArray().map((item) => {
                     item = $(item);
-                    const a = item.find('a').eq(2);
+                    const a = item.find('a').eq(1);
                     return {
                         title: a.text(),
                         link: a.attr('href'),
                         description: item.find('p.excerpt').text(),
                         category: item.find('a').last().text()
-                    }
-                }))
+                    };
+                }));
         }
 
         return {
             title: 'Market',
             link: 'https://cherrytimes.it/en/tag/markets',
             item: items
-        }
+        };
     },
 };
