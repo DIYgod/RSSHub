25b526f87380c5c3774ee7721776c9887c47006c
CanonNi <canonnizq@gmail.com>
2025-03-15 22:21:59 +0800

    more lint errors

--- lib/routes/cherrytimes/market.ts
+++ lib/routes/cherrytimes/market.ts
@@ -11,9 +11,9 @@ export const route: Route = {
 
     handler: async () => {
         const urls = Array.from({ length: 10 }, (_, i) => `https://cherrytimes.it/en/tag/markets?page=${i + 1}`);
-        const responses = await Promise.all(urls.map(url => ofetch(url)));
+        const responses = await Promise.all(urls.map((url) => ofetch(url)));
 
-        const items = responses.flatMap(response => {
+        const items = responses.flatMap((response) => {
             const $ = load(response);
             return $('div.post-container').toArray().map((item) => {
                 const element = $(item);
