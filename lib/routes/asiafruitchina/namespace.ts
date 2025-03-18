b98a8317db00cd5ca2691a9ccea049838dd481af
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-03-19 04:24:18 +0800

    feat(route): add 亚洲水果果蔬品项 (#18626)
    * feat(route): add 亚洲水果果蔬品项
    
    * fix: incorrect pr format

---
+++ lib/routes/asiafruitchina/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '亚洲水果',
+    url: 'asiafruitchina.net',
+    categories: ['new-media'],
+    description: '',
+    lang: 'zh-CN',
+};
