fd741f15411684aefc50db6b1adcfc739ac05657
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-03-22 20:42:58 +0800

    feat(route): add MyCard娛樂中心遊戲新聞 (#18663)

---
+++ lib/routes/mycard520/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'MyCard娛樂中心',
+    url: 'mycard520.com.tw',
+    categories: ['game'],
+    description: '',
+    lang: 'zh-TW',
+};
