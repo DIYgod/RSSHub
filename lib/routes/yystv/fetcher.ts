24c2ea3be390ae56930e2b7585b3929a92554124
Jiahao Lee <lijiahao34@live.com>
2025-04-02 03:10:56 +0800

    fix(route/yystv): fix cache collisions (#18739)
    * fix(route/yystv): fix cache collisions induced by these two implementations
    
    * chore(route/yystv): prefer existing types over new types
    
    * fix(route/yystv): prefer caching processed items over raw web pages
    
    * docs: fix maintainer github id
    
    ---------

---
+++ lib/routes/yystv/fetcher.ts
@@ -0,0 +1,10 @@
+import { DataItem } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+
+export function fetchDataItemCached(link: string, processor: (articleContent: string) => DataItem): Promise<DataItem> {
+    return cache.tryGet(link, async () => {
+        const page = await ofetch(link);
+        return processor(page);
+    }) as Promise<DataItem>;
+}
