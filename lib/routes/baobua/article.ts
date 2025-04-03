a97ee175b7966995798ff0a6b9eda04aeb9567e2
AiraNadih <128119996+AiraNadih@users.noreply.github.com>
2025-04-03 00:31:01 +0000

    feat(route): add route "BaoBua"

---
+++ lib/routes/baobua/article.ts
@@ -0,0 +1,54 @@
+import { load } from 'cheerio';
+import got from '@/utils/got';
+import { parseDate } from '@/utils/parse-date';
+
+async function loadArticle(link) {
+    const resp = await got(link);
+    const article = load(resp.body);
+
+    const title = article('title')
+        .text()
+        .replace('BaoBua.Com:', '')
+        .replace(/\| Page \d+\/\d+/, '')
+        .trim();
+    const totalPagesRegex = /Page \d+\/(\d+)/;
+    const totalPagesMatch = totalPagesRegex.exec(article('title').text());
+    const totalPages = totalPagesMatch ? Number.parseInt(totalPagesMatch[1]) : 1;
+
+    let pubDate = new Date();
+    const jsonldScripts = article('script[type="application/ld+json"]').toArray();
+    for (const script of jsonldScripts) {
+        const jsonData = JSON.parse(load(script).text() || '');
+        if (jsonData['@type'] === 'BlogPosting') {
+            pubDate = jsonData.datePublished ? parseDate(jsonData.datePublished) : new Date();
+            break;
+        }
+    }
+
+    const contentDiv = article('.contentme2');
+    let description = contentDiv.html() ?? '';
+
+    if (totalPages > 1) {
+        const additionalContents = await Promise.all(
+            Array.from({ length: totalPages - 1 }, async (_, i) => {
+                try {
+                    const response = await got(`${link}?page=${i + 2}`);
+                    const pageDom = load(response.body);
+                    return pageDom('.contentme2').html() ?? '';
+                } catch {
+                    return '';
+                }
+            })
+        );
+        description += additionalContents.join('');
+    }
+
+    return {
+        title,
+        description,
+        pubDate,
+        link,
+    };
+}
+
+export default loadArticle;
