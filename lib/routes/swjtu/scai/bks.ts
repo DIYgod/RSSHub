72ec84a16cc98e2e2e438f0617883025338ae250
superjeason <2675621480@qq.com>
2025-04-12 17:15:54 +0800

    fix(route): fix swjtu/scai/bks route)

--- lib/routes/swjtu/scai/bks.ts
+++ lib/routes/swjtu/scai/bks.ts
@@ -34,29 +34,40 @@ export const route: Route = {
 const getItem = (item, cache) => {
     const title = item.find('a').text();
     const link = `${rootURL}${item.find('a').attr('href').slice(2)}`;
+    // console.log(link);
 
     return cache.tryGet(link, async () => {
         const res = await ofetch(link);
         const $ = load(res);
 
+        let pubDate = new Date();
         // 尝试获取日期
         let dateText = $('div.news-info span:nth-of-type(2)').text();
         if (!dateText) {
             dateText = $('div.news-top-bar span:nth-of-type(1)').text();
         }
-        if (!dateText) {
-            //   console.error('Date not found for', link);
-            return null; // 或者返回一个默认值
+        // 转其他院的通知，获取不到具体时间，先从列表页获取具体信息
+        if (dateText) {
+            const dateMatch = dateText.match(/\d{4}(-|\/|.)\d{1,2}\1\d{1,2}/);
+            if (!dateMatch || !dateMatch[0]) {
+                return null;
+            }
+            pubDate = parseDate(dateMatch[0]);
+        } else {
+            const dateItem = item.find('.calendar'); // 注意 .calendar 是 class
+            const day = dateItem.find('.day').text().trim(); // "31" （文本需 trim 去空格）
+            const ymd = dateItem.find('.date').text().trim(); // "2025/03"
+            const [year, month] = ymd.split('/'); // ["2025", "03"]
+            const dateText = `${year}-${month}-${day.padStart(2, '0')}`;
+            pubDate = new Date(dateText);
         }
 
-        const dateMatch = dateText.match(/\d{4}(-|\/|.)\d{1,2}\1\d{1,2}/);
-        if (!dateMatch) {
-            //   console.error('Invalid date format for', link);
-            return null; // 或者返回一个默认值
-        }
-        const pubDate = parseDate(dateMatch[0]);
-        const description = $('div.content-main').html();
-
+        // console.log(pubDate);
+        // console.log(title);
+        let description = $('div.content-main').html();
+        const $content = $('div.content-main');
+        description = $content.length === 0 ? '暂无内容' : $content.html() || '暂无内容';
+        // console.log(title,pubDate,link)
         return {
             title,
             pubDate,
