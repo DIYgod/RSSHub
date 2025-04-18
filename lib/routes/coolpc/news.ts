c19449aadf5a90951c82a3ed23eedbdec8af0df1
David Tsai <david90103@gmail.com>
2025-04-18 21:27:50 +0800

    fix(route): fix pr issues

--- lib/routes/coolpc/news.ts
+++ lib/routes/coolpc/news.ts
@@ -1,14 +1,7 @@
-import { Route } from '@/types';
+import { DataItem, Route } from '@/types';
 import got from '@/utils/got';
 import { load } from 'cheerio';
 import { parseDate } from '@/utils/parse-date';
-
-type CoolPcArticle = {
-    title: string;
-    description: string;
-    link: string | undefined;
-    pubDate: Date;
-};
 
 export const route: Route = {
     path: '/news',
@@ -42,18 +35,15 @@ async function handler() {
 
     const $ = load(response.data);
 
-    const distinctItems: CoolPcArticle[] = [];
-    for (const item of $('#content article').toArray()) {
-        const title = $(item).find('h3 a').text();
-        if (distinctItems.filter((i) => i.title === title).length === 0) {
-            distinctItems.push({
-                title,
-                description: $(item).find('.ultimate-layouts-excerpt').text(),
-                link: $(item).find('h3 a').attr('href'),
-                pubDate: parseDate($(item).find('span').text(), 'YYYY/MM/DD'),
-            });
-        }
-    }
+    const distinctItems: DataItem[] = $('#content article')
+        .toArray()
+        .map((item) => ({
+            title: $(item).find('h3 a').text(),
+            description: $(item).find('.ultimate-layouts-excerpt').text(),
+            link: $(item).find('h3 a').attr('href'),
+            pubDate: parseDate($(item).find('span').text(), 'YYYY/MM/DD'),
+        }))
+        .filter((item, index, self) => index === self.findIndex((i) => i.title === item.title));
 
     return {
         title: '原價屋 - 促銷&開箱',
