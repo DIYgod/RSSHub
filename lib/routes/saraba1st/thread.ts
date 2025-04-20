7c6925e711d0418909111a949e71af60a46130eb
CrackTC <chenrke369@gmail.com>
2025-04-21 01:02:02 +0800

    fix(route/saraba1st): add host config & change default host

--- lib/routes/saraba1st/thread.ts
+++ lib/routes/saraba1st/thread.ts
@@ -9,7 +9,7 @@ import timezone from '@/utils/timezone';
 export const route: Route = {
     path: '/thread/:tid',
     categories: ['bbs'],
-    example: '/saraba1st/thread/1842868',
+    example: '/saraba1st/thread/751272',
     parameters: { tid: '帖子 id' },
     features: {
         requireConfig: false,
@@ -22,14 +22,15 @@ export const route: Route = {
     name: '帖子',
     maintainers: ['zengxs'],
     handler,
-    description: `帖子网址如果为 \`https://bbs.saraba1st.com/2b/thread-1842868-1-1.html\` 那么帖子 id 就是 \`1789863\`。`,
+    description: `帖子网址如果为 \`https://stage1st.com/2b/thread-751272-1-1.html\` 那么帖子 id 就是 \`751272\`。`,
 };
 
 async function handler(ctx) {
     const tid = ctx.req.param('tid');
     const cookieString = config.saraba1st.cookie ?? '';
+    const host = config.saraba1st.host;
 
-    const res = await got('https://bbs.saraba1st.com/2b/forum.php', {
+    const res = await got(`${host}/2b/forum.php`, {
         searchParams: queryString.stringify({
             mod: 'viewthread',
             tid,
@@ -49,6 +50,9 @@ async function handler(ctx) {
     for (let i = 0; i < Math.min(list.length, 20); i++) {
         count.push(i);
     }
+
+    const staticUrl = new URL('/image/common/none.gif', host);
+    staticUrl.hostname = `static.${staticUrl.hostname.split('.').slice(-2).join('.')}`;
     const resultItems = count.map((i) => {
         const each = $(list[i]);
         const floor = each.find('td.plc .pi a > em').text();
@@ -56,7 +60,7 @@ async function handler(ctx) {
         const contentHtml = $(each.find('td.t_f'));
         const imgsHtml = contentHtml.find('img');
         for (const element of imgsHtml) {
-            if (element.attribs.src === 'https://static.saraba1st.com/image/common/none.gif') {
+            if (element.attribs.src === staticUrl.href) {
                 element.attribs.src = element.attribs.file;
                 const imgHtml = $(element);
                 imgHtml.removeAttr('zoomfile');
@@ -68,7 +72,7 @@ async function handler(ctx) {
         contentHtml.find('div.aimg_tip').remove();
         return {
             title: `${title} #${floor}`,
-            link: new URL(floorUrl, 'https://bbs.saraba1st.com/2b/').href,
+            link: new URL(floorUrl, `${host}/2b/`).href,
             description: contentHtml.html(),
             author: each.find('.authi .xw1').text(),
             pubDate: timezone(parseDate(each.find('.authi em').text()), +8),
@@ -77,7 +81,7 @@ async function handler(ctx) {
 
     return {
         title: `Stage1 论坛 - ${title}`,
-        link: `https://bbs.saraba1st.com/2b/thread-${tid}-1-1.html`,
+        link: `${host}/2b/thread-${tid}-1-1.html`,
         item: resultItems,
     };
 }
