5098a77633c1b0c2a75400f032f837265ef114e7
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-04-23 03:20:35 +0800

    feat(route): add AFL-CIO Blog (#18905)

---
+++ lib/routes/aflcio/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'AFL-CIO',
+    url: 'aflcio.org',
+    categories: ['other'],
+    description: '',
+    lang: 'en',
+};
