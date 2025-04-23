35faba3bcb0d8fdb2349e5cbb95a2b56ccc6f59a
Tony <TonyRL@users.noreply.github.com>
2025-04-24 02:18:02 +0800

    revert: "chore: minify docker (#18914)" (#18931)
    This reverts commit c3da77c893aeba24a3a1dfb819fa5133f73866c4.

--- scripts/docker/minify-docker.js
+++ scripts/docker/minify-docker.js
@@ -1,23 +1,25 @@
 /* eslint-disable no-console */
-import fs from 'fs-extra';
-import path from 'node:path';
-import { nodeFileTrace } from '@vercel/nft';
+const fs = require('fs-extra');
+const path = require('path');
+const { nodeFileTrace } = require('@vercel/nft');
 // !!! if any new dependencies are added, update the Dockerfile !!!
 
-const projectRoot = path.resolve(process.env.PROJECT_ROOT || path.join(path.dirname(new URL(import.meta.url).pathname), '../..'));
+const projectRoot = path.resolve(process.env.PROJECT_ROOT || path.join(__dirname, '../..'));
 const resultFolder = path.join(projectRoot, 'app-minimal'); // no need to resolve, ProjectRoot is always absolute
-const files = ['dist/index.js', 'api/vercel.ts', 'node_modules/cross-env/src/bin/cross-env.js', 'node_modules/.bin/cross-env'].map((file) => path.join(projectRoot, file));
+const files = ['lib/index.ts', 'api/vercel.js'].map((file) => path.join(projectRoot, file));
 
-console.log('Start analyzing, project root:', projectRoot);
-const { fileList: fileSet } = await nodeFileTrace(files, {
-    base: projectRoot,
-});
-let fileList = [...fileSet];
-console.log('Total touchable files:', fileList.length);
-fileList = fileList.filter((file) => file.startsWith('node_modules/')); // only need node_modules
-console.log('Total files need to be copied (touchable files in node_modules/):', fileList.length);
-console.log('Start copying files, destination:', resultFolder);
-await Promise.all(fileList.map((e) => fs.copy(path.join(projectRoot, e), path.join(resultFolder, e)))).catch((error) => {
+(async () => {
+    console.log('Start analyzing, project root:', projectRoot);
+    const { fileList: fileSet } = await nodeFileTrace(files, {
+        base: projectRoot,
+    });
+    let fileList = [...fileSet];
+    console.log('Total touchable files:', fileList.length);
+    fileList = fileList.filter((file) => file.startsWith('node_modules/')); // only need node_modules
+    console.log('Total files need to be copied (touchable files in node_modules/):', fileList.length);
+    console.log('Start copying files, destination:', resultFolder);
+    return Promise.all(fileList.map((e) => fs.copy(path.join(projectRoot, e), path.join(resultFolder, e))));
+})().catch((error) => {
     // fix unhandled promise rejections
     console.error(error, error.stack);
     process.exit(1);
