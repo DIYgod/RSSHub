d4913eb27e4e40573357619ba3b2e35bbd3f9edd
Gus Libens <27970303+goestav@users.noreply.github.com>
2025-04-27 21:06:11 +0200

    refactor: read query string parameter instead of path parameter

--- lib/routes/openai/news.ts
+++ lib/routes/openai/news.ts
@@ -1,5 +1,6 @@
 import { Route } from '@/types';
 import { fetchArticles, BASE_URL } from './common';
+import { Context } from 'hono';
 
 export const route: Route = {
     path: '/news',
@@ -18,8 +19,8 @@ export const route: Route = {
     handler,
 };
 
-async function handler(ctx) {
-    const limit = Number.parseInt(ctx.req.param('limit') || '10');
+async function handler(ctx: Context) {
+    const limit = Number.parseInt(ctx.req.query('limit') || '10');
 
     const link = new URL('/news/', BASE_URL).href;
 
