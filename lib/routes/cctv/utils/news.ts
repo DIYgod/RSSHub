f19a725157ff7ea4c0bdd04f7708ff387b2926d8
Tony <TonyRL@users.noreply.github.com>
2025-04-30 04:16:32 +0800

    refactor: fix all no array reduce (#18973)
    * refactor: no-array-reduce cleanup
    
    * refactor: keep
    
    * refactor: nhk
    
    * refactor: rattibha
    
    * fix: cctv
    
    * fix: jiemian
    
    * refactor: juejin
    
    * refactor: kpopping
    
    * refactor: yoasobi-music
    
    * refactor: mycard520
    
    * refactor: newrank
    
    * refactor: sse
    
    * refactor: zhihu
    
    * feat!: remove trending
    
    https://github.com/huqi-pr/trending-in-one is archived and no longer being updated.
    https://github.com/huqi-pr/trending-in-one/commit/2ef4cd236fcccb908be7907d5ac9aab6f1f4a188
    
    * refactor: sspai
    
    * refactor: sysu
    
    * refactor: nikkei
    
    * refactor: utils
    
    * refactor: mydrivers
    
    * refactor: scmp
    
    * refactor: gov/moa
    
    * refactor: bloomberg
    
    * refactor: manhuagui
    
    * refactor: twitter

--- lib/routes/cctv/utils/news.ts
+++ lib/routes/cctv/utils/news.ts
@@ -70,14 +70,14 @@ const getNews = async (category) => {
                             break;
 
                         case 'PHO':
-                            description = data.photo_album_list.reduce((description, { photo_url, photo_name, photo_brief }) => {
+                            description = '';
+                            for (const { photo_url, photo_name, photo_brief } of data.photo_album_list) {
                                 description += `
                                     <img src=${photo_url} /><br>
                                     <strong>${photo_name}</strong><br>
                                     ${photo_brief}<br>
                                 `;
-                                return description;
-                            }, '');
+                            }
                             author = data.source;
                             break;
 
