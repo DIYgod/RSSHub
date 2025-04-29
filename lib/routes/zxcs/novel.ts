007dba9304b48fac4debe89e7e2af959c73663b1
mozhu <lcl_em@163.com>
2025-04-29 10:36:24 +0800

    fix:route description change

--- lib/routes/zxcs/novel.ts
+++ lib/routes/zxcs/novel.ts
@@ -4,6 +4,22 @@ import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
 import type { Route, DataItem } from '@/types';
 
+const types = {
+    jinqigengxin: '近期更新',
+    dushi: '都市',
+    xianxia: '仙侠',
+    xuanhuan: '玄幻',
+    qihuan: ' 奇幻',
+    lishi: '历史',
+    youxi: '游戏',
+    wuxia: '武侠',
+    kehuan: '科幻',
+    tiyu: '体育',
+    lingyi: '灵异',
+    junshi: '军事',
+    erciyuan: '轻小说',
+};
+
 export const route: Route = {
     path: '/novel/:type',
     name: '小说列表',
@@ -11,21 +27,9 @@ export const route: Route = {
     maintainers: ['liaocl'],
     example: '/zxcs/novel/jinqigengxin',
     parameters: { type: '小说类型, 可在对应类型页 URL 中找到' },
-    description: `支持小说类型：
-    - jinqigengxin 近期更新
-    - dushi 都市
-    - xianxia 仙侠
-    - xuanhuan 玄幻
-    - qihuan 奇幻
-    - lishi 历史
-    - youxi 游戏
-    - wuxia 武侠
-    - kehuan 科幻
-    - tiyu 体育
-    - lingyi 灵异
-    - junshi 军事
-    - erciyuan 轻小说
-    `,
+    description: `支持小说类型：${Object.entries(types)
+        .map(([key, value]) => `${key}-${value}`)
+        .join(',')}`,
     categories: ['reading'],
     features: {
         requireConfig: false,
@@ -42,22 +46,6 @@ export const route: Route = {
         },
     ],
     handler,
-};
-
-const types = {
-    jinqigengxin: '近期更新',
-    dushi: '都市',
-    xianxia: '仙侠',
-    xuanhuan: '玄幻',
-    qihuan: ' 奇幻',
-    lishi: '历史',
-    youxi: '游戏',
-    wuxia: '武侠',
-    kehuan: '科幻',
-    tiyu: '体育',
-    lingyi: '灵异',
-    junshi: '军事',
-    erciyuan: '轻小说',
 };
 
 async function handler(ctx) {
