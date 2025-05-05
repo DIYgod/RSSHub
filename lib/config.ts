47981e3f3d7ef591553a96aaa809063785d8c247
Kjasn <iz.kjasn@icloud.com>
2025-05-05 23:57:21 +0800

    feat(route): add Jumeili home route (#19021)

--- lib/config.ts
+++ lib/config.ts
@@ -204,6 +204,9 @@ export type Config = {
     javdb: {
         session?: string;
     };
+    jumeili: {
+        cookie?: string;
+    };
     keylol: {
         cookie?: string;
     };
@@ -641,6 +644,9 @@ const calculateValue = () => {
         },
         javdb: {
             session: envs.JAVDB_SESSION,
+        },
+        jumeili: {
+            cookie: envs.JUMEILI_COOKIE,
         },
         keylol: {
             cookie: envs.KEYLOL_COOKIE,
