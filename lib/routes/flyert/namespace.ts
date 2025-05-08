454bce25df5c03c1dcd282b828fdaf24c0c89515
独自等待 <3817100+hack2012@users.noreply.github.com>
2025-05-09 02:57:11 +0800

    fix: /flyert/creditcard 493错误 (#18974)
    * 修正域名，增加随机时间控制，限制并发为2
    
    * 使用new URL替换url.resolve并增加p-limit库
    
    * 使用官方推荐的库实现相同的功能
    
    * 使用官方已有函数减少重复代码
    
    * chore: remove p-limit
    
    ---------
    
    Co-authored-by: 独自等待 <liliang@LL-147.local>

--- lib/routes/flyert/namespace.ts
+++ lib/routes/flyert/namespace.ts
@@ -2,7 +2,7 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: '飞客茶馆',
-    url: 'flyert.com',
+    url: 'flyert.com.cn',
     description: '',
     lang: 'zh-CN',
 };
