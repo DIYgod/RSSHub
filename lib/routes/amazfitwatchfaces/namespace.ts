fcac1baac34d61d25d4b9c665ad72d01317da5df
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-05-08 22:22:03 +0800

    feat(route): add Amazfitwatchfaces (#19032)
    * feat(route): add Amazfitwatchfaces
    
    * fix: missing description for searchParams
    
    * refactor: fix Element type import
    
    ---------

---
+++ lib/routes/amazfitwatchfaces/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Amazfitwatchfaces',
+    url: 'amazfitwatchfaces.com',
+    categories: ['program-update'],
+    description:
+        "amazfitwatchfaces.com is the world's largest collection of watch faces for Amazfit, Zepp, Bip, Pace, Stratos, Cor, Verge, Verge Lite, GTR, GTS, T-Rex, watches. Here you can find everything you need to customize & personalize your device! The website also has catalogs of watch faces for Xiaomi, Haylou, Honor and Huawei watches.",
+    lang: 'en',
+};
