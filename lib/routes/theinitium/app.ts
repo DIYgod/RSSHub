2b4cf7db076e2b6b0a931d395155e202dc3cfe16
quiniapiezoelectricity <73748843+quiniapiezoelectricity@users.noreply.github.com>
2025-05-12 18:03:44 +0800

    fix theinitium footer

--- lib/routes/theinitium/app.ts
+++ lib/routes/theinitium/app.ts
@@ -101,6 +101,7 @@ async function fetchAppPage(url: URL) {
     });
     const article = $('.pp-article__body');
     article.find('.block-related-articles').remove();
+    article.find('.copyright').wrapInner('<small></small>').wrapInner('<figure></figure>');
     article.find('figure.wp-block-pullquote').children().unwrap();
     article.find('div.block-explanation-note').wrapInner('<blockquote></blockquote>');
     article.find('div.wp-block-tcc-author-note').wrapInner('<em></em>').after('<hr>');
@@ -110,7 +111,6 @@ async function fetchAppPage(url: URL) {
         coverImage: $('.pp-media__image').attr('src'),
         coverCaption: $('.pp-media__caption').html(),
         article: article.html(),
-        copyright: $('.copyright').html(),
     });
 }
 
@@ -119,6 +119,8 @@ async function fetchWebPage(url: URL) {
     const $ = load(response.data);
     const article = $('.entry-content');
     article.find('.block-related-articles').remove();
+    article.find('.cta-subscription').remove();
+    article.find('.entry-copyright').wrapInner('<small></small>').wrapInner('<figure></figure>');
     article.find('figure.wp-block-pullquote').children().unwrap();
     article.find('div.block-explanation-note').wrapInner('<blockquote></blockquote>');
     article.find('div.wp-block-tcc-author-note').wrapInner('<em></em>').after('<hr>');
@@ -128,7 +130,6 @@ async function fetchWebPage(url: URL) {
         coverImage: $('.wp-post-image').attr('src'),
         coverCaption: $('.image-caption').html(),
         article: article.html(),
-        copyright: $('.entry-copyright').html(),
     });
 }
 
