7d97ca8626e1136fa506717b8f8aee78298da812
yoyobase <95998436+yoyobase@users.noreply.github.com>
2025-05-13 15:04:43 +0800

    fix(route/bakamh): add item publishing date

--- lib/routes/bakamh/manga.ts
+++ lib/routes/bakamh/manga.ts
@@ -1,7 +1,9 @@
 import { Route } from '@/types';
 import ofetch from '@/utils/ofetch';
-import * as cheerio from 'cheerio';
+import {load} from 'cheerio';
 import cache from '@/utils/cache';
+import { parseDate } from '@/utils/parse-date';
+
 
 const url = 'https://bakamh.com';
 
@@ -11,29 +13,35 @@ const handler = async (ctx) => {
 
     const link = `${url}/manga/${name}/`;
     const response = await ofetch(link);
-    const $ = cheerio.load(response);
-
-    const list = $('li.wp-manga-chapter > a')
+    const $ = load(response);
+    const ldJson = JSON.parse($('script[type="application/ld+json"]').text());
+    const list = $('li.wp-manga-chapter')
         .toArray()
         .slice(0, limit)
         .map((item) => {
             const $item = $(item);
             return {
-                title: $item.text(),
-                link: $item.attr('href'),
-                guid: $item.attr('href'),
+                title: $item.find('a').text(),
+                link: $item.find('a').attr('href'),
+                guid: $item.find('a').attr('href'),
+                pubDate:  $item.find('i').text().replace(/ /g, ''),
             };
         });
+
+    if (list.length > 0) {
+        list[0].pubDate = ldJson.dateModified;
+    }
 
     const items = await Promise.all(
         list.map((item) =>
             cache.tryGet(item.link, async () => {
                 const response = await ofetch(item.link);
-                const $ = cheerio.load(response);
+                const $ = load(response);
                 const comicpage = $('div.reading-content img');
                 const containerDiv = $('<div class="image-container"></div>');
                 comicpage.appendTo(containerDiv);
-                item.description = $.html(containerDiv);
+                item.description = containerDiv.html();
+                item.pubDate = parseDate(item.pubDate,'YYYY年M月D日');
                 return item;
             })
         )
