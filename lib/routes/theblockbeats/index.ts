714853df663f93a7c372a29e7918d0b2e17afde8
DIYgod <i@diygod.me>
2025-05-26 16:07:12 +0800

    fix: theblockbeats apiBase

--- lib/routes/theblockbeats/index.ts
+++ lib/routes/theblockbeats/index.ts
@@ -8,7 +8,7 @@ import { art } from '@/utils/render';
 
 const domain = 'theblockbeats.info';
 const rootUrl = `https://www.${domain}`;
-const apiBase = `https://api.${domain}`;
+const apiBase = `https://api.blockbeats.cn`;
 
 const render = (data) => {
     const html = art(path.join(__dirname, 'templates/description.art'), data);
@@ -62,7 +62,7 @@ export const route: Route = {
         },
     },
     name: '新闻快讯',
-    maintainers: ['Fatpandac', 'jameshih'],
+    maintainers: ['Fatpandac', 'jameshih', 'DIYgod'],
     handler,
     radar: [
         {
