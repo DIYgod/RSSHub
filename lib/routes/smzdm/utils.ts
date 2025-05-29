5b84d70d27deb75a950d59ef07d6bea365c287d3
DIYgod <i@diygod.me>
2025-05-29 17:18:44 +0800

    fix: smzdm

--- lib/routes/smzdm/utils.ts
+++ lib/routes/smzdm/utils.ts
@@ -1,6 +1,7 @@
 import { config } from '@/config';
 
 export const getHeaders = () => ({
+    accept: 'application/json, text/javascript, */*; q=0.01',
     cookie: config.smzdm.cookie,
     'x-requested-with': 'XMLHttpRequest',
 });
