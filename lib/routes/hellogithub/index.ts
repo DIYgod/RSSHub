722f2e830ba352b98198d7e965a9bf3605c03312
Hwting <837479851@qq.com>
2025-06-11 14:35:17 +0800

    fix：Update index.ts
    1.fix：Fix the abnormal display of description in hellogithub

--- lib/routes/hellogithub/index.ts
+++ lib/routes/hellogithub/index.ts
@@ -66,7 +66,7 @@ async function handler(ctx) {
         link: `${rootUrl}/repository/${item.item_id}`,
         pubDate: parseDate(item.updated_at),
         name: `${item.author}/${item.name}`,
-        summary: item.summary,
+        description: item.summary,
         language: item.primary_lang,
     }));
 
