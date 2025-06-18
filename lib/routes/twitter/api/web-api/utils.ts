5048710a372b2a475a747882a5cc54645919bc3e
pseudoyu <pseudoyu@connect.hku.hk>
2025-06-18 09:58:54 +0800

    refactor(route/twitter): support both `timeline` and `timeline_v2` in twitter web api

--- lib/routes/twitter/api/web-api/utils.ts
+++ lib/routes/twitter/api/web-api/utils.ts
@@ -248,7 +248,7 @@ export const paginationTweets = async (endpoint: string, userId: number | undefi
             return instructions.instructions;
         }
 
-        const instructions = data?.user?.result?.timeline?.timeline?.instructions;
+        const instructions = data?.user?.result?.timeline?.timeline?.instructions || data?.user?.result?.timeline?.timeline_v2?.instructions;
         if (!instructions) {
             logger.debug(`twitter debug: instructions not found in data: ${JSON.stringify(data)}`);
         }
