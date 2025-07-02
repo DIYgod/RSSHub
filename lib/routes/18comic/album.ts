f942b627bc5f04cf1c0235e2fdd0bdecc420a30b
charles <30816317+charles7668@users.noreply.github.com>
2025-07-02 23:16:12 +0800

    fix(route): Fix 403 issue on 18comic route by using API calls instead of directly fetching the webpage. (#19485)
    * fix(route): Fix 403 issue on 18comic route by using API calls instead of directly fetching the webpage.
    
    * avoid using await in loop
    
    * Replace the original search implementation with API-based search.
    
    * using api in album route
    
    * Fix
    
    * Fix

--- lib/routes/18comic/album.ts
+++ lib/routes/18comic/album.ts
@@ -1,10 +1,8 @@
 import { Route } from '@/types';
+import { defaultDomain, getApiUrl, getRootUrl, processApiItems } from './utils';
+import { art } from '@/utils/render';
+import path from 'node:path';
 import cache from '@/utils/cache';
-import got from '@/utils/got';
-import { load } from 'cheerio';
-import { parseDate } from '@/utils/parse-date';
-
-import { defaultDomain, getRootUrl } from './utils';
 
 export const route: Route = {
     path: '/album/:id',
@@ -37,71 +35,68 @@ async function handler(ctx) {
     const id = ctx.req.param('id');
     const { domain = defaultDomain } = ctx.req.query();
     const rootUrl = getRootUrl(domain);
-
     const currentUrl = `${rootUrl}/album/${id}`;
 
-    const response = await got({
-        method: 'get',
-        url: currentUrl,
-    });
+    const apiUrl = `${getApiUrl()}/album?id=${id}`;
 
-    const $ = load(response.data);
+    const apiResult = await processApiItems(apiUrl);
 
-    const category = $('span[data-type="tags"]')
-        .first()
-        .find('a')
-        .toArray()
-        .map((c) => $(c).text());
-    const author = $('span[data-type="author"]')
-        .first()
-        .find('a')
-        .toArray()
-        .map((a) => $(a).text())
-        .join(', ');
-
-    let items = $('.btn-toolbar')
-        .first()
-        .find('a')
-        .toArray()
-        .map((item) => {
-            item = $(item);
-
-            return {
-                title: item.text(),
-                link: `${rootUrl}${item.attr('href')}`,
-                guid: `https://18comic.org${item.attr('href')}`,
-                pubDate: parseDate(item.find('.hidden-xs').text()),
-            };
+    const category = apiResult.tags;
+    const author = apiResult.author.join(', ');
+    const description = apiResult.description;
+    const addTime = apiResult.addtime;
+    let results: any[] = [];
+    if (apiResult.series.length === 0) {
+        results.push({
+            title: apiResult.name,
+            link: `${rootUrl}/photo/${id}`,
+            guid: `${rootUrl}/photo/${id}`,
+            updated: new Date(addTime * 1000),
+            pubDate: new Date(addTime * 1000),
+            category,
+            author,
+            description: art(path.join(__dirname, 'templates/description.art'), {
+                introduction: description,
+                // 不取图片，因为专辑的图片会被分割排序，所以只取封面图
+                images: [`https://cdn-msp3.${domain}/media/albums/${id}_3x4.jpg`],
+                cover: `https://cdn-msp3.${domain}/media/albums/${id}_3x4.jpg`,
+                category,
+            }),
         });
-
-    items = await Promise.all(
-        items.map((item) =>
-            cache.tryGet(item.guid, async () => {
-                const detailResponse = await got({
-                    method: 'get',
-                    url: item.link,
-                });
-
-                const content = load(detailResponse.data);
-
-                content('.tab-content').remove();
-
-                item.author = author;
-                item.category = category;
-                item.description = `<img src="${content('.thumb-overlay-albums img[data-original]')
-                    .toArray()
-                    .map((image) => content(image).attr('data-original'))
-                    .join('"><img src="')}">`;
-
-                return item;
-            })
-        )
-    );
+    } else {
+        results = await Promise.all(
+            apiResult.series.map((item, index) =>
+                cache.tryGet(`18comic:album:${item.id}`, async () => {
+                    const chapterApiUrl = `${getApiUrl()}/chapter?id=${item.id}`;
+                    const chapterResult = await processApiItems(chapterApiUrl);
+                    const result = {};
+                    const chapterNum = index + 1;
+                    result.title = `第${String(chapterNum)}話 ${item.name === '' ? `${String(chapterNum)}` : item.name}`;
+                    result.link = `${rootUrl}/photo/${item.id}`;
+                    result.guid = `${rootUrl}/photo/${item.id}`;
+                    result.updated = new Date(chapterResult.addtime * 1000);
+                    result.pubDate = addTime;
+                    result.category = category;
+                    result.author = author;
+                    result.description = art(path.join(__dirname, 'templates/description.art'), {
+                        introduction: description,
+                        // 不取图片，因为专辑的图片会被分割排序，所以只取封面图
+                        images: [`https://cdn-msp3.${domain}/media/albums/${item.id}_3x4.jpg`],
+                        cover: `https://cdn-msp3.${domain}/media/albums/${item.id}_3x4.jpg`,
+                        category,
+                    });
+                    return result;
+                })
+            )
+        );
+        results = results.reverse();
+    }
 
     return {
-        title: $('title').text(),
-        link: currentUrl,
-        item: items,
-        description: $('meta[property="og:description"]').attr('content'),
+        title: `${apiResult.name} - 禁漫天堂`,
+        link: currentUrl.replace(/\?$/, ''),
+        item: results,
+        allowEmpty: true,
+        description,
     };
 }
