41a749ce5db63683899d4e75898ce08efb2a00fe
Nishant Singh <57475999+Rjnishant530@users.noreply.github.com>
2025-07-08 20:35:49 +0530

    feat(css-tricks): add new routes for css-tricks (#19551)
    * init
    
    * use wordpress api
    
    * use WP
    
    * null check
    
    * clean up un used functions
    
    * remove check for type
    
    ---------
    
    Co-authored-by: Rjnishant530 <singhnisant530@gmail.com>

---
+++ lib/routes/css-tricks/articles.ts
@@ -0,0 +1,48 @@
+import { Data, Route, ViewType } from '@/types';
+import { load } from 'cheerio';
+import ofetch from '@/utils/ofetch';
+import { processWithWp } from './utils';
+export const route: Route = {
+    path: '/articles',
+    view: ViewType.Articles,
+    categories: ['programming'],
+    example: '/css-tricks/articles',
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['css-tricks.com/category/articles/'],
+            target: '/articles',
+        },
+    ],
+    name: 'Articles',
+    maintainers: ['Rjnishant530'],
+    handler,
+};
+
+async function handler() {
+    // const category = ctx.req.param('category') ?? '';
+    // const subCategory = ctx.req.param('subCategory') ?? '';
+
+    const rootUrl = 'https://css-tricks.com';
+    const currentUrl = `${rootUrl}/category/articles/`;
+    const response = await ofetch(currentUrl);
+    const $ = load(response);
+    const articleCards = $('article.article-card').toArray();
+    const items = await processWithWp(articleCards);
+    return {
+        title: 'Articles - CSS-Tricks',
+        description: 'Latest Articles - CSS-Tricks',
+        link: currentUrl,
+        item: items,
+        language: 'en',
+        logo: `${rootUrl}/favicon.ico`,
+        icon: `${rootUrl}/favicon.ico`,
+    } as Data;
+}
---
+++ lib/routes/css-tricks/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'CSS-Tricks',
+    url: 'css-tricks.com',
+    categories: ['programming'],
+    lang: 'en',
+};
