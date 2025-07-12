f4d69a21c415e1a3fb1a5e57ad19fd4b263a4799
Whalko <45971730+RtYkk@users.noreply.github.com>
2025-07-12 16:44:01 +0800

    docs(feeds): update guidance on obtaining private feed URL (#19590)

--- lib/routes/github/namespace.ts
+++ lib/routes/github/namespace.ts
@@ -9,7 +9,7 @@ GitHub provides some official RSS feeds:
 -   Repo releases: \`https://github.com/:owner/:repo/releases.atom\`
 -   Repo commits: \`https://github.com/:owner/:repo/commits.atom\`
 -   User activities: \`https://github.com/:user.atom\`
--   Private feed: \`https://github.com/:user.private.atom?token=:secret\` (You can find **Subscribe to your news feed** in [dashboard](https://github.com) page after login)
+-   Private feed: \`https://github.com/:user.private.atom?token=:secret\` (Note: You can ONLY obtain this url via an [API](https://docs.github.com/en/rest/activity/feeds?apiVersion=2022-11-28) call with a [Personal Access Token](https://github.com/settings/tokens/new) with **ENOUGH** scopes now.)
 -   Wiki history: \`https://github.com/:owner/:repo/wiki.atom\`
 :::`,
     lang: 'en',
