7ae9ff51198fb64f61bd923a4c5e058ca8391b0e
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2025-07-18 21:31:19 +0100

    feat(route): add Apple design updates

---
+++ lib/routes/apple/design.ts
@@ -0,0 +1,62 @@
+import { Route } from '@/types';
+import { parseDate } from '@/utils/parse-date';
+import ofetch from '@/utils/ofetch';
+import { load } from 'cheerio';
+import cache from '@/utils/cache';
+
+export const route: Route = {
+    categories: ['design'],
+    example: '/design',
+    handler,
+    maintainers: ['jean-jacket'],
+    name: 'Design updates',
+    path: '/design',
+    url: 'developer.apple.com/design/whats-new/',
+};
+
+// Average time between updates is 16 days
+const CACHE_EXPIRE = 1_382_400;
+
+async function handler() {
+    const LINK = 'https://developer.apple.com/design/whats-new/';
+
+    return await cache.tryGet(
+        'apple:design',
+        async () => {
+            const response = await ofetch(LINK);
+            const $ = load(response);
+
+            const items = $('table')
+                .toArray()
+                .flatMap((item) => {
+                    const table = $(item);
+                    const date = table.find('.date').first().text();
+
+                    return table
+                        .find('.topic-item')
+                        .toArray()
+                        .map((row) => {
+                            const update = $(row);
+                            const titleTag = update.find('span.topic-title a');
+                            const title = titleTag.text();
+                            const link = `https://developer.apple.com${titleTag.attr('href')}`;
+                            const description = update.find('span.description').text();
+                            return {
+                                description,
+                                link,
+                                pubDate: parseDate(date),
+                                title,
+                            };
+                        });
+                });
+
+            return {
+                item: items,
+                link: LINK,
+                title: 'Apple design updates',
+            };
+        },
+        CACHE_EXPIRE,
+        false
+    );
+}
