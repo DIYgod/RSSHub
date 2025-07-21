4bda4387cb2a4fb4ce60aac52775b6ffe2a9f8d9
NekoAria <NekoAria@users.noreply.github.com>
2025-07-21 16:43:29 +0800

    feat(route): add YouTrack issue comments

---
+++ lib/routes/youtrack/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'YouTrack',
+    url: 'youtrack.jetbrains.com',
+    description: 'YouTrack is a project management and bug tracking tool developed by JetBrains.',
+    lang: 'en',
+};
