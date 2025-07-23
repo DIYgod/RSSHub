f057e7626e212e22c833b3ee137a861e45abf2bb
Nishant Singh <57475999+Rjnishant530@users.noreply.github.com>
2025-07-24 03:31:05 +0530

    feat(route/capitalmind): add capital mind (#19601)
    * init
    
    * remove podcast details
    
    * update podcast details
    
    * remove temp string
    
    * Update lib/routes/capitalmind/utils.ts
    
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>
    
    * add image

---
+++ lib/routes/capitalmind/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Capitalmind',
+    url: 'capitalmind.in',
+    lang: 'en',
+    categories: ['finance'],
+};
