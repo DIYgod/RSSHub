f847dda7a444ebbd1672679b0f8e0a9323d51c07
renamebaby <285521946@qq.com>
2025-07-25 14:24:09 +0800

    namespace.ts

--- lib/routes/1lou/namespace.ts
+++ lib/routes/1lou/namespace.ts
@@ -2,7 +2,7 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: 'BT 之家 1LOU 站',
-    url: '1lou.me',
+    url: '1lou.pro',
     categories: ['multimedia'],
     description: '',
     lang: 'zh-CN',
