2a4ef475b8a5660553d5dd1d56eb42ee852f8842
Tony <TonyRL@users.noreply.github.com>
2025-07-31 23:24:36 +0800

    Update lib/routes/kiro/namespace.ts

--- lib/routes/kiro/namespace.ts
+++ lib/routes/kiro/namespace.ts
@@ -3,7 +3,7 @@ import type { Namespace } from '@/types';
 export const namespace: Namespace = {
     name: 'Kiro',
     url: 'kiro.dev',
-    categories: ['program-update'],
+    categories: ['programming'],
     description: 'The AI IDE for prototype to production',
     lang: 'en',
 };
