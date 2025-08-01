7fa9866adc7ead9d698f5ea55b2fda2943550a0e
Nishant Singh <57475999+Rjnishant530@users.noreply.github.com>
2025-08-01 19:13:24 +0530

    fix(route/grist): payload changed (#19717)
    * fix grist
    
    * remove a line

--- lib/routes/grist/utils.ts
+++ lib/routes/grist/utils.ts
@@ -6,16 +6,17 @@ const getData = (url) => ofetch(url);
 
 const getList = (data) =>
     data.map((value) => {
-        const { id, title, content, date_gmt, modified_gmt, link, parsely, _embedded } = value;
-        const { image, author } = parsely.meta;
+        const { id, title, content, date_gmt, modified_gmt, link, _embedded, featured_media } = value;
+        const { 'wp:featuredmedia': media, author } = _embedded;
+        const image = media?.find((v) => v.id === featured_media) || { source_url: '' };
         return {
             id,
             title: title.rendered,
             description: content.rendered,
             link,
-            itunes_item_image: image.url,
+            itunes_item_image: image.source_url,
             category: _embedded['wp:term'][0].map((v) => v.name),
-            author: author.map((v) => v.name).join(', '),
+            author: author?.map((v) => v.name).join(', '),
             pubDate: timezone(parseDate(date_gmt), 0),
             updated: timezone(parseDate(modified_gmt), 0),
         };
