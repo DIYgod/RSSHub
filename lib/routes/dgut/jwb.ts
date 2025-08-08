170d6c62d785654b528b8d7b41b7cb41ef6e1bc8
1200522928 <123849699+1200522928@users.noreply.github.com>
2025-08-08 23:33:20 +0800

    Add files via upload

---
+++ lib/routes/dgut/jwb.ts
@@ -0,0 +1,78 @@
+import { Route } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+import { load } from 'cheerio';
+import { parseDate } from 'tough-cookie';
+
+export const route: Route = {
+    path: '/tzgg/:type?',
+    categories: ['university'],
+    example: '/tzgg/jwtz.htm',
+    parameters: { type: '那种通知,默认为教务通知' },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    maintainers: ['1200522928'],
+    radar: [
+        {
+            source: ['jwb.dgut.edu.cn/tzgg/'],
+            target:'/tzgg/:type?'
+        },
+    ],
+    name: '东莞理工学院教务部',
+    description: `| 教学动态 | 教务通知 | 教研通知 | 实践通知 | 产业学院 |  通识教育  |"杨振宁"班|招生信息 |采购公告 |
+                  | ------- | -------  | ---------| --------| --------| ----------|---------|------- |--------|
+                  | jxdt    | jwtz     | jytz     |   sjtz  |   cyxy  |   tsjy    | yznb    |  zsxx  | cggg   |`,
+    handler,
+};
+
+async function handler(ctx) {
+    
+    const {type = 'jwtz'}= ctx.req.param();
+    const url = `https://jwb.dgut.edu.cn/tzgg/${type}.htm`;
+    const baseurl = 'https://jwb.dgut.edu.cn/';
+
+    const response = await ofetch(url);
+    const $ = load(response);
+    const list = $('ul.ul-new4 > li')
+        .toArray()
+        .map((item) => {
+                const $li = $(item);
+                const $a = $li.find('a.con')
+            return {
+            title: $a.find('.tit').text().trim(), 
+            date: parseDate(`${$a.find('.year').text().trim()}-${$a.find('.day').text().trim()}`),
+            link: `${baseurl}${$a.attr('href')}`,
+
+            };
+        })
+    
+    const items = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link, async () => {
+                const response = await ofetch(item.link);
+                const $ = load(response);
+
+                return {
+                    ...item,
+                    description: $('div.v_news_content').first().html(),
+                };
+                
+                })
+            )
+        );
+
+    return {
+        title: $('title').text(),
+        link: url,
+        allowEmpty: true,
+        item: items,
+    };
+};
+
+
