623798e5e61f7f741997f8ea25221b55cbda49c2
marcosteam <wdy71608161@gmail.com>
2025-08-09 12:35:31 +0800

    feat(route/nio): add NIO Radio support

---
+++ lib/routes/nio/namespace.ts
@@ -0,0 +1,12 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'NIO',
+    url: 'nio.com',
+    description: `
+    蔚来
+    `,
+    zh: {
+        name: '蔚来',
+    },
+};
