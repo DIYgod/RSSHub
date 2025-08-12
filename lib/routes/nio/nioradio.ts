43013f75aaef91f2d34cc5d38c76a2bd1fc4fae7
marcosteam <wdy71608161@gmail.com>
2025-08-12 15:18:03 +0800

    fix(nioradio): improve formatting and adjust podcast fetching parameters

--- lib/routes/nio/nioradio.ts
+++ lib/routes/nio/nioradio.ts
@@ -7,8 +7,9 @@ export const route: Route = {
     path: '/nioradio/:albumid',
     categories: ['multimedia'],
     description: `
-# 如何获取电台 ID？
-打开蔚来 APP 后，点击“此地”→NIO Radio，找到自己想要转换为播客的专辑，分享后获取链接，找到\`container_id=\`后方的数字即可。
+:::tip
+**如何获取电台 ID？**
+打开蔚来 APP 后，点击“此地”→“NIO Radio”，找到自己想要转换为播客的专辑，分享后在生成的链接中找到\`container_id=\`后方的数字即可。
 常见电台 ID：
 | 电台名称          | 电台 ID |
 | :------------ | :---- |
@@ -17,7 +18,8 @@ export const route: Route = {
 | E 次元财经报       | 148   |
 | 塞萌不塞车         | 661   |
 | 乐行记           | 11    |
-| Weekend Dance | 547   |`,
+| Weekend Dance | 547   |
+:::`,
     example: '/nio/nioradio/5',
     parameters: { albumid: '电台专辑 ID' },
     features: {
@@ -28,12 +30,6 @@ export const route: Route = {
         supportPodcast: true,
         supportScihub: false,
     },
-    radar: [
-        {
-            source: [],
-            target: '',
-        },
-    ],
     name: 'NIO Radio',
     maintainers: ['marcosteam'],
     handler: async (ctx) => {
@@ -42,35 +38,36 @@ export const route: Route = {
             albumId: albumid,
             sorttype: '2',
             pagenum: '1',
-            pagesize: '9999',
+            pagesize: '10',
         }).toString();
-        const data = await ofetch(`https://gateway-front-external.nio.com/moat/100914/v2/audio/list`, {
+        const data = await ofetch('https://gateway-front-external.nio.com/moat/100914/v2/audio/list', {
             method: 'POST',
             body: req,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
         });
+
         const podcasts = data.result.dataList;
         const podcastName = podcasts[0].albumName;
-        const podcastHost = podcasts[0].host.join(',');
         const podcastImage = podcasts[0].albumPic;
 
         const items = podcasts.map((podcast) => ({
             title: podcast.audioName,
-            link: `https://app.nio.com/app/radio/share/?item_type=1&item_id=${String(podcast.audioId).slice(1)}&container_id=${albumid}&wv=lg`,
+            link: `https://app.nio.com/app/radio/share/?item_type=1&item_id=${String(podcast.audioId).slice(1)}&container_id=${albumid}`,
             pubDate: parseDate(podcast.onlineTime),
             description: podcast.albumDesc,
-            author: podcastHost,
+            author: podcast.host.join(', '),
             itunes_item_image: podcast.albumPic,
+            itunes_duration: podcast.duration,
             enclosure_url: podcast.aacPlayUrl192,
             enclosure_length: podcast.aacFileSize192,
             enclosure_type: 'audio/x-m4a',
         }));
         return {
             title: `NIO Radio - ${podcastName}`,
-            link: `www.nio.com`,
-            itunes_author: podcastHost,
+            link: 'https://www.nio.com',
+            itunes_author: podcasts[0].host.join(', '),
             image: podcastImage,
             item: items,
         };
