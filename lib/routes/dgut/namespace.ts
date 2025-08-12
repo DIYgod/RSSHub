01cf1c0562fa96721af5310254ee81cc4f8b0f38
1200522928 <123849699+1200522928@users.noreply.github.com>
2025-08-12 09:41:14 +0800

    Update lib/routes/dgut/namespace.ts
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/dgut/namespace.ts
+++ lib/routes/dgut/namespace.ts
@@ -1,7 +1,7 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: 'dgut',
+    name: '东莞理工学院',
     url: 'www.dgut.edu.cn',
     zh: {
         name: '东莞理工学院',
