2170fe78eed848e8db0b8c1d3f85effdad2dbd6f
Yeye <150640980+Yeye-0426@users.noreply.github.com>
2025-08-12 19:24:49 +0800

    feat(route): add 四川人社厅人事考试专栏 (#19799)
    * feat(route): add 四川人社厅人事考试专栏
    
    * fix(route/scpta): address code review feedback
    
    1. Remove indentation in description table
    2. Got usage to single-line style
    3. Return html content instead of plain text
    
    Refs: #19799

---
+++ lib/routes/scpta/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '四川省人力资源和社会保障厅人事考试专栏',
+    url: 'www.scpta.com.cn',
+    categories: ['government'],
+    lang: 'zh-CN',
+};
