8774d2cd4e7eaf536080ac1d6d330484239de115
nczitzk <42264778+nczitzk@users.noreply.github.com>
2025-08-15 16:37:41 +0800

    feat(route): add Gitstar Ranking

---
+++ lib/routes/gitstar-ranking/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Gitstar Ranking',
+    url: 'gitstar-ranking.com',
+    categories: ['other'],
+    description: '',
+    lang: 'en',
+};
