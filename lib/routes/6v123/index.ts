0314ed56a0ab59d65c7f375da2188963b8897780
nczitzk <42264778+nczitzk@users.noreply.github.com>
2025-08-20 21:19:56 +0800

    fix: move to 6v123

--- lib/routes/6v123/index.ts
+++ lib/routes/6v123/index.ts
@@ -13,13 +13,15 @@ export const handler = async (ctx: Context): Promise<Data> => {
     const { category = 'dy' } = ctx.req.param();
     const limit: number = Number.parseInt(ctx.req.query('limit') ?? '25', 10);
 
-    const baseUrl: string = 'https://www.6v520.com';
+    const encoding: string = 'gb2312';
+
+    const baseUrl: string = 'https://www.hao6v.me';
     const targetUrl: string = new URL(category.startsWith('gvod') ? `${category}.html` : category, baseUrl).href;
 
     const response = await ofetch(targetUrl, {
         responseType: 'arrayBuffer',
     });
-    const $: CheerioAPI = load(iconv.decode(Buffer.from(response), 'gb2312'));
+    const $: CheerioAPI = load(iconv.decode(Buffer.from(response), encoding));
     const language = $('html').attr('lang') ?? 'zh';
 
     let items: DataItem[] = [];
@@ -62,7 +64,7 @@ export const handler = async (ctx: Context): Promise<Data> => {
                 const detailResponse = await ofetch(item.link, {
                     responseType: 'arrayBuffer',
                 });
-                const $$: CheerioAPI = load(iconv.decode(Buffer.from(detailResponse), 'gb2312'));
+                const $$: CheerioAPI = load(iconv.decode(Buffer.from(detailResponse), encoding));
 
                 $$('div#endText div.fl').remove();
                 $$('div#endText div.fr').remove();
@@ -118,7 +120,7 @@ export const handler = async (ctx: Context): Promise<Data> => {
     );
 
     return {
-        title: $('title').text(),
+        title: `${$('title').text().split(/，/).pop()} - ${$('div.t a').last().text()}`,
         description: $('meta[name="description"]').attr('content'),
         link: targetUrl,
         item: items,
@@ -132,10 +134,10 @@ export const handler = async (ctx: Context): Promise<Data> => {
 export const route: Route = {
     path: '/:category{.+}?',
     name: '分类',
-    url: 'www.6v520.com',
+    url: 'www.hao6v.me',
     maintainers: ['nczitzk'],
     handler,
-    example: '/6v520/dy',
+    example: '/6v123/dy',
     parameters: {
         category: {
             description: '分类，默认为 `dy`，即最新电影，可在对应分类页 URL 中找到',
@@ -268,46 +270,46 @@ export const route: Route = {
         },
     },
     description: `:::tip
-订阅 [最新电影](https://www.6v520.com/dy/)，其源网址为 \`https://www.6v520.com/dy/\`，请参考该 URL 指定部分构成参数，此时路由为 [\`/6v520/dy\`](https://rsshub.app/6v520/dy)。
+订阅 [最新电影](https://www.hao6v.me/dy/)，其源网址为 \`https://www.hao6v.me/dy/\`，请参考该 URL 指定部分构成参数，此时路由为 [\`/6v123/dy\`](https://rsshub.app/6v123/dy)。
 
 :::
 
 <details>
   <summary>更多分类</summary>
 
-| 分类                                                  | ID                                                                |
-| ----------------------------------------------------- | ----------------------------------------------------------------- |
-| [最新电影](https://www.6v520.com/dy/)                 | [dy](https://rsshub.app/6v520/dy)                                 |
-| [国语配音电影](https://www.6v520.com/gydy/)           | [gydy](https://rsshub.app/6v520/gydy)                             |
-| [动漫新番](https://www.6v520.com/zydy/)               | [zydy](https://rsshub.app/6v520/zydy)                             |
-| [经典高清](https://www.6v520.com/gq/)                 | [gq](https://rsshub.app/6v520/gq)                                 |
-| [动画电影](https://www.6v520.com/jddy/)               | [jddy](https://rsshub.app/6v520/jddy)                             |
-| [3D 电影](https://www.6v520.com/3D/)                  | [3D](https://rsshub.app/6v520/3D)                                 |
-| [真人秀](https://www.6v520.com/shoujidianyingmp4/)    | [shoujidianyingmp4](https://rsshub.app/6v520/shoujidianyingmp4)   |
-| [国剧](https://www.6v520.com/dlz/)                    | [dlz](https://rsshub.app/6v520/dlz)                               |
-| [日韩剧](https://www.6v520.com/rj/)                   | [rj](https://rsshub.app/6v520/rj)                                 |
-| [欧美剧](https://www.6v520.com/mj/)                   | [mj](https://rsshub.app/6v520/mj)                                 |
-| [综艺节目](https://www.6v520.com/zy/)                 | [zy](https://rsshub.app/6v520/zy)                                 |
-| [港台电影](https://www.6v520.com/s/gangtaidianying/)  | [s/gangtaidianying](https://rsshub.app/6v520/s/gangtaidianying)   |
-| [日韩电影](https://www.6v520.com/s/jingdiandianying/) | [s/jingdiandianying](https://rsshub.app/6v520/s/jingdiandianying) |
-| [喜剧](https://www.6v520.com/s/xiju/)                 | [s/xiju](https://rsshub.app/6v520/s/xiju)                         |
-| [动作](https://www.6v520.com/s/dongzuo/)              | [s/dongzuo](https://rsshub.app/6v520/s/dongzuo)                   |
-| [爱情](https://www.6v520.com/s/aiqing/)               | [s/aiqing](https://rsshub.app/6v520/s/aiqing)                     |
-| [科幻](https://www.6v520.com/s/kehuan/)               | [s/kehuan](https://rsshub.app/6v520/s/kehuan)                     |
-| [奇幻](https://www.6v520.com/s/qihuan/)               | [s/qihuan](https://rsshub.app/6v520/s/qihuan)                     |
-| [神秘](https://www.6v520.com/s/shenmi/)               | [s/shenmi](https://rsshub.app/6v520/s/shenmi)                     |
-| [幻想](https://www.6v520.com/s/huanxiang/)            | [s/huanxiang](https://rsshub.app/6v520/s/huanxiang)               |
-| [恐怖](https://www.6v520.com/s/kongbu/)               | [s/kongbu](https://rsshub.app/6v520/s/kongbu)                     |
-| [战争](https://www.6v520.com/s/zhanzheng/)            | [s/zhanzheng](https://rsshub.app/6v520/s/zhanzheng)               |
-| [冒险](https://www.6v520.com/s/maoxian/)              | [s/maoxian](https://rsshub.app/6v520/s/maoxian)                   |
-| [惊悚](https://www.6v520.com/s/jingsong/)             | [s/jingsong](https://rsshub.app/6v520/s/jingsong)                 |
-| [剧情](https://www.6v520.com/s/juqingpian/)           | [s/juqingpian](https://rsshub.app/6v520/s/juqingpian)             |
-| [传记](https://www.6v520.com/s/zhuanji/)              | [s/zhuanji](https://rsshub.app/6v520/s/zhuanji)                   |
-| [历史](https://www.6v520.com/s/lishi/)                | [s/lishi](https://rsshub.app/6v520/s/lishi)                       |
-| [纪录](https://www.6v520.com/s/jilu/)                 | [s/jilu](https://rsshub.app/6v520/s/jilu)                         |
-| [印度电影](https://www.6v520.com/s/yindudianying/)    | [s/yindudianying](https://rsshub.app/6v520/s/yindudianying)       |
-| [国产电影](https://www.6v520.com/s/guochandianying/)  | [s/guochandianying](https://rsshub.app/6v520/s/guochandianying)   |
-| [欧洲电影](https://www.6v520.com/s/xijudianying/)     | [s/xijudianying](https://rsshub.app/6v520/s/xijudianying)         |
+| 分类                                                 | ID                                                                |
+| ---------------------------------------------------- | ----------------------------------------------------------------- |
+| [最新电影](https://www.hao6v.me/dy/)                 | [dy](https://rsshub.app/6v123/dy)                                 |
+| [国语配音电影](https://www.hao6v.me/gydy/)           | [gydy](https://rsshub.app/6v123/gydy)                             |
+| [动漫新番](https://www.hao6v.me/zydy/)               | [zydy](https://rsshub.app/6v123/zydy)                             |
+| [经典高清](https://www.hao6v.me/gq/)                 | [gq](https://rsshub.app/6v123/gq)                                 |
+| [动画电影](https://www.hao6v.me/jddy/)               | [jddy](https://rsshub.app/6v123/jddy)                             |
+| [3D 电影](https://www.hao6v.me/3D/)                  | [3D](https://rsshub.app/6v123/3D)                                 |
+| [真人秀](https://www.hao6v.me/shoujidianyingmp4/)    | [shoujidianyingmp4](https://rsshub.app/6v123/shoujidianyingmp4)   |
+| [国剧](https://www.hao6v.me/dlz/)                    | [dlz](https://rsshub.app/6v123/dlz)                               |
+| [日韩剧](https://www.hao6v.me/rj/)                   | [rj](https://rsshub.app/6v123/rj)                                 |
+| [欧美剧](https://www.hao6v.me/mj/)                   | [mj](https://rsshub.app/6v123/mj)                                 |
+| [综艺节目](https://www.hao6v.me/zy/)                 | [zy](https://rsshub.app/6v123/zy)                                 |
+| [港台电影](https://www.hao6v.me/s/gangtaidianying/)  | [s/gangtaidianying](https://rsshub.app/6v123/s/gangtaidianying)   |
+| [日韩电影](https://www.hao6v.me/s/jingdiandianying/) | [s/jingdiandianying](https://rsshub.app/6v123/s/jingdiandianying) |
+| [喜剧](https://www.hao6v.me/s/xiju/)                 | [s/xiju](https://rsshub.app/6v123/s/xiju)                         |
+| [动作](https://www.hao6v.me/s/dongzuo/)              | [s/dongzuo](https://rsshub.app/6v123/s/dongzuo)                   |
+| [爱情](https://www.hao6v.me/s/aiqing/)               | [s/aiqing](https://rsshub.app/6v123/s/aiqing)                     |
+| [科幻](https://www.hao6v.me/s/kehuan/)               | [s/kehuan](https://rsshub.app/6v123/s/kehuan)                     |
+| [奇幻](https://www.hao6v.me/s/qihuan/)               | [s/qihuan](https://rsshub.app/6v123/s/qihuan)                     |
+| [神秘](https://www.hao6v.me/s/shenmi/)               | [s/shenmi](https://rsshub.app/6v123/s/shenmi)                     |
+| [幻想](https://www.hao6v.me/s/huanxiang/)            | [s/huanxiang](https://rsshub.app/6v123/s/huanxiang)               |
+| [恐怖](https://www.hao6v.me/s/kongbu/)               | [s/kongbu](https://rsshub.app/6v123/s/kongbu)                     |
+| [战争](https://www.hao6v.me/s/zhanzheng/)            | [s/zhanzheng](https://rsshub.app/6v123/s/zhanzheng)               |
+| [冒险](https://www.hao6v.me/s/maoxian/)              | [s/maoxian](https://rsshub.app/6v123/s/maoxian)                   |
+| [惊悚](https://www.hao6v.me/s/jingsong/)             | [s/jingsong](https://rsshub.app/6v123/s/jingsong)                 |
+| [剧情](https://www.hao6v.me/s/juqingpian/)           | [s/juqingpian](https://rsshub.app/6v123/s/juqingpian)             |
+| [传记](https://www.hao6v.me/s/zhuanji/)              | [s/zhuanji](https://rsshub.app/6v123/s/zhuanji)                   |
+| [历史](https://www.hao6v.me/s/lishi/)                | [s/lishi](https://rsshub.app/6v123/s/lishi)                       |
+| [纪录](https://www.hao6v.me/s/jilu/)                 | [s/jilu](https://rsshub.app/6v123/s/jilu)                         |
+| [印度电影](https://www.hao6v.me/s/yindudianying/)    | [s/yindudianying](https://rsshub.app/6v123/s/yindudianying)       |
+| [国产电影](https://www.hao6v.me/s/guochandianying/)  | [s/guochandianying](https://rsshub.app/6v123/s/guochandianying)   |
+| [欧洲电影](https://www.hao6v.me/s/xijudianying/)     | [s/xijudianying](https://rsshub.app/6v123/s/xijudianying)         |
 
 </details>
 `,
@@ -323,162 +325,162 @@ export const route: Route = {
     },
     radar: [
         {
-            source: ['www.6v520.com/:category'],
+            source: ['www.hao6v.me/:category'],
             target: '/:category',
         },
         {
             title: '最新电影',
-            source: ['www.6v520.com/dy/'],
+            source: ['www.hao6v.me/dy/'],
             target: '/dy',
         },
         {
             title: '国语配音电影',
-            source: ['www.6v520.com/gydy/'],
+            source: ['www.hao6v.me/gydy/'],
             target: '/gydy',
         },
         {
             title: '动漫新番',
-            source: ['www.6v520.com/zydy/'],
+            source: ['www.hao6v.me/zydy/'],
             target: '/zydy',
         },
         {
             title: '经典高清',
-            source: ['www.6v520.com/gq/'],
+            source: ['www.hao6v.me/gq/'],
             target: '/gq',
         },
         {
             title: '动画电影',
-            source: ['www.6v520.com/jddy/'],
+            source: ['www.hao6v.me/jddy/'],
             target: '/jddy',
         },
         {
             title: '3D电影',
-            source: ['www.6v520.com/3D/'],
+            source: ['www.hao6v.me/3D/'],
             target: '/3D',
         },
         {
             title: '真人秀',
-            source: ['www.6v520.com/shoujidianyingmp4/'],
+            source: ['www.hao6v.me/shoujidianyingmp4/'],
             target: '/shoujidianyingmp4',
         },
         {
             title: '国剧',
-            source: ['www.6v520.com/dlz/'],
+            source: ['www.hao6v.me/dlz/'],
             target: '/dlz',
         },
         {
             title: '日韩剧',
-            source: ['www.6v520.com/rj/'],
+            source: ['www.hao6v.me/rj/'],
             target: '/rj',
         },
         {
             title: '欧美剧',
-            source: ['www.6v520.com/mj/'],
+            source: ['www.hao6v.me/mj/'],
             target: '/mj',
         },
         {
             title: '综艺节目',
-            source: ['www.6v520.com/zy/'],
+            source: ['www.hao6v.me/zy/'],
             target: '/zy',
         },
         {
             title: '港台电影',
-            source: ['www.6v520.com/s/gangtaidianying/'],
+            source: ['www.hao6v.me/s/gangtaidianying/'],
             target: '/s/gangtaidianying',
         },
         {
             title: '日韩电影',
-            source: ['www.6v520.com/s/jingdiandianying/'],
+            source: ['www.hao6v.me/s/jingdiandianying/'],
             target: '/s/jingdiandianying',
         },
         {
             title: '喜剧',
-            source: ['www.6v520.com/s/xiju/'],
+            source: ['www.hao6v.me/s/xiju/'],
             target: '/s/xiju',
         },
         {
             title: '动作',
-            source: ['www.6v520.com/s/dongzuo/'],
+            source: ['www.hao6v.me/s/dongzuo/'],
             target: '/s/dongzuo',
         },
         {
             title: '爱情',
-            source: ['www.6v520.com/s/aiqing/'],
+            source: ['www.hao6v.me/s/aiqing/'],
             target: '/s/aiqing',
         },
         {
             title: '科幻',
-            source: ['www.6v520.com/s/kehuan/'],
+            source: ['www.hao6v.me/s/kehuan/'],
             target: '/s/kehuan',
         },
         {
             title: '奇幻',
-            source: ['www.6v520.com/s/qihuan/'],
+            source: ['www.hao6v.me/s/qihuan/'],
             target: '/s/qihuan',
         },
         {
             title: '神秘',
-            source: ['www.6v520.com/s/shenmi/'],
+            source: ['www.hao6v.me/s/shenmi/'],
             target: '/s/shenmi',
         },
         {
             title: '幻想',
-            source: ['www.6v520.com/s/huanxiang/'],
+            source: ['www.hao6v.me/s/huanxiang/'],
             target: '/s/huanxiang',
         },
         {
             title: '恐怖',
-            source: ['www.6v520.com/s/kongbu/'],
+            source: ['www.hao6v.me/s/kongbu/'],
             target: '/s/kongbu',
         },
         {
             title: '战争',
-            source: ['www.6v520.com/s/zhanzheng/'],
+            source: ['www.hao6v.me/s/zhanzheng/'],
             target: '/s/zhanzheng',
         },
         {
             title: '冒险',
-            source: ['www.6v520.com/s/maoxian/'],
+            source: ['www.hao6v.me/s/maoxian/'],
             target: '/s/maoxian',
         },
         {
             title: '惊悚',
-            source: ['www.6v520.com/s/jingsong/'],
+            source: ['www.hao6v.me/s/jingsong/'],
             target: '/s/jingsong',
         },
         {
             title: '剧情',
-            source: ['www.6v520.com/s/juqingpian/'],
+            source: ['www.hao6v.me/s/juqingpian/'],
             target: '/s/juqingpian',
         },
         {
             title: '传记',
-            source: ['www.6v520.com/s/zhuanji/'],
+            source: ['www.hao6v.me/s/zhuanji/'],
             target: '/s/zhuanji',
         },
         {
             title: '历史',
-            source: ['www.6v520.com/s/lishi/'],
+            source: ['www.hao6v.me/s/lishi/'],
             target: '/s/lishi',
         },
         {
             title: '纪录',
-            source: ['www.6v520.com/s/jilu/'],
+            source: ['www.hao6v.me/s/jilu/'],
             target: '/s/jilu',
         },
         {
             title: '印度电影',
-            source: ['www.6v520.com/s/yindudianying/'],
+            source: ['www.hao6v.me/s/yindudianying/'],
             target: '/s/yindudianying',
         },
         {
             title: '国产电影',
-            source: ['www.6v520.com/s/guochandianying/'],
+            source: ['www.hao6v.me/s/guochandianying/'],
             target: '/s/guochandianying',
         },
         {
             title: '欧洲电影',
-            source: ['www.6v520.com/s/xijudianying/'],
+            source: ['www.hao6v.me/s/xijudianying/'],
             target: '/s/xijudianying',
         },
     ],
--- lib/routes/6v520/namespace.ts
+++
@@ -1,9 +0,0 @@
-import type { Namespace } from '@/types';
-
-export const namespace: Namespace = {
-    name: '6v电影',
-    url: '6v520.com',
-    categories: ['multimedia'],
-    description: '',
-    lang: 'zh-CN',
-};
