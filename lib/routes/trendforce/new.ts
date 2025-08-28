1133369955e515ba46f2c8388fe4161647b0cf17
nczitzk <42264778+nczitzk@users.noreply.github.com>
2025-08-28 23:15:47 +0800

    fix: use wordpress api

--- lib/routes/trendforce/new.ts
+++ lib/routes/trendforce/new.ts
@@ -1,127 +1,75 @@
 import { type Data, type DataItem, type Route, ViewType } from '@/types';
 
-import { art } from '@/utils/render';
-import cache from '@/utils/cache';
 import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
 
-import { type CheerioAPI, type Cheerio, load } from 'cheerio';
-import type { Element } from 'domhandler';
+import { type CheerioAPI, load } from 'cheerio';
 import { type Context } from 'hono';
-import path from 'node:path';
 
 export const handler = async (ctx: Context): Promise<Data> => {
-    const limit: number = Number.parseInt(ctx.req.query('limit') ?? '5', 10);
+    const limit: number = Number.parseInt(ctx.req.query('limit') ?? '10', 10);
 
+    const apiSlug = 'wp-json/wp/v2';
     const baseUrl: string = 'https://www.trendforce.com';
-    const targetUrl: string = new URL('news', baseUrl).href;
+    const targetUrl: string = new URL('news/', baseUrl).href;
+    const apiUrl = new URL(`${apiSlug}/posts`, targetUrl).href;
 
-    const response = await ofetch(targetUrl);
-    const $: CheerioAPI = load(response);
+    const response = await ofetch(apiUrl, {
+        query: {
+            _embed: 'true',
+            per_page: limit,
+        },
+    });
+
+    const targetResponse = await ofetch(targetUrl);
+    const $: CheerioAPI = load(targetResponse);
     const language = $('html').attr('lang') ?? 'en';
 
-    let items: DataItem[] = [];
+    const items: DataItem[] = response.slice(0, limit).map((item): DataItem => {
+        const title: string = item.title?.rendered ?? item.title;
 
-    items = $('div.insight-list-item')
-        .slice(0, limit)
-        .toArray()
-        .map((el): Element => {
-            const $el: Cheerio<Element> = $(el);
-            const $aEl: Cheerio<Element> = $el.find('a.title-link').first();
+        const $$: CheerioAPI = load(item.content.rendered);
 
-            const title: string = $aEl.text();
-            const image: string | undefined = $el
-                .find('a.insight-list-item-img img')
-                .attr('src')
-                ?.replace(/(-\d+x\d+\.)/, '.');
-            const description: string | undefined = art(path.join(__dirname, 'templates/description.art'), {
-                images: image
-                    ? [
-                          {
-                              src: image,
-                              alt: title,
-                          },
-                      ]
-                    : undefined,
-                intro: $el.find('div.insight-list-item-summary p').text(),
-            });
-            const pubDateStr: string | undefined = $el.find('div.insight-tag').first().contents().first().text().trim();
-            const linkUrl: string | undefined = $aEl.attr('href');
-            const categoryEls: Element[] = $el.find('div.insight-tag a[title]').toArray();
-            const categories: string[] = [...new Set(categoryEls.map((el) => $(el).text()).filter(Boolean))];
-            const upDatedStr: string | undefined = pubDateStr;
+        $$('div.article_highlight-area-BG_wrap').remove();
 
-            const processedItem: DataItem = {
-                title,
-                description,
-                pubDate: pubDateStr ? parseDate(pubDateStr) : undefined,
-                link: linkUrl,
-                category: categories,
-                content: {
-                    html: description,
-                    text: description,
-                },
-                image,
-                banner: image,
-                updated: upDatedStr ? parseDate(upDatedStr) : undefined,
-                language,
-            };
+        const description: string | undefined = $$.html();
+        const pubDate: number | string = item.date_gmt;
+        const linkUrl: string | undefined = item.link;
 
-            return processedItem;
-        });
+        const terminologies = item._embedded?.['wp:term'];
 
-    items = await Promise.all(
-        items.map((item) => {
-            if (!item.link) {
-                return item;
-            }
+        const categories: string[] = terminologies?.flat().map((c) => c.name) ?? [];
+        const authors: DataItem['author'] =
+            item._embedded?.author.map((author) => ({
+                name: author.name,
+                url: author.url,
+                avatar: undefined,
+            })) ?? [];
+        const guid: string = item.guid?.rendered ?? item.guid;
+        const image: string | undefined = item._embedded?.['wp:featuredmedia']?.[0].source_url ?? undefined;
+        const updated: number | string = item.modified_gmt ?? pubDate;
 
-            return cache.tryGet(item.link, async (): Promise<DataItem> => {
-                const detailResponse = await ofetch(item.link);
-                const $$: CheerioAPI = load(detailResponse);
+        const processedItem: DataItem = {
+            title,
+            description,
+            pubDate: pubDate ? parseDate(pubDate) : undefined,
+            link: linkUrl ?? guid,
+            category: categories,
+            author: authors,
+            guid,
+            id: guid,
+            content: {
+                html: description,
+                text: description,
+            },
+            image,
+            banner: image,
+            updated: updated ? parseDate(updated) : undefined,
+            language,
+        };
 
-                const title: string = $$('h1').text();
-                const pubDateStr: string | undefined = $$('div.tag-row i.fa-calendar').parent().text();
-                const categoryEls: Element[] = $$('div.tag-row i.fa-bookmark').parent().find('a').toArray();
-                const categories: string[] = [...new Set(categoryEls.map((el) => $$(el).text()).filter(Boolean))];
-
-                $$('h1').remove();
-                $$('hr').remove();
-                $$('div.tag-row, div.tag-wrapper, div.content_ad, div.press-choose-post, div.article_highlight-area-BG_wrap').remove();
-
-                const description: string | undefined =
-                    item.description +
-                    art(path.join(__dirname, 'templates/description.art'), {
-                        description: $$('div.content div.presscenter').html(),
-                    });
-
-                const image: string | undefined = $$('meta[property="og:image"]')
-                    .attr('content')
-                    ?.replace(/(-\d+x\d+\.)/, '.');
-                const upDatedStr: string | undefined = pubDateStr;
-
-                const processedItem: DataItem = {
-                    title,
-                    description,
-                    pubDate: pubDateStr ? parseDate(pubDateStr) : item.pubDate,
-                    category: categories,
-                    content: {
-                        html: description,
-                        text: description,
-                    },
-                    image,
-                    banner: image,
-                    updated: upDatedStr ? parseDate(upDatedStr) : item.updated,
-                    language,
-                };
-
-                return {
-                    ...item,
-                    ...processedItem,
-                };
-            });
-        })
-    );
+        return processedItem;
+    });
 
     return {
         title: $('title').text(),
