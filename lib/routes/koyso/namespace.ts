5497efcaa4713c27222f6de76ca4a9a93abca137
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-09-01 09:09:44 +0800

    feat(route): add Koyso (#19940)
    * feat(route): add Koyso
    
    * fix typo

---
+++ lib/routes/koyso/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Koyso',
+    url: 'koyso.to',
+    categories: ['game'],
+    description: '',
+    lang: 'zh-CN',
+};
