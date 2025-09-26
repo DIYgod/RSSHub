78fe0a337e763738c4adde0e2a8d69a21d2a479c
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-09-27 01:39:33 +0800

    feat(route): add 经济观察网快讯 (#20168)
    * feat(route): add 经济观察网快讯
    
    * fix typo

---
+++ lib/routes/eeo/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '经济观察网',
+    url: 'eeo.com.cn',
+    categories: ['finance'],
+    description: '',
+    lang: 'zh-CN',
+};
