55242fe3c2b5360caa7bddfd0e8b11c125d5a564
I-Cheng, Ho <ichengho830@gmail.com>
2025-09-29 18:07:06 -0400

    fix: don't use `toString()` alias

--- lib/routes/udn/global/index.ts
+++ lib/routes/udn/global/index.ts
@@ -81,7 +81,7 @@ async function handler(ctx) {
                 item.pubDate = timezone(parseDate(content('meta[property="article:published_time"]').attr('content')), +8);
 
                 const mainImage = content('.article-content__focus').html();
-                const articleBodyHtml = content('.article-content__editor').find('p, figure, h2, .video-container').toString();
+                const articleBodyHtml = content('.article-content__editor').find('p, figure, h2, .video-container').html();
 
                 item.description = mainImage + articleBodyHtml;
                 item.category = content('meta[name="news_keywords"]').attr('content').split(',');
