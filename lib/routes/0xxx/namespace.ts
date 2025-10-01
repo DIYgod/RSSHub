f717bf5c33251a0874ac68196bfcd547282b1449
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2025-10-02 00:49:50 +0800

    feat(route): add 0xxx.ws (#20207)
    * feat(route): add 0xxx.ws
    
    * update lib/routes/0xxx/index.ts

---
+++ lib/routes/0xxx/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '0xxx.ws',
+    url: '0xxx.ws',
+    categories: ['multimedia'],
+    description: 'Best 0day Porn Source',
+    lang: 'en',
+};
