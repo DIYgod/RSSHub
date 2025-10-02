6be8cd1036e13789da2fbf80db8e214d9e1179e2
SimbaFs <simba.fs@gmail.com>
2025-10-03 01:04:56 +0800

    remove unrelated description

--- lib/routes/nycu/namespace.ts
+++ lib/routes/nycu/namespace.ts
@@ -4,17 +4,6 @@ export const namespace: Namespace = {
     name: 'National Yang Ming Chiao Tung University',
     url: 'nycu.edu.tw',
     lang: 'zh-TW',
-    description: `
-:::tip
-GitHub provides some official RSS feeds:
-
--   Repo releases: \`https://github.com/:owner/:repo/releases.atom\`
--   Repo commits: \`https://github.com/:owner/:repo/commits.atom\`
--   User activities: \`https://github.com/:user.atom\`
--   Private feed: \`https://github.com/:user.private.atom?token=:secret\` (You can find **Subscribe to your news feed** in [dashboard](https://github.com) page after login)
--   Wiki history: \`https://github.com/:owner/:repo/wiki.atom\`
-:::`,
-
     zh: {
         name: '國立陽明交通大學',
     },
