6d92be21a2df427c396601ca55d25e61cceea2be
Elijah Lopez <elijahllopezz@gmail.com>
2025-10-04 16:23:51 -0400

    feat(route): add Alto Canada Train (#20193)

---
+++ lib/routes/altotrain/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Alto - Toronto-Québec City High-Speed Rail Network',
+    url: 'altotrain.ca',
+    lang: 'en',
+};
