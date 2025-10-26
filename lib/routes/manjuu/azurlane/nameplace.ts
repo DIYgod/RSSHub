15a2f779dd3ff75e28c084899f450338df602f5c
AnitsuriW <moka@anitsuri.top>
2025-10-26 13:25:29 +0800

    feat(route): add AzurLane JP news

---
+++ lib/routes/manjuu/azurlane/nameplace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '蛮啾网络 Manjuu Games',
+    url: 'www.manjuu.com',
+    categories: ['game'],
+    lang: 'zh-CN',
+};
