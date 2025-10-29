aa81c4264c238ed35368a7f3287682807b68233a
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-10-29 11:20:48 +0800

    Update lib/middleware/cache.ts
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/middleware/cache.ts
+++ lib/middleware/cache.ts
@@ -17,8 +17,8 @@ const middleware: MiddlewareHandler = async (ctx, next) => {
     }
 
     const requestPath = ctx.req.path;
+    const format = `:${ctx.req.query('format') || 'rss'}`;
     const limit = ctx.req.query('limit') ? `:${ctx.req.query('limit')}` : '';
-    const format = ctx.req.query('format') === 'json' ? `:${ctx.req.query('format')}` : '';
     const { h64ToString } = await xxhash();
     const key = 'rsshub:koa-redis-cache:' + h64ToString(requestPath + limit + format);
     const controlKey = 'rsshub:path-requested:' + h64ToString(requestPath + limit + format);
