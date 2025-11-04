9e3ca7471c81af7c564822d55c9be0284be08aa9
Tony <TonyRL@users.noreply.github.com>
2025-11-04 22:47:28 +0800

    fix(route): bilibili ranking (#20426)
    * fix(route): bilibili ranking
    
    * fix: split apiUrl for signing
    
    * feat(route): add radar mapping for ranking redirects

--- lib/routes/bilibili/ranking.ts
+++ lib/routes/bilibili/ranking.ts
@@ -1,182 +1,220 @@
 import { Route, ViewType } from '@/types';
-import got from '@/utils/got';
+import ofetch from '@/utils/ofetch';
 import utils, { getVideoUrl } from './utils';
-import { parseDuration } from '@/utils/helpers';
+import { parseDate } from '@/utils/parse-date';
 import { config } from '@/config';
 import cache from './cache';
 
 // https://www.bilibili.com/v/popular/rank/all
 
-// 0 all https://api.bilibili.com/x/web-interface/ranking/v2?rid=0&type=all&web_location=333.934&w_rid=d4e0c1b83157e3d36836eb3c4258ef61&wts=1731320484
-// 1 bangumi https://api.bilibili.com/pgc/web/rank/list?day=3&season_type=1&web_location=333.934&w_rid=2d46eff2d363c4960bc875e63e24df6c&wts=1731320507
-// 2 guochan https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=4&web_location=333.934&w_rid=b26195dc9ee2f925bc196da68df341a5&wts=1731320523
-// 3 guochuang https://api.bilibili.com/x/web-interface/ranking/v2?rid=168&type=all&web_location=333.934&w_rid=f99e5982b011eb24643a2daffb7baf00&wts=1731320537
-// 4 documentary https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=3&web_location=333.934&w_rid=2067f7277cf49cbea4c5e5630eeb929a&wts=1731320556
-// 5 douga https://api.bilibili.com/x/web-interface/ranking/v2?rid=1&type=all&web_location=333.934&w_rid=14bf53ce651e8d575d5982b24e1cebdf&wts=1731320579
-// 6 music https://api.bilibili.com/x/web-interface/ranking/v2?rid=3&type=all&web_location=333.934&w_rid=70f4c870f860b9334ebe6e9fe835d3fe&wts=1731320595
-// 7 dance https://api.bilibili.com/x/web-interface/ranking/v2?rid=129&type=all&web_location=333.934&w_rid=691f713f7fc6d3cc08174affcc59f97c&wts=1731321260
-// 8 game https://api.bilibili.com/x/web-interface/ranking/v2?rid=4&type=all&web_location=333.934&w_rid=cac9f26f49da223cb8ab6f189250ec23&wts=1731320726
-// 9 knowledge https://api.bilibili.com/x/web-interface/ranking/v2?rid=36&type=all&web_location=333.934&w_rid=79c274d74e90d93ac7adfd2df968288e&wts=1731320750
-// 10 tech https://api.bilibili.com/x/web-interface/ranking/v2?rid=188&type=all&web_location=333.934&w_rid=115d9e69c48bf958622c4cc0ee861b57&wts=1731320766
-// 11 sports https://api.bilibili.com/x/web-interface/ranking/v2?rid=234&type=all&web_location=333.934&w_rid=c618d12f36e2379bda0c9a2754cd71e0&wts=1731320783
-// 12 car https://api.bilibili.com/x/web-interface/ranking/v2?rid=223&type=all&web_location=333.934&w_rid=753bc1395718051aa53aedaa3cd04d76&wts=1731320797
-// 13 life https://api.bilibili.com/x/web-interface/ranking/v2?rid=160&type=all&web_location=333.934&w_rid=3e8895d4749e905173886dd387f657e9&wts=1731320823
-// 14 food https://api.bilibili.com/x/web-interface/ranking/v2?rid=211&type=all&web_location=333.934&w_rid=9ec93cab672a98ea972dfb9cb7ed6368&wts=1731320838
-// 15 animal https://api.bilibili.com/x/web-interface/ranking/v2?rid=217&type=all&web_location=333.934&w_rid=794e69434ec4a818f4d589e5306e9a21&wts=1731320852
-// 16 kichiku https://api.bilibili.com/x/web-interface/ranking/v2?rid=119&type=all&web_location=333.934&w_rid=c5e35f3f247bc9294557ab90e0be166a&wts=1731320865
-// 17 fashion https://api.bilibili.com/x/web-interface/ranking/v2?rid=155&type=all&web_location=333.934&w_rid=f3711c888057a8fef1f47da9cf4bcd86&wts=1731320878
-// 18 ent https://api.bilibili.com/x/web-interface/ranking/v2?rid=5&type=all&web_location=333.934&w_rid=5ca1b2da22de1c9e818ac619d309fed2&wts=1731320889
-// 19 cinephile https://api.bilibili.com/x/web-interface/ranking/v2?rid=181&type=all&web_location=333.934&w_rid=8f5cae08b232025f93b74feaefdc95d9&wts=1731320903
-// 20 movie https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=2&web_location=333.934&w_rid=ccd42543ab1c4330e9f81fb52b098a9c&wts=1731320916
-// 21 tv https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=5&web_location=333.934&w_rid=10fae974e8d30dd6bba11527fe17e551&wts=1731320934
-// 22 variety https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=7&web_location=333.934&w_rid=c3105fd0dac70dcdf4f08ca6b5cbdb8f&wts=1731320948
-// 23 origin https://api.bilibili.com/x/web-interface/ranking/v2?rid=0&type=origin&web_location=333.934&w_rid=53100b7aeeca012399f4f8f3746bcbdb&wts=1731320960
-// 24 rookie https://api.bilibili.com/x/web-interface/ranking/v2?rid=0&type=rookie&web_location=333.934&w_rid=b8adda7447e2f115b2ed36495e436934&wts=1731320971
+// 0 all https://api.bilibili.com/x/web-interface/ranking/v2?rid=0&type=all&web_location=333.934&w_rid=&wts=
+// 1 anime https://api.bilibili.com/pgc/web/rank/list?day=3&season_type=1&web_location=333.934&w_rid=&wts=
+// 2 guochuang https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=4&web_location=333.934&w_rid=&wts=
+// 4 documentary https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=3&web_location=333.934&w_rid=&wts=
+// 5 movie https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=2&web_location=333.934&w_rid=&wts=
+// 6 tv https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=5&web_location=333.934&w_rid=&wts=
+// 7 variety https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=7&web_location=333.934&w_rid=&wts=
+// 8 douga https://api.bilibili.com/x/web-interface/ranking/v2?rid=1005&type=all&web_location=333.934&w_rid=&wts=
+// 9 game https://api.bilibili.com/x/web-interface/ranking/v2?rid=1008&type=all&web_location=333.934&w_rid=&wts=
+// 10 kichiku https://api.bilibili.com/x/web-interface/ranking/v2?rid=1007&type=all&web_location=333.934&w_rid=&wts=
+// 11 music https://api.bilibili.com/x/web-interface/ranking/v2?rid=1003&type=all&web_location=333.934&w_rid=&wts=
+// 12 dance https://api.bilibili.com/x/web-interface/ranking/v2?rid=1004&type=all&web_location=333.934&w_rid=&wts=
+// 13 cinephile https://api.bilibili.com/x/web-interface/ranking/v2?rid=1001&type=all&web_location=333.934&w_rid=&wts=
+// 14 ent https://api.bilibili.com/x/web-interface/ranking/v2?rid=1002&type=all&web_location=333.934&w_rid=&wts=
+// 15 knowledge https://api.bilibili.com/x/web-interface/ranking/v2?rid=1010&type=all&web_location=333.934&w_rid=&wts=
+// 16 tech https://api.bilibili.com/x/web-interface/ranking/v2?rid=1012&type=all&web_location=333.934&w_rid=&wts=
+// 17 food https://api.bilibili.com/x/web-interface/ranking/v2?rid=1020&type=all&web_location=333.934&w_rid=&wts=
+// 18 car https://api.bilibili.com/x/web-interface/ranking/v2?rid=1013&type=all&web_location=333.934&w_rid=&wts=
+// 19 fashion https://api.bilibili.com/x/web-interface/ranking/v2?rid=1014&type=all&web_location=333.934&w_rid=&wts=
+// 20 sports https://api.bilibili.com/x/web-interface/ranking/v2?rid=1018&type=all&web_location=333.934&w_rid=&wts=
+// 21 animal https://api.bilibili.com/x/web-interface/ranking/v2?rid=1024&type=all&web_location=333.934&w_rid=&wts=
 
-const ridNumberList = ['0', '1', '4', '168', '3', '1', '3', '129', '4', '36', '188', '234', '223', '160', '211', '217', '119', '155', '5', '181', '2', '5', '7', '0', '0'];
-const ridChineseList = [
-    '全站',
-    '番剧',
-    '国产动画',
-    '国创相关',
-    '纪录片',
-    '动画',
-    '音乐',
-    '舞蹈',
-    '游戏',
-    '知识',
-    '科技',
-    '运动',
-    '汽车',
-    '生活',
-    '美食',
-    '动物圈',
-    '鬼畜',
-    '时尚',
-    '娱乐',
-    '影视',
-    '电影',
-    '电视剧',
-    '综艺',
-    '原创',
-    '新人',
-];
-const ridEnglishList = [
-    'all',
-    'bangumi',
-    'guochan',
-    'guochuang',
-    'documentary',
-    'douga',
-    'music',
-    'dance',
-    'game',
-    'knowledge',
-    'tech',
-    'sports',
-    'car',
-    'life',
-    'food',
-    'animal',
-    'kichiku',
-    'fashion',
-    'ent',
-    'cinephile',
-    'movie',
-    'tv',
-    'variety',
-    'origin',
-    'rookie',
-];
-const ridTypeList = [
-    'x/rid',
-    'pgc/web',
-    'pgc/season',
-    'x/rid',
-    'pgc/season',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'x/rid',
-    'pgc/season',
-    'pgc/season',
-    'pgc/season',
-    'x/type',
-    'x/type',
-];
+const ridList = {
+    0: {
+        chinese: '全站',
+        english: 'all',
+        type: 'x/rid',
+    },
+    1: {
+        chinese: '番剧',
+        english: 'bangumi',
+        type: 'pgc/web',
+    },
+    4: {
+        chinese: '国创',
+        english: 'guochuang',
+        type: 'pgc/season',
+    },
+    3: {
+        chinese: '纪录片',
+        english: 'documentary',
+        type: 'pgc/season',
+    },
+    2: {
+        chinese: '电影',
+        english: 'movie',
+        type: 'pgc/season',
+    },
+    5: {
+        chinese: '电视剧',
+        english: 'tv',
+        type: 'pgc/season',
+    },
+    7: {
+        chinese: '综艺',
+        english: 'variety',
+        type: 'pgc/season',
+    },
+    1005: {
+        chinese: '动画',
+        english: 'douga',
+        type: 'x/rid',
+    },
+    1008: {
+        chinese: '游戏',
+        english: 'game',
+        type: 'x/rid',
+    },
+    1007: {
+        chinese: '鬼畜',
+        english: 'kichiku',
+        type: 'x/rid',
+    },
+    1003: {
+        chinese: '音乐',
+        english: 'music',
+        type: 'x/rid',
+    },
+    1004: {
+        chinese: '舞蹈',
+        english: 'dance',
+        type: 'x/rid',
+    },
+    1001: {
+        chinese: '影视',
+        english: 'cinephile',
+        type: 'x/rid',
+    },
+    1002: {
+        chinese: '娱乐',
+        english: 'ent',
+        type: 'x/rid',
+    },
+    1010: {
+        chinese: '知识',
+        english: 'knowledge',
+        type: 'x/rid',
+    },
+    1012: {
+        chinese: '科技数码',
+        english: 'tech',
+        type: 'x/rid',
+    },
+    1020: {
+        chinese: '美食',
+        english: 'food',
+        type: 'x/rid',
+    },
+    1013: {
+        chinese: '汽车',
+        english: 'car',
+        type: 'x/rid',
+    },
+    1014: {
+        chinese: '时尚美妆',
+        english: 'fashion',
+        type: 'x/rid',
+    },
+    1018: {
+        chinese: '体育运动',
+        english: 'sports',
+        type: 'x/rid',
+    },
+    1024: {
+        chinese: '动物',
+        english: 'animal',
+        type: 'x/rid',
+    },
+};
 
 export const route: Route = {
-    path: '/ranking/:rid_index?/:embed?/:redirect1?/:redirect2?',
+    path: '/ranking/:rid?/:embed?/:redirect1?/:redirect2?',
     name: '排行榜',
     maintainers: ['DIYgod', 'hyoban'],
     categories: ['social-media'],
     view: ViewType.Videos,
-    example: '/bilibili/ranking/0',
+    example: '/bilibili/ranking/all',
     parameters: {
-        rid_index: {
-            description: '排行榜分区 id 序号',
-            default: '0',
-            options: Array.from({ length: ridNumberList.length }, (_, i) => ({
-                value: String(i),
-                label: ridChineseList[i],
-            })).filter((_, i) => !ridTypeList[i].startsWith('pgc/')),
+        rid: {
+            description: '排行榜分区代号或 rid，可在 URL 中找到',
+            default: 'all',
+            options: Object.values(ridList)
+                .filter((v) => !v.type.startsWith('pgc/'))
+                .map((v) => ({
+                    value: v.english,
+                    label: v.chinese,
+                })),
         },
-        embed: '默认为开启内嵌视频, 任意值为关闭',
+        embed: '默认为开启内嵌视频，任意值为关闭',
         redirect1: '留空，用于兼容之前的路由',
         redirect2: '留空，用于兼容之前的路由',
     },
+    radar: [
+        {
+            source: ['www.bilibili.com/v/popular/rank/:rid'],
+            target: '/ranking/:rid',
+        },
+    ],
     handler,
 };
 
-function getRidIndexByRid(rid: string): number {
-    const index = ridNumberList.indexOf(rid);
-    if (index === -1) {
+function getAPI(isNumericRid: boolean, rid: string | number) {
+    if (isNumericRid) {
+        const zone = ridList[rid as number];
+        return {
+            apiBase: 'https://api.bilibili.com/x/web-interface/ranking/v2',
+            apiParams: `rid=${rid}&type=all&web_location=333.934`,
+            referer: 'https://www.bilibili.com/v/popular/rank/all',
+            ridChinese: zone?.chinese ?? '',
+            ridType: 'x/rid',
+            link: 'https://www.bilibili.com/v/popular/rank/all',
+        };
+    }
+
+    const zone = Object.entries(ridList).find(([_, v]) => v.english === rid);
+    if (!zone) {
         throw new Error('Invalid rid');
     }
-    return index;
-}
+    const numericRid = zone[0];
+    const ridType = zone[1].type;
+    const ridChinese = zone[1].chinese;
+    const ridEnglish = zone[1].english;
 
-function getAPI(ridIndex: number) {
-    if (ridIndex < 0 || ridIndex >= ridNumberList.length) {
-        throw new Error('Invalid rid index');
-    }
-    const rid = ridNumberList[ridIndex];
-    const ridType = ridTypeList[ridIndex];
-    const ridChinese = ridChineseList[ridIndex];
-    const ridEnglish = ridEnglishList[ridIndex];
-
-    let apiURL = '';
+    let apiBase = 'https://api.bilibili.com/x/web-interface/ranking/v2';
+    let apiParams = '';
 
     switch (ridType) {
         case 'x/rid':
-            apiURL = `https://api.bilibili.com/x/web-interface/ranking?rid=${rid}&type=all`;
+            apiParams = `rid=${numericRid}&type=all&web_location=333.934`;
             break;
         case 'pgc/web':
-            apiURL = `https://api.bilibili.com/pgc/web/rank/list?day=3&season_type=${rid}`;
+            apiBase = 'https://api.bilibili.com/pgc/web/rank/list';
+            apiParams = `day=3&season_type=${numericRid}&web_location=333.934`;
             break;
         case 'pgc/season':
-            apiURL = `https://api.bilibili.com/pgc/season/rank/web/list?day=3&season_type=${rid}`;
+            apiBase = 'https://api.bilibili.com/pgc/season/rank/web/list';
+            apiParams = `day=3&season_type=${numericRid}&web_location=333.934`;
             break;
-        case 'x/type':
-            apiURL = `https://api.bilibili.com/x/web-interface/ranking?rid=0&type=${ridEnglish}`;
-            break;
+        // case 'x/type':
+        //     apiUrl = `https://api.bilibili.com/x/web-interface/ranking?rid=0&type=${numericRid}&web_location=333.934`;
+        //     break;
         default:
             throw new Error('Invalid rid type');
     }
 
     return {
-        apiURL,
+        apiBase,
+        apiParams,
         referer: `https://www.bilibili.com/v/popular/rank/${ridEnglish}`,
         ridChinese,
         ridType,
@@ -190,27 +228,30 @@ async function handler(ctx) {
     if (args.redirect1 || args.redirect2) {
         // redirect old routes like /bilibili/ranking/0/3/1 or /bilibili/ranking/0/3/1/xxx
         const embedArg = args.redirect2 ? '/' + args.redirect2 : '';
-        ctx.set('redirect', `/bilibili/ranking/${getRidIndexByRid(args.rid_index)}${embedArg}`);
+        ctx.set('redirect', `/bilibili/ranking/${args.rid}${embedArg}`);
         return null;
     }
 
-    const ridIndex = ctx.req.param('rid_index') || '0';
+    const rid = ctx.req.param('rid') || 'all';
     const embed = !ctx.req.param('embed');
+    const isNumericRid = /^\d+$/.test(rid);
 
-    const { apiURL, referer, ridChinese, link, ridType } = getAPI(Number(ridIndex));
+    const { apiBase, apiParams, referer, ridChinese, link, ridType } = getAPI(isNumericRid, rid);
     if (ridType.startsWith('pgc/')) {
         throw new Error('This type of ranking is not supported yet');
     }
 
-    const response = await got({
-        method: 'get',
-        url: apiURL,
+    const response = await ofetch(`${apiBase}?${apiParams}`, {
         headers: {
             Referer: referer,
+            origin: 'https://www.bilibili.com',
         },
     });
 
-    const data = response.data.data || response.data.result;
+    if (response.code !== 0) {
+        throw new Error(response.message);
+    }
+    const data = response.data || response.result;
     const list = data.list || [];
     return {
         title: `bilibili 排行榜-${ridChinese}`,
@@ -220,17 +261,17 @@ async function handler(ctx) {
                 const subtitles = isJsonFeed && !config.bilibili.excludeSubtitles && item.bvid ? await cache.getVideoSubtitleAttachment(item.bvid) : [];
                 return {
                     title: item.title,
-                    description: utils.renderUGCDescription(embed, item.pic, item.description || item.title, item.aid, undefined, item.bvid),
-                    pubDate: item.create && new Date(item.create).toUTCString(),
-                    author: item.author,
-                    link: !item.create || (new Date(item.create).getTime() / 1000 > utils.bvidTime && item.bvid) ? `https://www.bilibili.com/video/${item.bvid}` : `https://www.bilibili.com/video/av${item.aid}`,
+                    description: utils.renderUGCDescription(embed, item.pic, item.desc || item.title, item.aid, undefined, item.bvid),
+                    pubDate: item.ctime && parseDate(item.ctime, 'X'),
+                    author: item.owner.name,
+                    link: !item.ctime || (item.ctime > utils.bvidTime && item.bvid) ? `https://www.bilibili.com/video/${item.bvid}` : `https://www.bilibili.com/video/av${item.aid}`,
                     image: item.pic,
                     attachments: item.bvid
                         ? [
                               {
                                   url: getVideoUrl(item.bvid),
                                   mime_type: 'text/html',
-                                  duration_in_seconds: parseDuration(item.duration),
+                                  duration_in_seconds: item.duration,
                               },
                               ...subtitles,
                           ]
