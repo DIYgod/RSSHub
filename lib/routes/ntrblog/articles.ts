39df3e887f15a1c86b1ee39fc0dba55c30dc3b48
Keo <keocheung@outlook.com>
2025-11-19 01:55:34 +0800

    fix(route/ntrblog): use utils/got to get feed content (#20511)

--- lib/routes/ntrblog/articles.ts
+++ lib/routes/ntrblog/articles.ts
@@ -35,7 +35,8 @@ export const route: Route = {
 };
 
 async function handler(): Promise<Data> {
-    const feed = await parser.parseURL('https://ntrblog.com/atom.xml');
+    const rsp = await got('https://ntrblog.com/atom.xml');
+    const feed = await parser.parseString(rsp.data);
 
     const items = await Promise.all(
         feed.items.map((item) => {
