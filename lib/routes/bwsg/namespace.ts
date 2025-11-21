74ee0c54c8004114d3aa3b001ff8be9e3948a08b
Samuel Kaiser <samuel.kaiser@timetoact.at>
2025-11-21 18:27:59 +0100

    feat(route): Add route for bwsg.at

---
+++ lib/routes/bwsg/namespace.ts
@@ -0,0 +1,7 @@
+import { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'BWSG',
+    url: 'bwsg.at',
+    description: 'BWS Gemeinnützige allgemeine Bau-, Wohn- und Siedlungsgenossenschaft, registrierte Genossenschaft mit beschränkter Haftung',
+};
