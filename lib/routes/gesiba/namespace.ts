306191d551785b5ce2aa482ed0437b813aafc684
sk22 <sk22@mailbox.org>
2025-11-25 16:36:44 +0100

    feat(route): Add route for gesiba.at (#20538)
    * feat(category): add housing category
    
    * feat(route): Add gesiba.at route
    
    * fix(route/gesiba): improve feed, fix imports
    
    * fix(route/gesiba): fix feed title
    
    * Revert "feat(category): add housing category"
    
    This reverts commit 953f59421717592dc5d8f2e0d8c42b5d54c77c72.
    
    * fix(route/gesiba): change category
    
    ---------
    
    Co-authored-by: Samuel Kaiser <samuel.kaiser@timetoact.at>

---
+++ lib/routes/gesiba/namespace.ts
@@ -0,0 +1,7 @@
+import { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Gesiba',
+    url: 'gesiba.at',
+    description: 'Gemeinnützige Siedlungs- und Bau AG',
+};
