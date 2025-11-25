c7c9cf5e3baf0e680feff6a920369cb803b1cc20
sk22 <sk22@mailbox.org>
2025-11-25 20:09:22 +0100

    feat(route): Add route for wiensued.at cooperative housing (#20552)
    * feat(category): add housing category
    
    * feat(route/wiensued): add route for wiensued.at
    
    * Revert "feat(category): add housing category"
    
    This reverts commit 953f59421717592dc5d8f2e0d8c42b5d54c77c72.
    
    * feat(route/wiensued): change category
    
    ---------
    
    Co-authored-by: Samuel Kaiser <samuel.kaiser@timetoact.at>

---
+++ lib/routes/wiensued/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Wien-Süd',
+    url: 'wiensued.at',
+    description: 'Gemeinnützige Bau- u. Wohnungsgenossenschaft „Wien-Süd“ eingetragene Genossenschaft m.b.H.',
+};
