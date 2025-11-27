46caba0036b6b0069867c978a04315ab386a83fc
Samuel Kaiser <samuel.kaiser@timetoact.at>
2025-11-27 12:42:48 +0100

    fix(route/wogem): remove redundant filter parameter

--- lib/routes/wogem/index.ts
+++ lib/routes/wogem/index.ts
@@ -1,6 +1,7 @@
 import { load } from 'cheerio';
-import ofetch from '@/utils/ofetch';
+
 import type { Data, DataItem, Route } from '@/types';
+import ofetch from '@/utils/ofetch';
 
 const FEED_TITLE = 'WOGEM' as const;
 const FEED_LANGUAGE = 'de' as const;
@@ -9,22 +10,17 @@ const BASE_URL = 'https://www.wogem.at/de/' as const;
 
 export const route: Route = {
     name: 'Angebote',
-    example: '/wogem/angebote',
-    path: '/:page?/:filter?',
+    example: '/wogem/angebote?filter=Graz',
+    path: '/:page?',
     maintainers: ['sk22'],
     categories: ['other'],
-    description: `
-Pass in the name of the php file, e.g. \`angebote\` for \`/de/angebote.php\`\`.
-Second parameter is a case-insensitve RegExp pattern to filter the results`,
+    description: `Pass in the name of the php file, e.g. \`angebote\` for \`/de/angebote.php\`\`.`,
     parameters: {
         page: 'Page name, e.g. `angebote` for `angebote.php. Defaults to `angebote`',
-        filter: 'RegExp pattern to filter results',
     },
 
     async handler(ctx) {
         const page = ctx.req.param('page') || 'angebote';
-        const filter = ctx.req.param('filter');
-        const regex = filter ? new RegExp(filter, 'i') : undefined;
         const link = `${BASE_URL}${page}.php`;
 
         const response = await ofetch(link);
@@ -38,15 +34,12 @@ Second parameter is a case-insensitve RegExp pattern to filter the results`,
                 const href = $el.attr('href');
                 const title = $el.text();
 
-                return regex && !regex.test(title)
-                    ? false
-                    : ({
-                          title,
-                          link: href?.startsWith('?') ? link + href : href,
-                          // no pubDate or image :(
-                      } satisfies DataItem);
-            })
-            .filter((item) => item !== false);
+                return {
+                    title,
+                    link: href?.startsWith('?') ? link + href : href,
+                    // no pubDate or image :(
+                } satisfies DataItem;
+            });
 
         return {
             title: `${heading} - ${FEED_TITLE}`,
