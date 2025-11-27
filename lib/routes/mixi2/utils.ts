56f98528cf3dbd02143f7cb0c42675c968db3d01
Tony <TonyRL@users.noreply.github.com>
2025-11-27 11:29:49 +0800

    style: fix import statement formatting

--- lib/routes/mixi2/utils.ts
+++ lib/routes/mixi2/utils.ts
@@ -1,4 +1,4 @@
-import type { Category, MixiClient, Persona, type Post } from 'mixi2';
+import { Category, MixiClient, type Persona, type Post } from 'mixi2';
 
 import { config } from '@/config';
 import ConfigNotFoundError from '@/errors/types/config-not-found';
