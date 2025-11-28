28d593b121737b4e65ce5662bffa0ee5468958d3
Samuel Kaiser <samuel.kaiser@timetoact.at>
2025-11-28 13:53:14 +0100

    fix(route/bwsg): fetch all pages

--- lib/routes/bwsg/index.ts
+++ lib/routes/bwsg/index.ts
@@ -1,13 +1,17 @@
-import { getSubPath } from '@/utils/common-utils';
+import type { CheerioAPI } from 'cheerio';
 import { load } from 'cheerio';
-import { ofetch } from 'ofetch';
+
 import type { Data, DataItem, Route } from '@/types';
+import { getSubPath } from '@/utils/common-utils';
+import ofetch from '@/utils/ofetch';
 
 const FEED_TITLE = 'Immobilien - BWSG' as const;
 const FEED_LANGUAGE = 'de' as const;
 const FEED_LOGO = 'https://www.bwsg.at/wp-content/uploads/2024/06/favicon-bwsg.png';
 const SITE_URL = 'https://www.bwsg.at' as const;
 const BASE_URL = `${SITE_URL}/immobilien/immobilie-suchen/`;
+
+const totalPagesRegex = /"total_pages":\s*(\d+)[,\s}]/;
 
 export const route: Route = {
     name: 'Angebote',
@@ -16,6 +20,10 @@ export const route: Route = {
     maintainers: ['sk22'],
     categories: ['other'],
     description: `
+Since there's no parameter available that sorts by "last added" (and there's no
+obvious pattern to the default ordering), this route **fetches all pages**, so
+please specify at lease some parameters.
+
 Copy the query parameters for your https://www.bwsg.at/immobilien/immobilie-suchen search,
 omitting the leading \`?\``,
 
@@ -26,36 +34,46 @@ omitting the leading \`?\``,
         }
 
         const link = `${BASE_URL}?${params}`;
-        const response = await ofetch(link);
-        const $ = load(response);
+        const firstPage = (await ofetch(link)) as string;
+        // extract number of total pages from the first page
+        const lastPageNumber = Number(totalPagesRegex.exec(firstPage)?.[1]);
+        // skipping first page (already fetched)
+        const tailPageNumbers = Array.from({ length: lastPageNumber - 1 }, (_, i) => i + 2);
 
-        const items = $('[data-objektnummer] > a')
-            .toArray()
-            .map((el) => {
-                const $el = $(el);
-                const link = el.attribs.href;
-                const image = $el.find('.res_immobiliensuche__immobilien__item__thumb > img').attr('src');
-                const title = $el.find('.res_immobiliensuche__immobilien__item__content__title').text().trim();
-                const location = $el.find('.res_immobiliensuche__immobilien__item__content__meta__location').text().trim();
-                const price = $el.find('.res_immobiliensuche__immobilien__item__content__meta__preis').text().trim();
-                const metadata = $el.find('.res_immobiliensuche__immobilien__item__content__meta__row_1').text().trim();
-
-                return {
-                    title: `${location}, ${title}`,
-                    description: (price ? `${price} | ` : '') + metadata,
-                    link,
-                    image,
-                    // no pubDate :(
-                } satisfies DataItem;
-            });
+        // fetching all pages (because the page doesn't allow sorting entries by date added...)
+        const results = await Promise.allSettled(tailPageNumbers.map((pageNumber) => ofetch(`${BASE_URL}?_paged=${pageNumber}&${params}`)));
+        // dismiss failed fetches (rss readers will refetch at some point anyway)
+        const pages: string[] = [firstPage, ...results.filter((result) => result.status === 'fulfilled').map((result) => result.value)];
 
         return {
             title: FEED_TITLE,
             language: FEED_LANGUAGE,
             logo: FEED_LOGO,
             allowEmpty: true,
-            item: items,
+            item: pages.flatMap((page) => mapPageItems(load(page))),
             link,
         } satisfies Data;
     },
 };
+
+function mapPageItems($: CheerioAPI) {
+    return $('[data-objektnummer] > a')
+        .toArray()
+        .map((el) => {
+            const $el = $(el);
+            const link = el.attribs.href;
+            const image = $el.find('.res_immobiliensuche__immobilien__item__thumb > img').attr('src');
+            const title = $el.find('.res_immobiliensuche__immobilien__item__content__title').text().trim();
+            const location = $el.find('.res_immobiliensuche__immobilien__item__content__meta__location').text().trim();
+            const price = $el.find('.res_immobiliensuche__immobilien__item__content__meta__preis').text().trim();
+            const metadata = $el.find('.res_immobiliensuche__immobilien__item__content__meta__row_1').text().trim();
+
+            return {
+                title: `${location}, ${title}`,
+                description: (price ? `${price} | ` : '') + metadata,
+                link,
+                image,
+                // no pubDate :(
+            } satisfies DataItem;
+        });
+}
