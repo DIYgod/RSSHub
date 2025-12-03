ce6261874c0f6570982d48aab9b9ceff0af68419
sk <sk22@mailbox.org>
2025-12-03 20:20:50 +0100

    chore(route/bwsg): only fetch first page

--- lib/routes/bwsg/index.ts
+++ lib/routes/bwsg/index.ts
@@ -1,4 +1,3 @@
-import type { CheerioAPI } from 'cheerio';
 import { load } from 'cheerio';
 
 import type { Data, DataItem, Route } from '@/types';
@@ -11,8 +10,6 @@ const FEED_LOGO = 'https://www.bwsg.at/wp-content/uploads/2024/06/favicon-bwsg.p
 const SITE_URL = 'https://www.bwsg.at' as const;
 const BASE_URL = `${SITE_URL}/immobilien/immobilie-suchen/`;
 
-const totalPagesRegex = /"total_pages":\s*(\d+)[,\s}]/;
-
 export const route: Route = {
     name: 'Angebote',
     example: '/bwsg/_vermarktungsart=miete&_objektart=wohnung&_zimmer=2,3&_wohnflaeche=45,70&_plz=1210,1220',
@@ -20,12 +17,16 @@ export const route: Route = {
     maintainers: ['sk22'],
     categories: ['other'],
     description: `
-Since there's no parameter available that sorts by "last added" (and there's no
-obvious pattern to the default ordering), this route **fetches all pages**, so
-please specify at lease some parameters.
+Copy the query parameters for your https://www.bwsg.at/immobilien/immobilie-suchen
+search, omitting the leading \`?\`
 
-Copy the query parameters for your https://www.bwsg.at/immobilien/immobilie-suchen search,
-omitting the leading \`?\``,
+::: tip
+Since there's no parameter available that sorts by "last added" (and there's no
+obvious pattern to the default ordering), and since this RSS feed only fetches
+the first page of results, you probably want to specify enough search
+parameters to make sure you only get one page of results – because else, your
+RSS feed might not get all items.
+:::`,
 
     async handler(ctx) {
         let params = getSubPath(ctx).slice(1);
@@ -34,46 +35,36 @@ omitting the leading \`?\``,
         }
 
         const link = `${BASE_URL}?${params}`;
-        const firstPage = (await ofetch(link)) as string;
-        // extract number of total pages from the first page
-        const lastPageNumber = Number(totalPagesRegex.exec(firstPage)?.[1]);
-        // skipping first page (already fetched)
-        const tailPageNumbers = Array.from({ length: lastPageNumber - 1 }, (_, i) => i + 2);
+        const response = await ofetch(link);
+        const $ = load(response);
 
-        // fetching all pages (because the page doesn't allow sorting entries by date added...)
-        const results = await Promise.allSettled(tailPageNumbers.map((pageNumber) => ofetch(`${BASE_URL}?_paged=${pageNumber}&${params}`)));
-        // dismiss failed fetches (rss readers will refetch at some point anyway)
-        const pages: string[] = [firstPage, ...results.filter((result) => result.status === 'fulfilled').map((result) => result.value)];
+        const items = $('[data-objektnummer] > a')
+            .toArray()
+            .map((el) => {
+                const $el = $(el);
+                const link = el.attribs.href;
+                const image = $el.find('.res_immobiliensuche__immobilien__item__thumb > img').attr('src');
+                const title = $el.find('.res_immobiliensuche__immobilien__item__content__title').text().trim();
+                const location = $el.find('.res_immobiliensuche__immobilien__item__content__meta__location').text().trim();
+                const price = $el.find('.res_immobiliensuche__immobilien__item__content__meta__preis').text().trim();
+                const metadata = $el.find('.res_immobiliensuche__immobilien__item__content__meta__row_1').text().trim();
+
+                return {
+                    title: `${location}, ${title}`,
+                    description: (price ? `${price} | ` : '') + metadata,
+                    link,
+                    image,
+                    // no pubDate :(
+                } satisfies DataItem;
+            });
 
         return {
             title: FEED_TITLE,
             language: FEED_LANGUAGE,
             logo: FEED_LOGO,
             allowEmpty: true,
-            item: pages.flatMap((page) => mapPageItems(load(page))),
+            item: items,
             link,
         } satisfies Data;
     },
 };
-
-function mapPageItems($: CheerioAPI) {
-    return $('[data-objektnummer] > a')
-        .toArray()
-        .map((el) => {
-            const $el = $(el);
-            const link = el.attribs.href;
-            const image = $el.find('.res_immobiliensuche__immobilien__item__thumb > img').attr('src');
-            const title = $el.find('.res_immobiliensuche__immobilien__item__content__title').text().trim();
-            const location = $el.find('.res_immobiliensuche__immobilien__item__content__meta__location').text().trim();
-            const price = $el.find('.res_immobiliensuche__immobilien__item__content__meta__preis').text().trim();
-            const metadata = $el.find('.res_immobiliensuche__immobilien__item__content__meta__row_1').text().trim();
-
-            return {
-                title: `${location}, ${title}`,
-                description: (price ? `${price} | ` : '') + metadata,
-                link,
-                image,
-                // no pubDate :(
-            } satisfies DataItem;
-        });
-}
