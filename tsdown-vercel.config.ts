add80cdb57bd3e9a8589de0d2566e0a3bdce118a
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-12-06 23:50:22 +0800

    chore: fix vercel serve static

--- tsdown-vercel.config.ts
+++ tsdown-vercel.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     shims: true,
     clean: true,
     plugins: [artTemplatesPlugin()],
-    copy: ['lib/assets'],
+    copy: [{ from: 'lib/assets', to: 'dist/public' }],
 });
