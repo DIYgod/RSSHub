9f6fb227bff390e4998984a874feca2d9fc03c9a
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-12-07 21:33:27 +0800

    save

--- tsdown-vercel.config.ts
+++ tsdown-vercel.config.ts
@@ -1,47 +1,12 @@
-import fs from 'node:fs';
-
 import { defineConfig } from 'tsdown';
 
 import artTemplatesPlugin from './plugins/rollup-plugin-art-templates.ts';
 
 export default defineConfig({
     entry: ['./lib/server.ts'],
-    outDir: '.vercel/output/functions/index.func/dist',
     minify: true,
     shims: true,
     clean: true,
     plugins: [artTemplatesPlugin()],
-    copy: [{ from: 'lib/assets', to: '.vercel/output/static' }],
-    hooks: {
-        'build:done'() {
-            fs.writeFileSync(
-                '.vercel/output/config.json',
-                JSON.stringify(
-                    {
-                        version: 3,
-                        routes: [
-                            {
-                                src: '/(.*)',
-                                dest: '/',
-                            },
-                        ],
-                    },
-                    null,
-                    2
-                )
-            );
-            fs.writeFileSync(
-                '.vercel/output/functions/index.func/.vc-config.json',
-                JSON.stringify(
-                    {
-                        runtime: 'nodejs24.x',
-                        handler: 'dist/server.mjs',
-                        launcherType: 'Nodejs',
-                    },
-                    null,
-                    2
-                )
-            );
-        },
-    },
+    // copy: [{ from: 'lib/assets', to: 'dist' }],
 });
