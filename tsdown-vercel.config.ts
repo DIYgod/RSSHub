fd98be10279cb31fefc9d4155324cfe7f0480573
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-12-08 00:21:14 +0800

    chore: fix art template on vercel (#20647)

--- tsdown-vercel.config.ts
+++ tsdown-vercel.config.ts
@@ -4,6 +4,9 @@ import artTemplatesPlugin from './plugins/rollup-plugin-art-templates.ts';
 
 export default defineConfig({
     entry: ['./lib/server.ts'],
+    // Wait for https://github.com/vercel/vercel/pull/14429
+    // Then we can set outDir to outputDirectory in vercel.json
+    outDir: 'src',
     minify: true,
     shims: true,
     clean: true,
