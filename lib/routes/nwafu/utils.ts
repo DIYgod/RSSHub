ff393898a0751604595cc8583ef98f4c9f5db9cc
凌 <i@ioioi.cn>
2025-12-12 09:08:01 +0800

    Refactor notification URLs in utils.ts

--- lib/routes/nwafu/utils.ts
+++ lib/routes/nwafu/utils.ts
@@ -1,11 +1,3 @@
-const libValue = ['https://lib.nwafu.edu.cn/gg/', '.pageList ul li', 'li a', '.pageArticle', `西北农林科技大学图书馆通知公告`];
-const youthValue = ['https://54youth.nwsuaf.edu.cn/twsy/tzgg//', 'section ul li', 'li a', 'article', `共青团西北农林科技大学委员会通知公告`];
-const cieValue = ['https://cie.nwsuaf.edu.cn/dtytz/tzgg/', 'ul.list  li', 'li a', '.article', `西北农林科技大学信息工程学院通知公告`];
-const gsValue = ['https://gs.nwsuaf.edu.cn/tzggB/', 'dl#sort ul.list  li', 'li a', '.content', `西北农林科技大学后勤管理处通知公告`];
-const jccValue = ['https://jcc.nwsuaf.edu.cn/tzgg/', 'dl#sort ul.list  li', 'li a', '.content', `西北农林科技大学计划财务处通知公告`];
-const jiaowuValue = ['https://jiaowu.nwsuaf.edu.cn/tzggB/', 'ul.list li', 'a', '.article2', `西北农林科技大学教务通知公告`];
-const newsValue = ['https://news.nwafu.edu.cn/yxxw/', 'ul.NWAFU-list01  li', 'li a', '.pageArticle', `西北农林科技大学新闻网聚焦院处`];
-const nicValue = ['https://nic.nwsuaf.edu.cn/gzdt2/tzgg1/', 'dl#sort ul.list  li', 'li a', '.article', `西北农林科技大学信息化管理处通知公告`];
 const libValue = ['https://lib.nwafu.edu.cn/gg/', '.pageList ul li', 'li a', '.pageArticle', '西北农林科技大学图书馆通知公告'];
 const youthValue = ['https://54youth.nwsuaf.edu.cn/twsy/tzgg//', 'section ul li', 'li a', 'article', '共青团西北农林科技大学委员会通知公告'];
 const cieValue = ['https://cie.nwsuaf.edu.cn/dtytz/tzgg/', 'ul.list  li', 'li a', '.article', '西北农林科技大学信息工程学院通知公告'];
