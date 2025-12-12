5b584ac2d27451ddf39ba09451b0532c771f04af
FrozenStar <frozenstars@qq.com>
2025-12-12 16:11:17 +0800

    feat(route): add two new routes of ECNU

--- lib/routes/ecnu/cxcy.ts
+++ lib/routes/ecnu/cxcy.ts
@@ -7,7 +7,7 @@ import { parseDate } from '@/utils/parse-date';
 import timezone from '@/utils/timezone';
 
 export const route: Route = {
-    path: '/cxcy',
+    path: '/cxcy/:type?',
     categories: ['university'],
     example: '/ecnu/cxcy',
     radar: [
@@ -18,40 +18,67 @@ export const route: Route = {
     ],
     name: '本科生院通知',
     maintainers: ['FrozenStarrrr', 'ChiyoYuki', 'ECNU-minus'],
-    handler: async () => {
+    handler: async (ctx) => {
+        const fragList = {
+            announcement: {
+                session: '通知公告',
+                frag: '窗口121',
+            },
+            news: {
+                session: '新闻动态',
+                frag: '窗口123',
+            },
+            contest: {
+                session: '学科竞赛',
+                frag: '窗口124',
+            },
+            resources: {
+                session: '常用资源',
+                frag: '窗口125',
+            },
+        };
+        const type = ctx.req.param('type') ?? 'announcement';
         const baseUrl = 'http://www.cxcy.ecnu.edu.cn/';
 
         const response = await got(baseUrl);
         const $ = load(response.data);
-        const links = $('table.wp_article_list_table')
-            .toArray()
-            .map((el) => ({
-                pubDate: timezone(parseDate($(el).find('.news_date').text()), +8),
-                link: new URL($(el).find('a').attr('href'), baseUrl).toString(),
-                title: $(el).find('.news_title').text(),
-            }));
+
+        const filteredEls = $('div.limit_style1 > table > tbody > tr > td')
+            .filter((_, el) => $(el).closest('div').attr('frag') === fragList[type].frag)
+            .toArray();
+        const links = filteredEls.map((el) => ({
+            pubDate: timezone(parseDate($(el).find('.data').text()), +8),
+            link: new URL($(el).find('a').attr('href'), baseUrl).toString(),
+            title: $(el).find('.news_title').text(),
+        }));
         const items = await Promise.all(
             links.map((item) =>
                 cache.tryGet(item.link, async () => {
-                    const { data } = await got(item.link);
-                    const $ = load(data);
-                    const $read = $('div.wp_articlecontent');
-                    $read.find('img[src], a[href]').each((i, el) => {
-                        const $el = $(el);
-                        const attr = el.tagName === 'img' ? 'src' : 'href';
-                        const val = $el.attr(attr);
-                        if (val) {
-                            $el.attr(attr, new URL(val, baseUrl).toString());
-                        }
-                    });
-                    item.description = $read.html()?.trim();
-                    return item;
+                    if (item.link.split('.').pop() === 'htm') {
+                        const { data } = await got(item.link);
+                        const $ = load(data);
+                        const $read = $('div.wp_articlecontent').length <= 0 ? $('div.m3nEditor') : $('div.wp_articlecontent');
+                        $read.find('img[src], a[href]').each((i, el) => {
+                            const $el = $(el);
+                            const attr = el.tagName === 'img' ? 'src' : 'href';
+                            const val = $el.attr(attr);
+                            if (val) {
+                                $el.attr(attr, new URL(val, baseUrl).toString());
+                            }
+                        });
+                        item.description = $read.html()?.trim();
+                        return item;
+                    } else {
+                        // file to download
+                        item.description = '请到原网页访问';
+                        return item;
+                    }
                 })
             )
         );
 
         return {
-            title: '学科竞赛通知',
+            title: fragList[type].session,
             link: 'http://www.cxcy.ecnu.edu.cn/',
             item: items,
         };
