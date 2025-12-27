f5f3cb85659b5e9c6c278e99cb7e40befd369811
Tony <TonyRL@users.noreply.github.com>
2025-12-28 04:35:00 +0800

    fix(utils): update pubDate parsing to include timezone

--- lib/routes/foodtalks/utils.ts
+++ lib/routes/foodtalks/utils.ts
@@ -2,6 +2,7 @@ import type { DataItem } from '@/types';
 import cache from '@/utils/cache';
 import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
+import timezone from '@/utils/timezone';
 
 export const apiBaseUrl = 'https://api-we.foodtalks.cn';
 export const baseUrl = 'https://www.foodtalks.cn';
@@ -9,7 +10,7 @@ export const baseUrl = 'https://www.foodtalks.cn';
 export const parseList = (records): DataItem[] =>
     records.map((item) => ({
         title: item.title,
-        pubDate: parseDate(item.publishTime),
+        pubDate: timezone(parseDate(item.publishTime), 8),
         link: `${baseUrl}/news/${item.id}`,
         category: [item.parentTagCode === 'category' ? item.tagCode : item.parentTagCode, ...item.seoKeywords.split(',')].filter(Boolean),
         author: item.author || item.sourceName,
