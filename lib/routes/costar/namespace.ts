4b6c445f73aba7473cf2892e8fe2df50a2ea47f6
Ethan Shen <42264778+nczitzk@users.noreply.github.com>
2026-01-01 01:48:27 +0800

    feat(route): add CoStar Press Releases (#20791)
    * feat(route): add CoStar Press Releases
    
    * fix typo

---
+++ lib/routes/costar/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'CoStar',
+    url: 'costar.com',
+    categories: ['new-media'],
+    description: '',
+    lang: 'en',
+};
