176305ba906e929daac1546b2df0a1df184627e4
chrisis <chrisis58@outlook.com>
2026-01-07 13:44:08 +0800

    fix: update url for maven namespace

--- lib/routes/maven/namespace.ts
+++ lib/routes/maven/namespace.ts
@@ -2,7 +2,7 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: 'Maven',
-    url: 'central.sonatype.com', // Central Repository, change to 'maven.apache.org' if needed
+    url: 'repo1.maven.org/maven2/',
     categories: ['programming'],
     description: "Apache Maven is a build tool for Java projects. Using a project object model (POM), Maven manages a project's compilation, testing, and documentation.",
     lang: 'en',
