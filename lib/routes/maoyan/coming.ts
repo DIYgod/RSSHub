40502c69969e6b246b52c20d39dc0d81bdd0d0b6
jxtan <dawenzaist@gmail.com>
2026-01-23 14:45:07 +0800

    feat(route): add maoyan (猫眼电影) routes
    Add three new routes for Maoyan:
    - /maoyan/box - Real-time box office ranking
    - /maoyan/hot - Currently showing movies
    - /maoyan/coming - Upcoming movies

---
+++ lib/routes/maoyan/coming.ts
@@ -0,0 +1,75 @@
+import type { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+
+interface MovieItem {
+    id: number;
+    nm: string;
+    img: string;
+    sc: number;
+    star: string;
+    rt: string;
+    showInfo?: string;
+    wish: number;
+    comingTitle: string;
+}
+
+interface ComingResponse {
+    coming: MovieItem[];
+}
+
+export const route: Route = {
+    path: '/coming',
+    categories: ['multimedia'],
+    example: '/maoyan/coming',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['www.maoyan.com/films?showType=2', 'www.maoyan.com/films'],
+            target: '/coming',
+        },
+    ],
+    name: '即将上映',
+    maintainers: ['JackyST0'],
+    handler,
+};
+
+async function handler() {
+    const apiUrl = 'https://m.maoyan.com/ajax/comingList?ci=1&limit=30&movieIds=&token=';
+
+    const response = await ofetch<ComingResponse>(apiUrl, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
+            Referer: 'https://m.maoyan.com/',
+        },
+    });
+
+    const movies = response.coming || [];
+
+    const items = movies.map((movie) => ({
+        title: `[${movie.comingTitle}] ${movie.nm}`,
+        link: `https://www.maoyan.com/films/${movie.id}`,
+        description: `
+            <img src="${movie.img}" />
+            <p><strong>${movie.nm}</strong></p>
+            <p>上映时间：${movie.rt}</p>
+            <p>主演：${movie.star}</p>
+            <p>想看人数：${movie.wish}</p>
+            ${movie.showInfo ? `<p>${movie.showInfo}</p>` : ''}
+        `,
+    }));
+
+    return {
+        title: '猫眼电影 - 即将上映',
+        link: 'https://www.maoyan.com/films?showType=2',
+        description: '猫眼电影即将上映列表',
+        item: items,
+    };
+}
