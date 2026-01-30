eb3965d8eb2d1be167b4176b20ebeb134c18774f
Kota <91359399+Skota11@users.noreply.github.com>
2026-01-30 23:33:21 +0000

    add scratch routes

---
+++ lib/routes/scratch/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Scratch',
+    url: 'scratch.mit.edu',
+    zh: {
+        name: 'Scratch',
+    },
+};
