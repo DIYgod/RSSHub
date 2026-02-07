b0211ee1b6f75562fce40226774e14e4afd4cf58
ZZJ <2111976953@qq.com>
2026-02-07 19:52:21 +0800

    new route:jtystHenan

---
+++ lib/routes/jtystHenan/index.ts
@@ -0,0 +1,71 @@
+import { load } from 'cheerio';
+
+import type { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/:fSeg?/:sSeg?/:tSeg?',
+    categories: ['government'],
+    example: '/jtystHenan/zc/zdgk/zcwj',
+    parameters: {
+        fSeg: {
+            description: '一级路径',
+        },
+        sSeg: {
+            description: '二级路径',
+        },
+        tSeg: {
+            description: '三级路径',
+        },
+    },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '河南省交通运输厅',
+    maintainers: ['OtacodeZ'],
+    handler,
+    description: `订阅河南省交通运输厅的news_box更新`,
+};
+
+async function handler(ctx) {
+    const fSeg = ctx.req.param('fSeg') ?? '';
+    const sSeg = ctx.req.param('sSeg') ?? '';
+    const tSeg = ctx.req.param('tSeg') ?? '';
+
+    const rootUrl = 'https://jtyst.henan.gov.cn';
+    const fUrl = fSeg === '' ? '' : `/${fSeg}`;
+    const sUrl = sSeg === '' ? '' : `/${sSeg}`;
+    const tUrl = tSeg === '' ? '' : `/${tSeg}`;
+
+    const currentUrl = `${rootUrl}${fUrl}${sUrl}${tUrl}`;
+
+    const response = await ofetch(currentUrl);
+    const $ = load(response);
+
+    const items = $('div.news_box li') // $('div.main_list ul.news_list li')
+        .toArray()
+        .map((item) => {
+            item = $(item);
+            const a = item.find('a').first();
+            return {
+                title: a.text(),
+                link: a.attr('href') ?? '',
+                pubDate: parseDate(item.find('span').text()),
+            };
+        });
+
+    return {
+        // 源标题
+        title: `河南省交通运输厅${fUrl}${sUrl}${tUrl}`,
+        // 源链接
+        link: currentUrl,
+        // 源文章
+        item: items,
+    };
+}
