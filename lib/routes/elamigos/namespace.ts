6e16f90bfb49a6af8ea2e74bfa3c882f87c9c881
Chris Sauermann <chris.sauermann@proton.me>
2026-02-11 15:41:08 +0100

    feat(route): add elamigos games route (#21111)
    * feat(route): add elamigos games route
    
    * fix(route): update elamigos security vulnerabilities + codefactor complex method error
    
    * fix(route): update elamigos reduce extractGames complexity
    
    * fix(route): update elamigos  required changes to adhere to coding standard
    
    * fix(route): update elamigos remove parseLimit, let user decide limit, default at 40

---
+++ lib/routes/elamigos/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'ElAmigos',
+    url: 'elamigos.site',
+    description: 'Game download site with daily releases',
+    lang: 'en-gb',
+};
