aea4e2df7fe732b6202a5bbed5e3d0c01205662f
CaoMeiYouRen233 <2774730186@qq.com>
2026-02-28 16:52:35 +0800

    feat: 添加 Iwara 排行功能，支持视频和图片的排序与筛选

---
+++ lib/routes/iwara/ranking.ts
@@ -0,0 +1,114 @@
+import { config } from '@/config';
+import type { Route } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+import { parseDate } from '@/utils/parse-date';
+
+const rootUrl = 'https://www.iwara.tv';
+const apiRootUrl = 'https://apiq.iwara.tv';
+const imageRootUrl = 'https://i.iwara.tv';
+
+const typeMap = {
+    video: 'Videos',
+    image: 'Images',
+};
+
+const sortMap = {
+    date: 'Latest',
+    trending: 'Trending',
+    popularity: 'Popularity',
+    views: 'Views',
+    likes: 'Likes',
+};
+
+const ratingMap = {
+    all: 'All',
+    general: 'General',
+    ecchi: 'Ecchi',
+};
+
+const parseThumbnail = (type, item) => {
+    if (type === 'image') {
+        return item.thumbnail ? `<img src="${imageRootUrl}/image/original/${item.thumbnail.id}/${item.thumbnail.name}">` : '';
+    }
+
+    if (item.embedUrl === null) {
+        return item.file ? `<img src="${imageRootUrl}/image/original/${item.file.id}/thumbnail-${String(item.thumbnail).padStart(2, '0')}.jpg">` : '';
+    }
+
+    // regex borrowed from https://stackoverflow.com/a/3726073
+    const match = /https?:\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w-]*)(&(amp;)?[\w=?]*)?/.exec(item.embedUrl);
+    if (match) {
+        return `<img src="${imageRootUrl}/image/embed/original/youtube/${match[1]}">`;
+    }
+
+    return '';
+};
+
+export const route: Route = {
+    path: '/ranking/:type?/:sort?/:rating?',
+    example: '/iwara/ranking/video/date/ecchi',
+    parameters: {
+        type: 'Content type, can be video or image, default is video',
+        sort: 'Sort type, can be date, trending, popularity, views, likes, default is date',
+        rating: 'Rating, can be all, general, ecchi, default is ecchi',
+    },
+    name: 'Ranking',
+    maintainers: ['Fatpandac', 'FeCCC'],
+    handler,
+    features: {
+        nsfw: true,
+    },
+    radar: [
+        {
+            source: ['www.iwara.tv/videos', 'www.iwara.tv/images'],
+            target: (params, url) => {
+                const searchParams = new URL(url).searchParams;
+                const type = url.includes('/videos') ? 'video' : 'image';
+                const sort = searchParams.get('sort') || 'date';
+                const rating = searchParams.get('rating') || 'ecchi';
+                return `/iwara/ranking/${type}/${sort}/${rating}`;
+            },
+        },
+    ],
+};
+
+async function handler(ctx) {
+    const { type = 'video', sort = 'date', rating = 'ecchi' } = ctx.req.param();
+
+    const url = `${apiRootUrl}/${type === 'video' ? 'videos' : 'images'}`;
+    const query = {
+        sort,
+        rating,
+        limit: ctx.req.query('limit') || 32,
+    };
+    const response = await cache.tryGet(
+        `iwara:ranking:${type}:${sort}:${rating}`,
+        async () =>
+            await ofetch(url, {
+                query,
+                headers: {
+                    origin: 'https://www.iwara.tv',
+                    referer: 'https://www.iwara.tv/',
+                    userAgent: config.ua,
+                },
+            }),
+        config.cache.routeExpire,
+        false
+    );
+
+    const items = response.results.map((item) => ({
+        title: item.title,
+        author: item.user.name,
+        link: `${rootUrl}/${type === 'video' ? 'video' : 'image'}/${item.id}/${item.slug || ''}`,
+        category: item.tags?.map((i) => i.id) || [],
+        description: parseThumbnail(type, item),
+        pubDate: parseDate(item.createdAt),
+    }));
+
+    return {
+        title: `Iwara Ranking - ${typeMap[type]} - ${sortMap[sort]} - ${ratingMap[rating]}`,
+        link: `${rootUrl}/${type === 'video' ? 'videos' : 'images'}?sort=${sort}&rating=${rating}`,
+        item: items,
+    };
+}
