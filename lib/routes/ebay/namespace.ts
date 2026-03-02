cfc0cabdd024f1542094d27a6f44c3a29b501665
phoeagon <phoeagon@gmail.com>
2026-03-01 23:00:39 -0800

    add ebay user/search routes

---
+++ lib/routes/ebay/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'eBay',
+    url: 'ebay.com',
+    categories: ['shopping'],
+    description: 'eBay search results and user listings.',
+};
