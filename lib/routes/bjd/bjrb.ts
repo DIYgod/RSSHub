19baf7bc57bb5822fadaf71050a6bd6d7c049b5f
zha303050@gmail.com <zha@zha303050gmailcomdeMacBook-Air.local>
2026-03-18 21:46:39 +0800

    feat(route): migrate legacy bjd routes

---
+++ lib/routes/bjd/bjrb.ts
@@ -0,0 +1,8 @@
+import type { Route } from '@/types';
+
+import { createRoute } from './utils';
+
+export const route: Route = createRoute({
+    paperId: 'bjrb',
+    paperName: '北京日报',
+});
---
+++ lib/routes/bjd/bjwb.ts
@@ -0,0 +1,8 @@
+import type { Route } from '@/types';
+
+import { createRoute } from './utils';
+
+export const route: Route = createRoute({
+    paperId: 'bjwb',
+    paperName: '北京晚报',
+});
