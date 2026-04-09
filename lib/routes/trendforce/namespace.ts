a4f0917a5231fca5ecb626b5c2b04cf3898e989e
Tony <TonyRL@users.noreply.github.com>
2026-04-09 08:24:19 +0800

    feat(route): add trendforce

--- lib/routes/trendforce/namespace.ts
+++ lib/routes/trendforce/namespace.ts
@@ -4,6 +4,9 @@ export const namespace: Namespace = {
     name: 'TrendForce',
     url: 'trendforce.com',
     categories: ['new-media'],
-    description: '',
     lang: 'en',
+    zh: {
+        name: '集邦咨询',
+        url: 'trendforce.cn',
+    },
 };
