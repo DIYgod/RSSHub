2af8e30e1c931b6b87676f2e47d5d1cba3800d74
Ananya <84459091+ananyatimalsina@users.noreply.github.com>
2026-04-12 20:19:09 +0200

    feat(route): add Castbox route (#21700)
    * feat(route): add Castbox route
    
    * remove User Agent and switch item_image
    
    * remove missed use of trueUA
    
    * remove unnecessary import
    
    * fix typo

---
+++ lib/routes/castbox/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Castbox',
+    url: 'castbox.fm',
+    description: `Castbox is a podcast distribution network and producer.`,
+};
