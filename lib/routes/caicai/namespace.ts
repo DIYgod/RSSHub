1d7f3dc8fcfc02a33f354b5db42cd290301d525a
Tony <TonyRL@users.noreply.github.com>
2026-04-15 01:56:50 +0800

    feat(route): add caicai blog (#21741)
    * feat(route): add caicai blog
    
    * fix: favicon

---
+++ lib/routes/caicai/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'CaiCai',
+    url: 'www.caicai.me',
+    categories: ['blog'],
+    zh: {
+        name: 'CaiCai 的博客',
+    },
+};
