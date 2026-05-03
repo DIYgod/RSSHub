11afd6b62d5aeac6a228cd9164c87f26ba84c0c6
magazian <magazian@gmail.com>
2026-05-03 21:17:28 +0800

    feat: add Shanghai Museum exhibition route

--- lib/routes/shanghaimuseum/shanghaimuseum.ts
+++ lib/routes/shanghaimuseum/shanghaimuseum.ts
@@ -1,24 +1,31 @@
-import { Route } from '@/types';
+import type { Route } from '@/types';
 import got from '@/utils/got';
 import { parseDate } from '@/utils/parse-date';
 
 export const route: Route = {
     path: '/display/index',
-    name: '展览列表',
+    categories: ['travel'],
+    example: '/shanghaimuseum/display/index',
+    parameters: {
+        limit: '条数，默认为 16',
+    },
+    name: '上海博物馆 - 展览',
     maintainers: ['magazian'],
-    handler: async () => {
+    handler: async (ctx) => {
         const baseUrl = 'https://www.shanghaimuseum.net';
         const apiUrl = `${baseUrl}/mu/frontend/pg/display/search-exhibit`;
 
+        const { limit = '16' } = ctx.req.query();
+
         const response = await got.post(apiUrl, {
             json: {
-                limit: 16,
+                limit: parseInt(limit),
                 page: 1,
                 params: {
-                    exhibitTypeCode: "OFFLINE_EXHIBITION",
-                    langCode: "CHINESE"
-                }
-            }
+                    exhibitTypeCode: 'OFFLINE_EXHIBITION',
+                    langCode: 'CHINESE',
+                },
+            },
         });
 
         const list = response.data.data || [];
@@ -28,17 +35,13 @@ export const route: Route = {
             link: `${baseUrl}/mu/frontend/pg/display/index`,
             item: list.map((item) => ({
                 title: item.name,
-                
-                link: `${baseUrl}/mu/frontend/pg/article/id/${item.code}`, 
-                
+                link: `${baseUrl}/mu/frontend/pg/article/id/${item.code}`,
                 description: `
-                    <img src="${baseUrl}/${item.picPath}"><br>
+                    <img src='${baseUrl}/${item.picPath}'><br>
                     <p><b>地点：</b>${item.exhibitPlace}</p>
                     <p><b>展期：</b>${item.exhibitDateRange}</p>
-                `,
-                
+                `.trim(),
                 pubDate: parseDate(item.issueTime),
-                
                 category: [item.exhibitPlace],
             })),
         };
