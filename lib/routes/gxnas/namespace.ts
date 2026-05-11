6800f031a48800adbf923c4aa01493d8fd762b9b
Franklittleboy <54013763+Franklittleboy@users.noreply.github.com>
2026-05-11 22:39:46 +0800

    feat: add GXNAS blog route

---
+++ lib/routes/gxnas/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'GXNAS博客',
+    url: 'wp.gxnas.com',
+    description: 'GXNAS博客 - NAS技术交流',
+    lang: 'zh-CN',
+};
