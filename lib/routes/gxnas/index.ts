700b9dcd15fd2cb040ba2743c729abbaed3b946a
Franklittleboy <54013763+Franklittleboy@users.noreply.github.com>
2026-05-13 12:14:22 +0800

    fix(route/gxnas): fix simple-import-sort order - type imports after value imports

--- lib/routes/gxnas/index.ts
+++ lib/routes/gxnas/index.ts
@@ -1,7 +1,7 @@
 import { load } from 'cheerio';
-import type { DataItem, Route } from '@/types';
 import { getPlaywrightPage } from '@/utils/playwright';
 import { parseDate } from '@/utils/parse-date';
+import type { DataItem, Route } from '@/types';
 
 export const route: Route = {
     path: '/',
