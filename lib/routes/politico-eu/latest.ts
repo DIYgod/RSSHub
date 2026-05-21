53eb09c9cb834899d3b4be0413499940d88fea7a
maxlixiang <qq188366125@gmail.com>
2026-05-22 00:36:23 +0800

    add some routes

---
+++ lib/routes/politico-eu/latest.ts
@@ -0,0 +1,41 @@
+﻿import type { Route } from '@/types';
+
+import parser from '@/utils/rss-parser';
+
+const feedUrl = 'https://www.politico.eu/feed/';
+const link = 'https://www.politico.eu/latest/';
+
+export const route: Route = {
+    path: '/latest',
+    categories: ['traditional-media'],
+    example: '/politico-eu/latest',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['politico.eu/latest/', 'politico.eu/'],
+            target: '/politico-eu/latest',
+        },
+    ],
+    name: 'Latest',
+    maintainers: ['maxlixiang'],
+    handler,
+};
+
+async function handler() {
+    const feed = await parser.parseURL(feedUrl);
+
+    return {
+        title: feed.title ?? 'POLITICO Europe - Latest',
+        link,
+        description: feed.description ?? 'Latest news from POLITICO Europe.',
+        item: feed.items,
+    };
+}
