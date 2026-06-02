21dd8bb84f1b37aef114de2c6a9f9ca958eb7400
mao <nyanyanyapu@gmail.com>
2026-06-02 17:19:55 +0800

    refactor(bangumi.tv): replace all occurrences of replace with replaceAll for better handling of BBCode

--- lib/routes/bangumi.tv/utils.ts
+++ lib/routes/bangumi.tv/utils.ts
@@ -2,60 +2,60 @@ export function bbcodeToHtml(content: string): string {
     let html = content;
 
     // 代码块（需要先处理，避免内部被转换）
-    html = html.replace(/\[code\]([\s\S]*?)\[\/code\]/gi, '<pre><code>$1</code></pre>');
+    html = html.replaceAll(/\[code\]([\s\S]*?)\[\/code\]/gi, '<pre><code>$1</code></pre>');
 
     // 引用
-    html = html.replace(/\[quote\]([\s\S]*?)\[\/quote\]/gi, '<blockquote>$1</blockquote>');
+    html = html.replaceAll(/\[quote\]([\s\S]*?)\[\/quote\]/gi, '<blockquote>$1</blockquote>');
 
     // 标题
-    html = html.replace(/\[h\]([\s\S]*?)\[\/h\]/gi, '<h3>$1</h3>');
+    html = html.replaceAll(/\[h\]([\s\S]*?)\[\/h\]/gi, '<h3>$1</h3>');
 
     // 文本格式
-    html = html.replace(/\[b\]([\s\S]*?)\[\/b\]/gi, '<strong>$1</strong>');
-    html = html.replace(/\[i\]([\s\S]*?)\[\/i\]/gi, '<em>$1</em>');
-    html = html.replace(/\[u\]([\s\S]*?)\[\/u\]/gi, '<u>$1</u>');
-    html = html.replace(/\[s\]([\s\S]*?)\[\/s\]/gi, '<del>$1</del>');
+    html = html.replaceAll(/\[b\]([\s\S]*?)\[\/b\]/gi, '<strong>$1</strong>');
+    html = html.replaceAll(/\[i\]([\s\S]*?)\[\/i\]/gi, '<em>$1</em>');
+    html = html.replaceAll(/\[u\]([\s\S]*?)\[\/u\]/gi, '<u>$1</u>');
+    html = html.replaceAll(/\[s\]([\s\S]*?)\[\/s\]/gi, '<del>$1</del>');
 
     // 字号
-    html = html.replace(/\[size=(\d+)\]([\s\S]*?)\[\/size\]/gi, '<span style="font-size:$1px">$2</span>');
+    html = html.replaceAll(/\[size=(\d+)\]([\s\S]*?)\[\/size\]/gi, '<span style="font-size:$1px">$2</span>');
 
     // 颜色
-    html = html.replace(/\[color=([^\]]+)\]([\s\S]*?)\[\/color\]/gi, '<span style="color:$1">$2</span>');
+    html = html.replaceAll(/\[color=([^\]]+)\]([\s\S]*?)\[\/color\]/gi, '<span style="color:$1">$2</span>');
 
     // 对齐
-    html = html.replace(/\[align=(left|center|right)\]([\s\S]*?)\[\/align\]/gi, '<div style="text-align:$1">$2</div>');
+    html = html.replaceAll(/\[align=(left|center|right)\]([\s\S]*?)\[\/align\]/gi, '<div style="text-align:$1">$2</div>');
 
     // 链接
-    html = html.replace(/\[url=([^\]]+)\]([\s\S]*?)\[\/url\]/gi, '<a href="$1" target="_blank">$2</a>');
-    html = html.replace(/\[url\]([\s\S]*?)\[\/url\]/gi, '<a href="$1" target="_blank">$1</a>');
+    html = html.replaceAll(/\[url=([^\]]+)\]([\s\S]*?)\[\/url\]/gi, '<a href="$1" target="_blank">$2</a>');
+    html = html.replaceAll(/\[url\]([\s\S]*?)\[\/url\]/gi, '<a href="$1" target="_blank">$1</a>');
 
     // 图片
-    html = html.replace(/\[img\]([\s\S]*?)\[\/img\]/gi, '<img src="$1" style="max-width:100%" />');
+    html = html.replaceAll(/\[img\]([\s\S]*?)\[\/img\]/gi, '<img src="$1" style="max-width:100%" />');
 
     // 视频（简化处理，转为链接），Bangumi 目前暂时没有此标签
-    // html = html.replace(/\[video\]([\s\S]*?)\[\/video\]/gi, '<a href="$1" target="_blank">[视频]</a>');
+    // html = html.replaceAll(/\[video\]([\s\S]*?)\[\/video\]/gi, '<a href="$1" target="_blank">[视频]</a>');
 
     // 列表
-    html = html.replace(/\[list\]([\s\S]*?)\[\/list\]/gi, (match, content) => {
-        const items = content.replace(/\[\*\]([^\[]*)/g, '<li>$1</li>');
+    html = html.replaceAll(/\[list\]([\s\S]*?)\[\/list\]/gi, (match, content) => {
+        const items = content.replaceAll(/\[\*\]([^\[]*)/g, '<li>$1</li>');
         return `<ul>${items}</ul>`;
     });
 
     // 分割线
-    html = html.replace(/\[hr\]/gi, '<hr>');
+    html = html.replaceAll(/\[hr\]/gi, '<hr>');
 
     // 剧透遮罩
-    html = html.replace(/\[mask\]([\s\S]*?)\[\/mask\]/gi, '<details><summary>Mask 内容</summary>$1</details>');
+    html = html.replaceAll(/\[mask\]([\s\S]*?)\[\/mask\]/gi, '<details><summary>Mask 内容</summary>$1</details>');
 
     // Bangumi 特有标签 - 关联条目
-    html = html.replace(/\[subject=(\d+)\]([\s\S]*?)\[\/subject\]/gi, '<a href="https://bgm.tv/subject/$1" target="_blank">$2</a>');
-    html = html.replace(/\[group=(\d+)\]([\s\S]*?)\[\/group\]/gi, '<a href="https://bgm.tv/group/$1" target="_blank">$2</a>');
+    html = html.replaceAll(/\[subject=(\d+)\]([\s\S]*?)\[\/subject\]/gi, '<a href="https://bgm.tv/subject/$1" target="_blank">$2</a>');
+    html = html.replaceAll(/\[group=(\d+)\]([\s\S]*?)\[\/group\]/gi, '<a href="https://bgm.tv/group/$1" target="_blank">$2</a>');
 
     // Bangumi 特有标签 - 用户
-    html = html.replace(/\[user=(\d+)\]([\s\S]*?)\[\/user\]/gi, '<a href="https://bgm.tv/user/$1" class="l">@$2</a>');
+    html = html.replaceAll(/\[user=(\d+)\]([\s\S]*?)\[\/user\]/gi, '<a href="https://bgm.tv/user/$1" class="l">@$2</a>');
 
     // Bangumi 特有标签 - 用户照片
-    html = html.replace(/\[photo=\d+\]([\s\S]*?)\[\/photo\]/gi, '<img src="//lain.bgm.tv/pic/photo/l/$1" alt="" loading="lazy" />');
+    html = html.replaceAll(/\[photo=\d+\]([\s\S]*?)\[\/photo\]/gi, '<img src="//lain.bgm.tv/pic/photo/l/$1" alt="" loading="lazy" />');
 
     // 换行处理：段落分组
     html = html
