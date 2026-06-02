de2ffdf1acc1ad65a46df4f1f9b69c8deca62acd
mao <nyanyanyapu@gmail.com>
2026-06-02 16:47:01 +0800

    fix(bangumi.tv): update author retrieval to use individual topic replies for accurate nickname

--- lib/routes/bangumi.tv/group/topic.ts
+++ lib/routes/bangumi.tv/group/topic.ts
@@ -75,8 +75,6 @@ async function handler(ctx) {
 
     // 获取小组名称
     const groupName = topics[0]?.group?.title || groupID;
-    // 获取用户昵称
-    const nickname = topics[0]?.replies[0]?.creator.nickname || '';
 
     const items = topics.map((item) => ({
         title: item.title,
@@ -84,7 +82,7 @@ async function handler(ctx) {
         description: bbcodeToHtml(item.replies[0].content),
         // API 内的 createdAt 是秒级 Unix 时间戳，乘 1000 转为毫秒
         pubDate: parseDate(item.createdAt * 1000),
-        author: nickname,
+        author: item.replies[0]?.creator.nickname || '',
         categories: (item.tags ?? []).map((tag) => tag.name),
     }));
 
