5542e787e3aa25f38960a474a3ad1d165576be2e
Tinko <TinkoLiu@users.noreply.github.com>
2026-06-06 11:33:59 +0800

    feat(route): add Shanghai weather alert (#22087)
    * feat: add Shanghai weather alert
    
    * chore: update `updated` field by alert deactivate time
    
    * chore: fix import path
    
    * chore: remove cache time
    
    * fix: disable cache age extend
    
    ---------
    
    Co-authored-by: Tinko <24890691+TinkoLiu@users.noreply.github.com>

---
+++ lib/routes/soweather/namespace.ts
@@ -0,0 +1,11 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'SoWeather',
+    url: 'wx.soweather.com',
+    categories: ['forecast'],
+    lang: 'zh-CN',
+    zh: {
+        name: '上海天气预警',
+    },
+};
