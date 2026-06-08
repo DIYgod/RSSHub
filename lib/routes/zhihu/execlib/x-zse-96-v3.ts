7bf9691842526195fcd498478100743a2dd1c3d8
Tony <TonyRL@users.noreply.github.com>
2026-06-08 23:07:13 +0800

    fix: prefer-single-call

--- lib/routes/zhihu/execlib/x-zse-96-v3.ts
+++ lib/routes/zhihu/execlib/x-zse-96-v3.ts
@@ -1,4 +1,4 @@
-// oxlint-disable unicorn/prefer-code-point
+// oxlint-disable unicorn/prefer-code-point unicorn-js/prefer-single-call
 // Credit:
 // https://blog.csdn.net/zjq592767809/article/details/126512798
 // https://blog.csdn.net/zhoumi_/article/details/126659351
