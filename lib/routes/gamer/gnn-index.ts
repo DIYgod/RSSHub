e055b31b88c122ff6f5871ebe8caae721dc8d123
Tony <TonyRL@users.noreply.github.com>
2026-06-20 04:24:39 +0800

    style: fix prefer-hoisting-branch-code

--- lib/routes/gamer/gnn-index.ts
+++ lib/routes/gamer/gnn-index.ts
@@ -103,17 +103,9 @@ async function handler(ctx) {
         .toArray()
         .map((item) => {
             item = $(item);
-            let aLabelNode;
-            let tag;
-            // a label with div
-            if (item.find('h1').length === 0) {
-                // a label without div
-                aLabelNode = item.find('a');
-                tag = item.find('div.platform-tag_list').text();
-            } else {
-                aLabelNode = item.find('h1').find('a');
-                tag = item.find('div.platform-tag_list').text();
-            }
+            // a label with div / a label without div
+            const aLabelNode = item.find('h1').length === 0 ? item.find('a') : item.find('h1').find('a');
+            const tag = item.find('div.platform-tag_list').text();
 
             return {
                 title: '[' + tag + ']' + aLabelNode.text(),
