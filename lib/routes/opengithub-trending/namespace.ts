0d34fce853e04ef2f367b15944bd86403704666b
Nemo <1774747097@qq.com>
2026-06-27 15:42:03 +0800

    style: fix description format in opengithub namespace

--- lib/routes/opengithub-trending/namespace.ts
+++ lib/routes/opengithub-trending/namespace.ts
@@ -4,8 +4,7 @@ export const namespace: Namespace = {
     name: 'opengithub-trending',
     url: 'github.com/OpenGithubs/github-weekly-rank/',
     description: `OpenGithubs Weekly 是一个开源项目，
-    旨在每周分享 GitHub 上最受欢迎的开源项目。
-    `,
+旨在每周分享 GitHub 上最受欢迎的开源项目。`,
     lang: 'zh-CN',
     zh: {
         name: 'github每周趋势',
