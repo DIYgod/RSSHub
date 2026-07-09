55d46b8587bdf360fd3442409b2c1ba438ae18ca
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:48:27 +0800

    feat(route): add Il Messaggero news

---
+++ lib/routes/ilmessaggero/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Il Messaggero',
+    url: 'ilmessaggero.it',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
