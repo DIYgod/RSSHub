351417c78588e09e547205658bafad0be650fd18
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:50:15 +0800

    feat(route): add Jerusalem Post news

---
+++ lib/routes/jpost/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Jerusalem Post',
+    url: 'jpost.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
