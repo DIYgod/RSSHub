017d5d9c69bd968612706e031295922fe3a3a25c
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:25:33 +0800

    feat(route): add Jutarnji List news

---
+++ lib/routes/jutarnji/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Jutarnji List',
+    url: 'jutarnji.hr',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
