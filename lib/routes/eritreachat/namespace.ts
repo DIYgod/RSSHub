d1a10807a7e8c71cb66d90df791e9a2137fdad1d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:13:21 +0800

    feat(route): add Eritrea Chat news

---
+++ lib/routes/eritreachat/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Eritrea Chat',
+    url: 'eritrea-chat.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
