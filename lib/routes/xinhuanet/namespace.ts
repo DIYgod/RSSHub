08b9d8dea4e930a49928b38de0bc5491c0afd400
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:22:57 +0800

    feat(route): add 新华社 news

---
+++ lib/routes/xinhuanet/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '新华社',
+    url: 'xinhuanet.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
