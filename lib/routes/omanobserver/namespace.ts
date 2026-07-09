7a77c6dc79e52a995489593af69828cf96e983dd
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:18:33 +0800

    feat(route): add Oman Observer news

---
+++ lib/routes/omanobserver/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Oman Observer',
+    url: 'omanobserver.om',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
