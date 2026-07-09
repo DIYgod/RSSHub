ce3cf38fb15054cf5989a2793ab2e98792d59456
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:47:33 +0800

    feat(route): add Corriere della Sera news

---
+++ lib/routes/corriere/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Corriere della Sera',
+    url: 'corriere.it',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
