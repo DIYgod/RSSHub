a6db5157d1caa7990564324b2fa25dc4bf50c590
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:13:37 +0800

    feat(route): add Maldives Independent news

---
+++ lib/routes/maldivesindependent/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Maldives Independent',
+    url: 'maldivesindependent.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
