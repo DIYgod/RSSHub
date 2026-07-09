deb155dab704804e6c9c59babe8c0b4c559e9325
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:14:34 +0800

    feat(route): add La Presse news

---
+++ lib/routes/lapresse/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'La Presse',
+    url: 'lapresse.tn',
+    lang: 'fr',
+    categories: ['traditional-media'],
+};
