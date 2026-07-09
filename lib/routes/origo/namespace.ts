9199a14abfaa256be523dbf55e5d8c831e572188
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:18:15 +0800

    feat(route): add Origo news

---
+++ lib/routes/origo/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Origo',
+    url: 'origo.hu',
+    lang: 'hu',
+    categories: ['traditional-media'],
+};
