f45f2fbe45ae47ff334df8697721d1cff27d224b
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:23:33 +0800

    feat(route): add Turkmen Portal news

---
+++ lib/routes/turkmenportal/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Turkmen Portal',
+    url: 'turkmenportal.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
