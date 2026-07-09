a56e5c489a1fbe06a7389148954ac8dd6fe58a53
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:28:17 +0800

    feat(route): add SANA news

---
+++ lib/routes/sana/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'SANA',
+    url: 'sana.sy',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
