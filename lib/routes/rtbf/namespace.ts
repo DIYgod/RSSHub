ca9dabf0bcfc5c1992faa2f66f069bd58e19bfc9
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:21:00 +0800

    feat(route): add RTBF news

---
+++ lib/routes/rtbf/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'RTBF',
+    url: 'rtbf.be',
+    lang: 'nl',
+    categories: ['traditional-media'],
+};
