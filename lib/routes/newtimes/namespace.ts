101c1e984f21506cdfa73648966f5a8ea53f6942
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:17:00 +0800

    feat(route): add The New Times news

---
+++ lib/routes/newtimes/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The New Times',
+    url: 'newtimes.co.rw',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
