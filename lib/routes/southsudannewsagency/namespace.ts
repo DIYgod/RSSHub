3b8e8ec347ae6e766cea797e78868b4fd705b1ca
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:27:58 +0800

    feat(route): add South Sudan News Agency news

---
+++ lib/routes/southsudannewsagency/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'South Sudan News Agency',
+    url: 'southsudannewsagency.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
