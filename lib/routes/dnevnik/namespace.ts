c54aa72405e4f1c62b1f509c2aa3a17acd6bc830
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:26:44 +0800

    feat(route): add Dnevnik news

---
+++ lib/routes/dnevnik/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Dnevnik',
+    url: 'dnevnik.bg',
+    lang: 'bg',
+    categories: ['traditional-media'],
+};
