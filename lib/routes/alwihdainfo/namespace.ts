eaaf06e095921c5327e283f0f08c65630b9f8aa3
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:22:37 +0800

    feat(route): add Alwihda Info news

---
+++ lib/routes/alwihdainfo/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Alwihda Info',
+    url: 'alwihdainfo.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
