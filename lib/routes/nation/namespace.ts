310cfc70e330dcd692345d042dce0342f700fbcc
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:15:08 +0800

    feat(route): add Daily Nation news

---
+++ lib/routes/nation/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Daily Nation',
+    url: 'nation.co.ke',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
