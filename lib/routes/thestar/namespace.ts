848e56733c1d421d8c9d095c5085c49347d7e92d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:27:39 +0800

    feat(route): add The Star news

---
+++ lib/routes/thestar/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The Star',
+    url: 'thestar.com.my',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
