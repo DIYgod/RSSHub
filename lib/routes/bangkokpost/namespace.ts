a51ed2710c6a968aab06b1eed677c61ba7df37e2
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:46:58 +0800

    feat(route): add Bangkok Post news

---
+++ lib/routes/bangkokpost/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Bangkok Post',
+    url: 'bangkokpost.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
