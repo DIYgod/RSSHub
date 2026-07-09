4d0f6e0c29c005e1dd811b617fcf8b7ac54bd622
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:24:58 +0800

    feat(route): add Granma news

---
+++ lib/routes/granma/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Granma',
+    url: 'granma.cu',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
