75cde6f8bea6bbe932a6f5f749696e823d0a9967
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:20:42 +0800

    feat(route): add Sierra Leone Times news

---
+++ lib/routes/sierraleonetimes/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Sierra Leone Times',
+    url: 'sierraleonetimes.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
