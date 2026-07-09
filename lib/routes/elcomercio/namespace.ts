b8c3c54e7f58c97c9e259f61cbf9f9dec4cf406a
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:47:51 +0800

    feat(route): add El Comercio news

---
+++ lib/routes/elcomercio/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'El Comercio',
+    url: 'elcomercio.pe',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
