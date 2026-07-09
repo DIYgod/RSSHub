4104aad2fbd5398efd770dbc59caa892f7f139d2
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:29:30 +0800

    feat(route): add St. Lucia Star news

---
+++ lib/routes/stluciastar/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'St. Lucia Star',
+    url: 'stluciastar.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
