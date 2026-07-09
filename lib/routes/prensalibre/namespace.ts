ea2b8a4c60f7c7a50974c65879fcca89b84cb952
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:17:20 +0800

    feat(route): add Prensa Libre news

---
+++ lib/routes/prensalibre/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Prensa Libre',
+    url: 'prensalibre.com',
+    lang: 'es',
+    categories: ['traditional-media'],
+};
