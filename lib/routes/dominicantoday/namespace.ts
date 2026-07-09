af5e6bf4e85c2a4c18bb9a6e0f303edbad5438d9
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:11:04 +0800

    feat(route): add Dominican Today news

---
+++ lib/routes/dominicantoday/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Dominican Today',
+    url: 'dominicantoday.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
