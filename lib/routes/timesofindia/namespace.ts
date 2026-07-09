484542532d70878c0d4973169ecaa6ebc37286f4
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:22:20 +0800

    feat(route): add Times of India news

---
+++ lib/routes/timesofindia/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Times of India',
+    url: 'timesofindia.indiatimes.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
