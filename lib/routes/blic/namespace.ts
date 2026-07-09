cd74166ccbc2c3c1bceeef6aafbe14895a8a8619
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:47:15 +0800

    feat(route): add Blic news

---
+++ lib/routes/blic/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Blic',
+    url: 'blic.rs',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
