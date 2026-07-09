62ca147d18178b38c7f43e2cc16a5b50411e84e5
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:12:58 +0800

    feat(route): add La Provence news

---
+++ lib/routes/laprovence/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'La Provence',
+    url: 'laprovence.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
