4cd5450efcf1fdb700fa6bbab6399cd129808749
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:24:28 +0800

    feat(route): add Gazeta Wyborcza news

---
+++ lib/routes/wyborcza/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Gazeta Wyborcza',
+    url: 'wyborcza.pl',
+    lang: 'pl',
+    categories: ['traditional-media'],
+};
