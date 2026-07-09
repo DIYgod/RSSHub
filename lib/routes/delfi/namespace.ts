84eda8b08a15067d1a837f079f3bd1ce4b7fc58d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:24:23 +0800

    feat(route): add Delfi news

---
+++ lib/routes/delfi/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Delfi',
+    url: 'delfi.lv',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
