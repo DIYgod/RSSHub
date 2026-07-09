04b9a015bd9bc6bae37ee85cf3b5abc5fe4da441
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:26:26 +0800

    feat(route): add Fiji Times news

---
+++ lib/routes/fijitimes/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Fiji Times',
+    url: 'fijitimes.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
