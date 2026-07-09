858c4d87750fd25b47c487a56b27f5edb41e7280
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:19:28 +0800

    feat(route): add Qatar Tribune news

---
+++ lib/routes/qatartribune/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Qatar Tribune',
+    url: 'qatar-tribune.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
