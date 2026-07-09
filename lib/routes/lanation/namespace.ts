13a3300d0e14407b1b65d9e3388b7731f42383c4
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:25:24 +0800

    feat(route): add La Nation news

---
+++ lib/routes/lanation/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'La Nation',
+    url: 'lanation.bj',
+    lang: 'fr',
+    categories: ['traditional-media'],
+};
