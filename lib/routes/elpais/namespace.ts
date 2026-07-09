e884f1f92c0afd91931aa466ce59ec63619ead17
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:12:20 +0800

    feat(route): add El Pais news

---
+++ lib/routes/elpais/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'El Pais',
+    url: 'elpais.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
