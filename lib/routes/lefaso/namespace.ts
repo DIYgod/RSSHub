5f877c617d71e98156d4d4b40748211ea91a8f1f
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:13:58 +0800

    feat(route): add Le Faso news

---
+++ lib/routes/lefaso/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Le Faso',
+    url: 'lefaso.net',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
