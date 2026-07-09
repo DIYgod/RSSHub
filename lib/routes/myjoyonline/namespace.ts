33890a8c3306a7bef6f2189ce49ce0db3e91350c
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:15:47 +0800

    feat(route): add Joy News news

---
+++ lib/routes/myjoyonline/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Joy News',
+    url: 'myjoyonline.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
