ea0295f8bd8f6833ea47cdf62a75b764c7862a43
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:50:53 +0800

    feat(route): add Fars News news

---
+++ lib/routes/farsnews/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Fars News',
+    url: 'farsnews.ir',
+    lang: 'fa',
+    categories: ['traditional-media'],
+};
