be7c45c2b33e612d4cdb91d75f96a1410ea520e3
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:22:20 +0800

    feat(route): add Adevărul news

---
+++ lib/routes/adevarul/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Adevărul',
+    url: 'adevarul.ro',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
