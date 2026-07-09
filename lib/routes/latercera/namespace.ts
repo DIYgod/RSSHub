39e6f2b724e7236576374aec9f0d1144009ea35c
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:26:09 +0800

    feat(route): add La Tercera news

---
+++ lib/routes/latercera/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'La Tercera',
+    url: 'latercera.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
