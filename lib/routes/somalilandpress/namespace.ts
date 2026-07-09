b28464ae7e994b384e0ed0338f0f4eb6a38b014d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 06:18:18 +0800

    feat(route): add Somaliland Press news

---
+++ lib/routes/somalilandpress/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Somaliland Press',
+    url: 'somalilandpress.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
