e0f0f99035e7689b79df29fe43631d8a3a76a4d6
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:23:16 +0800

    feat(route): add TRT World news

---
+++ lib/routes/trtworld/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'TRT World',
+    url: 'trtworld.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
