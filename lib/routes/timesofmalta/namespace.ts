b98cb2713326b618bc8c4c840c50c0567a306e5c
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:36:48 +0800

    feat(route): add Times of Malta news

---
+++ lib/routes/timesofmalta/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Times of Malta',
+    url: 'timesofmalta.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
