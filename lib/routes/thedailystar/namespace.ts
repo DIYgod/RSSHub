7e03825dd74c7631aee7feabe3e3c664176ef355
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:30:34 +0800

    feat(route): add The Daily Star news

---
+++ lib/routes/thedailystar/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The Daily Star',
+    url: 'thedailystar.net',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
