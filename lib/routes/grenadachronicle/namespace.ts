c06a88edab642696d0a662bd157c5c09099bdb37
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:13:40 +0800

    feat(route): add Grenada Chronicle news

---
+++ lib/routes/grenadachronicle/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Grenada Chronicle',
+    url: 'grenadachronicle.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
