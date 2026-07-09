3f2eb0183315151067d5b055c188ff32675bd653
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:37:26 +0800

    feat(route): add Le360 news

---
+++ lib/routes/le360/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Le360',
+    url: 'le360.ma',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
