0c666674d433c003b4741dd6f6dde5780d17ebee
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:28:50 +0800

    feat(route): add Libya Observer news

---
+++ lib/routes/libyaobserver/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Libya Observer',
+    url: 'libyaobserver.ly',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
