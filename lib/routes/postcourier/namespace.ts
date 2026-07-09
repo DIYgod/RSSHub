9846aa7dbc26fb458bdb576ee65c9e7b4ec6391f
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:17:01 +0800

    feat(route): add Post Courier news

---
+++ lib/routes/postcourier/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Post Courier',
+    url: 'postcourier.com.pg',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
