8bb8dbcd5c6fc778dac36804fa2d131beccc0803
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:12:25 +0800

    feat(route): add Diari d'Andorra news

---
+++ lib/routes/ara/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: "Diari d'Andorra",
+    url: 'ara.ad',
+    lang: 'fr',
+    categories: ['traditional-media'],
+};
