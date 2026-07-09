531c6d1df85f0b211a0edfa7fa148d45c8fe76da
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:21:42 +0800

    feat(route): add Timor Post news

---
+++ lib/routes/timorpost/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Timor Post',
+    url: 'timorpost.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
