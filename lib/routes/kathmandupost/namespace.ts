e0fa0fe786669502c5d3dfcfd7910355ec5e0444
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:25:51 +0800

    feat(route): add Kathmandu Post news

---
+++ lib/routes/kathmandupost/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Kathmandu Post',
+    url: 'kathmandupost.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
