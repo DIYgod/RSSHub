f9a4f74140815a6bd12a90a7d24018587bed732d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:28:35 +0800

    feat(route): add Seneweb news

---
+++ lib/routes/seneweb/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Seneweb',
+    url: 'seneweb.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
