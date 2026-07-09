7259a8a1e3c1807b98bcd28028acc0367f8a81ec
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:16:05 +0800

    feat(route): add Nation News news

---
+++ lib/routes/nationnews/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Nation News',
+    url: 'nationnews.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
