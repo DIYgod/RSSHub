89896ee4069a627029961b5ce6be140f23a2b629
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:30:54 +0800

    feat(route): add Luxemburger Wort news

---
+++ lib/routes/wort/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Luxemburger Wort',
+    url: 'wort.lu',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
