5e9f651164c6b589fddff11aa2da1a8e803a5774
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:22:01 +0800

    feat(route): add The Point news

---
+++ lib/routes/thepoint/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The Point',
+    url: 'thepoint.gm',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
