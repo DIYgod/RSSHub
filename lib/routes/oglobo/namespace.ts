217929624b7a5045181286c9d3d2a62f7deb134e
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:18:52 +0800

    feat(route): add O Globo news

---
+++ lib/routes/oglobo/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'O Globo',
+    url: 'oglobo.globo.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
