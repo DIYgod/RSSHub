48da0cc57ec4ba968d99d9e6ff767715ad23a38d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:15:46 +0800

    feat(route): add Maliweb news

---
+++ lib/routes/maliweb/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Maliweb',
+    url: 'maliweb.net',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
