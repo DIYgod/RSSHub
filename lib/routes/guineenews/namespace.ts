dba1035ffbf76788b97be0ac3f4ac6c1d6f92024
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:13:57 +0800

    feat(route): add Guinee News news

---
+++ lib/routes/guineenews/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Guinee News',
+    url: 'guineenews.org',
+    lang: 'fr',
+    categories: ['traditional-media'],
+};
