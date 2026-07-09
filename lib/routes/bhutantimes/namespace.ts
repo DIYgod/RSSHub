6ed41625fa0329549051c3e3e4eabf5b7f592e55
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:23:30 +0800

    feat(route): add Bhutan Times news

---
+++ lib/routes/bhutantimes/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Bhutan Times',
+    url: 'bhutantimes.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
