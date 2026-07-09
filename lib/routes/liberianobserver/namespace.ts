da02a6b66deedcb94ea59ae283200e67204ab212
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:13:17 +0800

    feat(route): add Liberian Observer news

---
+++ lib/routes/liberianobserver/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Liberian Observer',
+    url: 'liberianobserver.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
