343dae414599b2b650ae6b3916af3b03b20919ec
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:15:29 +0800

    feat(route): add Marshall Islands Journal news

---
+++ lib/routes/marshallislandsjournal/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Marshall Islands Journal',
+    url: 'marshallislandsjournal.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
