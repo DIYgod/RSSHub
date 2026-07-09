8ed5cf7fba608b54a0aa09584e090eaad1e43021
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:22:02 +0800

    feat(route): add The Punch news

---
+++ lib/routes/punchng/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The Punch',
+    url: 'punchng.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
