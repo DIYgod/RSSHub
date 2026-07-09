5ed03833182a76260e0d5779764b474f94b09c58
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:12:38 +0800

    feat(route): add Kompas news

---
+++ lib/routes/kompas/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Kompas',
+    url: 'kompas.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
