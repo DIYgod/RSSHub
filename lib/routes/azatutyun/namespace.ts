672302763bcb8d1a3a999d219b5475f99ec35c6d
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:46:41 +0800

    feat(route): add Radio Free Armenia news

---
+++ lib/routes/azatutyun/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Radio Free Armenia',
+    url: 'azatutyun.am',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
