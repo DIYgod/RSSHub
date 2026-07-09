6fa0472de9dfd39347b07b333ba705d3e093fddc
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:23:48 +0800

    feat(route): add Cyprus Mail news

---
+++ lib/routes/cyprusmail/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Cyprus Mail',
+    url: 'cyprus-mail.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
