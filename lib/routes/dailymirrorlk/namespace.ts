09c4f03a5df324a4dc0181e0bac40aee0123bda5
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:11:05 +0800

    feat(route): add Daily Mirror Sri Lanka news

---
+++ lib/routes/dailymirrorlk/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Daily Mirror Sri Lanka',
+    url: 'dailymirror.lk',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
