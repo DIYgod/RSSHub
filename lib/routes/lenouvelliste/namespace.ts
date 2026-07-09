5fb40b5261eb3a900a42052ed0eba001670d3d05
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:26:27 +0800

    feat(route): add Le Nouvelliste news

---
+++ lib/routes/lenouvelliste/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Le Nouvelliste',
+    url: 'lenouvelliste.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
