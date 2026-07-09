0ab9c80b1e808507a7d76060f3b1a897f9550957
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:23:52 +0800

    feat(route): add VnExpress news

---
+++ lib/routes/vnexpress/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'VnExpress',
+    url: 'vnexpress.net',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
