2fd6959c4ce4bb4ffc08b8e0bb378dd79a281ee0
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:16:17 +0800

    feat(route): add News24 news

---
+++ lib/routes/news24/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'News24',
+    url: 'news24.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
