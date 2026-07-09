9bfbcc3335d7e08905262a8fda81859c69329722
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:24:40 +0800

    feat(route): add Delo news

---
+++ lib/routes/delo/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Delo',
+    url: 'delo.si',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
