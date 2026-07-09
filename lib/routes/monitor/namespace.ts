ded32d3c5ac21339c9ad79ded988eac0ac68067a
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:26:44 +0800

    feat(route): add Daily Monitor news

---
+++ lib/routes/monitor/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Daily Monitor',
+    url: 'monitor.co.ug',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
