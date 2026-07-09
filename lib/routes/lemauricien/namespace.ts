728e44bf35272f87a35440c122b9effdc007dff4
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:14:52 +0800

    feat(route): add Le Mauricien news

---
+++ lib/routes/lemauricien/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Le Mauricien',
+    url: 'lemauricien.com',
+    lang: 'fr',
+    categories: ['traditional-media'],
+};
