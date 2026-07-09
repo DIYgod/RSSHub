72b8f4eba42861e5f106c7fcab27bb715afba605
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:38:36 +0800

    feat(route): add El Universal Mexico news

---
+++ lib/routes/eluniversalmx/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'El Universal Mexico',
+    url: 'eluniversal.com.mx',
+    lang: 'es',
+    categories: ['traditional-media'],
+};
