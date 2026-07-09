71fdbad16468812dadc25d5f98463c9d7847c99b
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:37:08 +0800

    feat(route): add Gulf Daily News news

---
+++ lib/routes/gdnonline/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Gulf Daily News',
+    url: 'gdnonline.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
