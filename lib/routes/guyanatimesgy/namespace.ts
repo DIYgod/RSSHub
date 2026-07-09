65b4903ff6ba15564745d9a72a1cba74a879ddf8
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:14:16 +0800

    feat(route): add Guyana Times news

---
+++ lib/routes/guyanatimesgy/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Guyana Times',
+    url: 'guyanatimesgy.com',
+    lang: 'es',
+    categories: ['traditional-media'],
+};
