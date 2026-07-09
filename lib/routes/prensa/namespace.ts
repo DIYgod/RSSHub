fe2e67ce24d0ded2df1e0c9574d80a596593be25
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:20:23 +0800

    feat(route): add La Prensa news

---
+++ lib/routes/prensa/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'La Prensa',
+    url: 'prensa.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
