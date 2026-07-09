43e85f313e2b325a282d1599ebe9e674a284ac31
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 06:17:50 +0800

    feat(route): add Samoa Observer news

---
+++ lib/routes/samoaobserver/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Samoa Observer',
+    url: 'samoaobserver.ws',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
