9c6bed207c59362f54da3373c029928257491a27
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:23:12 +0800

    feat(route): add Belsat news

---
+++ lib/routes/belsat/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Belsat',
+    url: 'belsat.eu',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
