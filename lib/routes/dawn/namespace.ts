7041111f40a9a1ce73f03f6ee9291f716d32f6dd
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:24:05 +0800

    feat(route): add Dawn news

---
+++ lib/routes/dawn/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Dawn',
+    url: 'dawn.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
