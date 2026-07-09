2d24c7da63486c38be6a027ef070c28e237ab909
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:25:44 +0800

    feat(route): add Guinea Ecuatorial Press news

---
+++ lib/routes/guineaecuatorialpress/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Guinea Ecuatorial Press',
+    url: 'guineaecuatorialpress.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
