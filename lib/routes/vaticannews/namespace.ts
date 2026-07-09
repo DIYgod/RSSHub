0334c08358e5a6e1c7313bf3d481f94cbf24d255
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:25:06 +0800

    feat(route): add Vatican News news

---
+++ lib/routes/vaticannews/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Vatican News',
+    url: 'vaticannews.va',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
