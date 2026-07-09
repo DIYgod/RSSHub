640c5596a2df9d15933641cc5ff8bdebe443d931
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:27:05 +0800

    feat(route): add The Namibian news

---
+++ lib/routes/namibian/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'The Namibian',
+    url: 'namibian.com.na',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
