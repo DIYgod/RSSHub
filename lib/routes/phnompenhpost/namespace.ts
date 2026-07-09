5279af900b08bdaf15374b64d5c3242e6cf918a0
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:16:43 +0800

    feat(route): add Phnom Penh Post news

---
+++ lib/routes/phnompenhpost/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Phnom Penh Post',
+    url: 'phnompenhpost.com',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
