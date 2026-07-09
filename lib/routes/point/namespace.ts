f08d1039aa556bca938be9aba952036c50f3aec4
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 02:27:20 +0800

    feat(route): add Point news

---
+++ lib/routes/point/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Point',
+    url: 'point.md',
+    lang: 'en',
+    categories: ['traditional-media'],
+};
