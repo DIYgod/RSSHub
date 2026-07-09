1cebfd136eb6ee7f02229e8d2bf58cbcc528f5cd
lisyer <17987545+lisyer@users.noreply.github.com>
2026-07-10 01:10:47 +0800

    feat(route): add Abarundi news

---
+++ lib/routes/abarundi/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Abarundi',
+    url: 'abarundi.org',
+    lang: 'fr',
+    categories: ['traditional-media'],
+};
