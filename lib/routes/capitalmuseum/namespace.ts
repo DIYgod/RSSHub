f33df8a1d17bf9bd2214bf5e6f241042ac8f0237
magazian <16831220+magazian@users.noreply.github.com>
2026-07-12 23:10:22 +0800

    feat: add capital museum route for exhibition and news

---
+++ lib/routes/capitalmuseum/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Capital Museum',
+    url: 'www.capitalmuseum.org.cn/',
+
+    zh: {
+        name: '首都博物馆',
+    },
+};
