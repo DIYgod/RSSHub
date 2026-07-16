287ecdd0b7f644681cbf709490005d9723c04d5e
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2026-07-16 18:04:58 +0200

    Address github-advanced-security problems

--- lib/routes/pawchive/index.tsx
+++ lib/routes/pawchive/index.tsx
@@ -17,7 +17,7 @@ function generateEnclosureInfo(htmlContent: string): { enclosure_url?: string; e
 
     $('audio source, video source').each((_, el) => {
         const src = $(el).attr('src');
-        if (!src) return;
+        if (!src) {return};
 
         const extension = src.replace(/.*\./, '').toLowerCase();
         const mimeType = MIME_TYPE_MAP[extension as keyof typeof MIME_TYPE_MAP];
