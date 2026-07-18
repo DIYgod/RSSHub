06296219166c9fc4bf3481365a4b37dddccd1419
magazian <16831220+magazian@users.noreply.github.com>
2026-07-19 02:40:36 +0800

    feat(route): add Chongqing China Three Gorges Museum temp exhibition route

---
+++ lib/routes/3gmuseum/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Chongqing China Three Gorges Museum',
+    url: 'www.3gmuseum.cn',
+
+    zh: {
+        name: '重庆中国三峡博物馆',
+    },
+};
