4669beead375704a9ce160c630cef238c5baca7a
张三 <zhangsan.duck@gmail.com>
2026-07-30 00:20:09 +0800

    feat(route): add People's Daily (人民日报) `rmrb` namespace definition

---
+++ lib/routes/rmrb/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '人民日报',
+    url: 'paper.people.com.cn',
+    description: '人民日报电子版（人民网报纸）',
+    lang: 'zh-CN',
+};
