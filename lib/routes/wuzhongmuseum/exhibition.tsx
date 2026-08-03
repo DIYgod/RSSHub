61ce35844dc1545d61636e79860ac21244330189
magazian <16831220+magazian@users.noreply.github.com>
2026-08-03 21:26:41 +0800

    fix: remove token

--- lib/routes/wuzhongmuseum/exhibition.tsx
+++ lib/routes/wuzhongmuseum/exhibition.tsx
@@ -2,54 +2,24 @@ import dayjs from 'dayjs';
 import { renderToString } from 'hono/jsx/dom/server';
 
 import type { Route } from '@/types';
-import cache from '@/utils/cache';
 import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
 
 import { namespace } from './namespace';
 
-const tcbApi = ofetch.create({
-    baseURL: 'https://tcb-api.tencentcloudapi.com/web?env=wzmuse-4gcvvt77cf4e3c3e',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        Origin: 'https://www.wuzhongmuseum.com',
-        Referer: 'https://www.wuzhongmuseum.com/',
-    },
-});
-
-const getToken = async () =>
-    await cache.tryGet(
-        'wuzhongmuseum:token',
-        async () => {
-            const res1 = await tcbApi('', {
-                body: {
-                    action: 'auth.signInAnonymously',
-                    dataVersion: '2020-01-10',
-                    env: 'wzmuse-4gcvvt77cf4e3c3e',
-                },
-            });
-            const res2 = await tcbApi('', {
-                body: {
-                    action: 'auth.fetchAccessTokenWithRefreshToken',
-                    dataVersion: '2020-01-10',
-                    env: 'wzmuse-4gcvvt77cf4e3c3e',
-                    refresh_token: res1.refresh_token,
-                },
-            });
-            return res2.access_token;
+const callCloudApi = async (path: string) => {
+    const res = await ofetch('', {
+        baseURL: 'https://tcb-api.tencentcloudapi.com/web?env=wzmuse-4gcvvt77cf4e3c3e',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            Origin: 'https://www.wuzhongmuseum.com',
+            Referer: 'https://www.wuzhongmuseum.com/',
         },
-        3500,
-        false
-    );
-
-const callCloudApi = async (token: string, path: string) => {
-    const res = await tcbApi('', {
         body: {
             action: 'functions.invokeFunction',
             dataVersion: '2020-01-10',
             env: 'wzmuse-4gcvvt77cf4e3c3e',
-            access_token: token,
             function_name: 'portal-api',
             request_data: JSON.stringify({
                 httpMethod: 'GET',
@@ -91,11 +61,9 @@ export const route: Route = {
         const museumName = namespace.zh?.name || namespace.name;
         const titleTag = fetchTypes.length === 3 ? '展览' : typeConfig[fetchTypes[0]];
 
-        const token = (await getToken()) as string;
-
         const lists = await Promise.all(
             fetchTypes.map(async (t) => {
-                const res = await callCloudApi(token, `/exhibition?type=${t}&current=1&pageSize=20`);
+                const res = await callCloudApi(`/exhibition?type=${t}&current=1&pageSize=20`);
                 return res?.data?.list || [];
             })
         );
