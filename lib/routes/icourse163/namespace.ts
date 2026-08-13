ada61f0e5214c4a8ec4ca1bca09252c8fa1c32b3
TonyRL <TonyRL@users.noreply.github.com>
2026-08-13 08:57:45 +0800

    feat: migrate batch-i
    Migrated (8 units)
    ┌───────────────┬────────────────────────────────┐
    │     route     │              path              │
    ├───────────────┼────────────────────────────────┤
    │ ichunqiu      │ /ichunqiu                      │
    ├───────────────┼────────────────────────────────┤
    │ iciba         │ /iciba/:days?/:img_type?       │
    ├───────────────┼────────────────────────────────┤
    │ icity         │ /icity/:id                     │
    ├───────────────┼────────────────────────────────┤
    │ icourse163    │ /icourse163/newest             │
    ├───────────────┼────────────────────────────────┤
    │ idownloadblog │ /idownloadblog                 │
    ├───────────────┼────────────────────────────────┤
    │ ifnews        │ /ifnews/:cid                   │
    ├───────────────┼────────────────────────────────┤
    │ im2maker      │ /im2maker/:channel?            │
    ├───────────────┼────────────────────────────────┤
    │ itslaw        │ /itslaw/judgements/:conditions │
    └───────────────┴────────────────────────────────┘
    
    Dropped (2 units)
    - interesting-sky
    - invisionapp/inside-design

---
+++ lib/routes/icourse163/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '中国大学 MOOC (慕课)',
+    url: 'www.icourse163.org',
+    lang: 'zh-CN',
+};
---
+++ lib/routes/im2maker/index.ts
@@ -0,0 +1,60 @@
+import type { Context } from 'hono';
+
+import type { Route } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/:channel?',
+    categories: ['new-media'],
+    example: '/im2maker',
+    parameters: { channel: '默认不填为 最新文章 ，频道如下' },
+    description: `| 最新文章 | 行业快讯 | 行业观察 | 镁客请讲 | 硬科技 100 人 | 投融界   | 万象       |
+| -------- | -------- | -------- | -------- | ------------- | -------- | ---------- |
+|          | fresh    | industry | talk     | intech        | investor | everything |`,
+    name: '频道',
+    maintainers: ['jin1218scu'],
+    handler,
+};
+
+async function handler(ctx: Context) {
+    const baseUrl = 'https://www.im2maker.com';
+    const channel = ctx.req.param('channel')?.toLowerCase();
+
+    let title = '最新文章';
+    let link = baseUrl;
+    const query: Record<string, any> = {
+        _embed: '',
+        per_page: ctx.req.query('limit') === '10' ? undefined : ctx.req.query('limit'),
+    };
+
+    if (channel) {
+        const category = await cache.tryGet(`im2maker:category:${channel}`, async () => {
+            const categories = await ofetch(`${baseUrl}/wp-json/wp/v2/categories`, { query: { slug: channel } });
+            return categories[0];
+        });
+        title = category.name;
+        link = category.link;
+        query.categories = category.id;
+    }
+
+    const posts = await ofetch(`${baseUrl}/wp-json/wp/v2/posts`, { query });
+
+    const items = posts.map((post) => ({
+        title: post.title.rendered,
+        link: post.link,
+        guid: post.guid.rendered,
+        description: post.content.rendered,
+        pubDate: parseDate(post.date_gmt),
+        author: post._embedded?.author?.[0]?.name,
+        category: post._embedded?.['wp:term']?.flat().map((term) => term.name),
+    }));
+
+    return {
+        title: `镁客网 ${title}`,
+        link,
+        description: '镁客网',
+        item: items,
+    };
+}
