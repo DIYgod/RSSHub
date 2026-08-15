5e96e9ea07771d330e146308fae758fb6c065f4e
TonyRL <TonyRL@users.noreply.github.com>
2026-08-16 05:03:31 +0800

    feat: migrate batch-o
    Migrated (3)
    ┌────────────┬─────────────────────┐
    │   route    │        path         │
    ├────────────┼─────────────────────┤
    │ ofweek     │ /ofweek/news        │
    ├────────────┼─────────────────────┤
    │ onejav     │ /onejav/:type/:key? │
    ├────────────┼─────────────────────┤
    │ onenotegem │ /onenotegem/release │
    └────────────┴─────────────────────┘
    
    Not needed (2)
    - one/index.js -> wufazhuce/one.ts
    - ornl/news -> ornl/all-news
    
    Dropped (1 units)
    - owspace/read

---
+++ lib/routes/onenotegem/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'OneNote Gem Add-Ins',
+    url: 'www.onenotegem.com',
+    categories: ['program-update'],
+    lang: 'en',
+};
---
+++ lib/routes/onenotegem/release.ts
@@ -0,0 +1,58 @@
+import { load } from 'cheerio';
+
+import type { DataItem, Route } from '@/types';
+import cache from '@/utils/cache';
+import ofetch from '@/utils/ofetch';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/release',
+    categories: ['program-update'],
+    example: '/onenotegem/release',
+    name: 'Release History',
+    maintainers: ['nczitzk'],
+    handler,
+};
+
+const rootUrl = 'http://www.onenotegem.com';
+const currentUrl = `${rootUrl}/a/release`;
+
+async function handler() {
+    const response = await ofetch(currentUrl);
+
+    const $ = load(response);
+
+    const list = $('.paragraph ul li a')
+        .slice(0, 10)
+        .toArray()
+        .map((item) => {
+            const $item = $(item);
+
+            return {
+                title: $item.text(),
+                link: `${rootUrl}${$item.attr('href')}`,
+            };
+        }) as DataItem[];
+
+    const items = await Promise.all(
+        list.map((item) =>
+            cache.tryGet(item.link!, async () => {
+                const detailResponse = await ofetch(item.link!);
+                const content = load(detailResponse);
+
+                const title = content('h1.wsite-content-title');
+
+                item.description = title.next().next().next().html();
+                item.pubDate = parseDate(title.next().text());
+
+                return item;
+            })
+        )
+    );
+
+    return {
+        title: 'OneNote Gem Addins Release History',
+        link: currentUrl,
+        item: items,
+    };
+}
