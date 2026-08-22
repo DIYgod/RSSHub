b257246fc489f77a27d44f51a5755076d64b0c69
TonyRL <TonyRL@users.noreply.github.com>
2026-08-22 10:27:38 +0800

    feat!: drop swust

--- lib/routes/swust/namespace.ts
+++
@@ -1,8 +0,0 @@
-import type { Namespace } from '@/types';
-
-export const namespace: Namespace = {
-    name: '西南科技大学',
-    url: 'www.dean.swust.edu.cn',
-    categories: ['university'],
-    lang: 'zh-CN',
-};
