0d5b8ff2fae30d8727b4b8f77d3099a15769207b
marcosteam <wdy71608161@gmail.com>
2026-09-06 09:46:00 +0800

    feat(route): add CPP doujin route

---
+++ lib/routes/cpp/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'CPP 无差别同人站',
+    url: 'cp.allcpp.cn',
+    categories: ['anime'],
+    lang: 'zh-CN',
+};
