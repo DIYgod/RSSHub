2fc765c6c5751e92d1949ca3e39f5b9673721ee6
magazian <16831220+magazian@users.noreply.github.com>
2026-09-07 23:55:55 +0800

    feat(route): add Namoc exhibition route

---
+++ lib/routes/namoc/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'National Art Museum of China',
+    url: 'www.namoc.cn',
+
+    zh: {
+        name: '中国美术馆',
+    },
+};
