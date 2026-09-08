f7630491b4ab99e836c4df4033d234e448bc95e2
Jiamin <16831220+magazian@users.noreply.github.com>
2026-09-08 11:50:38 +0800

    fix: update scriptText
    Co-authored-by: Tony <TonyRL@users.noreply.github.com>

--- lib/routes/namoc/zhanlan.tsx
+++ lib/routes/namoc/zhanlan.tsx
@@ -59,7 +59,7 @@ export const route: Route = {
                     const startDate = dateParts[0] || undefined;
                     const endDate = dateParts[1] || undefined;
 
-                    const scriptText = $li.find('p.image script').html() || '';
+                    const scriptText = $li.find('p.image script').text();
                     const images = JSON.parse(scriptText.match(/var\s+jsonImageStr\s*=\s*'(\[.*?\])'/s)![1]) as Array<{ savepath: string }>;
                     const imgUrl = new URL(images[0].savepath, baseUrl).href;
 
