222aaca136790797ec2ffeae83152a5118c339c6
magazian <16831220+magazian@users.noreply.github.com>
2026-09-10 01:36:36 +0800

    feat(route): Add Yinxu Museum news and exhibition route

---
+++ lib/routes/yinxubwg/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Yinxu Museum',
+    url: 'www.yinxubwg.cn',
+
+    zh: {
+        name: '殷墟博物馆',
+    },
+};
