719c0270e60dabedab15a65c6013804126c39344
Junior0679 <142976698+Junior0679@users.noreply.github.com>
2026-09-18 01:44:28 +0800

    Add namespace for Yunnan Provincial Government

---
+++ lib/routes/gov/yunnan/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '云南省人民政府',
+    url: 'www.yn.gov.cn',
+    description: '云南省人民政府及省直部门官方网站相关 RSS 源',
+    categories: ['government'],
+    lang: 'zh-CN',
+};
