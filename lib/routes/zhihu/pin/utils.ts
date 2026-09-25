0c39010480251531b1f81d25ee3d9db7228f12ff
Tony <TonyRL@users.noreply.github.com>
2026-09-26 03:21:40 +0800

    fix(route/zhihu): use old api

--- lib/routes/zhihu/pin/utils.ts
+++ lib/routes/zhihu/pin/utils.ts
@@ -13,21 +13,10 @@ const generateDescription = (target, description = '') => {
 
             case 'video':
                 try {
-                    description += `<video
-            controls="controls"
-            width="${item.playlist.hd.width}"
-            height="${item.playlist.hd.height}"
-            poster="${item.cover_info.thumbnail}"
-            src="${item.playlist.hd.play_url}"
-          >`;
+                    description += `<video controls metadata="preload" width="${item.playlist.hd.width}" height="${item.playlist.hd.height}" poster="${item.cover_info.thumbnail.split('?', 1)[0]}" src="${item.playlist.hd.play_url}"></video>`;
                 } catch {
-                    description += `<video
-                controls="controls"
-                width="${item.playlist.pop().width}"
-                height="${item.playlist.pop().height}"
-                poster="${item.thumbnail}"
-                src="${item.playlist.pop().play_url}"
-              >`;
+                    const video = item.playlist.find((v) => v.quality === 'hd') ?? item.playlist.at(-1);
+                    description += `<video controls metadata="preload" width="${video.width}" height="${video.height}" poster="${item.thumbnail.split('?', 1)[0]}" src="${video.url}"></video>`;
                 }
                 break;
 
@@ -42,12 +31,12 @@ const generateDescription = (target, description = '') => {
     return description;
 };
 
-const generateData = (data) =>
+export const generateData = (data) =>
     data.map((item) => {
         const target = item.target ?? item;
-        const pubDate = parseDate(target.created * 1000);
+        const pubDate = parseDate(target.created, 'X');
         const author = target.author.name;
-        const title = `${author}：${target.excerpt_title}`;
+        const title = target.excerpt_title;
         const link = `https://www.zhihu.com/pin/${target.id}`;
         let description = generateDescription(target, `<a href="https://www.zhihu.com${target.author.url}">${author}</a>：`);
         if (target.origin_pin !== undefined) {
@@ -64,5 +53,3 @@ const generateData = (data) =>
             link,
         };
     });
-
-export { generateData };
--- lib/routes/zhihu/zhuanlan.ts
+++ lib/routes/zhihu/zhuanlan.ts
@@ -1,9 +1,9 @@
-import { load } from 'cheerio';
-
 import type { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
 
-import { processImage, withZhihuClient } from './utils';
+import { generateData as generatePinData } from './pin/utils';
+import { processImage } from './utils';
 
 export const route: Route = {
     path: '/zhuanlan/:id',
@@ -11,12 +11,7 @@ export const route: Route = {
     example: '/zhihu/zhuanlan/googledevelopers',
     parameters: { id: '专栏 id，可在专栏主页 URL 中找到' },
     features: {
-        requireConfig: [
-            {
-                name: 'ZHIHU_COOKIES',
-                description: 'A complete d_c0 and __zse_ck cookie pair skips session initialization. Otherwise Workers use a Playwright browser session; Docker and Vercel generate credentials with JSDOM.',
-            },
-        ],
+        requireConfig: false,
         requirePuppeteer: false,
         antiCrawler: true,
         supportBT: false,
@@ -33,81 +28,79 @@ export const route: Route = {
     handler,
 };
 
-function handler(ctx) {
+async function handler(ctx) {
     const id = ctx.req.param('id');
     // 知乎专栏链接存在两种格式, 一种以 'zhuanlan.' 开头, 另一种新增的以 'c_' 结尾
     let url = `https://zhuanlan.zhihu.com/${id}`;
-    if (id.search('c_') === 0) {
+    if (id.startsWith('c_')) {
         url = `https://www.zhihu.com/column/${id}`;
     }
 
-    return withZhihuClient(url, async (client) => {
-        const listRes = await client.get(`/api/v4/columns/${id}/items`);
-        const pinnedRes = await client.get(`/api/v4/columns/${id}/pinned-items/v2`);
-        const list = [...listRes.data, ...pinnedRes.data];
+    const get = (path = '') => ofetch(`https://zhuanlan.zhihu.com/api/columns/${id}${path}`, { parseResponse: JSON.parse });
+    const [column, listRes, pinnedRes] = await Promise.all([get(), get('/items'), get('/pinned-items/v2')]);
+    const list = [...listRes.data, ...pinnedRes.data];
 
-        const html = await client.getPage();
-        const $ = load(html);
-        const title = $('.css-zyehvu').text();
-        const description = $('.css-1bnklpv').text();
+    const item = list.map((item) => {
+        if (item.type === 'pin') {
+            return generatePinData([item])[0];
+        }
 
-        const item = list.map((item) => {
-            // 当专栏内文章内容不含任何文字时, 返回空字符, 以免直接报错
-            let description = '';
-            if (item.content) {
-                description = processImage(item.content);
-            }
+        // 当专栏内文章内容不含任何文字时, 返回空字符, 以免直接报错
+        let description = '';
+        if (item.content) {
+            description = processImage(item.content);
+        }
 
-            let title: string;
-            let link: string;
-            let author: string;
-            let pubDate: Date;
+        let title: string;
+        let link: string;
+        let author: string;
+        let pubDate: Date;
 
-            switch (item.type) {
-                case 'answer':
-                    title = item.question.title;
-                    author = item.question.author ? item.question.author.name : '';
-                    link = `https://www.zhihu.com/question/${item.question.id}/answer/${item.id}`;
-                    pubDate = parseDate(item.created_time * 1000);
+        switch (item.type) {
+            case 'answer':
+                title = item.question.title;
+                author = item.question.author ? item.question.author.name : '';
+                link = `https://www.zhihu.com/question/${item.question.id}/answer/${item.id}`;
+                pubDate = parseDate(item.created_time, 'X');
 
-                    break;
+                break;
 
-                case 'article':
-                    title = item.title;
-                    link = item.url;
-                    author = item.author.name;
-                    pubDate = parseDate(item.created * 1000);
+            case 'article':
+                title = item.title;
+                link = item.url;
+                author = item.author.name;
+                pubDate = parseDate(item.created, 'X');
 
-                    break;
+                break;
 
-                case 'zvideo':
-                    // 如果类型是zvideo，id即为视频地址参数
-                    title = item.title;
-                    link = `https://www.zhihu.com/zvideo/${item.id}`;
-                    author = item.author.name;
-                    pubDate = parseDate(item.created_at * 1000);
-                    // 判断是否存在视频简介
-                    description = item.description ? `${item.description} <br> <br> <a href="${link}">视频内容请跳转至原页面观看</a>` : `<a href="${link}">视频内容请跳转至原页面观看</a>`;
+            case 'zvideo':
+                // 如果类型是zvideo，id即为视频地址参数
+                title = item.title;
+                link = `https://www.zhihu.com/zvideo/${item.id}`;
+                author = item.author.name;
+                pubDate = parseDate(item.created_at, 'X');
+                // 判断是否存在视频简介
+                description = item.description ? `${item.description} <br> <br> <a href="${link}">视频内容请跳转至原页面观看</a>` : `<a href="${link}">视频内容请跳转至原页面观看</a>`;
 
-                    break;
+                break;
 
-                default:
-                    throw new Error(`Unknown type: ${item.type}`);
-            }
-            return {
-                title,
-                link,
-                description,
-                pubDate,
-                author,
-            };
-        });
-
+            default:
+                throw new Error(`Unknown type: ${item.type}`);
+        }
         return {
+            title,
+            link,
             description,
-            item,
-            title: `知乎专栏-${title}`,
-            link: url,
+            pubDate,
+            author,
         };
     });
+
+    return {
+        description: column.description,
+        item,
+        title: `知乎专栏-${column.title}`,
+        link: url,
+        image: column.image_url.split('?', 1)[0],
+    };
 }
